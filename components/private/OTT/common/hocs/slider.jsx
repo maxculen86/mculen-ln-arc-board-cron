4009e747f71ca74ec798a2d0b0b722e6c378e70c
Alan <alancastro90@gmail.com>
2019-03-21 11:38:58 -0300

    modificaciones slider y en su uso

--- components/private/OTT/common/hocs/slider.jsx
+++ components/private/OTT/common/hocs/slider.jsx
@@ -11,46 +11,42 @@ export default function slider(WrappedComponent,pageSize) {
             pageSize: pageSize,
             currentStartIndex: 0,
             totalCount: this.props.children.length,
-            showNextPage: this.props.children.length > pageSize ,
-            showPrevPage: false,
+            hasNextPage: this.props.children.length > pageSize ,
+            hasPrevPage: false,
         }
         this.DEFAULT_SLIDE_COUNT = pageSize -1
-        console.log('constructor hoc', this.state)
         this.slider = {
-            nextPageHandler: this.nextPageHandler,
-            prevPageHandler: this.prevPageHandler,
-            showNextPage: () => this.state.showNextPage,
-            showPrevPage: () => this.state.showPrevPage
+            nextButtonHandler: this.nextButtonHandler,
+            prevButtonHandler: this.prevButtonHandler,
+            hasNextPage: () => this.state.hasNextPage,
+            hasPrevPage: () => this.state.hasPrevPage
         }
     }
 
-    showNextPage= (nextCurrentIndex)=>{
-        return this.props.children.length > nextCurrentIndex + this.state.pageSize
+    hasNextPage= (nextCurrentIndex)=>{
+        return this.state.totalCount > nextCurrentIndex + this.state.pageSize
     }
-    showPrevPage= (nextCurrentIndex)=>{
+    hasPrevPage= (nextCurrentIndex)=>{
         return  nextCurrentIndex > 0
     }
-    nextPageHandler = () =>{
-        console.log('next')
+    nextButtonHandler = () =>{
         this.setState({
             currentStartIndex: this.state.currentStartIndex + this.DEFAULT_SLIDE_COUNT,
-            showNextPage: this.showNextPage(this.state.currentStartIndex + this.DEFAULT_SLIDE_COUNT),
-            showPrevPage: true
+            hasNextPage: this.hasNextPage(this.state.currentStartIndex + this.DEFAULT_SLIDE_COUNT),
+            hasPrevPage: true
          })
     }
-    prevPageHandler = ()=>{
-        console.log('prev')
+    prevButtonHandler = ()=>{
         this.setState({
             currentStartIndex: this.state.currentStartIndex - this.DEFAULT_SLIDE_COUNT,
-            showPrevPage: this.showPrevPage(this.state.currentStartIndex - this.DEFAULT_SLIDE_COUNT),
-            showNextPage: true 
+            hasPrevPage: this.hasPrevPage(this.state.currentStartIndex - this.DEFAULT_SLIDE_COUNT),
+            hasNextPage: true 
         })
     }
 
   
 
     render(){
-        console.log('render hoc', this.state)
         return (<WrappedComponent slider={this.slider} {...this.props} >
             {this.props.children.slice(this.state.currentStartIndex, this.state.currentStartIndex + this.state.pageSize)}
         </WrappedComponent>)
