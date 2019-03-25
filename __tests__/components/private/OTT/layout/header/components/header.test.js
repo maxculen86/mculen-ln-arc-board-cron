a15f9afd0f9ba987af8b5a9d21e1948e4ee66cd7
asoto <asoto@lanacion.com.ar>
2019-03-25 19:32:30 -0300

    commit test

--- __tests__/components/private/OTT/layout/header/components/header.test.js
+++ __tests__/components/private/OTT/layout/header/components/header.test.js
@@ -1,12 +1,12 @@
 
 //retorno un elemento que luego busco en el container
-jest.mock('../../../../../../../components/private/OTT/layout/header/containers/headerItem',
+jest.mock('../../../../../../../components/private/OTT/features/header/containers/headerItem',
     () => 'mock-component');
 
 //Otros imports
 import React from 'react';
 import { mount } from 'enzyme';
-import HeaderComponent from '../../../../../../../components/private/OTT/layout/header/components/header';
+import HeaderComponent from '../../../../../../../components/private/OTT/features/header/components/header';
 
 describe('OTT - layout - header - components', () => {
     const child = <hijos>soy un child default</hijos>
@@ -14,42 +14,42 @@ describe('OTT - layout - header - components', () => {
         {
             description: 'description 1',
             href: 'href 1',
-            data: { dataEvent: 'link' },
             alt: 'alt 1'
         },
         {
             description: 'description 2',
             href: 'href 2',
-            data: { dataEvent: 'link 2' },
             alt: 'alt 2'
         }]
+    const data = { dataEvent: 'link 2' }
     const container = mount(
         <HeaderComponent
             items={items}
             children={child}
+            data={data}
         />
     )
     const children = container.find('hijos')
     const container1 = container.find('mock-component').at(0)
     const container2 = container.find('mock-component').at(1)
-    const container3 = container.find('mock-component').at(2)
+    const mockedContainers = container.find('mock-component')
 
     it('Testeo que no renderee los children', () => {
-        expect(children).toEqual({})
+        expect(children.length).toEqual(0)
     });
     it('Test 2 items - Testeo que el item 1 reciba las props del item 1', () => {
         expect(container1.prop('description')).toEqual(items[0].description)
-        expect(container1.prop('alt')).toEqual(items[0].alt)
+        expect(container1.prop('alt')).toEqual(items[0].description)
         expect(container1.prop('href')).toEqual(items[0].href)
-        expect(container1.prop('data')).toEqual(items[0].data)
+        expect(container1.prop('data')).toEqual(data)
     });
     it('Test 2 items - Testeo que el item 2 reciba las props del item 2', () => {
         expect(container2.prop('description')).toEqual(items[1].description)
-        expect(container2.prop('alt')).toEqual(items[1].alt)
+        expect(container2.prop('alt')).toEqual(items[1].description)
         expect(container2.prop('href')).toEqual(items[1].href)
-        expect(container2.prop('data')).toEqual(items[1].data)
+        expect(container2.prop('data')).toEqual(data)
     });
     it('Test 2 items - Testeo que no exista un item 3', () => {
-        expect(container3).toEqual({})
+        expect(mockedContainers.length).toEqual(2)
     });
 });
--- __tests__/components/private/OTT/layout/header/containers/header.test.js
+++ __tests__/components/private/OTT/layout/header/containers/header.test.js
@@ -1,26 +1,32 @@
 
 //retorno un elemento que luego busco en el container
-jest.mock('../../../../../../../components/private/OTT/layout/header/components/header',
+jest.mock('../../../../../../../components/private/OTT/features/header/components/header',
     () => 'mock-component');
 
 //Otros imports
 import React from 'react';
 import { mount } from 'enzyme';
-import HeaderContainer from '../../../../../../../components/private/OTT/layout/header/containers/header';
+import HeaderContainer from '../../../../../../../components/private/OTT/features/header/containers/header';
 
 describe('OTT - layout - header - containers', () => {
-    const child = <div>soy un child de frame default</div>
+    const child = <hijos>soy un child de frame default</hijos>
     const items = [
         {item: 'un item de prueba 1'},
         {item: 'otra item de prueba 2'}]
     const container = mount(
         <HeaderContainer
             items={items}
+            children={child}
         />
     )
     const component = container.find('mock-component')
 
     it('Testeo que pase al componente los items recibidos por el container', () => {
         expect(component.prop('items')).toEqual(items)
+    });
+    const children = container.find('hijos')
+
+    it('Testeo que no renderee los children', () => {
+        expect(children.length).toEqual(0)
     });
 });
