54d2dfa9dff01bd61e577f7b0503ac997543faf2
asoto <asoto@lanacion.com.ar>
2019-03-26 10:46:04 -0300

    agrego test

---
+++ __tests__/components/private/OTT/features/currentPrograms/components/currentProgram.test.js
@@ -0,0 +1,56 @@
+//retorno un elemento que luego busco en el container
+jest.mock('../../../../../../../components/private/OTT/features/currentPrograms/containers/currentProgramItem',
+    () => 'mock-component');
+
+//Otros imports
+import React from 'react';
+import { mount } from 'enzyme';
+import CurrentProgramItemContainer from '../../../../../../../components/private/OTT/features/currentPrograms/components/currentPrograms'; 
+
+
+describe('OTT - layout - currentProgramItem - component', () => {
+
+    const child = <hijos>soy un child de frame default</hijos>
+
+    const items = [
+        {
+            description: 'description 1',
+            href: 'href 1',
+            imgSrc: 'https://openningia.gallerycdn.vsassets.io/extensions/openningia/vscode-google-test-adapter/0.3.1/1546439950205/Microsoft.VisualStudio.Services.Icons.Default'
+        },
+        {
+            description: 'description 2',
+            href: 'href 2',
+            imgSrc: 'https://openningia.gallerycdn.vsassets.io/extensions/openningia/vscode-google-test-adapter/0.3.1/1546439950205/Microsoft.VisualStudio.Services.Icons.Default'
+        }]
+
+    const container = mount(
+        <CurrentProgramItemContainer
+            items={items}
+            children={child}
+        />
+    )
+
+    const children = container.find('hijos')
+    const container1 = container.find('mock-component').at(0)
+    const container2 = container.find('mock-component').at(1)
+    const mockedContainers = container.find('mock-component')
+
+
+    it('Testeo que no renderee los children', () => {
+        expect(children.length).toEqual(0)
+    });
+    it('Test 2 items - Testeo que el item 1 reciba las props del item 1', () => {
+        expect(container1.prop('description')).toEqual(items[0].description)
+        expect(container1.prop('href')).toEqual(items[0].href)
+    });
+    it('Test 2 items - Testeo que el item 2 reciba las props del item 2', () => {
+        expect(container2.prop('description')).toEqual(items[1].description)
+        expect(container2.prop('href')).toEqual(items[1].href)
+    });
+    it('Test 2 items - Testeo que no exista un item 3', () => {
+        expect(mockedContainers.length).toEqual(2)
+    });
+
+
+})
