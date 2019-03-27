09222eecf755db714c00039c45d691c85a304ac6
Dev Arc <iklimberg@lanacion.com.ar>
2019-03-27 15:34:12 -0300

    agrego layout con stylesheet hardcodeado

---
+++ components/layouts/LN-home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LNHomeLayout extends Component {
+  render() {
+    return (
+      <>
+        <link rel="stylesheet" src="https://static.glanacion.com/v2/ln/css/501afc6b3a82043455a906024435b42f.min.css" />
+      </>
+    )
+  }
+}
