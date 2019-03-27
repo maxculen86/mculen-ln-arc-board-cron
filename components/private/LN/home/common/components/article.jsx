ea70db4773597834bf570ea74f69cf6912c353a7
Dev Arc <iklimberg@lanacion.com.ar>
2019-03-27 17:29:56 -0300

    termino maqueta y meto adentro de image el a y el picture

--- components/private/LN/home/common/components/article.jsx
+++ components/private/LN/home/common/components/article.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 import Image from './articleImage'
 import Title from './articleTitle'
+import Bajada from './articleBajada'
 
 export default class Article extends Component {
     render() {
         console.log(this.props)
         return (
             <article className={this.props.renderClasses}>
-                <a className="figure" href={this.props.url}>
-                    <picture className="content-picture">
-                        <Image imgUrls={this.props.imgUrls} />
-                    </picture>
-                </a>
+                <Image imgUrls={this.props.imgUrls} url={this.props.url} />
                 <Title volanta={this.props.volanta} title={this.props.title} />
+                {this.props.bajada &&
+                    <Bajada bajada={this.props.bajada} url={this.props.url} />
+                }
             </article>
         )
     }
--- components/private/LN/home/common/components/articleImage.jsx
+++ components/private/LN/home/common/components/articleImage.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 export default function ArticleImage(props) {
     return (
-      <>
-        <source media="(min-width: 86.250em)" srcSet={props.imgUrls.desktop} className="img-desktop" alt="imagen-destacada" />
-        <source media="(min-width: 64.000em)" srcSet={props.imgUrls.desktopSM} className="img-desktop-sm" alt="imagen-destacada" />
-        <source media="(min-width: 54.000em)" srcSet={props.imgUrls.tablet} className="img-tablet" alt="imagen-destacada" />
-        <source media="(min-width: 42.000em)" srcSet={props.imgUrls.tabletSM} className="img-tablet-sm" alt="imagen-destacada" />
-        <source media="(min-width: 22.500em)" srcSet={props.imgUrls.mobile} className="img-mobile" alt="imagen-destacada" />
-        <source media="(min-width: 20.000em)" srcSet={props.imgUrls.mobileSM} className="img-mobile-sm" alt="imagen-destacada" />
-        <img alt="imagen-destacada" />
-      </>
+      <a className="figure" href={this.props.url}>
+            <picture className="content-picture">
+                <source media="(min-width: 86.250em)" srcSet={props.imgUrls.desktop} className="img-desktop" alt="imagen-destacada" />
+                <source media="(min-width: 64.000em)" srcSet={props.imgUrls.desktopSM} className="img-desktop-sm" alt="imagen-destacada" />
+                <source media="(min-width: 54.000em)" srcSet={props.imgUrls.tablet} className="img-tablet" alt="imagen-destacada" />
+                <source media="(min-width: 42.000em)" srcSet={props.imgUrls.tabletSM} className="img-tablet-sm" alt="imagen-destacada" />
+                <source media="(min-width: 22.500em)" srcSet={props.imgUrls.mobile} className="img-mobile" alt="imagen-destacada" />
+                <source media="(min-width: 20.000em)" srcSet={props.imgUrls.mobileSM} className="img-mobile-sm" alt="imagen-destacada" />
+                <img alt="imagen-destacada" />
+            </picture>
+        </a>
+      
     )
 }
