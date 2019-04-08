231270f78a829f4f97f7fbbcfffc2ae985880a53
asoto <asoto@lanacion.com.ar>
2019-04-08 11:34:48 -0300

    sarasa

--- components/private/OTT/features/LastVideosByProgram/containers/LastVideosByProgram.jsx
+++ components/private/OTT/features/LastVideosByProgram/containers/LastVideosByProgram.jsx
@@ -1,56 +1,68 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import LastVideosByProgramComponent from '../../LastVideosByProgram/components/LastVideosByProgram';
 import Consumer from 'fusion:consumer';
 import get from 'lodash.get';
 import filter from '../../../../../../content/filters/OTT/homeVideoItem';
 
-const PAGE_SIZE = 12
-class LastVideosByProgram extends Component {
+const PAGE_SIZE = 4
+class LastVideosByProgram extends PureComponent {
   constructor(props) {
+    12
+    console.log("constructor")
     super(props)
     this.state = {
-        from: 0,
-        videos: []
+      from: 0,
+      videos: []
     }
     this.getVideos();
   }
-  componentDidUpdate(){
-    
-    console.log('cwrp')
-    //loop of death
-    //this.getVideos();
-  }
-  getVideos(){
+
+
+  getVideos() {
+    console.log("getvideos fn")
     const { cached, fetched } = this.getContent({
-        sourceName: 'ottVideoSource',
-        query: {
-          query: `sort=publish_date:desc&from=${this.state.from}&size=${PAGE_SIZE}&q=taxonomy.sections._id="/${this.props.sectionId}"`
-        }
-        ,filter
-      });
-      // SI LO DEJO ASI, ME DESAPARECE EL STATE "from"
-      // this.state = { videos: get(cached, 'content_elements', null) };
-      this.state.videos = get(cached, 'content_elements', null)
+      sourceName: 'ottVideoSource',
+      query: {
+        query: `sort=publish_date:desc&from=${this.state.from}&size=${PAGE_SIZE}`
+      }
+      , filter
+    });
+    // SI LO DEJO ASI, ME DESAPARECE EL STATE "from"
+    // this.state = { videos: get(cached, 'content_elements', null) };
+    const cachedVideos = get(cached, 'content_elements', null)
+    this.setState({ videos: cachedVideos })
 
-      fetched.then(response => {
-        const fetchedVideos = get(response, 'content_elements', null)
-        if(fetchedVideos)
-          //CUANDO ACTUALIZA EL ESTADO CAE EN componentDidUpdate Y ese vuelve a llamar a getVideos() y se hace un loop
-          this.setState({ videos: fetchedVideos })
-      })
+    fetched.then(response => {
+      console.log("fetch")
+      const fetchedVideos = get(response, 'content_elements', null)
+      if (fetchedVideos) {
+        //CUANDO ACTUALIZA EL ESTADO CAE EN componentDidUpdate Y ese vuelve a llamar a getVideos() y se hace un loop
+        let resto = this.state.videos.length % PAGE_SIZE;
+        console.log('resto', resto)
+        console.log('videos',this.state.videos)
+        if (resto == 0)
+          this.state.videos = this.state.videos.splice(this.state.videos.length - PAGE_SIZE)
+        else
+          this.state.videos = this.state.videos.splice(this.state.videos.length - resto)
+        this.setState(ps => {return { videos: [...ps.videos, fetchedVideos] }})
+      }
+    })
   }
 
-  nextPage = () =>{
-      console.log('next')
-      this.setState({
-          from: this.state.from + 8
-      })
+  nextPage = () => {
+    console.log('next')
+    this.state.from = this.state.from + 8
+    this.getVideos();
   }
 
-render() {
-  if(!this.state.videos)
-    return <></>
-  return <LastVideosByProgramComponent videos={this.state.videos} nextPageHandler={this.nextPage} />
+  render() {
+    this.state.renders = this.state.renders + 1
+    if (this.state.renders > 10)
+      return null
+    console.log("render")
+    if (!this.state.videos)
+      return <></>
+    return <LastVideosByProgramComponent videos={this.state.videos} nextPageHandler={this.nextPage} />
   }
 }
 
