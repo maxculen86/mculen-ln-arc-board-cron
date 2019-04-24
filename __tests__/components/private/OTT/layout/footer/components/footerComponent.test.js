dbc7eaec2e6051004e66c3dc887ddc0db6b0bc9c
Ale Soto <asoto@lanacion.com.ar>
2019-04-24 08:49:06 -0300

    cambio html de test

--- __tests__/components/private/OTT/layout/footer/components/footerComponent.test.js
+++ __tests__/components/private/OTT/layout/footer/components/footerComponent.test.js
@@ -10,15 +10,15 @@ describe('private - OTT - layouts - footer - components - footer', () => {
     const footer = component.find('footer');
 
     const html =
-        '<footer class="footer"><div class="footer__left"><div class="footer__up"><span class="seguinos"> Seguinos:</span><button class="icon-facebook" id="pie-facebook"></button><button class="icon-twitter" id="pie-twitter"></button><button class="icon-instagram" id="pie-instagram"></button><button class="icon-rss" id="pie-rss"></button></div></div><div class="footer__center"><div class="footer__up"><a href="https://micuenta.lanacion.com.ar/ayuda" target="_blank" data-event="LinkClick" data-section="FooterLN">Ayuda</a><a href="http://especiales.lanacion.com.ar/varios/mapa-sitio/index.html" target="_blank" data-event="LinkClick" data-section="FooterLN">Mapa del sitio</a><a href="https://micuenta.lanacion.com.ar/tyc" target="_blank" data-event="LinkClick" data-section="FooterLN">Términos y condiciones</a><a href="http://www.lanacion.in/" target="_blank" data-event="LinkClick" data-section="FooterLN">¿Cómo anunciar?</a></div><div class="footer__bottom"><p class="copyright">Copyright 2019 SA LA NACION. Todos los derechos reservados</p></div></div><div class="footer__right"><div class="footer__up"><button class="icon-ios" id="pie-apple" target="_blank"></button><button class="icon-android" id="pie-android" target="_blank"></button><div class="footer__captcha"></div></div><div class="footer__bottom"><a href="http://qr.afip.gob.ar/?qr=HJMakbCpenWNdXYfqXtEDQ,," class="data-fiscal" target="_blank" data-event="LinkClick" data-section="FooterLN"></a></div></div></footer>';
+        '<footer class="footer"><div class="footer__left"><div class="footer__up"><span class="seguinos"> Seguinos:</span><button class="icon-facebook" id="pie-facebook" type="button"></button><button class="icon-twitter" id="pie-twitter" type="button"></button><button class="icon-instagram" id="pie-instagram" type="button"></button><button class="icon-rss" id="pie-rss" type="button"></button></div></div><div class="footer__center"><div class="footer__up"><a href="https://micuenta.lanacion.com.ar/ayuda" target="_blank" data-event="LinkClick" data-section="FooterLN">Ayuda</a><a href="http://especiales.lanacion.com.ar/varios/mapa-sitio/index.html" target="_blank" data-event="LinkClick" data-section="FooterLN">Mapa del sitio</a><a href="https://micuenta.lanacion.com.ar/tyc" target="_blank" data-event="LinkClick" data-section="FooterLN">Términos y condiciones</a><a href="http://www.lanacion.in/" target="_blank" data-event="LinkClick" data-section="FooterLN">¿Cómo anunciar?</a></div><div class="footer__bottom"><p class="copyright">Copyright 2019 SA LA NACION. Todos los derechos reservados</p></div></div><div class="footer__right"><div class="footer__up"><button class="icon-ios" id="pie-apple" target="_blank" type="button"></button><button class="icon-android" id="pie-android" target="_blank" type="button"></button><div class="footer__captcha"></div></div><div class="footer__bottom"><a href="http://qr.afip.gob.ar/?qr=HJMakbCpenWNdXYfqXtEDQ,," class="data-fiscal" target="_blank" data-event="LinkClick" data-section="FooterLN"></a></div></div></footer>';
 
     it('testeo que dibuje el año pasado', () => {
         testHelper.expectProp(component, 'year', year);
     });
 
-    // it('testeo que contenga el html definido dentro', () => {
-    //     testHelper.expectHTML(component, html);
-    // });
+    it('testeo que contenga el html definido dentro', () => {
+        testHelper.expectHTML(component, html);
+    });
 
     it('testeo que se dibuje el footer y su clase', () => {
         testHelper.expectSameValue(footer.length, 1);
