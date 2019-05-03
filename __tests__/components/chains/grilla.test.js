e1f4b970dae653dc5b445a4ba301049d8ff5e4e8
Alan Castro <alancastro@lanacion.com.ar>
2019-05-03 17:41:39 -0300

    fix test

--- __tests__/components/chains/grilla.test.js
+++ __tests__/components/chains/grilla.test.js
@@ -5,9 +5,11 @@ import testHelper from '../../utils/testHelper';
 
 describe('chains - grilla', () => {
     const childrenItems = [<hijo>1</hijo>, <hijo>2</hijo>];
-    const component = mount(<Grilla>{childrenItems}</Grilla>);
-    const childrenItemsComponent = component.find('hijo');
+    const title = 'ultimos videos';
+    const cf = { title: title };
 
+    const component = mount(<Grilla customFields={cf}>{childrenItems}</Grilla>);
+    const childrenItemsComponent = component.find('hijo');
     it('Testeo que se dibujen todos los hijos que le paso', () => {
         testHelper.expectSameValue(childrenItemsComponent.length, 2);
         testHelper.expectSameValue(childrenItemsComponent.at(0).text(), '1');
