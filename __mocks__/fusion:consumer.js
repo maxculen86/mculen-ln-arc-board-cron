f8e3eaf318708db3b79c0908798268efcdc01a67
Ivan Klimberg <iklimberg@lanacion.com.ar>
2019-05-17 18:33:44 -0300

    comienzo test de program image

--- __mocks__/fusion:consumer.js
+++ __mocks__/fusion:consumer.js
@@ -27,6 +27,9 @@ jest.mock('fusion:consumer', component => {
                     case 'articleSource':
                         return require(`./data/articles/${query.id}`);
                         break;
+                    case 'imageSource':
+                        return require(`./data/images/${query.id}`);
+                        break;
                     case 'videosSearchSource':
                         switch (query.query) {
                             case 'q=type:videoANDtaxonomy.sections._id="/terapia-noticias"&sort=publish_date:desc&from=0&size=12':
