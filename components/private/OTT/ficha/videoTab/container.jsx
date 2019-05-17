84d76643c067ecadd7425c373d7cc2ca548c8309
Alan Castro <alancastro@lanacion.com.ar>
2019-05-17 19:05:03 -0300

    agregando esquemas para google parte 1

--- components/private/OTT/ficha/videoTab/container.jsx
+++ components/private/OTT/ficha/videoTab/container.jsx
@@ -16,11 +16,41 @@ class VideoTab extends PureComponent {
         );
         this.date = get(this.props.globalContent, 'publish_date', null);
         if (this.date) this.date = dateHelper.getVideoDateFormat(this.date);
+        console.log('PROPS', props);
     }
 
     render() {
         return (
             <>
+                <meta
+                    itemprop={'description'}
+                    content={this.props.globalContent.description.basic}
+                />
+                <meta
+                    itemprop={'name'}
+                    content={this.props.globalContent.headlines.basic}
+                />
+                <meta
+                    itemprop={'thumbnailUrl'}
+                    content={this.props.globalContent.promo_image.url}
+                />
+                <meta
+                    itemprop={'uploadDate'}
+                    content={new Date(
+                        this.props.globalContent.publish_date
+                    ).toISOString()}
+                />
+
+                <meta
+                    itemprop={'contentUrl'}
+                    content={this.props.globalContent.streams[0].url}
+                />
+                <meta
+                    itemprop={'duration'}
+                    content={new Date(
+                        this.props.globalContent.duration
+                    ).toISOString()}
+                />
                 <VideoTabComponent
                     videoId={this.videoId}
                     videoHtml={this.videoHtml}
