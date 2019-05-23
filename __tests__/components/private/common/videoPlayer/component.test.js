ad353901eb9991662d40c3aa79c3377c1dddbf59
Ivan Klimberg <iklimberg@lanacion.com.ar>
2019-05-23 14:01:25 -0300

    armo test de container de video player

---
+++ __tests__/components/private/common/videoPlayer/component.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import component from '../../../../../components/private/common/videoPlayer/component';
+import video from '../../../../../__mocks__/data/videos/videoParaPlayer';
+import TestHelper from '../../../../utils/testHelper';
+
+describe('private - common - videoPlayer - component', () => {});
