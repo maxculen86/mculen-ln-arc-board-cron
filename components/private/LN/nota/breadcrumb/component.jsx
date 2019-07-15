c5253d0b46b471373a6a3c0f393060ee0e485ebe
mdlopez <mdlopez@lanacion.com.ar>
2019-07-15 16:18:12 -0300

    Feature: Created Breadcrumb component

--- components/private/LN/nota/breadcrumb/component.jsx
+++ components/private/LN/nota/breadcrumb/component.jsx
@@ -1,5 +1,18 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 
 export default props => {
-    return <div>SOY EL BREADCRUMB</div>;
+    const { sections } = props;
+    const listSections = sections.map((section, index) => {
+        return (
+            <Fragment key={index}>
+                <strong className={section.type}>
+                    {' '}
+                    <a href={section.path}>{section.name}</a>{' '}
+                </strong>
+                {index !== sections.length - 1 && <span> | </span>}
+            </Fragment>
+        );
+    });
+
+    return <div className="breadcrumb">{listSections}</div>;
 };
