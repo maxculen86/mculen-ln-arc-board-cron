ba957aead2d9c954f14289ca57ecafa8e2b98aad
dlopez <dlopez@lanacion.com.ar>
2019-07-17 14:30:47 -0300

    Utilizar consumer en Feature

--- components/features/LN-nota/aperturaReceta.jsx
+++ components/features/LN-nota/aperturaReceta.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import Consumer from 'fusion:consumer';
 
-export default class AperturaReceta extends Component {
+class AperturaReceta extends Component {
     render() {
         return <div></div>;
     }
 }
+
+export default Consumer(AperturaReceta);
