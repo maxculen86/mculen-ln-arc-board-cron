0e75a6cd6773a2b73d635cad3024f870bc3e3b16
dlopez <dlopez@lanacion.com.ar>
2019-07-18 16:58:30 -0300

    mend

--- __tests__/components/private/LN/nota/TagsNota.test.js
+++ __tests__/components/private/LN/nota/TagsNota.test.js
@@ -5,7 +5,7 @@ import { render } from 'enzyme';
 
 import nota from '../../../../../__mocks__/data/articles/TWKFZQ6FCNF3ZKPHGGZPMSSOGQ';
 
-import TagsNota from '../../../../../components/private/LN/nota/apertura/tagsNota/tagsNota';
+import TagsNota from '../../../../../components/private/LN/nota/apertura/tagsNota';
 
 describe('features - LaNacion - Nota - TituloNota', () => {
     const component = render(<TagsNota globalContent={nota} />);
--- __tests__/components/private/LN/nota/TituloNota.test.js
+++ __tests__/components/private/LN/nota/TituloNota.test.js
@@ -5,7 +5,7 @@ import { render } from 'enzyme';
 
 import nota from '../../../../../__mocks__/data/articles/TWKFZQ6FCNF3ZKPHGGZPMSSOGQ';
 
-import TituloNota from '../../../../../components/private/LN/nota/apertura/tituloNota/tituloNota';
+import TituloNota from '../../../../../components/private/LN/nota/apertura/tituloNota';
 
 describe('features - LaNacion - Nota - TituloNota', () => {
     const component = render(<TituloNota globalContent={nota} />);
