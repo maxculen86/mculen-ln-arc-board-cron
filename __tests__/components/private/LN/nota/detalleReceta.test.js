0c75f1aaa05f2ab139a660a1492923c954c1908a
mdlopez <mdlopez@lanacion.com.ar>
2019-08-01 15:44:33 -0300

    Feature:(Componente Porciones/Tiempo en Arc)

--- __tests__/components/private/LN/nota/detalleReceta.test.js
+++ __tests__/components/private/LN/nota/detalleReceta.test.js
@@ -7,7 +7,7 @@ import nota from '../../../../../__mocks__/data/articles/TWKFZQ6FCNF3ZKPHGGZPMSS
 
 import DetalleReceta from '../../../../../components/private/LN/nota/apertura/detalleReceta';
 
-describe('features - LaNacion - Nota - TituloNota', () => {
+describe('features - LaNacion - Nota - Detalle Receta', () => {
     const component = render(<DetalleReceta globalContent={nota} />);
     it('Test de snapshot Detalle Receta', () => {
         expect(component).toMatchSnapshot();
