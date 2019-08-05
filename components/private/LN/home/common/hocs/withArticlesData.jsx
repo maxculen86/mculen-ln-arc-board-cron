1d8ce67a677292efa394810d6711f7eac817236a
Ivan Klimberg <iklimberg@lanacion.com.ar>
2019-08-05 19:33:24 -0300

    agrego hoc de articles

---
+++ components/private/LN/home/common/hocs/withArticlesData.jsx
@@ -0,0 +1,47 @@
+'use strict';
+
+import React, { PureComponent } from 'react';
+import Consumer from 'fusion:consumer';
+import get from 'lodash.get';
+
+function WithArticlesData() {
+    return Consumer(
+        class extends PureComponent {
+            state = {
+                articles: undefined
+            };
+
+            constructor(props) {
+                super(props);
+
+                const id = 'recetas';
+
+                const { cached, fetched } = this.getContent({
+                    sourceName: 'articlesSource',
+                    query: {
+                        id
+                    },
+                    filter
+                });
+
+                this.state = { articles: cached.content_elements };
+
+                fetched.then(response => {
+                    console.log('FETCHED: ', response);
+                    this.setState({ articles: response.content_elements });
+                });
+            }
+
+            render() {
+                return (
+                    <WrappedArticles
+                        articles={this.state.articles}
+                        {...this.props}
+                    />
+                );
+            }
+        }
+    );
+}
+
+export default WithArticlesData;
