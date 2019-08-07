bd59611142e63ec1f008edc6ab4feeb04ef92413
Ivan Klimberg <iklimberg@lanacion.com.ar>
2019-08-07 14:26:01 -0300

    escribo las props del img

--- components/private/LN/common/ArticleTypes/articleMain.jsx
+++ components/private/LN/common/ArticleTypes/articleMain.jsx
@@ -3,21 +3,19 @@ import Article from '../articleBase';
 
 class ArticleMain extends Component {
     render() {
-        console.log(this.props);
         return (
             <Article extraClasses={this.props.extraClasses}>
                 <section id="" className="cont-figure">
                     <a href={this.props.href} className="figure">
-                        <picture id="" className="content-pic picture">
+                        <picture className="content-pic picture">
                             <img
-                                src={this.props.img}
-                                alt=""
+                                src={this.props.image.src}
+                                alt={this.props.image.altText}
                                 className="content-img"
                             />
                         </picture>
                     </a>
                 </section>
-
                 <div className="mod-caja-nota__descrip">
                     <h2 className="com-title-acu">
                         <a href={this.props.href}>
@@ -25,7 +23,6 @@ class ArticleMain extends Component {
                             {this.props.title}
                         </a>
                     </h2>
-                    {/* <h4 className="com-date">1 de Julio de 2019 • 08:05</h4> */}
                     {this.props.children}
                 </div>
             </Article>
