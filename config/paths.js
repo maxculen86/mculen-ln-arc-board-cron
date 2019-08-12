b8b8364ae6d14f08472ea9a172f9e47cbbced238
Diego Anllo <danllo@lanacion.com.ar>
2019-08-12 16:38:08 -0300

    Cambio configuracion destino assets

--- config/paths.js
+++ config/paths.js
@@ -3,7 +3,7 @@ const path = require('path');
 const URL_BASE = '/pf';
 const DIRNAME_SOURCE = 'src/statics';
 const DIRNAME_PUBLIC = 'resources/dist';
-const DIRNAME_PRIVATE = 'src/bundles';
+const DIRNAME_PRIVATE = 'assets/bundles';
 
 const DIRNAME_JS = 'js';
 const DIRNAME_CSS = 'css';
