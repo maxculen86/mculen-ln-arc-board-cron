2017536599fcca2d27d15bec7ea1654f980716b3
dlopez <dlopez@lanacion.com.ar>
2019-08-26 10:01:01 -0300

    Correciones en nota. imagen default en nota.

--- properties/sites/la-nacion-ar.js
+++ properties/sites/la-nacion-ar.js
@@ -52,6 +52,38 @@ export default {
                             class: 'img-mobile',
                             type: 'cuerpo'
                         }
+                    },
+                    default: {
+                        apertura_big: {
+                            width: 1033,
+                            media: '(min-width: 768px)',
+                            class: 'img-desktop',
+                            type: 'apertura'
+                        },
+                        apertura_medium: {
+                            width: 768,
+                            media: '(min-width: 740px)',
+                            class: 'img-desktop-sm',
+                            type: 'apertura'
+                        },
+                        apertura_small: {
+                            width: 340,
+                            media: '(min-width: 320px)',
+                            class: 'img-mobile',
+                            type: 'apertura'
+                        },
+                        cuerpo_medium: {
+                            width: 768,
+                            media: '(min-width: 740px)',
+                            class: 'img-desktop-sm',
+                            type: 'cuerpo'
+                        },
+                        cuerpo_small: {
+                            width: 340,
+                            media: '(min-width: 320px)',
+                            class: 'img-mobile',
+                            type: 'cuerpo'
+                        }
                     }
                 }
             },
