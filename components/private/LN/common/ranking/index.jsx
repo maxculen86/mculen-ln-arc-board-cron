de14da663a13c5f79f79e8fdc7a7840400db55cc
Abraham <asap_acedeno@lnexternos.com.ar>
2019-08-26 15:35:51 -0300

    changes ranking component

--- components/private/LN/common/ranking/index.jsx
+++ components/private/LN/common/ranking/index.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import '../../../../../assets/bundles/css/ln/components/ranking.css';
+import PropTypes from 'fusion:prop-types';
 import TitleSection from '../titles/titleSection';
 import OrderedList from '../lists/ordered';
 import ArticleMain from '../articleTypes/articleMain';
 
+// mock array
+import { articles } from './mock';
+
 const Ranking = props => {
-    const recipes = () => {
+    const articleList = () => {
         // TODO: pasarle el objeto con la receta (articleData)
-        return props.recipes.map(recipe => <ArticleMain border={true} />);
+        console.log('articulos: ', articles);
+        return articles.map(article => (
+            <ArticleMain border articleData={article} />
+        ));
     };
 
     return (
         <div className="com-ranking hlp-none hlp-tablet-none">
             <TitleSection size="m" text="Recetas más leídas" />
-            <OrderedList items={recipes} />
+            <OrderedList items={articleList()} />
         </div>
     );
 };
+
+/*Ranking.propTypes = {
+    recipes: PropTypes.shape.isRequired
+};*/
 
 export default Ranking;
