cdbe39f033e12ccb9784b16921a219a29b427cbf
mdlopez <mdlopez@lanacion.com.ar>
2019-08-27 13:08:53 -0300

    Expiration Cookie helper

---
+++ components/private/LN/common/utils/expiredCookie.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const expiredCookie = () => {
+    function setCookie(timeExpiration = 5) {
+        const now = new Date();
+        const exp = new Date(now.getTime() + timeExpiration * 1000);
+        document.cookie = `ExpirationCookieTest=1; expires=${exp.toUTCString()}`;
+        return !!(
+            document.cookie &&
+            document.cookie.indexOf('ExpirationCookieTest=1') !== -1
+        );
+    }
+
+    function getCookie() {
+        return !!(
+            document.cookie &&
+            document.cookie.indexOf('ExpirationCookieTest=1') !== -1
+        );
+    }
+
+    return {
+        setCookie,
+        getCookie
+    };
+};
+
+export default expiredCookie;
