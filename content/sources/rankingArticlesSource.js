927ce8d18a0ac771bc0b73ba623aba8da5164076
mdlopez <mdlopez@lanacion.com.ar>
2019-08-28 17:27:49 -0300

    Creación del Hoc y ContentSource de Ranking

---
+++ content/sources/rankingArticlesSource.js
@@ -0,0 +1,77 @@
+import { addResizedUrls } from '@arc-core-components/content-source_content-api-v4';
+import { RESIZER_SECRET, RESIZER_URL } from 'fusion:environment';
+import getProperties from 'fusion:properties';
+import SourceSetSizes from '../../components/private/LN/home/common/config/sourceSets';
+
+const resolve = key => {
+    const { sectionId, size, page, website } = key;
+    const arcSite = key['arc-site'];
+    const from = ((page || 1) - 1) * size;
+    const basePath = `/content/v4/search/published/?website=${website ||
+        arcSite}`;
+
+    const query = `&body={
+            "query":{
+                "bool": {
+                    "must": [
+                        {
+                            "term":
+                            {
+                                "type":"story"
+                            }
+                        },
+                        {
+                            "nested":{
+                                "path":"taxonomy.sections",
+                                "query":{
+                                    "bool":{
+                                        "must":[
+                                            {
+                                                "term":{
+                                                    "taxonomy.sections._id":"${sectionId}"
+                                                }
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    ]}
+                }
+            }`;
+    const final = `${basePath}${query}&size=${size || 30}&from=${from}
+            &sort=publish_date:desc`;
+    return final;
+};
+
+const getPresets = () => {
+    const presets = {};
+    SourceSetSizes.forEach(ss => {
+        ss.values.forEach(v => {
+            presets[`${ss.name}_${v.name}`] = {
+                height: v.value
+            };
+        });
+    });
+    return presets;
+};
+
+const transform = data => {
+    const presets = getPresets();
+    return addResizedUrls(data, {
+        resizerSecret: RESIZER_SECRET,
+        resizerUrl: RESIZER_URL,
+        presets
+    });
+};
+
+export default {
+    resolve,
+    params: {
+        sectionId: 'text',
+        size: 'text',
+        page: 'text',
+        website: 'text'
+    },
+    transform
+};
