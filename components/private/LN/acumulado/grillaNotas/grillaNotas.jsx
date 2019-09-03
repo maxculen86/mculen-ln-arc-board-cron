c9a150946ff50779025d3da33fbca532a2643f87
Ivan Klimberg <iklimberg@lanacion.com.ar>
2019-09-03 12:24:24 -0300

    convierto en un componente funcional el de grilla Notas

--- components/private/LN/acumulado/grillaNotas/grillaNotas.jsx
+++ components/private/LN/acumulado/grillaNotas/grillaNotas.jsx
@@ -1,46 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ArticleMain from '../../common/articleTypes/articleMain';
 import ArticleDate from '../../common/dateArticle';
 import WithAcuArticlesData from '../../common/hocs/WithAcuArticlesData';
-import filter from '../../../../../content/filters/LN/acumulado/articleAcu';
 import BtnMasNotas from '../botonVerMasNotas';
 
 const CLASS_W_100 = 'w-100-mobile';
-class GrillaNotas extends Component {
-    render() {
-        let articles = [];
-        if (this.props.articles && this.props.articles.length) {
-            articles = this.props.articles.map((a, i) => {
-                const dateComponent = (
-                    <ArticleDate display_date={a.display_date} />
-                );
-                return (
-                    <ArticleMain
-                        key={i}
-                        children={dateComponent}
-                        articleData={a}
-                        extraClasses={CLASS_W_100}
-                    />
-                );
-            });
-        }
 
-        return (
-            <>
-                <section className="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
-                    {articles}
+const GrillaNotas = ({
+    articles,
+    hayMasNotas,
+    obtenerMasNotas,
+    globalContent
+}) => {
+    return (
+        <>
+            <section className="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
+                {articles.map((a, i) => {
+                    const dateComponent = (
+                        <ArticleDate display_date={a.display_date} />
+                    );
+                    return (
+                        <ArticleMain
+                            key={i}
+                            articleData={a}
+                            extraClasses={CLASS_W_100}
+                        >
+                            {dateComponent}
+                        </ArticleMain>
+                    );
+                })}
+            </section>
+            {hayMasNotas && (
+                <section className="row">
+                    <BtnMasNotas
+                        onClickHandler={obtenerMasNotas}
+                        name={globalContent.name}
+                    />
                 </section>
-                {this.props.hayMasNotas && (
-                    <section className="row">
-                        <BtnMasNotas
-                            onClickHandler={this.props.obtenerMasNotas}
-                            name={this.props.globalContent.name}
-                        />
-                    </section>
-                )}
-            </>
-        );
-    }
-}
+            )}
+        </>
+    );
+};
 
 export default WithAcuArticlesData(GrillaNotas, null, 'masNotas'); // TODO: dejo el filter comentado porque no me trae datos. REVISAR, filter);
