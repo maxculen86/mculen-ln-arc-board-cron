40497de7c4db2821be9487cc1b7dfeae867737d1
Ivan Klimberg <iklimberg@lanacion.com.ar>
2019-09-10 07:37:12 -0300

    fix grilla Notas

--- components/private/LN/acumulado/grillaNotas/grillaNotas.jsx
+++ components/private/LN/acumulado/grillaNotas/grillaNotas.jsx
@@ -1,45 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ArticleMain from '../../common/articleTypes/articleMain';
 import ArticleDate from '../../common/dateArticle';
 import WithAcuArticlesData from '../../common/hocs/WithAcuArticlesData';
+import filter from '../../../../../content/filters/LN/acumulado/articleAcu';
 import BtnMasNotas from '../botonVerMasNotas';
 
 const CLASS_W_100 = 'w-100-mobile';
-
-const GrillaNotas = ({
-    articles,
-    hayMasNotas,
-    obtenerMasNotas,
-    globalContent
-}) => {
-    return (
-        <>
-            <section className="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
-                {articles.map((a, i) => {
-                    const dateComponent = (
-                        <ArticleDate display_date={a.display_date} />
-                    );
-                    return (
-                        <ArticleMain
-                            key={i}
-                            articleData={a}
-                            extraClasses={CLASS_W_100}
-                        >
-                            {dateComponent}
-                        </ArticleMain>
-                    );
-                })}
-            </section>
-            {hayMasNotas && (
-                <section className="row">
-                    <BtnMasNotas
-                        onClickHandler={obtenerMasNotas}
-                        name={globalContent.name}
+class GrillaNotas extends Component {
+    render() {
+        let articlesComponents = [];
+        const { articles, imageResizePresets } = this.props;
+        if (articles && articles.length) {
+            articlesComponents = articles.map((a, i) => {
+                const dateComponent = (
+                    <ArticleDate display_date={a.display_date} />
+                );
+                return (
+                    <ArticleMain
+                        key={i}
+                        imageResizePresets={imageResizePresets}
+                        children={dateComponent}
+                        articleData={a}
+                        extraClasses={CLASS_W_100}
                     />
+                );
+            });
+        }
+
+        return (
+            <>
+                <section className="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
+                    {articlesComponents}
                 </section>
-            )}
-        </>
-    );
-};
+                {this.props.hayMasNotas && (
+                    <section className="row">
+                        <BtnMasNotas
+                            onClickHandler={this.props.obtenerMasNotas}
+                            name={this.props.globalContent.name}
+                        />
+                    </section>
+                )}
+            </>
+        );
+    }
+}
 
 export default WithAcuArticlesData(GrillaNotas, null, 'masNotas'); // TODO: dejo el filter comentado porque no me trae datos. REVISAR, filter);
