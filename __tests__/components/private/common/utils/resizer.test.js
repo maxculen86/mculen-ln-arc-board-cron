f1a19b9865525fec58b78ecb9a7109a90bcf88c3
dlopez <dlopez@lanacion.com.ar>
2019-09-17 12:46:34 -0300

    Resize de AnsDoc (credits, promo_items, content_elements)

--- __tests__/components/private/common/utils/resizer.test.js
+++ __tests__/components/private/common/utils/resizer.test.js
@@ -1,37 +1,96 @@
 import {
     createResizer,
-    resizeArcImage
+    resizeArcImage,
+    addResizedUrls
 } from '../../../../../components/private/common/utils/image/resizer';
 import nota from '../../../../../__mocks__/data/articles/JZQDUAOPSRF3LLDZOT6374IDOM.json';
 
 describe('LN - Common - Utils - Imagen - Resizer', () => {
-    it('Test de resizeo', () => {
-        const resizer = createResizer(
-            'Fmkgru2rZ2uPZ5wXs7B2HbVDHS2SZuA7',
-            'http://demo-prod.origin.arcpublishing.com/resizer'
-        );
+    const resizerSecret = 'Fmkgru2rZ2uPZ5wXs7B2HbVDHS2SZuA7';
+    const resizerUrl = 'http://demo-prod.origin.arcpublishing.com/resizer';
+    const resizer = createResizer(resizerSecret, resizerUrl);
+    const presets = {
+        big: {
+            width: 200,
+            height: 123,
+            media: '(min-width: 768px)',
+            class: 'img-desktop',
+            type: 'apertura'
+        },
+        medium: {
+            width: 150,
+            height: 175,
+            media: '(min-width: 240px)',
+            class: 'img-desktop-sm',
+            type: 'apertura'
+        }
+    };
 
+    it('Test de resizeo horizontal', () => {
         const newNota = resizeArcImage(
             nota.promo_items.basic,
-            {
-                big: {
-                    width: 200,
-                    height: 123,
-                    media: '(min-width: 768px)',
-                    class: 'img-desktop',
-                    type: 'apertura'
-                },
-                medium: {
-                    width: 150,
-                    height: 175,
-                    media: '(min-width: 240px)',
-                    class: 'img-desktop-sm',
-                    type: 'apertura'
-                }
-            },
+            presets,
             resizer
         );
 
-        console.log('resized urls---------------------', newNota.resized_urls);
+        expect(newNota.resized_urls.big.resizedUrl).toBe(
+            'http://demo-prod.origin.arcpublishing.com/resizer/k_-nxquhw4MOtCGTufFgn1lPu1I=/200x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/VASAYYYBLVFIJCFDSH22JS5X2Q.jpg'
+        );
+
+        expect(newNota.resized_urls.medium.resizedUrl).toBe(
+            'http://demo-prod.origin.arcpublishing.com/resizer/W9UakO2brdaoE3v4FIjLT1AcNyc=/150x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/VASAYYYBLVFIJCFDSH22JS5X2Q.jpg'
+        );
+
+        Object.keys(presets.big).forEach(p => {
+            expect(newNota.resized_urls.big.option[p]).toBe(presets.big[p]);
+        });
+    });
+
+    it('Test de resizeo vertical', () => {
+        const notaVertical = { ...nota.promo_items.basic };
+        const height = notaVertical.height;
+        notaVertical.height = notaVertical.width;
+        notaVertical.width = height;
+
+        const newNota = resizeArcImage(notaVertical, presets, resizer);
+
+        expect(newNota.resized_urls.big.resizedUrl).toBe(
+            'http://demo-prod.origin.arcpublishing.com/resizer/JY6c4oxLnV_X7OovdRp7KLqPhH4=/0x123/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/VASAYYYBLVFIJCFDSH22JS5X2Q.jpg'
+        );
+    });
+
+    it('Test de resizeo en nota ANS', () => {
+        const newNota = addResizedUrls(nota, {
+            presets,
+            resizerSecret,
+            resizerUrl
+        });
+
+        expect(newNota.promo_items.basic.resized_urls.big.resizedUrl).toBe(
+            'http://demo-prod.origin.arcpublishing.com/resizer/k_-nxquhw4MOtCGTufFgn1lPu1I=/200x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/VASAYYYBLVFIJCFDSH22JS5X2Q.jpg'
+        );
+        Object.keys(presets.big).forEach(p => {
+            expect(newNota.promo_items.basic.resized_urls.big.option[p]).toBe(
+                presets.big[p]
+            );
+        });
+
+        expect(newNota.content_elements[3].resized_urls.big.resizedUrl).toBe(
+            'http://demo-prod.origin.arcpublishing.com/resizer/NQSUlTHGlhDAsv_StDOU4tEaWUY=/200x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/L6DRYSNKH5BHNHQC2CPUVQ7ILQ.png'
+        );
+        Object.keys(presets.big).forEach(p => {
+            expect(newNota.content_elements[3].resized_urls.big.option[p]).toBe(
+                presets.big[p]
+            );
+        });
+
+        expect(newNota.credits.by[0].image.resized_urls.big.resizedUrl).toBe(
+            'http://demo-prod.origin.arcpublishing.com/resizer/DlXn4M5x_q0_XJHXWh7nvOi4SJ8=/0x123/s3.amazonaws.com/arc-authors/lanacionar/d9ada84c-fee5-42c8-b34f-8dc3c28ea585.png'
+        );
+        Object.keys(presets.big).forEach(p => {
+            expect(newNota.credits.by[0].image.resized_urls.big.option[p]).toBe(
+                presets.big[p]
+            );
+        });
     });
 });
--- components/private/common/utils/image/resizer.js
+++ components/private/common/utils/image/resizer.js
@@ -1,8 +1,13 @@
 export const createResizer = (resizerKey, resizerUrl) => {
     const Thumbor = require('thumbor');
 
-    const resizeUrl = (originalUrl, resizeOptions) => {
-        if (!resizeOptions.width || !resizeOptions.height)
+    const resizeUrl = (
+        originalUrl,
+        originalWidth,
+        originalHeight,
+        resizeOptions
+    ) => {
+        if (!resizeOptions.width && !resizeOptions.height)
             throw new Error(
                 'Se requiere width o heigth para realizar el resize'
             );
@@ -11,7 +16,7 @@ export const createResizer = (resizerKey, resizerUrl) => {
         let finalWidth = resizeOptions.width;
         let finalHeight = resizeOptions.height;
         if (!resizeOptions.useFullSize) {
-            if (resizeOptions.width >= resizeOptions.height) {
+            if (originalWidth >= originalHeight) {
                 if (finalWidth) finalHeight = 0;
             } else if (finalHeight) finalWidth = 0;
         }
@@ -26,26 +31,124 @@ export const createResizer = (resizerKey, resizerUrl) => {
         return newUrl;
     };
 
+    const resizeUrls = (
+        originalUrl,
+        originalWidth,
+        originalHeight,
+        presets
+    ) => {
+        const resp = {};
+        Object.keys(presets).forEach(opt => {
+            resp[opt] = {
+                resizedUrl: resizeUrl(
+                    originalUrl,
+                    originalWidth,
+                    originalHeight,
+                    presets[opt]
+                ),
+                option: presets[opt]
+            };
+        });
+        return resp;
+    };
     return {
-        resizeUrl
+        resizeUrl,
+        resizeUrls
     };
 };
 
 export const resizeArcImage = (arcImage, resizeOptions, resizer) => {
     if (arcImage.type !== 'image' || !arcImage.url)
-        throw new Error('Tipo de dato no valido');
-
-    const resizedUrls = {};
-    Object.keys(resizeOptions).forEach(opt => {
-        resizedUrls[opt] = resizer.resizeUrl(arcImage.url, resizeOptions[opt]);
-    });
+        throw new Error(
+            'Tipo de dato no valido. Se necesita un tipo "image" y una url para realizar el resize'
+        );
 
     return {
         ...arcImage,
-        resized_urls: resizedUrls
+        resized_urls: resizer.resizeUrls(
+            arcImage.url,
+            arcImage.width,
+            arcImage.height,
+            resizeOptions
+        )
     };
 };
 
-const resizeAnsDocument = ansDoc => {};
+const resizeCredits = (credits, resizeOptions, resizer) => {
+    const resp = {};
+    Object.keys(credits).forEach(key => {
+        const credit = credits[key];
+        resp[key] = credit.map(c => {
+            if (!!c.image && !!c.image.url) {
+                const resizes = resizer.resizeUrls(
+                    c.image.url,
+                    c.image.width,
+                    c.image.height,
+                    resizeOptions
+                );
+                return {
+                    ...c,
+                    image: {
+                        ...c.image,
+                        resized_urls: resizes
+                    }
+                };
+            } else {
+                return c;
+            }
+        });
+    });
+    return resp;
+};
 
-export default resizeAnsDocument;
+const resizePromoItems = (promoItems, resizeOptions, resizer) => {
+    const resp = {};
+    Object.keys(promoItems).forEach(key => {
+        const pi = promoItems[key];
+        if (pi.type === 'image') {
+            resp[key] = resizeArcImage(pi, resizeOptions, resizer);
+        } else {
+            resp[key] = pi;
+        }
+    });
+    return resp;
+};
+
+export const addResizedUrls = (ansDoc, option) => {
+    if (!option.resizerSecret || !option.resizerUrl || !option.presets)
+        throw new Error(
+            'Debe proporcionar el resizerSecret, resizerUrl y presets'
+        );
+
+    const resizer = createResizer(option.resizerSecret, option.resizerUrl);
+
+    const respDoc = {
+        ...ansDoc,
+        content_elements:
+            ansDoc.content_elements &&
+            ansDoc.content_elements.map(elem => {
+                if (elem.type === 'image') {
+                    return resizeArcImage(elem, option.presets, resizer);
+                }
+                return elem;
+            })
+    };
+
+    if (!!ansDoc.promo_items) {
+        respDoc.promo_items = resizePromoItems(
+            ansDoc.promo_items,
+            option.presets,
+            resizer
+        );
+    }
+
+    if (!!ansDoc.credits) {
+        respDoc.credits = resizeCredits(
+            ansDoc.credits,
+            option.presets,
+            resizer
+        );
+    }
+
+    return respDoc;
+};
