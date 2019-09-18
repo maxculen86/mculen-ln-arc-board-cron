e4fe6fc9114f2e55aa146bf68a0398aa1945761e
mdlopez <mdlopez@lanacion.com.ar>
2019-09-18 12:03:15 -0300

    Separar en componentes mas atomicos AperturaReceta Con/Sin Destacado

---
+++ components/private/LN/nota/apertura/AperturaReceta/aperturaReceta.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'fusion:prop-types';
+import AperturaConDestacado from './AperturaConDestacado';
+import AperturaSinDestacado from './AperturaSinDestacado';
+
+import '../../../../../../resources/dist/css/ln/layouts/grid.css';
+
+const aperturaReceta = props => {
+    const {
+        globalContent: {
+            promo_items: { receta, basic },
+            taxonomy,
+            taxonomy: { tags }
+        }
+    } = props;
+
+    const hasMultimedia = !!(basic && basic._id);
+
+    return (
+        <div
+            className={`row aper-receta w-100 hlp-marginBottom-40 ${
+                hasMultimedia ? '' : 'sin-foto'
+            }`}
+        >
+            {hasMultimedia ? (
+                <AperturaConDestacado {...props} />
+            ) : (
+                <AperturaSinDestacado
+                    tags={tags}
+                    taxonomy={taxonomy}
+                    receta={receta}
+                />
+            )}
+        </div>
+    );
+};
+
+aperturaReceta.propTypes = {
+    globalContent: PropTypes.shape({
+        taxonomy: PropTypes.shape({
+            tags: PropTypes.arrayOf(
+                PropTypes.shape({
+                    description: PropTypes.string,
+                    slug: PropTypes.string,
+                    text: PropTypes.string
+                })
+            ).isRequired
+        }),
+        promo_items: PropTypes.shape({
+            receta: PropTypes.object,
+            basic: PropTypes.object
+        })
+    })
+};
+
+export default aperturaReceta;
