e87c2a18dc9503f1b1202d7560ea114b0d9d393d
Diego Anllo <danllo@lanacion.com.ar>
2019-09-19 18:36:15 -0300

    Agrego version a ArcAds.js

--- components/private/common/scriptManager/arcAds.jsx
+++ components/private/common/scriptManager/arcAds.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default () => {
-    return <script src="/pf/resources/common/js/arcAds.js" />;
+    return <script src="/pf/resources/common/js/arcAds.js?d=45" />;
 };
