72dc86bd69ac4330d195686ad2afc4b16f8b5d6f
Abraham <asap_acedeno@lnexternos.com.ar>
2019-09-19 17:02:05 -0300

    filtrado de articles para mas recetas de

--- components/private/LN/nota/masNotas/articleList.jsx
+++ components/private/LN/nota/masNotas/articleList.jsx
@@ -5,9 +5,23 @@ import articleFiltered from '../../../../../content/filters/LN/nota/articleFilte
 import withAcuArticlesData from '../../common/hocs/WithAcuArticlesData';
 
 const ArticleList = props => {
-    const { articles, imageResizePresets } = props;
+    const {
+        articles,
+        imageResizePresets,
+        globalContent: { promo_items }
+    } = props;
+
     if (!articles) return null;
-    return articles.map(e => (
+
+    const articleId = promo_items.basic._id;
+    const articlesArray = articles.filter(article => {
+        if (article.promo_items.hasOwnProperty('receta')) {
+            return article.promo_items.basic._id !== articleId;
+        }
+        return true;
+    });
+
+    return articlesArray.map(e => (
         <ArticleMain imageResizePresets={imageResizePresets} articleData={e} />
     ));
 };
