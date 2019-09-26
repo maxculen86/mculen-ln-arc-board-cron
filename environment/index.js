498dcca31459ab60983d23a07d272263a058b06e
dlopez <dlopez@lanacion.com.ar>
2019-09-26 16:33:31 -0300

    Todo de encrypt key

--- environment/index.js
+++ environment/index.js
@@ -2,7 +2,7 @@ export default {
     IS_DEV: false,
     RESIZER_URL:
         'https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer',
-    RESIZER_KEY: 'Fmkgru2rZ2uPZ5wXs7B2HbVDHS2SZuA7',
+    RESIZER_KEY: 'Fmkgru2rZ2uPZ5wXs7B2HbVDHS2SZuA7', // TODO: pasar a encriptado
     APIingresar: 'https://api-ingresar.lanacion.com.ar',
     ReloginValidation: '8121600000',
     SitioSeguroRegistracion: 'https://ingresar.lanacion.com.ar',
