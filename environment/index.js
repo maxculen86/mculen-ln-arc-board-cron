18e1c2a4aa07bcce72cf99daafc1a7da0571b30d
dlopez <dlopez@lanacion.com.ar>
2019-09-26 16:25:45 -0300

    Uso key de resizer productivo sin encryptar hasta que nos resuelvan los de ARC

--- environment/index.js
+++ environment/index.js
@@ -2,8 +2,7 @@ export default {
     IS_DEV: false,
     RESIZER_URL:
         'https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer',
-    RESIZER_KEY:
-        '%{AQICAHh/411m8FGwOHtrUTs9KkxN3n0LVOK3XAvOUKflIN5VygEE/uDYNteKFx76NFP3VOxjAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMfryx2uRDDDp6oE11AgEQgDvr9man39S9z6bK2W1MUdC5TJLXLIWgVwn9qFCEXay+yeNFM31lqDHy3NqxELcfJP9rSmi0mGb6QzLYpg==}',
+    RESIZER_KEY: 'Fmkgru2rZ2uPZ5wXs7B2HbVDHS2SZuA7',
     APIingresar: 'https://api-ingresar.lanacion.com.ar',
     ReloginValidation: '8121600000',
     SitioSeguroRegistracion: 'https://ingresar.lanacion.com.ar',
