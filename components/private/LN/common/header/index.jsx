4596fd4fb27826d65e27cb9199443344459c2a5b
Ivan Klimberg <iklimberg@lanacion.com.ar>
2019-09-27 08:54:39 -0300

    agrego padding al top

--- components/private/LN/common/header/index.jsx
+++ components/private/LN/common/header/index.jsx
@@ -13,13 +13,18 @@ class Index extends Component {
         const { isMobile } = this.props;
         const idHeader = isMobile ? 'header-mobile' : 'header';
         const header = document.getElementById(idHeader);
-        window.addEventListener('scroll', () => this.onScrollHandler(header));
+        const headerHeigth = header.clientHeight || header.offsetHeight;
+        const main = document.querySelector('main');
+        window.addEventListener('scroll', () =>
+            this.onScrollHandler(header, main, headerHeigth)
+        );
     }
 
-    onScrollHandler = header => {
+    onScrollHandler = (header, main, heigth) => {
         const scrollPos = window.scrollY;
         const { classList } = header;
         if (scrollPos) {
+            main.style.paddingTop = `${heigth}px`;
             if (scrollPos < lastScrollPosition) {
                 classList.remove(CLASS_SCROLL_DOWN);
                 classList.add(CLASS_SCROLL_UP);
@@ -30,6 +35,8 @@ class Index extends Component {
                 //SCROLL DOWN
             }
         } else {
+            //16px es el alto que viene por defecto en la clase.
+            main.style.paddingTop = '16px';
             classList.remove(CLASS_SCROLL_UP);
             classList.remove(CLASS_SCROLL_DOWN);
         }
