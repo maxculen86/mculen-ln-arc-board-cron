99643f3b92df4d03dac4a7b3b421df3b37b2aa88
dlopez <dlopez@lanacion.com.ar>
2019-09-27 11:12:39 -0300

    Aperterua sin destacado: no mostrar divcol3 si no hay recetaComponent

--- components/private/LN/nota/apertura/AperturaReceta/AperturaSinDestacado.jsx
+++ components/private/LN/nota/apertura/AperturaReceta/AperturaSinDestacado.jsx
@@ -12,9 +12,11 @@ const AperturaSinDestacado = props => {
     return (
         <div className="col-desksm-12 cont-aper">
             <div className="row">
-                <div className="col-desksm-3">
-                    {receta && <DetalleReceta receta={receta} />}
-                </div>
+                {receta && (
+                    <div className="col-desksm-3">
+                        <DetalleReceta receta={receta} />
+                    </div>
+                )}
                 <div className="col-desksm-9">
                     <Sections taxonomy={taxonomy} destacado />
                     {!!tags && tags.length > 0 && (
