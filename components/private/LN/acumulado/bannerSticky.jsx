a2d5ebb98864ac20c14ba78e0770912b3513e604
Ivan Klimberg <iklimberg@lanacion.com.ar>
2019-10-03 19:53:54 -0300

    agrego funcion on scroll a banner sticky2

--- components/private/LN/acumulado/bannerSticky.jsx
+++ components/private/LN/acumulado/bannerSticky.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Consumer from 'fusion:consumer';
 import Banner from '../common/banner';
 
-function BannerSticky({ isAdmin, siteProperties }) {
-    return (
-        <Banner
-            siteProperties={siteProperties}
-            slotGroup="acumulado"
-            selectedSlots={{
-                mobileSlot: 'sticky2_mob'
-            }}
-            isAdmin={isAdmin}
-            sticky
-        />
-    );
+const CLASS_SCROLL_DOWN = '--showSticky';
+class BannerSticky extends Component {
+    lastScroll = 0;
+
+    componentDidMount() {
+        const sticky1 = document.getElementById('sticky1_mob');
+        if (sticky1) {
+            const wrap = document.getElementById('wrap');
+            const sticky1Position =
+                sticky1.offsetTop +
+                (sticky1.clientHeight || sticky1.offsetHeight);
+            window.addEventListener('scroll', () =>
+                this.onScrollHandler(wrap, sticky1Position)
+            );
+        }
+    }
+
+    onScrollHandler = (wrap, sticky1Position) => {
+        const sp = window.scrollY;
+        if (sp > this.lastScroll && sp > sticky1Position)
+            wrap.classList.add(CLASS_SCROLL_DOWN);
+        else wrap.classList.remove(CLASS_SCROLL_DOWN);
+        this.lastScroll = sp;
+    };
+
+    render() {
+        const { isAdmin, siteProperties } = this.props;
+        return (
+            <Banner
+                siteProperties={siteProperties}
+                slotGroup="acumulado"
+                selectedSlots={{
+                    mobileSlot: 'sticky2_mob'
+                }}
+                isAdmin={isAdmin}
+                sticky
+            />
+        );
+    }
 }
 
 export default Consumer(BannerSticky);
