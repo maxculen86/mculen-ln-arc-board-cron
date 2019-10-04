bac5c7958fc7512e2b25113df128bb6ac638e84a
Abraham <asap_acedeno@lnexternos.com.ar>
2019-10-04 14:44:49 -0300

    comment component change

--- components/private/LN/nota/comments.jsx
+++ components/private/LN/nota/comments.jsx
@@ -1,6 +1,16 @@
 import React, { Fragment, useEffect } from 'react';
 
 const Comments = props => {
+    console.log("############ GLOBALCONTENT ##########: ", props.globalContent);
+    const { globalContent: { _id }} = props;
+
+    const metadata = {
+        title: '',
+        url: '',
+        tags: '',
+        type: 'livecomment'
+    };
+    
     useEffect(() => {
         const networkConfig = {
             network: 'la-nacion.fyre.co'
@@ -8,9 +18,9 @@ const Comments = props => {
 
         const convConfig = {
             siteId: '356483',
-            articleId: dataLiveFyre.getAttribute("data-entrada"),
+            articleId: _id,
             el: 'livefyre',
-            collectionMeta: dataLiveFyre.getAttribute("data-id"),
+            collectionMeta: 'dataLiveFyre.getAttribute("data-id")',
             "datetimeFormat": {
                 minutesUntilAbsoluteTime: 4,
                 absoluteFormat: 'HH:mm dd/MM/y'
@@ -34,8 +44,12 @@ const Comments = props => {
 
     return (
         <Fragment>
-            <div id="tokenLF" data-id="" data-entrada="" data-lf-siteId="356483"></div>
-            <div className="livefyre" />
+            <section id="comentarios" data-module="nota-sugeridas-comentarios">
+                <div id="tokenLF" data-id="" data-entrada={_id} data-lf-siteId="356483"></div>
+                <p class="legales">Los comentarios publicados son de exclusiva responsabilidad de sus autores y las consecuencias derivadas de ellos pueden ser pasibles de sanciones legales. Aquel usuario que incluya en sus mensajes algún comentario violatorio del reglamento será eliminado e inhabilitado para volver a comentar. Enviar un comentario implica la aceptación del Reglamento.</p>
+                <div class="recordar-logueo">Para poder comentar tenés que ingresar con tu usuario de LA NACION.</div>
+                <div className="livefyre" />
+            </section>
         </Fragment>
     );
 };
