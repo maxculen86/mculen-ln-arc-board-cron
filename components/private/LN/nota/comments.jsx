7d92ce9bf67ecd9adec76bbdc7966ac85a81e5fb
Abraham <asap_acedeno@lnexternos.com.ar>
2019-10-04 11:42:39 -0300

    fixing filename typo

--- components/private/LN/nota/comments.jsx
+++ components/private/LN/nota/comments.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 
 const Comments = props => {
     useEffect(() => {
@@ -8,10 +8,18 @@ const Comments = props => {
 
         const convConfig = {
             siteId: '356483',
-            articleId: 'articleId',
+            articleId: dataLiveFyre.getAttribute("data-entrada"),
             el: 'livefyre',
-            collectionMeta: 'collectionMeta',
-            checksum: 'checksum'
+            collectionMeta: dataLiveFyre.getAttribute("data-id"),
+            "datetimeFormat": {
+                minutesUntilAbsoluteTime: 4,
+                absoluteFormat: 'HH:mm dd/MM/y'
+            },
+            editorCss: {
+                background: '#ccc',
+                color: 'red',
+                font: '30px "Helvetica Neue", Helvetica, Arial, Geneva, sans-serif'
+            }
         };
 
         Livefyre.require(['fyre.conv#3', 'auth'], (Conv, auth) => {
@@ -24,7 +32,12 @@ const Comments = props => {
         });
     }, []);
 
-    return <div className="livefyre" />;
+    return (
+        <Fragment>
+            <div id="tokenLF" data-id="" data-entrada="" data-lf-siteId="356483"></div>
+            <div className="livefyre" />
+        </Fragment>
+    );
 };
 
 export default Comments;
