66389e812f2d55bb0b05f0744892f7012a09a530
mdlopez <mdlopez@lanacion.com.ar>
2019-10-04 11:52:57 -0300

    Nuevos cambios en LiveFyre

--- components/private/common/scriptManager/Livefire.jsx
+++ components/private/common/scriptManager/Livefire.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Layfyre extends Component {
+export default class Livefyre extends Component {
     constructor(props) {
         super(props);
 
