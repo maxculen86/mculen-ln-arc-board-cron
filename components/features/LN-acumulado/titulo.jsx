c271a349f37fbe471cf7112205d89f4dcfa6a512
Ivan Klimberg <iklimberg@lanacion.com.ar>
2019-10-08 08:47:35 -0300

    convierto acumulado title en feature

---
+++ components/features/LN-acumulado/titulo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Title from '../../private/LN/acumulado/acumuladoTitle';
+
+function Titulo() {
+    return <Title />;
+}
+
+Titulo.label = 'LN-Acumulado-Titulo';
+export default Titulo;
