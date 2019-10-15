715c8129f5f46a5d669d61980cb7674e5c8eac86
dlopez <dlopez@lanacion.com.ar>
2019-10-15 11:20:27 -0300

    Llamada al script de ads usando deployment function

--- components/private/common/scriptManager/arcAds.jsx
+++ components/private/common/scriptManager/arcAds.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import PropTypes from 'fusion:prop-types';
 
-export default () => {
-    return <script src="/pf/resources/common/js/arcAds.js?d=45" />;
+const arcAds = ({ deployment, contextPath }) => {
+    return (
+        <script
+            src={deployment(`${contextPath}/resources/common/js/arcAds.js`)}
+        />
+    );
 };
+
+arcAds.propTypes = {
+    deployment: PropTypes.func.isRequired,
+    contextPath: PropTypes.string
+};
+
+arcAds.defaultProps = {
+    contextPath: ''
+};
+
+export default arcAds;
