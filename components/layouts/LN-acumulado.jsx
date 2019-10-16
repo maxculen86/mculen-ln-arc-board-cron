6dadc7618a811afa1b545fa1e01e9f6cea69d1e6
Daro <daguilar@lanacion.com.ar>
2019-10-16 16:24:53 -0300

    update

--- components/layouts/LN-acumulado.jsx
+++ components/layouts/LN-acumulado.jsx
@@ -10,6 +10,8 @@ import '../../resources/dist/css/ln/pages/acu.css';
 import '../../resources/dist/css/ln/components/ordered.css';
 import '../../resources/dist/css/ln/components/unordered.css';
 
+import '../../resources/dist/css/ln/pages/acu-revista.css';
+
 const layoutItems = [
     'Pre-Apertura',
     'Breadcrumb',
@@ -22,11 +24,32 @@ const layoutItems = [
 class LNAcumuladoLayout extends Component {
     render() {
         return (
-            <div id="wrapper">
+            <div id="wrapper acu-revista ohlala">
                 <Header />
                 <main>
                     {/* BANNERS: CABEZAL Y STICKY */}
                     {this.props.children[0]}
+                    <div className="row mod-opening-revista">
+                        <section className="lay">
+                            <div className="com-share">
+                                <div className="share-left">
+                                    <button href="" className="icon-facebook"></button>
+                                    <button href="" className="icon-twitter"></button>
+                                    <button href="" className="icon-instagram"></button>
+                                </div>
+                            </div>
+                            <div className="logo hlp-text-center hlp-marginBottom-desk-40">
+                                <i className="logo-ohlala hlp-marginBottom-desk-10"></i>
+                            </div>
+                            <div className="hlp-text-center hlp-marginBottom-40">
+                                <a className="com-link" href=" ">Cocina healthy</a>
+                                <a className="com-link" href=" ">OHLALÁ! Viaja</a>
+                                <a className="com-link" href=" ">Project planner</a>
+                                <a className="com-link" href=" ">Fábrica OHLALÁ!</a>
+                                <a className="com-link" href=" ">OHLALÁ! Fest</a>
+                            </div>
+                        </section>
+                    </div>
                     <div id="content-main" className="lay-sidebar">
                         <div className="sidebar__main">
                             <div className="row">
