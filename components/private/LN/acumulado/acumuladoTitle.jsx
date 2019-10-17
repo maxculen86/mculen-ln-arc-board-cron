fb91d11efb0d3ffb6548201255a8afef07d827ff
Julio Quintana <it_jquintana@lanacion.com.ar>
2019-10-17 12:57:25 -0300

    Validando proptTypes de AcuTitle

--- components/private/LN/acumulado/acumuladoTitle.jsx
+++ components/private/LN/acumulado/acumuladoTitle.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Consumer from 'fusion:consumer';
+import PropTypes from 'fusion:prop-types';
 import WithAcuArticlesData from '../common/hocs/WithAcuArticlesData';
 import filter from '../../../../content/filters/LN/acumulado/articleAcu';
 
@@ -14,6 +15,12 @@ const ItemSubSection = ({ id, navTitle, website }) => (
     </li>
 );
 
+ItemSubSection.propTypes = {
+    id: PropTypes.string.isRequired,
+    navTitle: PropTypes.string.isRequired,
+    website: PropTypes.string.isRequired
+};
+
 const AcumuladoTitle = ({ globalContent, orderAndCountTags }) => {
     const [withCategory, setWithCategory] = useState('');
     const [children, setChildren] = useState([]);
@@ -23,21 +30,24 @@ const AcumuladoTitle = ({ globalContent, orderAndCountTags }) => {
     useEffect(() => {
         setChildren(globalContent.children);
 
-        /**
-         * TODO: Para fase de 2Arc activar la siguiente funcion
-         * para mostrar tags y seccions 
-         setIsPrimarySection(
-             globalContent &&
-                 globalContent._id &&
-                 globalContent._id.split('/').splice(1).length === 1
-         );
-         if (children && children.length > 0) setWithCategory('with-category');
-         */
+        setIsPrimarySection(
+            // TODO: LLevar el lengt -1 a 1 cuando se active Navigation Arc2
+            globalContent &&
+                globalContent._id &&
+                globalContent._id.split('/').splice(1).length === -1
+        );
+        // TODO: Cambiar < a > cuando se active Navigation Arc2
+        if (children && children.length < 0) setWithCategory('with-category');
 
         setTitle(() => {
-            const { Payload, node_type, byline, name } = globalContent;
+            const {
+                Payload,
+                node_type: nodeType,
+                byline,
+                name
+            } = globalContent;
             if (Payload) return Payload.items[0].name;
-            if (node_type === 'section') return name;
+            if (nodeType === 'section') return name;
             if (byline) return byline;
             return '';
         });
@@ -84,6 +94,47 @@ const AcumuladoTitle = ({ globalContent, orderAndCountTags }) => {
             )}
         </div>
     );
+};
+
+AcumuladoTitle.propTypes = {
+    globalContent: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        Payload: PropTypes.shape({
+            items: PropTypes.shape({
+                name: PropTypes.string
+            })
+        }),
+        byline: PropTypes.string,
+        name: PropTypes.string,
+        node_type: PropTypes.string,
+        children: PropTypes.arrayOf(
+            PropTypes.shape({
+                _id: PropTypes.string,
+                navigation: PropTypes.shape({
+                    nav_title: PropTypes.string
+                }),
+                _website: PropTypes.string
+            })
+        )
+    }),
+    orderAndCountTags: PropTypes.arrayOf(
+        PropTypes.shape({
+            tag: PropTypes.shape({
+                slug: PropTypes.string,
+                text: PropTypes.string
+            })
+        })
+    ).isRequired
+};
+
+AcumuladoTitle.defaultProps = {
+    globalContent: {
+        Payload: undefined,
+        byline: undefined,
+        name: undefined,
+        node_type: undefined,
+        children: []
+    }
 };
 
 export default WithAcuArticlesData(Consumer(AcumuladoTitle), filter, 'notaM');
