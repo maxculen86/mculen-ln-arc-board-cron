8b30a98f781851fded9ba2dd189b1a19329f0e02
mdlopez <mdlopez@lanacion.com.ar>
2019-10-22 17:53:32 -0300

    Test de authorAndDate

--- __tests__/components/private/LN/nota/apertura/authorAndDate.test.js
+++ __tests__/components/private/LN/nota/apertura/authorAndDate.test.js
@@ -4,8 +4,6 @@ import { render, shallow } from 'enzyme';
 import nota from '../../../../../../__mocks__/data/articles/JZQDUAOPSRF3LLDZOT6374IDOM';
 
 import AuthorAndDate from '../../../../../../components/private/LN/nota/apertura/authorAndDate';
-import AuthorArticle from '../../../../../../components/private/LN/nota/apertura/authorArticle';
-import DateHeader from '../../../../../../components/private/LN/common/dateHeader';
 
 describe('features - LaNacion - Nota - AuthorAndDate', () => {
     const component = render(<AuthorAndDate globalContent={nota} />);
