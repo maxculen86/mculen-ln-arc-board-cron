3d1c1c9892b64cf043448bdb4c803197ebebfc29
dlopez <dlopez@lanacion.com.ar>
2019-10-22 10:20:51 -0300

    Correccion al obtener el sectionClass y el sectionPath

--- __tests__/components/private/common/utils/sectionUtils.test.js
+++ __tests__/components/private/common/utils/sectionUtils.test.js
@@ -2,18 +2,4 @@ import sectionUtils from '../../../../../components/private/common/utils/section
 import nota from '../../../../../__mocks__/data/articles/TWKFZQ6FCNF3ZKPHGGZPMSSOGQ';
 import siteProps from '../../../../../__mocks__/data/properties/lnSiteProps';
 
-describe('Utils - SectionUtils', () => {
-    it('Test de primarySectionTreeResolver', () => {
-        const sectionTree = sectionUtils.primarySectionTreeResolver({
-            globalContent: nota,
-            siteProperties: siteProps
-        });
-
-        expect(sectionTree.length).toBe(4);
-        expect(sectionTree[0].type).toBe('site');
-        expect(sectionTree[1].type).toBe('category');
-
-        expect(sectionTree[0].name).toBe('LA NACION');
-        expect(sectionTree[3].name).toBe('Pollo');
-    });
-});
+describe('Utils - SectionUtils', () => {});
