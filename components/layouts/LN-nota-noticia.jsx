2924370f88745c7f5cc7fc2befcc691731dcd4a3
dlopez <dlopez@lanacion.com.ar>
2019-10-22 16:04:42 -0300

    Commit inicial de template de noticia

---
+++ components/layouts/LN-nota-noticia.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function lnNotaNoticia() {
+    return (
+        <div id="wrapper" className="nota">
+            <h1>Soy el titulo</h1>
+            <p>algun parrafo</p>
+        </div>
+    );
+}
