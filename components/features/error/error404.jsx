70c4d4161a155eafba7eb3f95b7ad87406318398
mdlopez <mdlopez@lanacion.com.ar>
2019-10-24 17:01:05 -0300

    Limpieza de Codigo en OTT

--- components/features/error/error404.jsx
+++ components/features/error/error404.jsx
@@ -4,7 +4,7 @@ import Arc404 from '../../private/OTT/common/error404';
 
 const Error404 = () => {
     this.props.arcSite;
-    if (this.props.arcSite == 'ott') return <Arc404 />;
+    if (this.props.arcSite === 'ott') return <Arc404 />;
 
     return <div>este sitio no tiene un error 404</div>;
 };
