aeeea68bb90a43a5afe483c7e8b9f4ca47c2934f
Ivan Klimberg <iklimberg@lanacion.com.ar>
2019-10-23 22:43:15 -0300

    fix de proptype

--- components/private/LN/common/articleTypes/articleMain.jsx
+++ components/private/LN/common/articleTypes/articleMain.jsx
@@ -53,7 +53,7 @@ articleMain.propTypes = {
         PropTypes.node
     ]),
     articleData: PropTypes.shape({
-        subtype: PropTypes.number,
+        subtype: PropTypes.string,
         website_url: PropTypes.string
     }).isRequired,
     dataSection: PropTypes.string,
