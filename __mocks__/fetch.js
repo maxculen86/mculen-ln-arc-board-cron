e36e67749d16f88fc0679a25e871f534cc50f92c
Abraham <asap_acedeno@lnexternos.com.ar>
2019-10-24 12:05:33 -0300

    hoc newsletter test

--- __mocks__/fetch.js
+++ __mocks__/fetch.js
@@ -1,6 +1,11 @@
 // Mock diseñado para hacer tests de la apiIngresar en el HOC de login
-const fetch = {
-    get: jest.fn(() => Promise.resolve({ data: {} }))
-};
+const fetch = () => Promise.resolve({
+    data: [
+        'Promise',
+        'has',
+        'been',
+        'resolved'
+    ]
+})
 
 module.exports = fetch;
