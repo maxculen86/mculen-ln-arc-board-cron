a01e958ab5a060387033d891b5b9d0e4e8e6fc4b
fremy <fremy@lanacion.com.ar>
2019-10-25 12:33:42 -0300

    template noticia

--- components/layouts/LN-nota-noticia.jsx
+++ components/layouts/LN-nota-noticia.jsx
@@ -11,6 +11,8 @@ import '../../resources/dist/css/ln/components/date.css';
 import '../../resources/dist/css/ln/components/tag.css';
 import '../../resources/dist/css/ln/components/author.css';
 import '../../resources/dist/css/ln/components/lead.css';
+import '../../resources/dist/css/ln/components/blockquote.css';
+import '../../resources/dist/css/ln/components/subtitle.css';
 import '../../resources/dist/css/ln/components/ordered.css';
 import '../../resources/dist/css/ln/components/unordered.css';
 import '../../resources/dist/css/ln/components/input.css';
@@ -23,6 +25,41 @@ export default function lnNotaNoticia() {
                 <div className="row">
                     <div className="lay-sidebar">
                         <div className="sidebar-main">
+                            <section className="cont-figure">
+                                <a
+                                    href="/deportes/probando-breaking-news-nid/"
+                                    className="figure"
+                                >
+                                    <picture className="content-pic picture ">
+                                        <source
+                                            media="(min-width: 64em)"
+                                            srcset="https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/fPfH8mFZiDzpKFkxeE7HELbvSlE=/600x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/272ELZPBAJAMTJ6NLZISAQ5S3Q.jpg"
+                                            alt=""
+                                        />
+                                        <source
+                                            media="(min-width: 48em)"
+                                            srcset="https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/-Aolf8vHyfnyih9BvkqlmgCYBc8=/520x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/272ELZPBAJAMTJ6NLZISAQ5S3Q.jpg"
+                                            alt=""
+                                        />
+                                        <source
+                                            media="(min-width: 20em)"
+                                            srcset="https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/PCnznfpymUaBD6bUQuSEB8d6KQY=/375x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/272ELZPBAJAMTJ6NLZISAQ5S3Q.jpg"
+                                            alt=""
+                                        />
+                                        <img
+                                            src="https://arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/272ELZPBAJAMTJ6NLZISAQ5S3Q.jpg"
+                                            className="content-img"
+                                            alt=""
+                                        />
+                                    </picture>
+                                </a>
+                                <p className="text">Epigrafe de foto</p>
+                                <p className="small">
+                                    Fuente: LA NACION - Crédito: Enrique García
+                                    Medina
+                                </p>
+                            </section>
+
                             <p className="text capital">soy un texto</p>
                             <p className="text">
                                 soy un texto mas largo soy un texto mas largo
@@ -30,20 +67,22 @@ export default function lnNotaNoticia() {
                                 soy un texto mas largo soy un texto mas largo
                                 soy un texto mas largo soy un texto mas largo
                                 soy un texto mas largo soy un texto mas largo
-                                soy un texto mas largo soy un texto mas largo{' '}
+                                soy un texto mas largo soy un texto mas largo
                             </p>
                             <p className="text">
-                                soy un texto mas largo soy un texto{' '}
+                                soy un texto mas largo soy un texto
                                 <b>mas largo soy un</b> texto mas largo soy un
                                 <a href="" className="link">
-                                    {' '}
-                                    texto mas largo soy un
-                                </a>{' '}
+                                    xto mas largo soy un
+                                </a>
                                 texto mas largo soy un texto mas largo soy un
                                 texto mas largo soy un texto mas largo soy un
                                 texto mas largo soy un texto mas largo soy un
-                                texto mas largo soy un texto mas largo{' '}
+                                texto mas largo soy un texto mas largo
                             </p>
+                            <h2 className="com-subtitle-nota-1">Subtitulo 1</h2>
+                            <h2 className="com-subtitle-nota-2">Subtitulo 2</h2>
+                            <h2 className="com-subtitle-nota-3">Subtitulo 3</h2>
                             <blockquote className="blockquote">
                                 Para la nueva campaña, según el USDA quedarían
                                 como remanente final unas 21,63 millones de
