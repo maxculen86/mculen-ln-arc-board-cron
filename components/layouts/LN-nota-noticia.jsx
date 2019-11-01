8ba5edd2080837f455dee5f130c07f06e88c1e7b
fremy <fremy@lanacion.com.ar>
2019-11-01 15:23:28 -0300

    externos

--- components/layouts/LN-nota-noticia.jsx
+++ components/layouts/LN-nota-noticia.jsx
@@ -11,7 +11,6 @@ import '../../resources/dist/css/ln/components/date.css';
 import '../../resources/dist/css/ln/components/tag.css';
 import '../../resources/dist/css/ln/components/author.css';
 import '../../resources/dist/css/ln/components/lead.css';
-import '../../resources/dist/css/ln/components/subtitle.css';
 import '../../resources/dist/css/ln/components/ordered.css';
 import '../../resources/dist/css/ln/components/unordered.css';
 import '../../resources/dist/css/ln/components/input.css';
@@ -19,7 +18,7 @@ import '../../resources/dist/css/ln/modules/newsletter.css';
 import '../../resources/dist/css/ln/components/blockquote.css';
 import '../../resources/dist/css/ln/components/text.css';
 import '../../resources/dist/css/ln/components/link.css';
-import '../../resources/dist/css/ln/components/subtitles.css';
+import '../../resources/dist/css/ln/components/subtitle.css';
 
 export default function lnNotaNoticia() {
     return (
@@ -33,7 +32,7 @@ export default function lnNotaNoticia() {
                                     href="/deportes/probando-breaking-news-nid/"
                                     className="figure"
                                 >
-                                    <picture className="content-pic picture ">
+                                    <picture className="content-pic picture zoom">
                                         <source
                                             media="(min-width: 64em)"
                                             srcset="https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/fPfH8mFZiDzpKFkxeE7HELbvSlE=/600x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/272ELZPBAJAMTJ6NLZISAQ5S3Q.jpg"
@@ -76,6 +75,16 @@ export default function lnNotaNoticia() {
                                 soy un texto mas largo soy un texto mas largo
                                 soy un texto mas largo soy un texto mas largo
                             </p>
+                            <div className="externo">
+                                <iframe
+                                    width="560"
+                                    height="315"
+                                    src="https://www.youtube.com/embed/zIY87vU33aA"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen
+                                ></iframe>
+                            </div>
                             <p className="text">
                                 soy un texto mas largo soy un texto mas largo
                                 soy un texto mas largo soy un texto mas largo
@@ -84,6 +93,7 @@ export default function lnNotaNoticia() {
                                 soy un texto mas largo soy un texto mas largo
                                 soy un texto mas largo soy un texto mas largo
                             </p>
+                            <div class="externo"></div>
                             <p className="text">
                                 soy un texto mas largo soy un texto
                                 <b>mas largo soy un</b> texto mas largo soy un
@@ -95,6 +105,16 @@ export default function lnNotaNoticia() {
                                 texto mas largo soy un texto mas largo soy un
                                 texto mas largo soy un texto mas largo
                             </p>
+                            <div class="externo">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13147.032931451173!2d-58.47448225000001!3d-34.53435279999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb6a61cfe4d7f%3A0xc1ef0efa1c3ed2b1!2sKansas%20Grill!5e0!3m2!1sen!2sar!4v1572631280897!5m2!1sen!2sar"
+                                    width="600"
+                                    height="450"
+                                    frameborder="0"
+                                    style="border:0;"
+                                    allowfullscreen=""
+                                ></iframe>
+                            </div>
                             <h2 className="com-subtitle-nota-1">Subtitulo 1</h2>
                             <h2 className="com-subtitle-nota-2">Subtitulo 2</h2>
                             <h2 className="com-subtitle-nota-3">Subtitulo 3</h2>
