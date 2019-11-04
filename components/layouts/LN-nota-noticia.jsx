8182c7fc3bdb488b6c8e0a41edf4b30cde79e60f
sromero <sromero@lanacion.com>
2019-11-04 18:27:21 -0300

    Se agrega cita de autor y sus variantes

--- components/layouts/LN-nota-noticia.jsx
+++ components/layouts/LN-nota-noticia.jsx
@@ -56,11 +56,14 @@ export default function lnNotaNoticia() {
                                         />
                                     </picture>
                                 </a>
-                                <p className="text">Epigrafe de foto</p>
-                                <p className="small">
-                                    Fuente: LA NACION - Crédito: Enrique García
-                                    Medina
-                                </p>
+
+                                <section className="com-epigrafe">
+                                    <p className="text">Epigrafe de foto</p>
+                                    <p className="small">
+                                        Fuente: LA NACION - Crédito: Enrique
+                                        García Medina
+                                    </p>
+                                </section>
                             </section>
 
                             <p className="text capital">
@@ -72,6 +75,71 @@ export default function lnNotaNoticia() {
                                 soy un texto mas largo soy un texto mas largo H
                                 soy un texto mas largo soy un texto mas largo
                                 soy un texto mas largo soy un texto mas largo H
+                                soy un texto mas largo soy un texto mas largo
+                                soy un texto mas largo soy un texto mas largo
+                                soy un texto mas largo soy un texto mas largo
+                            </p>
+                            <section className="com-cita autor">
+                                <section className="cont-figure">
+                                    <a
+                                        href="/deportes/probando-breaking-news-nid/"
+                                        className="figure"
+                                    >
+                                        <picture className="content-pic picture zoom">
+                                            <source
+                                                media="(min-width: 64em)"
+                                                srcset="https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/fPfH8mFZiDzpKFkxeE7HELbvSlE=/600x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/272ELZPBAJAMTJ6NLZISAQ5S3Q.jpg"
+                                                alt=""
+                                            />
+                                            <source
+                                                media="(min-width: 48em)"
+                                                srcset="https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/-Aolf8vHyfnyih9BvkqlmgCYBc8=/520x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/272ELZPBAJAMTJ6NLZISAQ5S3Q.jpg"
+                                                alt=""
+                                            />
+                                            <source
+                                                media="(min-width: 20em)"
+                                                srcset="https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/PCnznfpymUaBD6bUQuSEB8d6KQY=/375x0/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/272ELZPBAJAMTJ6NLZISAQ5S3Q.jpg"
+                                                alt=""
+                                            />
+                                            <img
+                                                src="https://arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/272ELZPBAJAMTJ6NLZISAQ5S3Q.jpg"
+                                                className="content-img"
+                                                alt=""
+                                            />
+                                        </picture>
+                                    </a>
+                                </section>
+                                <section className="cont-cita">
+                                    <h2 className="title-cita">
+                                        ¨He fallado una y otra vez en mi vida,
+                                        por eso he conseguido el éxito”
+                                    </h2>
+                                    <div className="cont-firma-autor">
+                                        {' '}
+                                        <h3 className="nombre-firma">
+                                            Michael Jordan |{' '}
+                                        </h3>{' '}
+                                        <h3 className="especialidad-firma">
+                                            {' '}
+                                            Basquet
+                                        </h3>{' '}
+                                    </div>
+                                    <div className="cont-data-firma">
+                                        {' '}
+                                        <h3 className="fecha-firma">
+                                            Enero 2018 -
+                                        </h3>{' '}
+                                        <h3 className="lugar-firma">
+                                            {' '}
+                                            Entrevista para LN+
+                                        </h3>{' '}
+                                    </div>
+                                </section>
+                            </section>
+                            <p className="text">
+                                soy un texto mas largo soy un texto mas largo
+                                soy un texto mas largo soy un texto mas largo
+                                soy un texto mas largo soy un texto mas largo
                                 soy un texto mas largo soy un texto mas largo
                                 soy un texto mas largo soy un texto mas largo
                                 soy un texto mas largo soy un texto mas largo
