940a5966838d16f1a763dc1da5d398ddcb010d41
dlopez <dlopez@lanacion.com.ar>
2019-11-14 10:48:42 -0300

    Fix webSite en sectionSource

--- content/sources/sectionSource.js
+++ content/sources/sectionSource.js
@@ -1,15 +1,17 @@
 const resolve = key => {
     const { id, website } = key;
+    const finalWebsite = website || key['arc-site'];
     if (!id)
         throw new Error(
             'Debe definir un id para realizar la consulta - Section Source'
         );
-    if (!website) throw new Error('Debe indicar el website - Section Source');
+    if (!finalWebsite)
+        throw new Error('Debe indicar el website - Section Source');
     if (!id.startsWith('/'))
         throw new Error(
             'El id de sección debe comenzar con / - Section Source'
         );
-    return `/site/v3/navigation/${website}/?_id=${id}`;
+    return `/site/v3/navigation/${finalWebsite}/?_id=${id}`;
 };
 export default {
     resolve,
