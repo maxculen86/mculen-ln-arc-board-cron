b9dbd029892a5e3f30e6baf9e49e6795b741ea16
dlopez <dlopez@lanacion.com.ar>
2019-11-15 15:18:28 -0300

    Fix en error404 ott

--- components/features/error/error404.jsx
+++ components/features/error/error404.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import Consumer from 'fusion:consumer';
 import Arc404 from '../../private/OTT/common/error404';
 
-const Error404 = () => {
-    this.props.arcSite;
-    if (this.props.arcSite === 'ott') return <Arc404 />;
+const Error404 = ({ arcSite }) => {
+    if (arcSite === 'ott') return <Arc404 />;
 
     return <div>este sitio no tiene un error 404</div>;
 };
