44486d947a43128ee4b7471df623b9f02e5c59e0
dlopez <dlopez@lanacion.com.ar>
2019-11-19 09:21:46 -0300

    Funcion para obtener parametros de la url

---
+++ components/private/common/utils/browser.js
@@ -0,0 +1,18 @@
+const getParameterByName = (parameter, urlP) => {
+    let url = urlP;
+    if (!url) {
+        if (typeof window === 'undefined')
+            throw new Error('El parametro de Url es obligatorio en SSR');
+        url = window.location.href;
+    }
+    const name = parameter.replace(/[\[\]]/g, '\\$&');
+
+    const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
+    const results = regex.exec(url);
+
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
+
+export default { getParameterByName };
