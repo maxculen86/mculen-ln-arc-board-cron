91e96f17c4aa13d76f8b504ed7025d55d76255d7
fremy <fremy@lanacion.com.ar>
2019-11-22 10:34:27 -0300

    fix externo

--- components/private/LN/nota/cuerpo/html.jsx
+++ components/private/LN/nota/cuerpo/html.jsx
@@ -12,6 +12,23 @@ export default function html() {
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen
             ></iframe>
+            <blockquote className="twitter-tweet">
+                <p lang="und" dir="ltr">
+                    ᵒᵒᶠ{' '}
+                    <a href="https://t.co/WslAeSvMFg">
+                        pic.twitter.com/WslAeSvMFg
+                    </a>
+                </p>
+                &mdash; 🥖 Kéké 🥖 (@Kekeflipnote){' '}
+                <a href="https://twitter.com/Kekeflipnote/status/1193619442636337158?ref_src=twsrc%5Etfw">
+                    November 10, 2019
+                </a>
+            </blockquote>{' '}
+            <script
+                async
+                src="https://platform.twitter.com/widgets.js"
+                charset="utf-8"
+            ></script>
         </div>
     );
 }
