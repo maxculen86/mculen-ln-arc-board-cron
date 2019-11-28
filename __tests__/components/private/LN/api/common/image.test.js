d7a396dd945ac700b8b3da62c70c0eb0d0297b53
dlopez <dlopez@lanacion.com.ar>
2019-11-28 18:52:06 -0300

    Json test video common

--- __tests__/components/private/LN/api/common/image.test.js
+++ __tests__/components/private/LN/api/common/image.test.js
@@ -1,11 +1,11 @@
-import acuImage from '../../../../../../components/private/LN/api/common/image';
+import Image from '../../../../../../components/private/LN/api/common/image';
 import article from '../../../../../../__mocks__/data/articles/newsNoteWithCompleteAttrs.json';
 
-describe('Json imagen en acumulado', () => {
+describe('Json imagen common', () => {
     test('Render imagen correcto', () => {
         const img = article.globalContent.promo_items.basic;
 
-        const resp = acuImage(img);
+        const resp = Image(img);
 
         expect(resp.id).toBe(img._id);
         expect(resp.src).toBe(img.resized_urls[0].resizedUrl);
