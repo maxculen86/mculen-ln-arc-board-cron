783af15fb23c213e08abdb0aac2fafdfbc0f70ae
Julio Quintana <it_jquintana@lanacion.com.ar>
2019-12-06 10:58:42 -0300

    Fixeando blancos por rows rendereados sin contenido

--- components/layouts/LN-acumulado.jsx
+++ components/layouts/LN-acumulado.jsx
@@ -50,22 +50,28 @@ const LNAcumuladoLayout = props => {
             <main>
                 {/* CABEZAL REVISTA Y BANNERS: CABEZAL Y STICKY */}
                 {children[0]}
-                <div className="row">
-                    <div className="lay">
-                        {/* BREADCRUMB, TITULO Y APERTURA */}
-                        {children[1]}
+                {children[1] && (
+                    <div className="row">
+                        <div className="lay">
+                            {/* BREADCRUMB, TITULO Y APERTURA */}
+                            {children[1]}
+                        </div>
                     </div>
-                </div>
+                )}
                 <div id="content-main" className="lay-sidebar">
                     <div className="sidebar__main">
-                        <div className="row">
-                            {/* LUGAR PARA UN ANEXO */}
-                            {children[2]}
-                        </div>
-                        <div className="row">
-                            {/* LINKS DE NAVEGACION */}
-                            {children[3]}
-                        </div>
+                        {children[2] && (
+                            <div className="row">
+                                {/* LUGAR PARA UN ANEXO */}
+                                {children[2]}
+                            </div>
+                        )}
+                        {children[3] && (
+                            <div className="row">
+                                {/* LINKS DE NAVEGACION */}
+                                {children[3]}
+                            </div>
+                        )}
                         {/* NOTAS */}
                         {children[4]}
                     </div>
