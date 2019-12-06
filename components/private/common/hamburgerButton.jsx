26cf320f1c07cc401206778b930b7ed351741e4b
fremy <fremy@lanacion.com.ar>
2019-12-06 11:30:14 -0300

    hambuguer

--- components/private/common/hamburgerButton.jsx
+++ components/private/common/hamburgerButton.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 
 export default function HamburgerButton({ className, children, onClick }) {
     return (
-        <button
-            onClick={onClick}
-            className={className}
-            role="button"
-            tabIndex="0"
-        >
+        <button onClick={onClick} className={className}>
             {children}
         </button>
     );
