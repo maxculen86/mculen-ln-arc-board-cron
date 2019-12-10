df7c27e65786954a5f4fcc4291e04d6f3c597251
dlopez <dlopez@lanacion.com.ar>
2019-12-10 10:59:00 -0300

    Dejo comentado cuerpoDefault

--- components/private/LN/nota/cuerpo/cuerpoDefault.jsx
+++ components/private/LN/nota/cuerpo/cuerpoDefault.jsx
@@ -24,12 +24,12 @@ const Cuerpo = props => {
     resp.push(<Subtitles />);
     resp.push(<Ordered />);
     resp.push(<Unordered />); */
-    console.log('------------------- cuerpo', props);
-    props.globalContent.content_elements.forEach(element => {
-        if (element.type === 'gallery') {
-            resp.push(<Gallery {...element} />);
-        }
-    });
+    // console.log('------------------- cuerpo', props);
+    // props.globalContent.content_elements.forEach(element => {
+    //     if (element.type === 'gallery') {
+    //         resp.push(<Gallery {...element} />);
+    //     }
+    // });
     /* resp.push(<Gallery {...props} />); */
     /* resp.push(<Html />);
     resp.push(<PullQuote />);
