9fa753c364afc310f2627ac936ff80896c63a642
fremy <fremy@lanacion.com.ar>
2019-12-13 09:15:39 -0300

    Maquetado opinion sin placeholder lapicera

--- components/layouts/LN-nota-noticia.jsx
+++ components/layouts/LN-nota-noticia.jsx
@@ -26,6 +26,7 @@ import '../../resources/dist/css/ln/components/epigraph.css';
 import '../../resources/dist/css/ln/components/appointment.css';
 import '../../resources/dist/css/ln/components/colecciones.css';
 import '../../resources/dist/css/ln/components/carta-lectores.css';
+import '../../resources/dist/css/ln/components/opinion-author.css';
 
 const lnNotaNoticia = ({ children }) => {
     return (
@@ -62,6 +63,25 @@ const lnNotaNoticia = ({ children }) => {
                                 <div className="row">
                                     {/* Pos-Apertura */}
                                     {children[4]}
+                                    <div className="opinion-autor row">
+                                        <section id="" className="cont-figure">
+                                            <div className="figure">
+                                                <picture className="content-pic picture">
+                                                    <img
+                                                        src="https://bucket1.glanacion.com/anexos/fotos/12/2089212w82.png"
+                                                        alt=""
+                                                        className="content-img"
+                                                    />
+                                                </picture>
+                                            </div>
+                                        </section>
+                                        <div className="opinion-calc">
+                                            <h1 className="link hlp-bold">
+                                                <a href="">Bruno Pittón</a>
+                                            </h1>
+                                            <label>PARA LA NACION</label>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
