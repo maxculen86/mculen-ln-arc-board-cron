fcde5442fff5b06b9491cb317c91fa293cdfec27
mdlopez <mdlopez@lanacion.com.ar>
2020-01-10 18:02:46 -0300

    Apertura Noticia

--- components/features/LN-nota/aperturaNoticia.jsx
+++ components/features/LN-nota/aperturaNoticia.jsx
@@ -1,14 +1,18 @@
 // TODO: unificar aperturas decidiendo cual agarrar segun subtype
 
 import React from 'react';
+import Consumer from 'fusion:consumer';
 import Static from 'fusion:static';
-import Imagen from '../../private/LN/nota/cuerpo/image';
+import AperturaNoticia from '../../private/LN/nota/apertura/aperturaNoticia';
 
-export default function aperturaNoticia(props) {
-    const { id: featureId } = props;
+const aperturaNoticia = ({ id: featureId, globalContent }) => {
     return (
         <Static id={featureId}>
-            <Imagen />
+            <AperturaNoticia basic={globalContent.promo_items.basic} />
         </Static>
     );
-}
+};
+
+aperturaNoticia.label = 'LN-Nota-AperturaNoticia';
+
+export default Consumer(aperturaNoticia);
