7c60c37f79a3f1d2a401e5253e0b2b25bb84c5ee
Abraham <asap_acedeno@lnexternos.com.ar>
2020-01-15 12:31:06 -0300

    fix arcWrapper test (clean)

--- __tests__/components/private/LN/common/banner/arcWrapper.test.js
+++ __tests__/components/private/LN/common/banner/arcWrapper.test.js
@@ -2,23 +2,16 @@ import React from 'react';
 import { render, shallow } from 'enzyme';
 import { slotsConfig } from '../../../../../../components/private/LN/common/banner/config';
 import ArcWrapper from '../../../../../../components/private/LN/common/banner/arcWrapper';
-//import { MutationObserver } from '../../../../../../__mocks__/mutationObserver';
 
 const registerAdFn = jest.fn();
 global.ArcAds = jest.fn().mockImplementationOnce(() => ({
     registerAd: registerAdFn
 }));
-
-/* const observeFn = jest.fn();
-const MutationObserver = jest.fn().mockImplementationOnce(() => ({
-    observe: jest.fn()
-})); */
 global.MutationObserver = class {
     constructor(callback) {}
     disconnect() {}
     observe(element, initObject) {}
 };
-//Object.defineProperty(window, 'MutationObserver', { value: MutationObserver });
 
 describe('LaNacion - Common - Banner - ArcWrapper', () => {
     const slotId = 'caja1_dsk';
