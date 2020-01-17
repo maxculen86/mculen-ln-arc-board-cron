00df48517244757604e25e8baf5728edafc36199
fremy <fremy@lanacion.com.ar>
2020-01-17 12:22:28 -0300

    se declara el css

--- components/layouts/LN-nota-noticia.jsx
+++ components/layouts/LN-nota-noticia.jsx
@@ -25,6 +25,7 @@ import '../../resources/dist/css/ln/components/slider.css';
 import '../../resources/dist/css/ln/components/epigraph.css';
 import '../../resources/dist/css/ln/components/appointment.css';
 import '../../resources/dist/css/ln/components/colecciones.css';
+import '../../resources/dist/css/ln/components/opinion-author.css';
 import '../../resources/dist/css/ln/components/carta-lectores.css';
 
 const lnNotaNoticia = ({ children }) => {
