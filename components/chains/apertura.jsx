65a83a99790e76723b4a33da10c6fd1422267a84
Ariel Mergen <rh_amergen@lnexternos.com.ar>
2020-02-11 17:34:39 -0300

    feat: reutilización componente collection chain

--- components/chains/apertura.jsx
+++ components/chains/apertura.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import PropTypes from 'fusion:prop-types';
 import Consumer from 'fusion:consumer';
+import Nota from '../features/LN-home/notaChain';
+
+/*
+    Reutilizo collection
+*/
+const CollectionsNotes = props => {
+    // collectionsNotes from useContent
+    // iterar collectionsNotes
+    // armar array de Nota con cada nota de la collection
+};
 
 const Apertura = props => {
-    console.log(props);
-    return <section>{props.children}</section>;
+    const {
+        children,
+        customFields: { idCollection }
+    } = props;
+
+    if (hasIdCollection) {
+        // TODO: pasar id
+        return <CollectionsNotes idCollection={idCollection} />;
+    }
+
+    console.log(children);
+    return <section>{children}</section>;
 };
 
 Apertura.propTypes = {
