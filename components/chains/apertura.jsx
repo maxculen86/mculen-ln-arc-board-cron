d5e5c93dc8a8c2019510303b6a26d8fb66cbc826
Julio <jquintana1801@gmail.com>
2020-02-13 15:04:15 -0300

    Ajustes de FocalFactory

--- components/chains/apertura.jsx
+++ components/chains/apertura.jsx
@@ -3,12 +3,61 @@ import PropTypes from 'fusion:prop-types';
 import { useContent } from 'fusion:content';
 import { NoteCard } from '../features/LN-home/noteCard';
 
-const CollectionsNotes = props => {
-    const { idCollection: id } = props;
+const FocalDerecho = ({ children }) => (
+    <section className="row">
+        <div className="col-tablet-8">{children[0]}</div>
+        <div className="col-tablet-4">
+            {children[1]}
+            {children[2]}
+        </div>
+        <div className="row-gap-tablet-3">
+            {children[3]}
+            {children[4]}
+            {children[5]}
+        </div>
+    </section>
+);
 
+FocalDerecho.propTypes = {
+    children: PropTypes.arrayOf(PropTypes.node).isRequired
+};
+
+const FocalIzquierdo = ({ children }) => (
+    <section className="row">
+        <div className="col-tablet-4">
+            {children[1]}
+            {children[2]}
+        </div>
+        <div className="col-tablet-8">{children[0]}</div>
+        <div className="row-gap-tablet-3">
+            {children[3]}
+            {children[4]}
+            {children[5]}
+        </div>
+    </section>
+);
+
+FocalIzquierdo.propTypes = {
+    children: PropTypes.arrayOf(PropTypes.node).isRequired
+};
+
+const FocalFactory = ({ directionFocal, children }) => {
+    if (directionFocal === 'FocalDerecho')
+        return <FocalDerecho>{children}</FocalDerecho>;
+    if (directionFocal === 'FocalIzquierdo')
+        return <FocalIzquierdo>{children}</FocalIzquierdo>;
+    return null;
+};
+
+FocalFactory.propTypes = {
+    directionFocal: PropTypes.string.isRequired,
+    children: PropTypes.arrayOf(PropTypes.node).isRequired
+};
+
+const CollectionsNotes = idCollection => {
     const content = useContent({
         source: 'collectionsV2Source',
-        query: { id }
+        query: { id: idCollection }
     });
 
     if (content) {
@@ -28,18 +77,31 @@ CollectionsNotes.propTypes = {
 const Apertura = props => {
     const {
         children,
-        customFields: { idCollection }
+        customFields: { idCollection, directionFocal }
     } = props;
 
     if (idCollection && idCollection !== '') {
+        const Notes = CollectionsNotes(idCollection);
         return (
-            <section>
-                <CollectionsNotes idCollection={idCollection} />
-            </section>
+            <div className="row hlp-margintop-20">
+                <div className="lay">
+                    <FocalFactory directionFocal={directionFocal}>
+                        {Notes}
+                    </FocalFactory>
+                </div>
+            </div>
         );
     }
 
-    return <section>{children}</section>;
+    return (
+        <div className="row hlp-margintop-5">
+            <div className="lay">
+                <FocalFactory directionFocal={directionFocal}>
+                    {children}
+                </FocalFactory>
+            </div>
+        </div>
+    );
 };
 
 Apertura.label = 'LN Home Apertura';
@@ -52,7 +114,18 @@ Apertura.propTypes = {
             description: 'Ingrese aquí el ID de la collection',
             defaultValue: '',
             group: 'Custom Fields'
-        })
+        }),
+        directionFocal: PropTypes.oneOf(['FocalDerecho', 'FocalIzquierdo']).tag(
+            {
+                label: {
+                    FocalDerecho: 'Focal Derecho',
+                    FocalIzquierdo: 'Focal Izquierdo'
+                },
+                description: 'Ingrese aquí el ID de la collection',
+                defaultValue: 'FocalDerecho',
+                group: 'Custom Fields'
+            }
+        )
     }).isRequired
 };
 
