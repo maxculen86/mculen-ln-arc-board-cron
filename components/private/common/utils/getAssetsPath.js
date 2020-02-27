ecf141ff12bf9f0aea82e8fa1113821ae2600741
Julio <jquintana1801@gmail.com>
2020-02-27 12:40:36 -0300

    Probando snippet con path relativo

--- components/private/common/utils/getAssetsPath.js
+++ components/private/common/utils/getAssetsPath.js
@@ -1,8 +1,6 @@
-import { LANACIONAR_URLASSETS } from 'fusion:environment';
-
 const getAssetsPath = contextPath => deployment => assets => {
     const path = `${contextPath}/resources/dist/images`;
-    return `${LANACIONAR_URLASSETS}${deployment(`${path}/${assets}`)}`;
+    return `${deployment(`${path}/${assets}`)}`;
 };
 
 export default getAssetsPath;
