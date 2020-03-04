cbd6dcd06cef8b33c41f552d9bf63d83de6a7560
mdlopez <mdlopez@lanacion.com.ar>
2020-03-04 10:32:47 -0300

    meta SEO noticia

--- components/private/common/robot.jsx
+++ components/private/common/robot.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'fusion:prop-types';
 
 const Robot = ({ subtype, canonicalUrl }) => {
     return (
-        subtype !== 7 && (
+        subtype !== '7' && (
             <>
                 <meta name="robots" content="noindex, nofollow" />
                 <link
