f1278cc22649d2e06e991b9e9aa74ecb665dd045
Mariaelisa Rodrigues <mrodrigues@lanacion.com.ar>
2020-03-04 19:05:46 -0300

    feature noteCard with customFields

---
+++ components/features/LN-home/noteFeature.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'fusion:prop-types';
+import { useContent } from 'fusion:content';
+import NoteCard from '../../private/LN/home/noteCard/noteCard';
+
+const NoteFeature = ({ customFields }) => {
+    const { noteId: id } = customFields;
+
+    const content = useContent({
+        source: 'articleSourceNota',
+        query: { id }
+    });
+    if (!id) throw Error('El campo Id de la Nota es obligatorio.');
+    if (!content) throw Error(`No se encuentran resultados para el id ${id}.`);
+    return <NoteCard content={content} customFields={customFields} />;
+};
+
+NoteFeature.label = 'LN Home NoteCard';
+
+NoteFeature.propTypes = {
+    customFields: PropTypes.shape({
+        noteId: PropTypes.string.tag({
+            label: 'Ingresar id de nota',
+            description: 'Ingrese aquí el id de la nota',
+            group: 'Custom Fields'
+        }),
+        imageId: PropTypes.string.tag({
+            label: 'Ingresar id de imagen',
+            description: 'Ingrese aquí el id de la imagen',
+            group: 'Custom Fields'
+        }),
+        lead: PropTypes.string.tag({
+            label: 'Volanta',
+            description: 'Ingrese aquí el texto de la volanta',
+            group: 'Custom Fields'
+        }),
+        title: PropTypes.string.tag({
+            label: 'Título',
+            description: 'Ingrese aquí el texto del título',
+            group: 'Custom Fields'
+        }),
+        description: PropTypes.string.tag({
+            label: 'Bajada',
+            description: 'Ingrese aquí el texto de la bajada',
+            group: 'Custom Fields'
+        }),
+        authors: PropTypes.string.tag({
+            label: 'Marquesina',
+            description: 'Ingrese aquí el texto de la marquesina',
+            group: 'Custom Fields'
+        })
+    }).isRequired
+};
+
+export default NoteFeature;
---
+++ components/private/LN/home/noteCard/noteCard.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'fusion:prop-types';
+import Image from './image';
+import { getTitle, getDescription, getAuthors, getImageId } from './getData';
+
+const NoteCard = ({ content, customFields }) => {
+    const [lead, setLead] = useState(customFields && customFields.lead);
+    const [title, setTitle] = useState(getTitle(customFields, content));
+    const [imageId, setImageId] = useState(getImageId(customFields, content));
+    const [authors, setAuthors] = useState(getAuthors(customFields, content));
+    const [description, setDescription] = useState(
+        getDescription(customFields, content)
+    );
+
+    useEffect(() => {
+        setLead(customFields && customFields.lead);
+        setTitle(getTitle(customFields, content));
+        setDescription(getDescription(customFields, content));
+        setAuthors(getAuthors(customFields, content));
+        setImageId(getImageId(customFields, content));
+    }, [content, customFields]);
+
+    if (!(title && (imageId || description)))
+        throw Error(
+            'La nota debe contar con una imagen o bajadad y con un título'
+        );
+    return (
+        <article className={`m art-02 ${lead ? 'lead' : ''}`}>
+            {imageId && <Image imageId={imageId} />}
+            <h2 className="content-titulo">
+                {lead && <span className="lead">{`${lead}. `}</span>}
+                {content && customFields && title}
+            </h2>
+            {description && <p>{content && customFields && description}</p>}
+            {authors && <h3>{content && customFields && authors}</h3>}
+        </article>
+    );
+};
+
+NoteCard.propTypes = {
+    content: PropTypes.shape({
+        headlines: PropTypes.shape({
+            basic: PropTypes.string
+        }).isRequired,
+        subheadlines: PropTypes.shape({
+            basic: PropTypes.string
+        }),
+        promo_items: PropTypes.shape({
+            basic: PropTypes.shape({
+                resized_urls: PropTypes.array
+            })
+        }),
+        credits: PropTypes.shape({
+            by: PropTypes.array
+        })
+    }).isRequired,
+    customFields: PropTypes.shape({
+        imageId: PropTypes.string,
+        lead: PropTypes.string,
+        title: PropTypes.string,
+        description: PropTypes.string,
+        authors: PropTypes.string
+    }).isRequired
+};
+
+export default NoteCard;
