52d9b19996bcb771649c1a028b1f7d9591f41c16
Mariaelisa Rodrigues <mrodrigues@lanacion.com.ar>
2020-03-05 17:31:48 -0300

    test y getLead

---
+++ __tests__/components/private/LN/home/noteCard/noteCard.js
@@ -0,0 +1,65 @@
+jest.mock(
+    './../../../../../../components/private/LN/home/noteCard/noteCard.jsx',
+    () => 'mock-component'
+);
+
+import React from 'react';
+import { mount } from 'enzyme';
+import NoteCard from './../../../../../../components/private/LN/home/noteCard/noteCard';
+
+describe('Test del Feature - <NoteCard />', () => {
+    const noteId = 'TXQ62BPYN5AEFKLPRNM66WS63I';
+    const imageId = 'UUEO45CS2ZEHFEOMB5H3EY5J5Q';
+    const lead = 'Esta es una volanta';
+    const title = 'Este es un titulo';
+    const description = 'Esta es una bajada';
+    const authors = 'Estos son los autores';
+
+    const content = {
+        headlines: { basic: 'Titulo de la Nota' },
+        subheadlines: { basic: 'Bajada de la Nota' },
+        promo_items: {
+            basic: {
+                resized_urls: 'url_de_la_imagen.jpg'
+            }
+        },
+        credits: { by: [{ name: 'Nombre Apellido' }] }
+    };
+
+    const customFields = {
+        imageId: imageId,
+        lead: lead,
+        title: title,
+        description: description,
+        authors: authors
+    };
+
+    const component = mount(
+        <NoteCard content={content} customFields={customFields}></NoteCard>
+    );
+
+    const mock = component.find('mock-component');
+    it('Montaje del componente', () => {
+        expect(mock.length).toBe(1);
+    });
+    it('Recibe content', () => {
+        expect(mock.props('content')).toBeTruthy();
+        expect(mock.props('content').content).toEqual(content);
+    });
+    it('Recibe customFields', () => {
+        expect(mock.props('content')).toBeTruthy();
+        expect(mock.props('customFields').customFields).toEqual(customFields);
+    });
+    it('Recibe de customFields el campo obligatorio title e imageId', () => {
+        expect(mock.props('customFields').customFields.title).toBeTruthy();
+        expect(mock.props('customFields').customFields.title).toBe(title);
+        expect(mock.props('customFields').customFields.imageId).toBeTruthy();
+        expect(mock.props('customFields').customFields.imageId).toBe(imageId);
+    });
+    it('Recibe de customFields el campo obligatorio title y bajada', () => {
+        expect(mock.props('customFields').customFields.title).toBeTruthy();
+        expect(mock.props('customFields').customFields.title).toBe(title);
+        expect(mock.props('customFields').customFields.imageId).toBeTruthy();
+        expect(mock.props('customFields').customFields.imageId).toBe(imageId);
+    });
+});
---
+++ __tests__/components/private/LN/home/noteFeature/noteFeature.js
@@ -0,0 +1,43 @@
+jest.mock(
+    './../../../../../../components/features/LN-home/noteFeature',
+    () => 'mock-component'
+);
+
+import React from 'react';
+import { mount } from 'enzyme';
+import NoteFeature from './../../../../../../components/features/LN-home/noteFeature';
+
+describe('Test del Feature - <NoteFeature />', () => {
+    const noteId = 'TXQ62BPYN5AEFKLPRNM66WS63I';
+    const imageId = 'UUEO45CS2ZEHFEOMB5H3EY5J5Q';
+    const lead = 'Esta es una volanta';
+    const title = 'Este es un titulo';
+    const description = 'Esta es una bajada';
+    const authors = 'Estos son los autores';
+
+    const customFields = {
+        noteId: noteId,
+        imageId: imageId,
+        lead: lead,
+        title: title,
+        description: description,
+        authors: authors
+    };
+
+    const component = mount(
+        <NoteFeature customFields={customFields}></NoteFeature>
+    );
+
+    const mock = component.find('mock-component');
+    it('Montaje del componente', () => {
+        expect(mock.length).toBe(1);
+    });
+    it('Recibe customFields', () => {
+        expect(mock.props('customFields').customFields).toBeTruthy();
+        expect(mock.props('customFields').customFields).toEqual(customFields);
+    });
+    it('Recibe de customFields el campo obligatorio noteId', () => {
+        expect(mock.props('customFields').customFields.noteId).toBeTruthy();
+        expect(mock.props('customFields').customFields.noteId).toBe(noteId);
+    });
+});
