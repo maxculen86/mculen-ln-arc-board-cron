e58f505eb69aae547f92d3764d08205ed7e20141
Abraham <asap_acedeno@lnexternos.com.ar>
2020-03-10 12:29:57 -0300

    no se dibuja te puede interesar si no hay notas

--- components/private/LN/nota/tePuedeInteresar/articleList.jsx
+++ components/private/LN/nota/tePuedeInteresar/articleList.jsx
@@ -5,14 +5,18 @@ import Article from './article';
 const ArticleList = props => {
     const { articles } = props;
     return (
-        <div className="row interest">
-            <h4 className="com-title-section-l">Te puede interesar</h4>
-            <section className="row-gap-tablet-3 row-gap-desksm-3">
-                {articles.map(article => (
-                    <Article articleData={article} key={article._id} />
-                ))}
-            </section>
-        </div>
+        <>
+            {articles.length > 0 && (
+                <div className="row interest">
+                    <h4 className="com-title-section-l">Te puede interesar</h4>
+                    <section className="row-gap-tablet-3 row-gap-desksm-3">
+                        {articles.map(article => (
+                            <Article articleData={article} key={article._id} />
+                        ))}
+                    </section>
+                </div>
+            )}
+        </>
     );
 };
 
