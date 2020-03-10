5032c495865a74c82128e81d360622a61530679b
Abraham <asap_acedeno@lnexternos.com.ar>
2020-03-10 12:21:45 -0300

    no se dibuja mas notas si no hay notas

--- components/private/LN/nota/masNotas/articleList.jsx
+++ components/private/LN/nota/masNotas/articleList.jsx
@@ -5,16 +5,38 @@ import articleFiltered from '../../../../../content/filters/LN/nota/articleFilte
 import withAcuArticlesData from '../../common/hocs/WithAcuArticlesData';
 
 const ArticleList = props => {
-    const { articles, border, outputType } = props;
+    const { articles, border, outputType, title } = props;
     if (!articles) return null;
-    return articles.map(e => (
-        <ArticleMain
-            articleData={e}
-            border={border}
-            key={e._id}
-            outputType={outputType}
-        />
-    ));
+
+    return (
+        <>
+            {articles.length > 0 && (
+                <div className="row more-articles">
+                    <div className="col-12">
+                        <h2 className="com-title-section-l">{title || ''}</h2>
+                        <section className="row-gap-tablet-3 row-gap-desksm-3">
+                            {articles.length > 0 &&
+                                articles.map(e => (
+                                    <ArticleMain
+                                        articleData={e}
+                                        border={border}
+                                        key={e._id}
+                                        outputType={outputType}
+                                    />
+                                ))}
+                        </section>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+ArticleList.propTypes = {
+    title: PropTypes.string,
+    articles: PropTypes.arrayOf(PropTypes.any),
+    border: PropTypes.bool,
+    outputType: PropTypes.string
 };
 
 export default withAcuArticlesData(ArticleList, null, 'notaM');
