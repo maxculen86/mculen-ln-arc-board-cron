cfdd098777b073ea4d59e990c8b244f68d4185d7
Julio Quintana <jquintana@lanacion.com.ar>
2020-04-20 14:26:42 -0300

    fix(gallerySource): Migrando gallery

--- content/sources/gallerySource.js
+++ content/sources/gallerySource.js
@@ -1,5 +1,5 @@
 import request from 'request-promise-native';
-import { CONTENT_BASE } from 'fusion:environment';
+import { CONTENT_BASE, ARC_ACCESS_TOKEN } from 'fusion:environment';
 import sourceSetting from './utils/sourceSetting';
 
 const resolve = key => {
@@ -13,11 +13,16 @@ const resolve = key => {
 };
 
 const fetch = query => {
-    const url = `${CONTENT_BASE}${resolve(query)}`;
-    return request({
-        uri: url,
+    const opt = {
+        uri: `${CONTENT_BASE}${resolve(query)}`,
         json: true
-    });
+    };
+    if (ARC_ACCESS_TOKEN) {
+        opt.auth = {
+            bearer: ARC_ACCESS_TOKEN
+        };
+    }
+    return request(opt);
 };
 
 export default {
