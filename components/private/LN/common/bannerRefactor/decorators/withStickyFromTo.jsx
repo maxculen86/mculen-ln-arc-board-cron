e338596470b0fa51e843d1242980b9377a7bab23
Abraham <asap_acedeno@lnexternos.com.ar>
2020-04-29 15:42:25 -0300

    decorator withStickyFromTo

---
+++ components/private/LN/common/bannerRefactor/decorators/withStickyFromTo.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useRef, useLayoutEffect } from 'react';
+
+const show = element => {
+    if (!element.classList.contains('--sticky')) {
+        element.classList.add('--sticky');
+    }
+
+    if (!element.classList.contains('--active')) {
+        element.classList.add('--active');
+    }
+};
+
+const hide = element => {
+    if (element.classList.contains('--sticky')) {
+        element.classList.remove('--sticky');
+    }
+
+    if (element.classList.contains('--active')) {
+        element.classList.remove('--active');
+    }
+};
+
+const componentDidReachTarget = (component, target) =>
+    component.offsetTop + component.clientHeight > target.offsetTop;
+
+export default Component => Target => {
+    const scrollPos = useRef(0);
+
+    const ref = React.createRef();
+
+    useLayoutEffect(() => {
+        const handleScroll = () => {
+            const windowY = window.scrollY;
+            const target = document.getElementById(`${Target}`);
+            show(ref.current);
+            if (windowY < scrollPos.current) {
+                // scrolls up
+                scrollPos.current = windowY;
+                hide(ref.current);
+            } else if (windowY >= scrollPos.current) {
+                // scrolls down
+                scrollPos.current = windowY;
+                if (!componentDidReachTarget(ref.current, target)) {
+                    show(ref.current);
+                } else {
+                    hide(ref.current);
+                }
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    return props => {
+        return <Component {...props} ref={ref} />;
+    };
+};
--- components/private/LN/common/bannerRefactor/factory/default/index.jsx
+++ components/private/LN/common/bannerRefactor/factory/default/index.jsx
@@ -1,11 +1,19 @@
+/* eslint-disable no-case-declarations */
 /* eslint-disable react/jsx-props-no-spreading */
 
 import React from 'react';
+
 import Sticky1Mob from './types/sticky1Mob';
 import Sticky2Mob from './types/sticky2Mob';
 
+import Default from './types';
+
+import withStickyFromTo from '../../decorators/withStickyFromTo';
+
 export default config => {
     const { slotId: type } = config;
+
+    console.log('confg en default:', config);
 
     return props => {
         switch (type) {
@@ -13,8 +21,13 @@ export default config => {
                 return <Sticky1Mob {...config} />;
             case 'sticky2_mob':
                 return <Sticky2Mob {...config} />;
+            case 'cabezal_dsk':
+                const Banner = withStickyFromTo(Default)('caja1_dsk');
+                return <Banner {...config} />;
+            // return <Default {...config} />;
             default:
-                return null;
+                console.log('pasa pordefault');
+                return <Default {...config} />;
         }
     };
 };
