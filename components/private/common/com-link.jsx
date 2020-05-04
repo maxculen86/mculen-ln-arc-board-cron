58820cfc16aa49c273ce0660a7559f4b7b39e050
Daro <daro@Daros-MacBook-Pro.local>
2020-05-04 09:27:19 -0300

    update componente link

--- components/private/common/com-link.jsx
+++ components/private/common/com-link.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/require-default-props */
 import React from 'react';
-// import PropTypes from 'fusion:prop-types';
+import PropTypes from 'fusion:prop-types';
 import '../../../resources/dist/css/ln/components/com-link.css';
+import '../../../resources/dist/css/ln/components/com-text.css';
 
 const ComLink = props => {
     const { children, link, textname, target, title, classCondition } = props;
@@ -24,15 +26,16 @@ const ComLink = props => {
     );
 };
 
-// ComLink.propTypes = {
-//     children: PropTypes.oneOf([
-//         PropTypes.arrayOf(PropTypes.node),
-//         PropTypes.string
-//     ]).isRequired,
-//     link: PropTypes.string.isRequired,
-//     textname: PropTypes.string.isRequired,
-//     blank: PropTypes.string.isRequired,
-//     classCondition: PropTypes.string
-// };
+ComLink.propTypes = {
+    children: PropTypes.oneOf([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.string
+    ]).isRequired,
+    link: PropTypes.string.isRequired,
+    textname: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    target: PropTypes.string,
+    classCondition: PropTypes.string
+};
 
 export default ComLink;
