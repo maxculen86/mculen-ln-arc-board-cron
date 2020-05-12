24a34ffd88cfa6ee145035cc5158c09a20c96b89
Daro <daro@Daros-MacBook-Pro.local>
2020-05-12 16:05:21 -0300

    update placeholder

--- components/private/common/com-picture.jsx
+++ components/private/common/com-picture.jsx
@@ -5,7 +5,12 @@ import '../../../resources/dist/css/ln/modules/mod-picture.css';
 
 const ComPicture = props => {
     const { href, classCondition, children } = props;
-    if (!children) return null;
+    if (!children)
+        return (
+            <picture className="mod-picture --placeholder">
+                <a href={href}></a>
+            </picture>
+        );
     return (
         <picture className={`mod-picture ${classCondition || ''}`}>
             {href ? <a href={href}>{children}</a> : children}
