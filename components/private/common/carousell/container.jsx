9d78cf40171672591ed5bc222366ae07ec41de46
Mariaelisa Rodrigues <mrodrigues@lanacion.com.ar>
2020-05-13 15:47:53 -0300

    fix carousell ott

--- components/private/common/carousell/container.jsx
+++ components/private/common/carousell/container.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import PropTypes from 'fusion:prop-types';
 import ModMedia from '../mod-media';
 import CarousellNextButton from './carousellNextButton';
 import CarousellPrevButton from './carousellPrevButton';
-import ComButton from '../../common/com-button';
+import ComButton from '../com-button';
 import withSlider from '../hocs/withSlider';
 
 const DEFAULT_PAGESIZE = 4;
 
-const Carousell = props => {
-    const { slider, children, itsGallery, active, media, width } = props;
+const Carousell = ({ slider, children, itsGallery, active, arcSite }) => {
+    if (arcSite === 'ott')
+        return (
+            <section className="slider com-slider">
+                {slider.hasPrevPage() && (
+                    <CarousellPrevButton onClick={slider.prevButtonHandler} />
+                )}
+                {children}
+                {slider.hasNextPage() && (
+                    <CarousellNextButton onClick={slider.nextButtonHandler} />
+                )}
+            </section>
+        );
     return (
         <ModMedia
             classCondition="--slider"
@@ -21,7 +33,7 @@ const Carousell = props => {
                     <ComButton
                         onClick={slider.prevButtonHandler}
                         classCondition="icon-left"
-                    ></ComButton>
+                    />
                 )}
                 {children}
                 {slider.hasNextPage() && (
@@ -29,11 +41,27 @@ const Carousell = props => {
                     <ComButton
                         onClick={slider.nextButtonHandler}
                         classCondition="icon-right"
-                    ></ComButton>
+                    />
                 )}
             </div>
         </ModMedia>
     );
 };
 
+Carousell.propTypes = {
+    slider: PropTypes.shape({
+        hasPrevPage: PropTypes.func,
+        hasNextPage: PropTypes.func,
+        prevButtonHandler: PropTypes.func,
+        nextButtonHandler: PropTypes.func
+    }).isRequired,
+    children: PropTypes.arrayOf(PropTypes.node).isRequired,
+    itsGallery: PropTypes.bool,
+    active: PropTypes.bool,
+    arcSite: PropTypes.string.isRequired
+};
+Carousell.defaultProps = {
+    itsGallery: undefined,
+    active: undefined
+};
 export default withSlider(Carousell, DEFAULT_PAGESIZE);
