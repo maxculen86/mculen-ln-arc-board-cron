fa0d0c32201b5c24b0f8504d3a1e90bb72e20df1
Julio Quintana <jquintana@lanacion.com.ar>
2020-05-14 19:31:24 -0300

    Ajustes en test

--- components/private/LN/common/media/imageBase/container.jsx
+++ components/private/LN/common/media/imageBase/container.jsx
@@ -2,7 +2,6 @@ import Consumer from 'fusion:consumer';
 import React from 'react';
 import PropTypes from 'fusion:prop-types';
 import ImageBase from './component';
-import AmpImage from '../../ampImage';
 
 /**
  *
@@ -10,58 +9,64 @@ import AmpImage from '../../ampImage';
  * esta rendereando multiples veces
  */
 
-const ImageArticle = ({ image, zoom, href, outputType, active }) => {
-    const { alt_text: altText, caption } = image;
-    const altBasic = altText || caption || '';
-    if (!image.url) return null;
-    const amp = outputType === 'amp';
+class ImageArticle extends React.PureComponent {
+    render() {
+        const { image, zoom, href, outputType, active } = this.props;
+        const { alt_text: altText, caption } = image;
+        const altBasic = altText || caption || '';
+        if (!image.url) return null;
+        const amp = outputType === 'amp';
 
-    const sources =
-        image.resized_urls && image.resized_urls.filter(v => !!v.option);
-    const sourcesZoom =
-        image.resized_urls_zoom &&
-        image.resized_urls_zoom.filter(v => !!v.option);
+        const sources =
+            image.resized_urls && image.resized_urls.filter(v => !!v.option);
+        const sourcesZoom =
+            image.resized_urls_zoom &&
+            image.resized_urls_zoom.filter(v => !!v.option);
 
-    /**
-     * TODO: Cambiar esta negrada
-     */
-    const isVertical = image.height > image.width;
-    const sourceActive = active ? sourcesZoom : sources;
+        /**
+         * TODO: Cambiar esta negrada
+         */
+        const isVertical = image.height > image.width;
+        const sourceActive = active ? sourcesZoom : sources;
 
-    // TODO: ver este tema de source sets con maquetacion
-    let srcset = sourceActive.map(src => {
-        const {
-            option: { width: _w, height: _h }
-        } = src;
+        // TODO: ver este tema de source sets con maquetacion
+        let srcset =
+            sourceActive &&
+            sourceActive.map(src => {
+                const {
+                    option: { width: _w, height: _h }
+                } = src;
 
-        if (src.resizedUrl && !isVertical && _w)
-            return `${src.resizedUrl} ${src.option.width}w`;
+                if (src.resizedUrl && !isVertical && _w)
+                    return `${src.resizedUrl} ${src.option.width}w`;
 
-        if (src.resizedUrl && isVertical && _h)
-            return `${src.resizedUrl} ${src.option.height}w`;
+                if (src.resizedUrl && isVertical && _h)
+                    return `${src.resizedUrl} ${src.option.height}w`;
 
-        return '';
-    });
-    srcset = srcset && srcset.length > 1 ? srcset.join(', ') : srcset;
+                return '';
+            });
+        srcset = srcset && srcset.length > 1 ? srcset.join(', ') : srcset;
 
-    if (srcset.length === 1) srcset = `${image.url} ${image.width}w`;
+        if (srcset && srcset.length === 1)
+            srcset = `${image.url} ${image.width}w`;
 
-    return (
-        <ImageBase
-            srcsetAMP={srcset}
-            active={active}
-            urlDefault={image.url}
-            sources={sources || []}
-            altText={altBasic}
-            zoom={zoom}
-            sourcesZoom={sourcesZoom || []}
-            href={href}
-            width={image.width}
-            height={image.height}
-            amp={amp}
-        />
-    );
-};
+        return (
+            <ImageBase
+                srcsetAMP={srcset}
+                active={active}
+                urlDefault={image.url}
+                sources={sources || []}
+                altText={altBasic}
+                zoom={zoom}
+                sourcesZoom={sourcesZoom || []}
+                href={href}
+                width={image.width}
+                height={image.height}
+                amp={amp}
+            />
+        );
+    }
+}
 
 ImageArticle.propTypes = {
     outputType: PropTypes.string.isRequired,
