7c6d9727d5f8fb10013c3a6b8755ea0b205800b6
sromero <sromero@lanacion.com.ar>
2020-05-19 18:07:48 -0300

    Se agrega componente text

--- components/private/common/com-text.jsx
+++ components/private/common/com-text.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
-import PropTypes from 'fusion:prop-types';
 
 import '../../../resources/dist/css/ln/components/com-text.css';
 
 const ComText = props => {
-    const { children, textname, classCondition } = props;
-    //if (!textname) return null;
+    const { children, textname, classCondition, tag, size } = props;
+    if (tag === 'h1')
+        return (
+            <h1
+                className={`com-title ${size} ${
+                    classCondition ? classCondition : ''
+                }`}
+            >
+                {children}
+            </h1>
+        );
+    else if (tag === 'h2')
+        return (
+            <h2
+                className={`com-title --section ${size} ${
+                    classCondition ? classCondition : ''
+                }`}
+            >
+                {children}
+            </h2>
+        );
+    else if (tag === 'h3')
+        return (
+            <h3
+                className={`com-title --section ${size} ${
+                    classCondition ? classCondition : ''
+                }`}
+            >
+                {children}
+            </h3>
+        );
+    else if (tag === 'h4')
+        return (
+            <h4
+                className={`com-subtitle --module ${size} ${
+                    classCondition ? classCondition : ''
+                }`}
+            >
+                {children}
+            </h4>
+        );
 
     return (
         <span className={`com-text ${classCondition ? classCondition : ''}`}>
-            {children || textname}
+            {children ? children : ``}
+            {textname ? textname : ``}
         </span>
     );
-};
-
-ComText.propTypes = {
-    textname: PropTypes.string,
-    classCondition: PropTypes.string
 };
 
 export default ComText;
