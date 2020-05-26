c7e1204a366927d52fab3d2a43a520214e435b3f
Julio Quintana <jquintana@lanacion.com.ar>
2020-05-26 10:55:04 -0300

    Eliminando console.logs

--- components/private/LN/api/v1/nota/cuerpo/customRecetaList.js
+++ components/private/LN/api/v1/nota/cuerpo/customRecetaList.js
@@ -3,7 +3,6 @@ import get from 'lodash.get';
 const customReceta = (listData, type) => {
     const config = get(listData, 'embed.config');
     if (!config) return null;
-    console.log(config);
     return {
         _t: type,
         title: config.titleList,
