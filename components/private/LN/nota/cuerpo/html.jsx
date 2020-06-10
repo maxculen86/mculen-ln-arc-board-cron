960bc388dc292b2368d8456c5ec6db1b03ccd00a
Abraham <asap_acedeno@lnexternos.com.ar>
2020-06-10 13:27:51 -0300

    DOM parse in html raw component

--- components/private/LN/nota/cuerpo/html.jsx
+++ components/private/LN/nota/cuerpo/html.jsx
@@ -1,16 +1,27 @@
-/* eslint-disable react/no-danger */
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/no-danger             */
 
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'fusion:prop-types';
 import '../../../../../resources/dist/css/ln/components/com-embed.css';
 
-const Html = ({ data }) => {
+const Html = props => {
+    const { data } = props;
     const { content } = data || { content: null };
+    const parser = useRef();
+
+    if (!parser.current) {
+        parser.current = new DOMParser();
+    }
+
     if (!content) return null;
     return (
         <div
             className="com-embed --html"
-            dangerouslySetInnerHTML={{ __html: content }}
+            dangerouslySetInnerHTML={{
+                __html: parser.current.parseFromString(content, 'text/html')
+                    .documentElement.innerHTML
+            }}
         />
     );
 };
@@ -20,7 +31,7 @@ Html.arcType = 'raw_html';
 Html.propTypes = {
     data: PropTypes.shape({
         content: PropTypes.string
-    }).isRequired
+    })
 };
 
 export default Html;
