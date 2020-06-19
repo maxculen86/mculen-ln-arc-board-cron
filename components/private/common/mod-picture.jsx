6e68cb7e1a8078a8c2bccbfd1983963952819e07
Mariaelisa Rodrigues <mrodrigues@lanacion.com.ar>
2020-06-19 16:35:04 -0300

    add prop amp to ModPicture

--- components/private/common/mod-picture.jsx
+++ components/private/common/mod-picture.jsx
@@ -9,16 +9,17 @@ import ModVideo from './mod-video';
 import '../../../resources/dist/css/ln/modules/mod-picture.css';
 
 const ModImage = props => {
-    const { media, src, srcset, alt, classCondition, video } = props;
+    const { media, src, srcset, alt, classCondition, video, amp } = props;
     // if (!src || !srcset) return null;
 
     return (
         <ComPicture
             classCondition={classCondition}
             video={video ? '--video-background' : ''}
+            amp={amp}
         >
             <ComSource media={media} srcset={srcset} />
-            <ComImage src={src} alt={alt} />
+            <ComImage src={src} alt={alt} amp={amp} />
             {video ? <ModVideo image={src} video={video} /> : <></>}
         </ComPicture>
     );
@@ -30,7 +31,8 @@ ModImage.propTypes = {
     media: PropTypes.string,
     alt: PropTypes.string,
     classCondition: PropTypes.string,
-    video: PropTypes.string
+    video: PropTypes.string,
+    amp: PropTypes.string
 };
 
 export default ModImage;
