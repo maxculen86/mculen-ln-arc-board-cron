88dfd0250ed9c886785773bff66cdd7110dbf599
Julio Quintana <jquintana@lanacion.com.ar>
2020-06-24 17:45:11 -0300

    Agregando estilos y analitycs a PowaPlayer de LN

--- components/private/common/scriptManager/scriptVideoPowa.jsx
+++ components/private/common/scriptManager/scriptVideoPowa.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'fusion:prop-types';
 import get from 'lodash.get';
+import loadOTTVideoStyles from '../../OTT/ficha/ottVideoStyles';
+import loadOTTVideoAnalytics from '../../OTT/ficha/ottVideoAnalytics';
 
 const videosBody = contentElements =>
     contentElements.filter(element => element.type === 'video').length;
 
+// TODO: Validar cuantas veces renderiza este componente
 const ScriptVideoPowa = ({ globalContent }) => {
+    const {
+        _id,
+        headlines: { basic }
+    } = globalContent;
     const contentElements = get(globalContent, 'content_elements');
     const promoItems = get(globalContent, 'promo_items');
     const basicPromoItems = get(promoItems, 'basic');
     const storytellingPromoItems = get(promoItems, 'storytelling');
-
     const typeBasic = get(basicPromoItems, 'type');
     const typeStorytelling = get(storytellingPromoItems, 'type');
 
-    return videosBody(contentElements) > 0 ||
+    const loadVideo =
+        videosBody(contentElements) > 0 ||
         typeBasic === 'video' ||
-        typeStorytelling === 'video' ? (
+        typeStorytelling === 'video';
+
+    useEffect(() => {
+        if (window && loadVideo) {
+            loadOTTVideoStyles();
+            loadOTTVideoAnalytics(basic, _id);
+        }
+    }, [_id, basic, loadVideo]);
+
+    // FIXME: Cambiar parametro en url que indentifica ambiente (prod-sandbox) a dinamico
+    return loadVideo ? (
         <script
             async
             src="https://lanacionar.video-player.arcpublishing.com/prod/powaBoot.js"
@@ -28,6 +45,8 @@ const ScriptVideoPowa = ({ globalContent }) => {
 
 ScriptVideoPowa.propTypes = {
     globalContent: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        headlines: PropTypes.shape({ basic: PropTypes.string.isRequired }),
         content_elements: PropTypes.node.isRequired,
         promo_items: PropTypes.shape({
             basic: PropTypes.shape({
