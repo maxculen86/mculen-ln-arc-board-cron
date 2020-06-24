9dd200e99f3ff1e90a93b65b500e91ea54165d15
Julio Quintana <jquintana@lanacion.com.ar>
2020-06-24 18:18:35 -0300

    Sacando estilo y analitycs

--- components/private/common/scriptManager/scriptVideoPowa.jsx
+++ components/private/common/scriptManager/scriptVideoPowa.jsx
@@ -1,18 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'fusion:prop-types';
 import get from 'lodash.get';
-import loadOTTVideoStyles from '../../OTT/ficha/ottVideoStyles';
-import loadOTTVideoAnalytics from '../../OTT/ficha/ottVideoAnalytics';
 
 const videosBody = contentElements =>
     contentElements.filter(element => element.type === 'video').length;
 
 // TODO: Validar cuantas veces renderiza este componente
 const ScriptVideoPowa = ({ globalContent }) => {
-    const {
-        _id,
-        headlines: { basic }
-    } = globalContent;
     const contentElements = get(globalContent, 'content_elements');
     const promoItems = get(globalContent, 'promo_items');
     const basicPromoItems = get(promoItems, 'basic');
@@ -24,13 +18,6 @@ const ScriptVideoPowa = ({ globalContent }) => {
         videosBody(contentElements) > 0 ||
         typeBasic === 'video' ||
         typeStorytelling === 'video';
-
-    useEffect(() => {
-        if (window && loadVideo) {
-            loadOTTVideoStyles();
-            loadOTTVideoAnalytics(basic, _id);
-        }
-    }, [_id, basic, loadVideo]);
 
     // FIXME: Cambiar parametro en url que indentifica ambiente (prod-sandbox) a dinamico
     return loadVideo ? (
@@ -45,8 +32,6 @@ const ScriptVideoPowa = ({ globalContent }) => {
 
 ScriptVideoPowa.propTypes = {
     globalContent: PropTypes.shape({
-        _id: PropTypes.string.isRequired,
-        headlines: PropTypes.shape({ basic: PropTypes.string.isRequired }),
         content_elements: PropTypes.node.isRequired,
         promo_items: PropTypes.shape({
             basic: PropTypes.shape({
