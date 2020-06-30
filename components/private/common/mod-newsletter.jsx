af225c8d4bdad2fed3f0f5f8ee3c4199a3404b76
sromero <sromero@lanacion.com.ar>
2020-06-30 16:06:15 -0300

    Se agregan estilos

--- components/private/common/mod-newsletter.jsx
+++ components/private/common/mod-newsletter.jsx
@@ -3,17 +3,20 @@ import ComLink from '../common/com-link';
 import '../../../resources/dist/css/ln/modules/mod-newsletter.css';
 import ComTitle from './com-title';
 import ComButton from './com-button';
+import ComIco from './com-icon';
 
 const ModNewsletter = props => {
     const { classCondition } = props;
     return (
         <section className="mod-newsletter">
-            <div className="row">
-                <div className="col-6">
-                    <ComTitle size="l">
-                        Recibí las noticias de Coronavirus en la Argentina por
-                        e-mail
-                    </ComTitle>
+            <div className="container">
+                <div className="container-text">
+                    <ComIco iconName="mail" size="xl" />
+                    <ComTitle
+                        tag="h3"
+                        size="l"
+                        content="Recibí las noticias de Coronavirus en la Argentina por e-mail"
+                    />
                     <ComLink link="#" size="l">
                         Mirá todos los newsletters que tenemos para vos
                     </ComLink>
