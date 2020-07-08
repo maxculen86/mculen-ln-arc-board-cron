5806d3c7c521abb19a0f87daefc0be8bdcd52d15
Julio Quintana <jquintana@lanacion.com.ar>
2020-07-08 14:41:32 -0300

    test elmah ssr content/source

--- components/features/core/error.jsx
+++ components/features/core/error.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
+import request from 'request-promise-native';
 
 function error() {
     useEffect(() => {
-        throw new Error('Probando elmah');
+        // throw new Error('Probando elmah');
     });
 
     return <h3>Sin errores</h3>;
--- components/layouts/core.jsx
+++ components/layouts/core.jsx
@@ -5,11 +5,60 @@ const core = ({ children }) => {
     if (typeof document !== 'undefined') {
         const log = new Elmahio({
             apiKey: 'e6ce19c37ca046348f6afb5a11bc3fdb',
-            logId: 'ae9f7b57-424b-46d9-9a9e-c20b8383956d',
+            logId: '00f817a7-48fa-4335-b551-ca953b7342fd',
             debug: true
         });
 
-        log.information('Initialized');
+        // log.information('Initialized');
+    }
+
+    if (typeof document === 'undefined') {
+        const fecha = new Date().toISOString();
+        const elmahJson = {
+            application: 'ln/arc',
+            detail: 'Prueba desde LN/ARC',
+            hostname: 'localhost',
+            title: 'Test Elmah SSR',
+            titleTemplate: 'titleTemplate',
+            source: 'content/source',
+            statusCode: 0,
+            dateTime: fecha,
+            type: 'string',
+            user: 'string',
+            severity: 'string',
+            url: 'string',
+            method: 'string',
+            version: 'string',
+            cookies: [{ key: 'string', value: 'string' }],
+            form: [{ key: 'string', value: 'string' }],
+            queryString: [{ key: 'string', value: 'string' }],
+            serverVariables: [{ key: 'string', value: 'string' }],
+            data: [{ key: 'string', value: 'string' }]
+        };
+
+        console.log('============================');
+        console.log('');
+        console.log('fetch a elmah en componente');
+        console.log('');
+        console.log('============================');
+
+        /*         fetch(
+            'https://api.elmah.io/v3/messages/00f817a7-48fa-4335-b551-ca953b7342fd?api_key=e6ce19c37ca046348f6afb5a11bc3fdb',
+            {
+                method: 'POST',
+                headers: {
+                    accept: 'text/plain',
+                    'Content-Type': 'application/json-patch+json'
+                },
+                body: JSON.stringify(elmahJson)
+            }
+        )
+            .then(res => {
+                console.log('elmah -> res', res);
+            })
+            .catch(e => {
+                console.log('elmah -> error', e);
+            }); */
     }
 
     return (
