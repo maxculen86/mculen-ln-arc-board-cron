e0b8b6a8e33b553df1c09bcc890f03a56b290eb9
Fernando Caíno <pit_fcaino@lnexternos.com.ar>
2020-07-14 17:31:01 -0300

    Se agrego funcionalidad de compartir en AMP y retoco un test que fallaba

--- components/private/LN/nota/share.jsx
+++ components/private/LN/nota/share.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/control-has-associated-label,jsx-a11y/label-has-associated-control,react/jsx-curly-newline */
 import React from 'react';
 import getProperties from 'fusion:properties';
-import { useFusionContext } from 'fusion:context';
+import { useAppContext } from 'fusion:context';
 import PropTypes from 'fusion:prop-types';
 import '../../../../resources/dist/css/ln/components/share.css';
 import config from '../../../../properties/sites/la-nacion-ar';
@@ -12,6 +12,7 @@ import {
     popUpCompartirMailTo
 } from '../common/utils/shareHelper';
 import AmpContainer from '../../common/ampContainer';
+import get from '../../common/utils/get';
 
 const Share = props => {
     const {
@@ -20,18 +21,18 @@ const Share = props => {
             headlines: { basic: title, mobile: mobileTitle }
         }
     } = props;
-    const { arcSite } = useFusionContext();
+    const { arcSite } = useAppContext();
     const siteVars = getProperties(arcSite);
     const twiterTitle =
         mobileTitle !== '' && mobileTitle !== undefined ? mobileTitle : title;
 
-    const facebookId = siteVars.shareConfig.facebook.appID;
-    // TODO: arreglar el tema de las URL's
+    const facebookId = get(siteVars, 'shareConfig.facebook.appID', undefined);
 
+    // TODO: arreglar el tema de las URL's
     return (
         <div id="v-share" className="com-share">
-            <div className="share-left">
-                <AmpContainer>
+            <AmpContainer isForAmp={false}>
+                <div className="share-left">
                     <button
                         type="button"
                         onClick={() =>
@@ -58,19 +59,9 @@ const Share = props => {
                         }
                         className="icon-whatsapp"
                     />
-                </AmpContainer>
-                <AmpContainer isForAmp>
-                    <amp-social-share
-                        type="facebook"
-                        data-param-app_id={facebookId}
-                    />
-                    <amp-social-share type="twitter" />
-                    <amp-social-share type="whatsapp" />
-                </AmpContainer>
-            </div>
+                </div>
 
-            <div className="share-right">
-                <AmpContainer>
+                <div className="share-right">
                     <button
                         type="button"
                         className="icon-mail"
@@ -78,13 +69,29 @@ const Share = props => {
                             popUpCompartirMailTo(requestUri, config.host)
                         }
                     />
-                </AmpContainer>
-                <AmpContainer isForAmp>
-                    <amp-social-share type="email" />
-                </AmpContainer>
-                <button type="button" className="icon-comment" />
-                <label htmlFor="">145</label>
-            </div>
+                    <button type="button" className="icon-comment" />
+                    <label htmlFor="">145</label>
+                </div>
+            </AmpContainer>
+
+            <AmpContainer isForAmp>
+                <div className="share-left">
+                    <amp-social-share
+                        type="facebook"
+                        data-param-app_id={facebookId}
+                    />
+                    <amp-social-share type="twitter" data-param-text={title} />
+                    <amp-social-share type="whatsapp" />
+                </div>
+
+                <div className="share-right">
+                    <amp-social-share
+                        type="email"
+                        data-param-subject="Te recomiendo esta nota de LA NACION"
+                        data-param-body={`Lee esta nota de LA NACION ${config.host}${requestUri}`}
+                    />
+                </div>
+            </AmpContainer>
         </div>
     );
 };
