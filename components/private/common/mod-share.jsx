c1f451e8bca50686343e9e3d3220a1b938187b09
sromero <sromero@lanacion.com.ar>
2020-07-15 12:52:06 -0300

    Se suma el componente mod-share

---
+++ components/private/common/mod-share.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ComIcon from './com-icon';
+import '../../../../../resources/dist/css/ln/modules/mod-share.css';
+
+const ModShare = props => {
+    const { classesNames, classCondition } = props;
+
+    return (
+        <section
+            className={`mod-shared ${classesNames ? classesNames : ``} ${
+                classCondition ? classCondition : ``
+            }`}
+        >
+            <ComIcon link="https://www.facebook.com/lanacion" icon="facebook" />
+            <ComIcon link="https://twitter.com/LANACION" icon="twitter" />
+            <ComIcon
+                link="https://www.instagram.com/lanacioncom/"
+                icon="instagram"
+            />
+            <ComIcon
+                link="http://servicios.lanacion.com.ar/herramientas/rss/ayuda"
+                icon="rss"
+            />
+        </section>
+    );
+};
+
+export default ModShare;
