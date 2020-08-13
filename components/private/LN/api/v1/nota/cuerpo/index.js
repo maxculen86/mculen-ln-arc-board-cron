36104dbcb964d4006b5a38a0842c9abaf6b13f3f
ianzola <vates_ianzola@lnexternos.com.ar>
2020-08-13 15:17:51 -0300

    Fix para notas que no son de tipo infografia

--- components/private/LN/api/v1/nota/cuerpo/index.js
+++ components/private/LN/api/v1/nota/cuerpo/index.js
@@ -1,17 +1,15 @@
 import DefaultCuerpo from './defaultCuerpo';
 import RecetaCuerpo from './recetaCuerpo';
+import get from 'lodash.get';
 
 const cuerpoIndex = article => {
 
     if (!article.content_elements) return null;
 
-    const {        
-        promo_items: { basic: infographic }
-    } = article;
-
     const contentElements = article.content_elements
+    const infographic = get(article, 'promo_items.basic');
 
-    if (article.subtype == 2) {
+    if (article.subtype === '2' && infographic) {
         contentElements.unshift(infographic);
     }
 
