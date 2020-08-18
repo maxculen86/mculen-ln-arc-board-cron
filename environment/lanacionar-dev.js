e62ba5218ac67839f7875132271f69527b0d00ec
Julio Quintana <jquintana@lanacion.com.ar>
2020-08-18 11:52:13 -0300

    Actualizando credenciales de elmah

--- environment/lanacionar-dev.js
+++ environment/lanacionar-dev.js
@@ -9,5 +9,9 @@ export default {
     SITE_LANACION:
         'https://lanacionar-la-nacion-ar-sandbox.cdn.arcpublishing.com',
     SITE_RECETAS:
-        'https://lanacionar-la-nacion-ar-sandbox.cdn.arcpublishing.com'
+        'https://lanacionar-la-nacion-ar-sandbox.cdn.arcpublishing.com',
+    ELMAH_API_KEY:
+        'AQICAHh/411m8FGwOHtrUTs9KkxN3n0LVOK3XAvOUKflIN5VygFd7mwOcSYVIOLLMO29lzDTAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMYBe956lWyOERqo8aAgEQgDtNva+NtF0j7Kwj4PN49+cVGOVxS/K5BVKdbfYisymDAUbtW1hW0jQXVo02rGVRNPn67OmnOvrbEGjlHw==',
+    ELMAH_LOG_ID:
+        'AQICAHh/411m8FGwOHtrUTs9KkxN3n0LVOK3XAvOUKflIN5VygF35naRKzRzYQEBIUemQ76WAAAAgzCBgAYJKoZIhvcNAQcGoHMwcQIBADBsBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDFq7H688NBKZLhX2YwIBEIA/tXSeSR2/jjGhM14XaUXYwPAkzfAZ99xZq0pHexC1BjaBYNpoD60bZpbmSFdnkng5EcnnoBULpkuvRbdWAj4E'
 };
