8b88593d921c5032d737b624957c43fc1deeb4a9
Julio Quintana <jquintana@lanacion.com.ar>
2020-08-20 10:29:28 -0300

    Agregando variables encriptadas

--- environment/lanacionar-dev.js
+++ environment/lanacionar-dev.js
@@ -10,6 +10,8 @@ export default {
         'https://lanacionar-la-nacion-ar-sandbox.cdn.arcpublishing.com',
     SITE_RECETAS:
         'https://lanacionar-la-nacion-ar-sandbox.cdn.arcpublishing.com',
-    ELMAH_API_KEY: 'e6ce19c37ca046348f6afb5a11bc3fdb',
-    ELMAH_LOG_ID: '00f817a7-48fa-4335-b551-ca953b7342fd'
+    ELMAH_API_KEY:
+        '%{AQICAHh/411m8FGwOHtrUTs9KkxN3n0LVOK3XAvOUKflIN5VygG6WzXPpGVSB0hSg4Jvr4G1AAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMF9qiz0Zg6IrgOAfLAgEQgDve2h/plIRvoo9om5H2vsgDbZi7ysHNwCSNlYzrYIvp9ZnPzPK/SFuR4WNEPJN5/Q4OMMHHk9RBN0LrJQ==}',
+    ELMAH_LOG_ID:
+        '%{AQICAHh/411m8FGwOHtrUTs9KkxN3n0LVOK3XAvOUKflIN5VygEBLPddcPRqU9iQcXIwAJ3YAAAAgzCBgAYJKoZIhvcNAQcGoHMwcQIBADBsBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDNzUL34XTa2n9f7ZLAIBEIA/Va4wDFUWaHVJ6ev6Jezp+95Siv8fBJsW9yeRSuiKfSTOujeLfZePQUFYGLYqmhb4cYsJn7Zyl46aJsJrvnQ7}'
 };
