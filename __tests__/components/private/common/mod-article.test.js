f6d2f7c1bb93c17b627f96c2a314fb09ea4ed429
Mariaelisa Rodrigues <mrodrigues@lanacion.com.ar>
2020-09-09 17:34:22 -0300

    add size class to com-link

--- __tests__/components/private/common/mod-article.test.js
+++ __tests__/components/private/common/mod-article.test.js
@@ -55,7 +55,7 @@ describe('Private - Common - ModArticle', () => {
         expect(component.find('.mod-marquee')).toHaveLength(1);
         expect(component.find('.com-date')).toHaveLength(1);
         expect(component.find('.com-title').html()).toBe(
-            '<h1 class="com-title --s"><a href="http://google.com" class="com-link ">Este es el titulo</a></h1>'
+            '<h1 class="com-title --s"><a href="http://google.com" class="com-link">Este es el titulo</a></h1>'
         );
     });
 
