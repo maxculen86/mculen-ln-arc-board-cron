b3a5d6e7ded5e914506d053aba10be6b1da7d858
Fernando Caíno <pit_fcaino@lnexternos.com.ar>
2020-09-21 11:51:56 -0300

    Se omite el paginado en version AMP

--- components/private/LN/acumulado/grillaNotas/grillaNotas.jsx
+++ components/private/LN/acumulado/grillaNotas/grillaNotas.jsx
@@ -8,6 +8,7 @@ import WithAcuArticlesData from '../../common/hocs/WithAcuArticlesData';
 import filter from '../../../../../content/filters/LN/acumulado/articleAcu';
 import withScreenUtils from '../../../common/hocs/withScreenUtils';
 import WithNavigation from '../../common/hocs/WithNavigation';
+import ArticlesAcumAMP from '../articlesAcumAMP';
 // import useGlobalProviderAcu from '../../acumulado/hooks/useGlobalProviderAcu';
 
 class GrillaNotas extends Component {
@@ -71,7 +72,7 @@ class GrillaNotas extends Component {
                     outputType={outputType}
                 />
 
-                {hayMasNotas > 0 && (
+                {outputType !== 'amp' && hayMasNotas > 0 && (
                     <section className="row">
                         <BtnMasNotas
                             onClickHandler={obtenerMasNotas}
