6ff259193a8e8cbca0d4442aa3fcd4bf3f20d16d
Diego Lopez <dlopez@lanacion.com.ar>
2020-10-07 09:50:06 -0300

    Test pos release para prueba de CD

---
+++ e2eTests/tests/posRelease/prueba2.test.js
@@ -0,0 +1,18 @@
+describe('Test imagen en nota', () => {
+    let frame;
+    beforeAll(async () => {
+        frame = await arc.goto({
+            path:
+                '/sociedad/polemica-en-la-quema-de-munecos-de-la-plata-nid1086000/'
+        });
+    });
+
+    test('Imagen existe?', async () => {
+        const selector =
+            'section.cuerpo__nota div.row div.col-deskxl-10.offset-deskxl-1.col-desksm-11 div div section figure picture';
+        // await page.waitForSelector(selector);
+        const image = await frame.$(selector);
+        expect((await image.$$('source')).length).toBe(5);
+        expect((await image.$$('img')).length).toBe(1);
+    });
+});
