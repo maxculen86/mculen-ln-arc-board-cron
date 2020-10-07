4ac7398c31481de1d6a152c00a3ed681a44d40fa
Diego Lopez <dlopez@lanacion.com.ar>
2020-10-07 09:49:44 -0300

    Refactor tes e2e de prueba para mostrar reutilizacion de request

--- e2eTests/tests/preRelease/prueba.test.js
+++ e2eTests/tests/preRelease/prueba.test.js
@@ -1,17 +1,24 @@
 describe('Test imagen en nota', () => {
-    beforeAll(async () => {});
-    afterAll(async () => {});
-
-    test('Imagen existe?', async () => {
-        const frame = await arc.goto({
+    let frame;
+    beforeAll(async () => {
+        frame = await arc.goto({
             path:
                 '/sociedad/polemica-en-la-quema-de-munecos-de-la-plata-nid1086000/'
         });
+    });
+
+    test('Imagen existe?', async () => {
         const selector =
             'section.cuerpo__nota div.row div.col-deskxl-10.offset-deskxl-1.col-desksm-11 div div section figure picture';
-        // await page.waitForSelector(selector);
         const image = await frame.$(selector);
         expect((await image.$$('source')).length).toBe(5);
         expect((await image.$$('img')).length).toBe(1);
+    });
+
+    test('Titulo existe?', async () => {
+        const selector =
+            'div.lay.--apertura div.row div.col-12 h1.com-title.--threexl';
+        const titulo = await frame.$(selector);
+        expect(titulo.innerText).not.toBe('');
     });
 });
