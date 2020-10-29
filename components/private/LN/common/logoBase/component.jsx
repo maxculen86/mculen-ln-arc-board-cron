80953731589ea2fe08d5d70e67f967a4c73baf50
Fernando Caíno <pit_fcaino@lnexternos.com.ar>
2020-10-29 17:14:40 -0300

    Los logos que se muestran en Storytelling y Foto al 100 no debe ir con la clase --medium

--- components/private/LN/common/logoBase/component.jsx
+++ components/private/LN/common/logoBase/component.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import PropTypes from 'fusion:prop-types';
 import ComLogo from '../../../common/com-logo';
 import ComLink from '../../../common/com-link';
+import {
+    FOTOAL100,
+    STORYTELLING
+} from '../../../common/utils/subtypes/subtypeHelper';
 
-const LogoBaseComponent = ({ path, logoName, color }) => {
+const LogoBaseComponent = ({ path, logoName, color, subtype }) => {
     if (!logoName) return null;
-    const Logo = <ComLogo logoName={logoName} color={color} size="--medium" />;
+    const size =
+        subtype === FOTOAL100 || subtype === STORYTELLING ? '' : '--medium';
+    const Logo = <ComLogo logoName={logoName} color={color} size={size} />;
     const Link = <ComLink link={path}>{Logo}</ComLink>;
     return <>{path ? Link : Logo}</>;
 };
@@ -13,7 +19,8 @@ const LogoBaseComponent = ({ path, logoName, color }) => {
 LogoBaseComponent.propTypes = {
     path: PropTypes.string.isRequired,
     logoName: PropTypes.string.isRequired,
-    color: PropTypes.bool.isRequired
+    color: PropTypes.bool.isRequired,
+    subtype: PropTypes.string
 };
 
 export default LogoBaseComponent;
