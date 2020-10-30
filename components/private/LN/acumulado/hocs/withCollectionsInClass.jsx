198a047d1c777c9d3329371c60261ac4859a1d1b
Fernando Caíno <pit_fcaino@lnexternos.com.ar>
2020-10-30 17:37:28 -0300

    Fix propiedades cuando hay collections en apertura y en las cajas

--- components/private/LN/acumulado/hocs/withCollectionsInClass.jsx
+++ components/private/LN/acumulado/hocs/withCollectionsInClass.jsx
@@ -3,20 +3,12 @@ import Consumer from 'fusion:consumer';
 import PropTypes from 'fusion:prop-types';
 import get from '../../../common/utils/get';
 
-function withCollectionsInClass(WrappedComponent, filter, size, imageConfig) {
+function withCollectionsInClass(WrappedComponent, filter, size) {
     return Consumer(
         class extends PureComponent {
             static get propTypes() {
                 return {
-                    globalContent: PropTypes.shape({
-                        acumuladoGeneral: PropTypes.shape({
-                            id_collection_promo_items:
-                                PropTypes.string.isRequired
-                        })
-                    }).isRequired,
-                    customFields: PropTypes.shape({
-                        idCollection: PropTypes.string
-                    }),
+                    idCollection: PropTypes.string.isRequired,
                     size: PropTypes.number.isRequired,
                     website: PropTypes.number.isRequired,
                     outputType: PropTypes.number.isRequired
@@ -26,19 +18,13 @@ function withCollectionsInClass(WrappedComponent, filter, size, imageConfig) {
             constructor(props) {
                 super(props);
                 this.state = { articlesInCollection: [] };
-                const {
-                    globalContent: { acumuladoGeneral },
-                    website = 'la-nacion-ar',
-                    customFields: { idCollection }
-                } = props;
+                const { website = 'la-nacion-ar', idCollection } = props;
 
-                const id = get(acumuladoGeneral, 'id_collection_promo_items');
-
-                if (!id && !idCollection) return null;
+                if (!idCollection) return null;
                 const { fetched } = this.getContent({
                     sourceName: 'collectionsSource',
                     query: {
-                        id: id || idCollection,
+                        id: idCollection,
                         size,
                         website
                     },
