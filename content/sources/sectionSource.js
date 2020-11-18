7df0d9644d7020bcf2f56efb52996aa36606c111
Abraham <asap_acedeno@lnexternos.com.ar>
2020-11-18 15:36:03 -0300

    fix sectionSource

--- content/sources/sectionSource.js
+++ content/sources/sectionSource.js
@@ -1,10 +1,10 @@
-// import { transform } from "@babel/core";
-
 import request from 'request-promise-native';
 import { CONTENT_BASE, ARC_ACCESS_TOKEN } from 'fusion:environment';
 import getProperties from 'fusion:properties';
-import logger from '../../components/private/common/utils/logger';
+import collectionsSource from './collectionsSource';
 import navigationTreeSource from './navigationTreeSource';
+import get from '../../components/private/common/utils/get';
+import logger from '../../components/private/common/utils/logger';
 
 const resolve = key => {
     const { id, website } = key;
@@ -21,10 +21,8 @@ const resolve = key => {
         );
     return `/site/v3/navigation/${finalWebsite}/?_id=${id}`;
 };
-
 const fetch = query => {
     const { url = '' } = query;
-    // console.log("query", query)
     const arcSite = query['arc-site'];
     const opt = {
         uri: `${CONTENT_BASE}${resolve(query)}`,
@@ -44,12 +42,10 @@ const fetch = query => {
             throw error;
         });
 };
-
-const transform = (data, query) => {
+const transform = (data, siteProps) => {
     const { _id: idData } = data;
-    const { id: idQuery } = query;
-    const { website } = query;
-
+    const { id: idQuery } = siteProps;
+    const arcSite = siteProps['arc-site'];
     /**
      * Se valida que la sección consultada tenga
      * consistencia con la data respondida en la data
@@ -62,20 +58,29 @@ const transform = (data, query) => {
         err.statusCode = 404;
         throw err;
     }
+    return transformContent(data, siteProps, arcSite);
+};
+const transformContent = (data, siteProps, arcSite) => {
+    const promiseArr = [];
+    const resp = { ...data, articlesInCollection: [] };
 
-    const response = data;
+    promiseArr.push(
+        getNavigationSiteProperties(arcSite).then(result => {
+            resp.siteService = {
+                banners: result.banners,
+                adserver: result.adserver,
+                termicas: result.termicas
+            };
+            return resp;
+        })
+    );
 
-    return getNavigationSiteProperties(null, website).then(result => {
-        response.siteService = {
-            banners: result.banners,
-            adserver: result.adserver,
-            termicas: result.termicas
-        };
-        return response;
+    return Promise.all(promiseArr).then(() => {
+        return resp;
     });
 };
 
-const getNavigationSiteProperties = (anotherNoteData, arcSite) => {
+const getNavigationSiteProperties = arcSite => {
     const urlNavigationTreeSource = navigationTreeSource.resolve({
         website: arcSite
     });
@@ -96,7 +101,6 @@ const getNavigationSiteProperties = (anotherNoteData, arcSite) => {
                 Termicas: {}
             };
             const { sitio_adserver: sitioAdserver } = site;
-
             // Banner dimensions
             const banners = [];
             Object.keys(bannerConfig).forEach(key => {
@@ -105,7 +109,6 @@ const getNavigationSiteProperties = (anotherNoteData, arcSite) => {
                     dimensions: bannerConfig[key]
                 });
             });
-
             // Termicas
             const termicas = [];
             Object.keys(termicasConfig).forEach(key => {
@@ -114,7 +117,6 @@ const getNavigationSiteProperties = (anotherNoteData, arcSite) => {
                     value: termicasConfig[key]
                 });
             });
-
             // Banner segments
             const adserver = [];
             Object.keys(sitioAdserver).forEach(key => {
@@ -123,7 +125,6 @@ const getNavigationSiteProperties = (anotherNoteData, arcSite) => {
                     value: sitioAdserver[key]
                 });
             });
-
             const resp = {
                 banners,
                 adserver,
@@ -135,17 +136,14 @@ const getNavigationSiteProperties = (anotherNoteData, arcSite) => {
             // console.log('Error article source: getNavigationSiteProperties -> e', e);
         });
 };
-
 const ttlValue = () => {
     const properties = getProperties('la-nacion-ar');
     const value = properties.ttlConfig.sectionSource.ttl;
     return value;
 };
-
 /**
  * TODO: Revisar ttl para este contentSource
  */
-
 export default {
     fetch,
     schemaName: 'section-schema',
