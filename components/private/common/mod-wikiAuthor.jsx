7d41e7da5113f81527c99abe7bc5ba3214a6ac27
sromero <sromero@lanacion.com.ar>
2020-11-19 12:50:32 -0300

    Subo cambios en wiki, se cambia el componente image

--- components/private/common/mod-wikiAuthor.jsx
+++ components/private/common/mod-wikiAuthor.jsx
@@ -8,6 +8,7 @@ import ModImage from './mod-image';
 import ComTitle from './com-title';
 import ComParagraph from './com-paragraph';
 import ModDescriptionList from './mod-descriptionList';
+import ModFigure from './mod-figure';
 
 const ModWikiAuthor = props => {
     const { classesNames, classCondition, size } = props;
@@ -19,18 +20,7 @@ const ModWikiAuthor = props => {
         >
             <div className="row">
                 <div className="col-12 col-desksm-4">
-                    <figure role="button" class="mod-figure --horizontal">
-                        <div class="mod-picture ">
-                            <img
-                                decoding="async"
-                                sizes="(max-width: 320px) 320px, 100vw"
-                                alt="ver que onda"
-                                src="https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/nMbuBq0SHLPv9uoOJsHiZBeyoYw=/768x513/smart/filters:quality(70)/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/2TEGAEFZO5B3DLRIP7P47ZTCPY.jpg"
-                                srcset="https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/ezY1Y2EPJ03-B3VE7w9-BkdbE7M=/878x585/smart/filters:quality(70)/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/2TEGAEFZO5B3DLRIP7P47ZTCPY.jpg 878w, https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/6dIOTcXXWdSxIlsyai8ipYO2xng=/1120x746/smart/filters:quality(70)/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/2TEGAEFZO5B3DLRIP7P47ZTCPY.jpg 1120w, https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/ggRLo5On47hTa_NwiK6ro9kBLHo=/768x512/smart/filters:quality(70)/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/2TEGAEFZO5B3DLRIP7P47ZTCPY.jpg 768w, https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/xA7kNFjlRPlM9W2b3WcgWpoMvvg=/350x233/smart/filters:quality(70)/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/2TEGAEFZO5B3DLRIP7P47ZTCPY.jpg 350w, https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/TDfmYuDyT8YmvJSFgN6abplHYMM=/310x206/smart/filters:quality(70)/arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com/public/2TEGAEFZO5B3DLRIP7P47ZTCPY.jpg 310w"
-                                class="i-amphtml-fill-content i-amphtml-replaced-content"
-                            />
-                        </div>
-                    </figure>
+                    <ModFigure />
                 </div>
                 <div className="col-12 col-desksm-8">
                     <ComContainer classCondition="--info">
