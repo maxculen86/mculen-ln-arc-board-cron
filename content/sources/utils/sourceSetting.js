f132e81ae3fd62140f739493349d47ac767e722b
Julio Quintana <jquintana@lanacion.com.ar>
2020-11-19 16:52:41 -0300

    Nuevo ScriptLoader

--- content/sources/utils/sourceSetting.js
+++ content/sources/utils/sourceSetting.js
@@ -1,19 +1,20 @@
 import getProperties from 'fusion:properties';
-import environment from 'fusion:environment';
 import get from '../../../components/private/common/utils/get';
 
-console.log('************ environment', environment);
-
 const getTTLValue = contentSource => {
-    // TODO: buscar forma dinámica de agregar el arcSite.
-    const arcSite = 'la-nacion-ar';
-    const properties = getProperties(arcSite);
-    return (
-        (arcSite &&
-            contentSource &&
-            get(properties, `ttlConfig.${contentSource}`, null)) ||
-        null
-    );
+    try {
+        // TODO: buscar forma dinámica de agregar el arcSite.
+        const arcSite = 'la-nacion-ar';
+        const properties = getProperties(arcSite);
+        return (
+            (arcSite &&
+                contentSource &&
+                get(properties, `ttlConfig.${contentSource}`, null)) ||
+            null
+        );
+    } catch (error) {
+        return 120;
+    }
 };
 
 export default getTTLValue;
