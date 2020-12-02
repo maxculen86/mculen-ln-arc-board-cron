ba68bc5cd592b86ca66f76abe5c07f3963b492a0
Fernando Caíno <pit_fcaino@lnexternos.com.ar>
2020-12-02 10:16:31 -0300

    Se agrega dataLayer en AMP

---
+++ components/private/LN/nota/dataLayer/dataLayerScriptNotaAmp.js
@@ -0,0 +1,72 @@
+/* eslint-disable camelcase */
+import getAuthorByline from '../../../common/utils/getAuthorByline';
+import { Subtypes } from '../../../common/utils/subtypes/subtypeHelper';
+
+const extractDataFromTags = tags => {
+    let keywords = [];
+    if (tags) {
+        keywords = tags.map(tag => tag.description);
+    }
+
+    return { keywords };
+};
+
+const extracDataFromCredits = by => {
+    let authors = [];
+
+    if (by) {
+        authors = by
+            .filter(v => v.type === 'author')
+            .map(author => getAuthorByline(author));
+    }
+    return { authors: authors.length ? authors : ['Redacción LA NACION'] };
+};
+
+const dataLayerScriptNotaAmp = globalContent => {
+    const {
+        headlines,
+        content_restrictions,
+        subtype,
+        _id,
+        first_publish_date: firstPublishDate,
+        taxonomy: { primary_section: primarySection, tags },
+        credits: { by },
+        display_date: displayDate
+    } = globalContent;
+    const valor =
+        (content_restrictions && content_restrictions.content_code) || 'comun';
+    // TODO: por ahora fijo nota, pero en el futuro debe ser dinamico segun sea home, acu o nota
+    const pageType = 'nota';
+    const mySubtype = Subtypes.find(sub => sub.id === subtype);
+    const { name } = primarySection || {};
+    const { authors } = extracDataFromCredits(by);
+    const { keywords } = extractDataFromTags(tags);
+    const title = (headlines && headlines.basic) || 'LA NACION - Noticia';
+
+    return `
+    {
+        "vars" : {
+            "pagetype": "${pageType}",
+            "subtype":"${(mySubtype &&
+                mySubtype.nombre &&
+                mySubtype.nombre.toLowerCase()) ||
+                ''}",
+            "valor": "${valor}",
+            "nota_id": "${_id}",
+            "articleSection": "${name || ''}",
+            "articlePublicationDate": "${new Date(
+                firstPublishDate
+            ).toUTCString() || ''}",
+            "articleAuthor": "${authors}",
+            "articleTags": "${keywords}",
+            "articleTitle": "${title}",
+            "articleLastUpdate": "${new Date(displayDate).toUTCString() || ""}",
+            "credentialType": "",
+            "payUser": "",
+            "usuarioRegistrado": ""
+        }
+    } 
+        `;
+};
+
+export default dataLayerScriptNotaAmp;
