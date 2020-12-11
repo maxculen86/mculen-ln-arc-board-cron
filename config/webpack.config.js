d027f5a2728969f9f97e7a6cdf9e454efbe2e3bf
Diego Anllo <danllo@lanacion.com.ar>
2020-12-11 13:01:36 -0300

    prod por default

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,12 +2,5 @@ const paths = require('./paths');
 const getConfig = require('./getConfig');
 const config = require('./config');
 
-module.exports = (env = 'develop') => {
-    const webpackConfig = config.map(siteConfig =>
-        getConfig(siteConfig, { env, paths })
-    );
-
-    return webpackConfig;
-};
-
-module.exports();
+module.exports = (env = 'prod') =>
+    config.map(siteConfig => getConfig(siteConfig, { env, paths }));
