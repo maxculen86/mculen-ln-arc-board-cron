ec5c2e31755420e9dbe949cb6d21c7f2cf82b4e8
ianzola <vates_ianzola@lnexternos.com.ar>
2021-01-20 14:36:52 -0300

    Snapshot

--- __tests__/components/private/LN/api/nota/article.test.js
+++ __tests__/components/private/LN/api/nota/article.test.js
@@ -1,3 +1,4 @@
+import env from '../../../../../../__mocks__/fusion:environment';
 import Nota from '../../../../../../components/private/LN/api/v1/nota';
 import article from '../../../../../../__mocks__/data/articles/QAZ7BVHG5BCNFN7S67XCBP6PA4.json';
 
