9087f80c7c46ea14e501ee99f07669b8b81b2875
rioyi <wilbert.angarita@gmail.com>
2021-02-22 09:18:05 -0300

    filter liftigniter article without promo item or include placehoderNL

--- content/sources/liftigniterSource.js
+++ content/sources/liftigniterSource.js
@@ -16,19 +16,25 @@ import { addResizedUrls } from '../../components/private/common/utils/image/resi
 import getPresets from './utils/presets';
 
 const transformArticles = (liftigniterArticles = []) =>
-    liftigniterArticles.map(({ url, id, title, image }) => ({
-        subtype: 1,
-        by: {},
-        website_url: url,
-        _id: id,
-        headlines: { basic: title },
-        promo_items: {
-            basic: {
-                type: 'image',
-                url: image
+    liftigniterArticles
+        .filter(
+            e => !(e.image || e.image.includes('/images/placeholderLN.jpg'))
+        )
+        .map(({ url, id, title, image }) => ({
+            subtype: 1,
+            by: {},
+            website_url: url,
+            _id: id,
+            headlines: {
+                basic: title
+            },
+            promo_items: {
+                basic: {
+                    type: 'image',
+                    url: image
+                }
             }
-        }
-    }));
+        }));
 
 /**
  * TODO: Por completar de tarea
