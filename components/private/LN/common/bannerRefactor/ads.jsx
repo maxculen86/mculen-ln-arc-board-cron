bd55ae3fa1532128e6bb79fc2617502f0e07fa52
Julio Quintana <jquintana@lanacion.com.ar>
2021-03-08 18:18:20 -0300

    Actualizacion de nuevo script productivo Rubicon y Arcads

--- components/private/LN/common/bannerRefactor/ads.jsx
+++ components/private/LN/common/bannerRefactor/ads.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/require-default-props */
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import PropTypes from 'fusion:prop-types';
 import useMutationObserver from '../../../common/hooks/useMutationObserver';
 import hasAdsTestParam from '../utils/hasAdsTesParam';
@@ -10,139 +10,158 @@ import ArcAdLib from './arcAdLib';
 
 const bannersLoaded = [];
 
-const Ads = React.memo(
-    props => {
-        const {
-            id,
-            slotName,
-            dimensions,
-            targeting,
-            bidding,
-            sizemap,
-            display,
-            dfpId,
-            children
-        } = props;
-        const [instanced, setInstanced] = useState(() => false);
+const Ads = props => {
+    const {
+        id,
+        slotName,
+        dimensions,
+        targeting,
+        bidding,
+        sizemap,
+        display,
+        dfpId,
+        children
+    } = props;
+    const [instanced, setInstanced] = useState(() => false);
 
-        const onMutate = useCallback(
-            mutations => {
-                mutations.forEach(mutation => {
-                    const nodes = mutation.addedNodes;
-                    nodes.forEach(node => {
-                        const {
-                            nodeId,
-                            style: { width },
-                            localName
-                        } = node;
+    useEffect(() => {
+        googletag;
 
-                        const nodeDimension =
-                            width && parseInt(width.replace('px', ''), 10);
-
-                        if (nodeId === ADHESION_DSK && nodeDimension === 728)
-                            document
-                                .querySelector(`#${id}`)
-                                .parentNode.classList.add('--small');
-
-                        if (localName === 'iframe') {
-                            document
-                                .querySelector(`#${id}`)
-                                .parentNode.classList.remove('hlp-none');
-                        }
-                    });
-                });
-            },
-            [id]
-        );
-
-        useMutationObserver(true, onMutate, id, {
-            subtree: true,
-            childList: true
+        googletag.cmd.push(() => {
+            /* googletag.pubads().enableSingleRequest();
+            googletag.pubads().disableInitialLoad();
+            googletag.enableServices(); */
+            /* console.log(
+                '🚀 ~ file: ads.jsx ~ line 41 ~ googletag.cmd.push ~ dimensions',
+                `/${dfpId}/${slotName}`,
+                dimensions,
+                id
+            ); */
+            // googletag.pubads().collapseEmptyDivs();
+            // console.log('🚀 ~ file: ads.jsx ~ line 25 ~ targeting', targeting);
+            // googletag.pubads().setTargeting
+            // navegg();
+            // console.log("🚀 ~ file: ads.jsx ~ line 70 ~ navegg", navegg)
         });
+    }, [dimensions]);
 
-        if (typeof window === 'undefined') return <></>;
+    const onMutate = useCallback(
+        mutations => {
+            mutations.forEach(mutation => {
+                I;
+                const nodes = mutation.addedNodes;
+                nodes.forEach(node => {
+                    const { nodeId, style, localName } = node;
+                    const { width } = style || {};
 
-        window.arcAdsPrerenderer = adDetails => {
-            return new Promise(resolve => {
-                setInstanced(() => true);
-                const { prebid = {} } = bidding || {};
+                    const nodeDimension =
+                        width && parseInt(width.replace('px', ''), 10);
 
-                if (
-                    Object.keys(prebid || {}).length > 0 &&
-                    typeof pbjs === 'object' &&
-                    Object.keys(pbjs || {}).length > 0 &&
-                    typeof googletag === 'object' &&
-                    Object.keys(googletag || {}).length > 0
-                ) {
-                    googletag.cmd = googletag.cmd || [];
-                    pbjs.que = pbjs.que || [];
+                    if (nodeId === ADHESION_DSK && nodeDimension === 728)
+                        document
+                            .querySelector(`#${id}`)
+                            .parentNode.classList.add('--small');
 
-                    googletag.cmd.push(() => {
-                        console.time();
-                        console.info(
-                            `::: Se cargara un PREBID ::: /${dfpId}/${slotName}_0`
-                        );
-
-                        googletag.pubads().disableInitialLoad();
-                        googletag.pubads().enableSingleRequest();
-                        googletag.enableServices();
-
-                        const callAdserver = gptSlots => {
-                            if (pbjs.adserverCalled) return;
-                            pbjs.adserverCalled = true;
-                            googletag.pubads().refresh(gptSlots);
-                            resolve(adDetails);
-                        };
-
-                        // request pbjs bids when it loads
-                        pbjs.que.push(() => {
-                            pbjs.rp.requestBids({
-                                callback: callAdserver,
-                                gptSlotObjects: [adDetails.adUnit]
-                            });
-                        });
-
-                        // failsafe in case PBJS doesn't load
-                        setTimeout(() => {
-                            callAdserver([adDetails.adUnit]);
-                        }, 3500);
-                        console.timeEnd();
-                    });
-                }
-
-                resolve(adDetails);
+                    if (localName === 'iframe') {
+                        document
+                            .querySelector(`#${id}`)
+                            .parentNode.classList.remove('hlp-none');
+                    }
+                });
             });
-        };
+        },
+        [id]
+    );
 
-        const adInstancer = () => {
-            bannersLoaded.push(`/${dfpId}/${slotName}`);
-            ArcAdLib.getInstance().registerAd(
-                {
-                    id,
-                    slotName,
-                    dimensions,
-                    display,
-                    targeting: { ...targeting, adstest: hasAdsTestParam() },
-                    sizemap,
-                    prerender: window.arcAdsPrerenderer
-                },
-                dfpId,
-                bidding
-            );
-        };
+    useMutationObserver(true, onMutate, id, {
+        subtree: true,
+        childList: true
+    });
 
-        !instanced &&
-            !bannersLoaded.includes(`/${dfpId}/${slotName}`) &&
-            adInstancer();
+    if (typeof window === 'undefined') return <></>;
 
-        return (
-            <div id={id} className="com-banner">
-                <div>{children}</div>
-            </div>
+    window.arcAdsPrerenderer = adDetails => {
+        return new Promise(resolve => {
+            setInstanced(() => true);
+            const { prebid = {} } = bidding || {};
+
+            if (
+                Object.keys(prebid || {}).length > 0 &&
+                typeof pbjs === 'object' &&
+                Object.keys(pbjs || {}).length > 0 &&
+                typeof googletag === 'object' &&
+                Object.keys(googletag || {}).length > 0
+            ) {
+                googletag.cmd = googletag.cmd || [];
+                pbjs.que = pbjs.que || [];
+
+                googletag.cmd.push(() => {
+                    console.time();
+                    console.info(
+                        `::: Se cargara un PREBID ::: /${dfpId}/${slotName}_0`
+                    );
+
+                    googletag.pubads().disableInitialLoad();
+                    googletag.pubads().enableSingleRequest();
+                    googletag.enableServices();
+
+                    const callAdserver = gptSlots => {
+                        if (pbjs.adserverCalled) return;
+                        pbjs.adserverCalled = true;
+
+                        setTimeout(() => {
+                            googletag.pubads().refresh(gptSlots, {
+                                changeCorrelator: false
+                            });
+                        }, 1000);
+                    };
+
+                    // request pbjs bids when it loads
+                    pbjs.que.push(() => {
+                        pbjs.rp.requestBids({
+                            callback: callAdserver,
+                            gptSlotObjects: [adDetails.adUnit]
+                        });
+                    });
+
+                    // failsafe in case PBJS doesn't load
+                    setTimeout(() => {
+                        callAdserver([adDetails.adUnit]);
+                    }, 2500);
+                    console.timeEnd();
+                });
+            }
+
+            resolve(adDetails);
+        });
+    };
+
+    const adInstancer = () => {
+        bannersLoaded.push(`/${dfpId}/${slotName}`);
+        ArcAdLib.getInstance().registerAd(
+            {
+                id,
+                slotName,
+                dimensions,
+                display,
+                targeting: { ...targeting, adstest: hasAdsTestParam() },
+                sizemap,
+                prerender: window.arcAdsPrerenderer
+            },
+            dfpId
         );
-    },
-    (prevProps, nextProps) => prevProps.slotName !== nextProps.slotName
-);
+    };
+
+    !instanced &&
+        !bannersLoaded.includes(`/${dfpId}/${slotName}`) &&
+        adInstancer();
+
+    return (
+        <div id={id} className="com-banner">
+            <div>{children}</div>
+        </div>
+    );
+};
 
 Ads.propTypes = {
     id: PropTypes.string.isRequired,
