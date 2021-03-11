d5152f650f463f12609553f816a55866e6844fbc
Alejandro Sanoja <hsanoja@lanacion.com.ar>
2021-03-11 08:54:52 -0300

    Add related image to snippet

--- components/private/common/snippet/snippetRender.jsx
+++ components/private/common/snippet/snippetRender.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'fusion:prop-types';
+/* import addRelatedImage from "../../LN/common/utils/addRelatedImage" */
 
 const snippetRender = ({ data, id = null }) => {
     const stringData = JSON.stringify(data, null, 2);
+    //console.log(data)
+
     return (
         <script
             id={id}
