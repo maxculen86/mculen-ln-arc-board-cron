6c660ad360e76b198cc40dd6161272e0d4731dbe
Fernando Caíno <pit_fcaino@lnexternos.com.ar>
2021-03-18 17:01:12 -0300

    Se valida que document exista y fix en funcion

--- components/private/LN/common/utils/dynamicallyLoadScript.js
+++ components/private/LN/common/utils/dynamicallyLoadScript.js
@@ -1,7 +1,8 @@
 const dynamicallyLoadScript = (script, section) => {
+    if (!document) return false;
     if (!document.querySelector(`script[src="${script}"]`)) {
         const baseScript = document.createElement('script');
-        baseScript.src = '//www.queryly.com/js/queryly.v4.js';
+        baseScript.src = script;
         document[section].appendChild(baseScript);
         return true;
     }
