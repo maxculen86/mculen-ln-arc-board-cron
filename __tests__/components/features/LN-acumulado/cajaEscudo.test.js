b979e24f602cd52d1b64c8dd6e88ce09ad87a3ca
Alejandro Sanoja <hsanoja@lanacion.com.ar>
2021-03-26 12:07:14 -0300

    Fix shield URL

--- __tests__/components/features/LN-acumulado/cajaEscudo.test.js
+++ __tests__/components/features/LN-acumulado/cajaEscudo.test.js
@@ -19,7 +19,7 @@ describe('Features - LN-acumulado - Caja Escudo Feature =>', () => {
 
         it('should render ModShield component with correctly props', () => {
             const { data, title } = ModShieldComponent.props();
-            const { data: dataMock, title: titleMock } = SHIELD_DATA;
+            const { title: titleMock } = SHIELD_DATA;
 
             expect(data.length).toBe(27);
             expect(title).toStrictEqual(titleMock);
