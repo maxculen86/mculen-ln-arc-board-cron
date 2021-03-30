f5a916b965564ab8ba42fcea6be1c7dd1fd3d270
Alejandro Sanoja <hsanoja@lanacion.com.ar>
2021-03-30 14:58:45 -0300

    Only CallToCommentsButton

--- components/features/LN-nota/callToCommentsButton.jsx
+++ components/features/LN-nota/callToCommentsButton.jsx
@@ -3,18 +3,23 @@ import { useAppContext } from 'fusion:context';
 import { SITE_LANACION } from 'fusion:environment';
 import PropTypes from 'fusion:prop-types';
 import Consumer from 'fusion:consumer';
-import ModCallToComments from '../../private/common/mod-callToComments';
+import ComLink from '../../private/common/com-link';
 
 const CallToCommentsButton = ({ outputType }) => {
     const { globalContent } = useAppContext();
     const toUrl = `${SITE_LANACION}${globalContent.canonical_url}`;
 
     return (
-        <ModCallToComments
-            defaultLink={toUrl}
-            commentsLink={`${toUrl}#comentarios`}
-            outputType={outputType}
-        />
+        (toUrl && outputType === 'amp' && (
+            <section className="mod-commentamp">
+                <ComLink
+                    textname="VER COMENTARIOS"
+                    link={`${toUrl}#comentarios`}
+                />
+                <ComLink link={toUrl} textname="Ir a la nota original" />
+            </section>
+        )) ||
+        null
     );
 };
 
--- components/private/LN/acumulado/author/wikiAuthor.jsx
+++ components/private/LN/acumulado/author/wikiAuthor.jsx
@@ -18,7 +18,6 @@ import ComLink from '../../../common/com-link';
 import ComSubtitle from '../../../common/com-subtitle';
 import getSocialsNetwork from '../../common/utils/getSocialsNetwork';
 import ComAdvance from '../../../common/com-advance';
-import ModCallToComments from '../../../common/mod-callToComments';
 // import ModWikiAuthor from '../../../common/mod-wikiAuthor';
 // import '../../../../../resources/dist/css/ln/base/helpers.css';
 
