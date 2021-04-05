a5f75f74a6562e655c5f306b36849d07c3394335
Alejandro Sanoja <hsanoja@lanacion.com.ar>
2021-04-05 10:22:47 -0300

    New MetaSectionParsely

---
+++ components/private/common/metaSectionParsely.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'fusion:prop-types';
+import CapitalizeFIrstLetter from '../common/utils/capitalizeFirstLetter';
+
+const validateCampo = _id =>
+    _id.includes('/economia/campo') ? 'Campo' : false;
+
+const getContent = id => {
+    const isCampo = validateCampo(id);
+
+    return isCampo
+        ? isCampo
+        : CapitalizeFIrstLetter(
+              id
+                  .slice(1, id.length)
+                  .replace(/[\/]/g, '|')
+                  .split('|')[0]
+          );
+};
+
+const MetaSectionParsely = ({ arcSite, _id = '', taxonomy = '' }) => {
+    if (arcSite !== 'la-nacion-ar') return <></>;
+
+    const content = getContent(taxonomy ? taxonomy.primary_section._id : _id);
+
+    return <meta name="parsely-section" content={content} />;
+};
+
+MetaSectionParsely.propTypes = {
+    arcSite: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired,
+    taxonomy: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
+};
+
+MetaSectionParsely.defaultProps = {
+    _id: '',
+    taxonomy: ''
+};
+
+export default MetaSectionParsely;
