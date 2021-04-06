392dd43042b50bd0637a2a4e3b4fb7893550772e
sromero <sromero@lanacion.com.ar>
2021-04-06 13:24:46 -0300

    tamaño de titulo

--- components/features/LN-acumulado/cajaEscudo.jsx
+++ components/features/LN-acumulado/cajaEscudo.jsx
@@ -157,7 +157,7 @@ const CajaEscudo = () => {
                 key={data}
                 title="Superliga: Todos los clubes"
                 data={data}
-                size="1"
+                size="--1"
             />
         )) ||
         null
