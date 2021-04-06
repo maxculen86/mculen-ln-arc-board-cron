9155b4e5deda3e9b2ae48f93a0c1567e764658af
Julio Quintana <jquintana@lanacion.com.ar>
2021-04-06 09:36:08 -0300

    Nuevo componente para inicializar datadog

---
+++ components/private/common/scriptManager/dataDog.jsx
@@ -0,0 +1,117 @@
+/* eslint-disable react/no-danger */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { DATADOG_CONFIG } from 'fusion:environment';
+import { useAppContext } from 'fusion:context';
+
+function DataDog({ location = 'head' }) {
+    const {
+        clientToken,
+        applicationId,
+        site,
+        forwardErrorsToLogs,
+        sampleRateLog,
+        sampleRateRum,
+        service,
+        env,
+        trackInteractions
+    } = DATADOG_CONFIG;
+    const { deployment: version } = useAppContext();
+
+    const scriptLog = `
+    (function (h, o, u, n, d) {
+        h = h[d] = h[d] || {
+          q: [],
+          onReady: function (c) {
+            h.q.push(c);
+          },
+        };
+        d = o.createElement(u);
+        d.async = 1;
+        d.src = n;
+        n = o.getElementsByTagName(u)[0];
+        n.parentNode.insertBefore(d, n);
+      })(
+        window,
+        document,
+        "script",
+        "https://www.datadoghq-browser-agent.com/datadog-logs.js",
+        "DD_LOGS"
+      );
+      
+      DD_LOGS.onReady(function () {
+        DD_LOGS.init({
+          clientToken: "${clientToken}",
+          site: "${site}",
+          forwardErrorsToLogs: ${forwardErrorsToLogs},
+          sampleRate: ${sampleRateLog},
+          service: "${service}",
+          env: "${env}",
+          version: "${version}",
+        });
+        if ("${env}" !== "prod")
+          console.log(
+            \`Datadog initialized. Version: ${version}, sampleRate: ${sampleRateLog}, env: ${env}\`
+          );
+      });
+    `;
+
+    const scriptRum = `
+    (function (h, o, u, n, d) {
+        h = h[d] = h[d] || {
+          q: [],
+          onReady: function (c) {
+            h.q.push(c);
+          },
+        };
+        d = o.createElement(u);
+        d.async = 1;
+        d.src = n;
+        n = o.getElementsByTagName(u)[0];
+        n.parentNode.insertBefore(d, n);
+      })(
+        window,
+        document,
+        "script",
+        "https://www.datadoghq-browser-agent.com/datadog-rum.js",
+        "DD_RUM"
+      );
+      
+      DD_RUM.onReady(function () {
+        DD_RUM.init({
+          clientToken: "${clientToken}",
+          applicationId: "${applicationId}",
+          site: "${site}",
+          service: "${service}",
+          env: "${env}",
+          version: "${version}",
+          sampleRate: ${sampleRateRum},
+          trackInteractions: ${trackInteractions},
+        });
+        if ("${env}" !== "prod")
+            console.log(
+              \`Datadog RUM initialized. Version: ${version}, sampleRate: ${sampleRateRum}, env: ${env}\`
+            );
+      });`;
+
+    return (
+        location === 'head' && (
+            <>
+                <script
+                    async
+                    type="text/javascript"
+                    dangerouslySetInnerHTML={{ __html: scriptLog }}
+                />
+                <script
+                    async
+                    type="text/javascript"
+                    dangerouslySetInnerHTML={{ __html: scriptRum }}
+                />
+            </>
+        )
+    );
+}
+
+DataDog.propTypes = { location: PropTypes.string.isRequired };
+
+export default DataDog;
