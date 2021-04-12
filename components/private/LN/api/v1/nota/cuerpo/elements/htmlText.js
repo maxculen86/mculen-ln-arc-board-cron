b51d527466a80c8f178f57bc5620b3d2ac82cffa
Sally Flores <bg_sflores@lanacion.com.ar>
2021-04-12 11:01:51 -0300

    cambios para mark

--- components/private/LN/api/v1/nota/cuerpo/elements/htmlText.js
+++ components/private/LN/api/v1/nota/cuerpo/elements/htmlText.js
@@ -43,25 +43,14 @@ const htmlText = (nodo, dataNota) => {
         }
     );
 
-    // walker.addCondition(
-    //     node => node.nodeType === 1 && node.tagName === 'mark',
-    //     (data, next) => {
-    //         const classRegex = new RegExp('class="hl_(.*)"');
-    //         const attrs = classRegex.exec(data.rawAttrs);
-    //         const resp = {
-    //             _t: 'data.tagName',
-    //             color: attrs[1],
-    //             valor: next(data.childNodes)
-    //         };
-    //         return resp;
-    //     }
-    // );
-
     walker.addCondition(
         node => node.nodeType === 1 && node.tagName === 'mark',
         (data, next) => {
+            const classRegex = new RegExp('class="hl_(.*)"');
+            const attrs = classRegex.exec(data.rawAttrs);
             const resp = {
-                _t: 'b',
+                _t: 'mark',
+                color: attrs[1],
                 valor: next(data.childNodes)
             };
             return resp;
