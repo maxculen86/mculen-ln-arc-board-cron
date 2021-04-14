8af6516319378dbc07458ad120f54032fb3ccfa9
Mariaelisa Rodrigues <mrodrigues@lanacion.com.ar>
2021-04-14 11:06:30 -0300

    optimization tagList feature & WithAcuArticlesData

--- components/features/LN-acumulado/tagList.jsx
+++ components/features/LN-acumulado/tagList.jsx
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppContext } from 'fusion:context';
+import { useContent } from 'fusion:content';
 import useGlobalProviderAcu from '../../private/LN/acumulado/hooks/useGlobalProviderAcu';
-import TagsNavigation from '../../private/LN/acumulado/tagsNavigation';
+import ComLinkList from '../../private/common/com-link-list';
 import withStatic from '../../private/common/hocs/withStatic';
-// TODO: Agregar HOC withStatic luego de refactorizar withAcuArticlesData
-const TagsListFeature = props => {
-    const { acumuladoGeneral, acumuladoColor } = useGlobalProviderAcu();
-    const { hidetagslist = 'false' } = acumuladoGeneral;
-    const { navigation_color_tags: colorTags } = acumuladoColor;
-    const {
-        globalContent: { _id: sectionId }
-    } = useAppContext() || {};
+import get from '../../private/common/utils/get';
+import {
+    getOrderAndCountTags,
+    transformTagsForAcu
+} from '../../private/common/utils/tags';
 
-    if (hidetagslist === 'true') return null;
+const TagsListFeature = () => {
+    const { globalContent: { _id: sectionId } = {}, arcSite = 'la-nacion-ar' } =
+        useAppContext() || {};
+
+    const {
+        acumuladoGeneral: { hidetagslist = false } = {},
+        acumuladoColor: { navigation_color_tags: colorTags } = {}
+    } = useGlobalProviderAcu() || {};
+
+    const orderAndCountTags = useContent({
+        sourceName: 'acuArticlesSource',
+        query: {
+            website: arcSite,
+            sectionId,
+            page: 0,
+            promoItemsOnly: false
+        },
+        filter: `{
+            content_elements {
+                taxonomy {
+                    tags {
+                        text
+                        slug
+                    }
+                }
+            }
+        }`,
+        transform: data => {
+            return getOrderAndCountTags(get(data, 'content_elements', []));
+        }
+    });
+
+    const [tagList] = useState(
+        transformTagsForAcu(orderAndCountTags, colorTags)
+    );
 
     return (
-        <TagsNavigation
-            {...props}
-            sectionId={sectionId}
-            colorTags={colorTags}
-            hidetagslist={hidetagslist}
-        />
+        (hidetagslist !== 'true' && tagList && (
+            <ComLinkList list={tagList} extraClass="--tags" />
+        )) ||
+        null
     );
 };
 
