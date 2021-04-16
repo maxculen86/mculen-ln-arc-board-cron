29b2f07eea93bc860da529f28d355ab9d6431c78
Mariaelisa Rodrigues <mrodrigues@lanacion.com.ar>
2021-04-16 10:23:58 -0300

    implementation livefyre comment count library

--- components/output-types/default.jsx
+++ components/output-types/default.jsx
@@ -12,6 +12,7 @@ import PostBid from '../private/common/scriptManager/postbid';
 import FacebookSDK from '../private/common/scriptManager/facebookSDK';
 import MetasOG from '../private/common/metaTags/metasOG';
 // import Livefyre from '../private/common/scriptManager/Livefyre';
+import LivefyreCommentCount from '../private/common/scriptManager/LivefyreCommentCount';
 import LiftIgniter from '../private/common/scriptManager/Liftigniter';
 import Datadog from '../private/common/scriptManager/dataDog';
 import ScriptLoadingList from '../private/common/scriptManager/scriptLoadingList';
@@ -79,6 +80,13 @@ const scriptList = [
     {
         component: { name: 'ScriptHtmlLibre', function: ScriptHtmlLibre },
         feature: 'none'
+    },
+    {
+        component: {
+            name: 'LivefyreCommentCount',
+            function: LivefyreCommentCount
+        },
+        feature: ['LN-nota/share']
     }
 ];
 
--- properties/sites/la-nacion-ar.js
+++ properties/sites/la-nacion-ar.js
@@ -36,6 +36,10 @@ export default {
             props: {},
             location: ['head']
         },
+        LivefyreCommentCount: {
+            props: {},
+            location: ['head']
+        },
         LiftIgniter: {
             props: {},
             location: ['body-top']
