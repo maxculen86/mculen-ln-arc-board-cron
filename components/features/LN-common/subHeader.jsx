99e2b04b7349f07871e1b1a7fce124c4e1094eab
Daro <daguilar@lanacion.com.ar>
2021-05-04 16:03:35 -0300

    fix en nombre subheader

--- components/features/LN-common/subHeader.jsx
+++ components/features/LN-common/subHeader.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useContent } from 'fusion:content';
-import SubHeather from '../../private/LN/common/header/subHeather';
+import Subheader from '../../private/LN/common/header/subHeader';
 
 const SubHeader = () => {
     const { data: dolar } = useContent({ source: 'dolarSource' }) || {};
     const { weather } = useContent({ source: 'weatherSource' }) || {};
 
-    return <SubHeather dolar={dolar} weather={weather} />;
+    return <Subheader dolar={dolar} weather={weather} />;
 };
 
 SubHeader.label = 'LN Subheader Home';
