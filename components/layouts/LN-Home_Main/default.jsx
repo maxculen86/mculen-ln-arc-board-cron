8015c827ded6dbbf066706d1146ace2a1f10669c
Fernando Caíno <pit_fcaino@lnexternos.com.ar>
2021-05-13 09:30:56 -0300

    se agregan validaciones para cada section en la home

--- components/layouts/LN-Home_Main/default.jsx
+++ components/layouts/LN-Home_Main/default.jsx
@@ -19,27 +19,27 @@ import validateSectionHome from '../../private/common/utils/validateSectionHome'
 
 const pageBuilderSections = [
     'Anticipo',
-    'Anexo-1',
+    'Anexo_1',
     'Bomba',
     'Apertura',
-    'Anexo-2',
-    'Breaking-1',
-    'Breaking-2',
-    'Breaking-3',
-    'Anexo-3',
+    'Anexo_2',
+    'Breaking_1',
+    'Breaking_2',
+    'Breaking_3',
+    'Anexo_3',
     'Opinion',
-    'Breaking-4',
-    'Breaking-5',
-    'Breaking-6',
-    'Comercial-1',
-    'Bloque-2',
-    'Comercial-2',
-    'Bloque-3',
-    'Bloque-4',
-    'Bloque-5',
-    'Bloque-6',
-    'Bloque-7',
-    'Bloque-8'
+    'Breaking_4',
+    'Breaking_5',
+    'Breaking_6',
+    'Comercial_1',
+    'Bloque_2',
+    'Comercial_2',
+    'Bloque_3',
+    'Bloque_4',
+    'Bloque_5',
+    'Bloque_6',
+    'Bloque_7',
+    'Bloque_8'
 ];
 
 const reducer = (state, action) => {
@@ -109,39 +109,42 @@ const sectionsWithBlocks = {
 };
 
 const LNMainHome = props => {
-    const {
-        children: [
-            anticipo,
-            anexo1,
-            bomba,
-            apertura,
-            anexo2,
-            breaking1,
-            breaking2,
-            breaking3,
-            anexo3,
-            opinion,
-            breaking4,
-            breaking5,
-            breaking6,
-            comercial1,
-            bloque2,
-            comercial2,
-            bloque3,
-            bloque4,
-            bloque5,
-            bloque6,
-            bloque7,
-            bloque8
-        ],
-        outputType,
-        tree,
-        isAdmin,
-        renderables
-    } = props;
-        console.log("🚀 ~ file: default.jsx ~ line 142 ~ tree", tree)
-    // const megatop = getBannerMegatop(bannerMegatop, outputType, tree, isAdmin);
-    const anticipoValidated = validateSectionHome(anticipo, 0, renderables, isAdmin);
+    const { children, outputType, isAdmin, renderables } = props;
+
+    const [
+        anticipo,
+        anexo1,
+        bomba,
+        apertura,
+        anexo2,
+        breaking1,
+        breaking2,
+        breaking3,
+        anexo3,
+        opinion,
+        breaking4,
+        breaking5,
+        breaking6,
+        comercial1,
+        bloque2,
+        comercial2,
+        bloque3,
+        bloque4,
+        bloque5,
+        bloque6,
+        bloque7,
+        bloque8
+    ] = pageBuilderSections.map((section, index) => {
+        return validateSectionHome(
+            children[index],
+            section,
+            index,
+            renderables,
+            outputType,
+            isAdmin
+        );
+    });
+
     const [blocksToLoad, dispatch] = useReducer(reducer, {
         bloque1: true,
         bloque2: isAdmin,
@@ -377,8 +380,8 @@ const LNMainHome = props => {
 
 LNMainHome.propTypes = {
     children: PropTypes.node.isRequired,
+    renderables: PropTypes.node.isRequired,
     outputType: PropTypes.string.isRequired,
-    tree: PropTypes.shape(PropTypes.arrayOf(PropTypes.node)).isRequired,
     isAdmin: PropTypes.bool.isRequired,
     globalContent: PropTypes.shape({
         style: PropTypes.shape({
--- components/private/common/utils/validateSectionHome.js
+++ components/private/common/utils/validateSectionHome.js
@@ -1,12 +1,7 @@
 import React from 'react';
+import sectionsValidation from '../../../layouts/config/LN-Home.config';
 import PageBuilderMessage from '../../LN/home/common/components/pageBuilderMessage/pageBuilderMessage';
-
-const validationsConfig = {
-    anticipo: {
-        quantity: 1,
-        type: ['LN-common/cajaAnticipo']
-    }
-};
+import get from './get';
 
 const findSectionChildren = (renderables, position) => {
     const sectionFinded = renderables.find(
@@ -15,27 +10,50 @@ const findSectionChildren = (renderables, position) => {
     return sectionFinded.children || [];
 };
 
-const validateSectionHome = (section, position, renderables, isAdmin) => {
-    console.log(
-        '🚀 ~ file: validateSectionHome.js ~ line 24 ~ validateSectionHome ~ renderables',
-        renderables
+const checkIfValid = (name, children) => {
+    const childrenWithoutHide = children.filter(
+        child => get(child, 'props.customFields.hideCaja', false) !== true
     );
+    const sectionRule = sectionsValidation[name] || {};
+    if (sectionRule.quantity !== childrenWithoutHide.length)
+        return `solo permite ${sectionRule.quantity} componente`;
+
+    if (
+        !childrenWithoutHide.every(component =>
+            sectionRule.types.includes(component.type)
+        )
+    )
+        return `solo permite componentes del tipo ${sectionRule.types.join(
+            ','
+        )}`;
+
+    return true;
+};
+
+const validateSectionHome = (
+    section,
+    name,
+    position,
+    renderables,
+    outputType,
+    isAdmin
+) => {
     const sectionChildren = findSectionChildren(renderables, position);
-    // const { children } = tree;
-    // children[0] => Section BannerMegatop
-    // const { children: childrenSectionBannerMegatop } = children[0];
-    const isValid = false;
-    const component = isValid ? (
-        section
-    ) : (
-        <PageBuilderMessage
-            id="LN-nota-noticia-error"
-            type="warning"
-            message="La sección BannerMegatop solo permite un banner y no se mostrará en salida AMP"
-        />
-    );
+    const message = checkIfValid(name, sectionChildren);
+
+    if (outputType === 'json') return 'TODO';
+    const component =
+        message === true ? (
+            section
+        ) : (
+            <PageBuilderMessage
+                id="LN-Home-error"
+                type="warning"
+                message={`La sección ${name} ${message}`}
+            />
+        );
     if (isAdmin) return component;
-    return isValid ? component : null;
+    return message === true ? component : null;
 };
 
 export default validateSectionHome;
