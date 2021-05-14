17330e3c84206376afbd83238cb51e96349f708b
Sally Flores <bg_sflores@lanacion.com.ar>
2021-05-14 14:27:47 -0300

    Adecuacion de Bomba

--- components/features/LN-common/bomba/json.js
+++ components/features/LN-common/bomba/json.js
@@ -1,12 +1,21 @@
 import Consumer from 'fusion:consumer';
+import FeatureArticulo from '../articulo/json';
 
 class BombaFeature {
     constructor(props) {
         this.props = props;
+        this.state = {};
     }
 
     render() {
-        return {};
+        try {
+            return {
+                information: this.props.customFields,
+                articles: new FeatureArticulo(this.props)
+            };
+        } catch (err) {
+            return { Success: false, Message: err.message };
+        }
     }
 }
 
