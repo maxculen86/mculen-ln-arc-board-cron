42b6a3de6a1fa9a6b37dd287f01925dcb23b7dfb
Sally Flores <bg_sflores@lanacion.com.ar>
2021-05-21 14:18:35 -0300

    Adpatacion Bitacora Deportes

--- components/layouts/LN-Home_Sports/json.js
+++ components/layouts/LN-Home_Sports/json.js
@@ -1,35 +1,11 @@
+import bitacora from '../../private/LN/api/v1/bitacora';
+import Consumer from 'fusion:consumer';
+import home from '../../private/LN/api/v1/home';
+import pageBuilderSections from '../config/LN-PageBuilder.config.json';
+/* 
 const LNMainHome = ({ children }) => {
     const listItems = [];
-    const ArticlesbyBox = [];
-    let posnum = 0;
-    let cajanum = 0;
-
-    children.map(element => {
-        const itemBoxes = element.map(elem => {
-            if (elem && elem.diagramacion_caja) {
-                cajanum += 1;
-                posnum = 0;
-
-                const subChild = elem.notas.map(item => {
-                    posnum += 1;
-                    return {
-                        ...item,
-                        posicion: `${String(posnum).padStart(2, '0')}`
-                    };
-                });
-                const result = {
-                    ...elem,
-                    id_caja: `${String(cajanum).padStart(2, '0')}`,
-                    notas: subChild
-                };
-                ArticlesbyBox.push(result);
-                return result;
-            }
-            return elem;
-        });
-
-        return itemBoxes;
-    });
+    const ArticlesbyBox = bitacora(children);
 
     listItems.push({
         // fecha_foto: dateToday, //Data pendiente de añadir
@@ -50,3 +26,61 @@ LNMainHome.sections = [
 ];
 
 export default LNMainHome;
+ */
+
+import {
+    checkIfValid,
+    findSectionChildren
+} from '../../private/common/utils/validateSectionHome';
+
+const homeMobileSections = [
+    'Banner-Megatop',
+    'Sticky-Mobile',
+    'Cabezal',
+    'Apertura',
+    'Cuerpo',
+    'Aside'
+];
+
+const validateSections = (section, name, position, renderables) => {
+    const sectionChildren = findSectionChildren(renderables, position);
+    const result =
+        checkIfValid(name, sectionChildren) === true ? section : null;
+    return result;
+};
+
+const getHomeElements = props => {
+    const { children, renderables, arcSite } = props;
+    const configurations = {
+        arcSite
+    };
+    return pageBuilderSections.reduce((r, e, i) => {
+        const child = children[i];
+        if (child && Array.isArray(child) && child.length > 0) {
+            return r.concat(
+                [].concat(
+                    child
+                        .filter(
+                            b => b && b.information && !b.information.hideCaja
+                        )
+                        .map(b => {
+                            const addedInfo = { ...b, configurations };
+                            return {
+                                feature: homeMobileSections[i],
+                                ...addedInfo
+                            };
+                        })
+                ) || []
+            );
+        }
+        return r;
+    }, []);
+};
+
+const LNMainHome = props => {
+    const homeSections = getHomeElements(props);
+    // return [homeSections];
+    return bitacora(homeSections) || null;
+};
+
+export default Consumer(LNMainHome);
