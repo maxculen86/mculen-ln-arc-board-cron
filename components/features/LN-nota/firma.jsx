5e81cc82f28c49624f6a081df9a31913fd47d78e
Alejandro Sanoja <hsanoja@lanacion.com.ar>
2021-06-04 13:53:33 -0300

    Change author link source

--- components/features/LN-nota/firma.jsx
+++ components/features/LN-nota/firma.jsx
@@ -40,18 +40,17 @@ const getPropsBuilderFromContentElements = position => contentElements =>
               );
 
 const getPropsBuilder = position => authors =>
-    authors
-        .map(author => ({
+    authors.map(author =>
+        ({
             name:
                 get(author, 'additional_properties.original.author_type') === ''
                     ? get(author, 'name')
                     : get(author, 'additional_properties.original.byline') ||
                       get(author, 'name'),
-            link: get(author, 'additional_properties.original.bio_page'),
+            link: `/pf/autor/${get(author, 'name')}/`,
             photo: get(author, 'additional_properties.original.image'),
             medio: get(author, 'additional_properties.original.role')
-        }))
-        .reduce(
+        }.reduce(
             (accumulator, value) => {
                 const props = {
                     ...accumulator,
@@ -80,7 +79,8 @@ const getPropsBuilder = position => authors =>
                 return props;
             },
             { authors: [], photo: null, medio: null, smth: 'credits' }
-        );
+        ))
+    );
 
 const FirmaFeature = props => {
     const {
