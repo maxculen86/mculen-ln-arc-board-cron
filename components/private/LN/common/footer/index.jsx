7d50186eccb42b50d80b2dfb1fb1cce47b4ad49d
Daro <daguilar@lanacion.com.ar>
2021-06-10 13:18:11 -0300

    ajusto textos footer

--- components/private/LN/common/footer/index.jsx
+++ components/private/LN/common/footer/index.jsx
@@ -23,43 +23,101 @@ const Index = ({
     const listText = [
         {
             text: 'Últimas noticias',
+            alt: 'Ir a Últimas noticias',
             href: 'https://www.lanacion.com.ar/ultimas-noticias/'
         },
-        { text: 'Política', href: 'https://www.lanacion.com.ar/politica/' },
-        { text: 'Economía', href: 'https://www.lanacion.com.ar/economia/' },
-        { text: 'El mundo', href: 'https://www.lanacion.com.ar/el-mundo/' },
-        { text: 'Sociedad', href: 'https://www.lanacion.com.ar/sociedad/' },
-        { text: 'Opinión', href: 'https://www.lanacion.com.ar/opinion/' },
-        { text: 'Deportes', href: 'https://www.lanacion.com.ar/deportes/' },
-        { text: 'Lifestyle', href: 'https://www.lanacion.com.ar/lifestyle/' },
+        {
+            text: 'Política',
+            alt: 'Ir a Política',
+            href: 'https://www.lanacion.com.ar/politica/'
+        },
+        {
+            text: 'Economía',
+            alt: 'Ir a Economía',
+            href: 'https://www.lanacion.com.ar/economia/'
+        },
+        {
+            text: 'El mundo',
+            alt: 'Ir a El mundo',
+            href: 'https://www.lanacion.com.ar/el-mundo/'
+        },
+        {
+            text: 'Sociedad',
+            alt: 'Ir a Sociedad',
+            href: 'https://www.lanacion.com.ar/sociedad/'
+        },
+        {
+            text: 'Opinión',
+            alt: 'Ir a Opinión',
+            href: 'https://www.lanacion.com.ar/opinion/'
+        },
+        {
+            text: 'Deportes',
+            alt: 'Ir a Deportes',
+            href: 'https://www.lanacion.com.ar/deportes/'
+        },
+        {
+            text: 'Lifestyle',
+            alt: 'Ir a Lifestyle',
+            href: 'https://www.lanacion.com.ar/lifestyle/'
+        },
         {
             text: 'Espectáculos',
+            alt: 'Ir a Espectáculos',
             href: 'https://www.lanacion.com.ar/espectaculos/'
         },
         {
             text: 'Edición impresa',
+            alt: 'Ir a Edición impresa',
             href: 'https://www.lanacion.com.ar/edicion-impresa/'
         },
-        { text: 'LN+', href: 'https://lnmas.lanacion.com.ar/' },
-        { text: 'Club LA NACION', href: 'https://club.lanacion.com.ar/' }
+        {
+            text: 'LN+',
+            alt: 'Ir a LN+',
+            href: 'https://lnmas.lanacion.com.ar/'
+        },
+        {
+            text: 'Club LA NACION',
+            alt: 'Ir a Club LA NACION',
+            href: 'https://club.lanacion.com.ar/'
+        }
     ];
 
     const listText1 = [
         { text: 'Revistas' },
         {
             text: 'OHLALÁ!',
+            alt: 'Ir a Revista OHLALÁ',
             href: 'https://www.lanacion.com.ar/revista-ohlala/'
         },
-        { text: '¡HOLA!', href: 'https://www.lanacion.com.ar/revista-hola/' },
+        {
+            text: '¡HOLA!',
+            alt: 'Ir a Revista HOLA',
+            href: 'https://www.lanacion.com.ar/revista-hola/'
+        },
         {
             text: 'ROLLING STONE',
+            alt: 'Ir a Revista Rolling Stone',
             href: 'https://www.lanacion.com.ar/revista-rolling-stone/'
         },
-        { text: 'LIVING', href: 'https://www.lanacion.com.ar/revista-living/' },
-        { text: 'BRANDO', href: 'https://www.lanacion.com.ar/revista-brando/' },
-        { text: 'JARDÍN', href: 'https://www.lanacion.com.ar/revista-jardin/' },
+        {
+            text: 'LIVING',
+            alt: 'Ir a Revista Living',
+            href: 'https://www.lanacion.com.ar/revista-living/'
+        },
+        {
+            text: 'BRANDO',
+            alt: 'Ir a Revista Brando',
+            href: 'https://www.lanacion.com.ar/revista-brando/'
+        },
+        {
+            text: 'JARDÍN',
+            alt: 'Ir a Revista Jardín',
+            href: 'https://www.lanacion.com.ar/revista-jardin/'
+        },
         {
             text: 'LUGARES',
+            alt: 'Ir a Revista Lugares',
             href: 'https://www.lanacion.com.ar/revista-lugares/'
         }
     ];
@@ -68,6 +126,7 @@ const Index = ({
         { text: 'Club del vino:' },
         {
             text: 'Bon vivir',
+            alt: 'Ir a tienda online de Bonvivir',
             href: 'https://www.bonvivir.com/',
             target: '_blank'
         }
@@ -77,23 +136,32 @@ const Index = ({
         { text: 'Envíos:' },
         {
             text: 'HOP',
+            alt: 'Ir a página de envíos de HOP',
             href: 'https://www.hopenvios.com.ar/',
             target: '_blank'
         },
-        { text: 'Colecciones', href: 'https://colecciones.lanacion.com.ar/' },
+        {
+            text: 'Colecciones',
+            alt: 'Ir a tienda online de Colecciones LA NACION',
+            href: 'https://colecciones.lanacion.com.ar/',
+            target: '_blank'
+        },
         {
             text: 'Máster en periodismo',
+            alt: 'Ir a la Maestría en periodismo',
             href:
                 'https://www.utdt.edu/ver_contenido.php?id_contenido=1111&id_item_menu=2327',
             target: '_blank'
         },
         {
             text: 'Fundación LA NACION',
+            alt: 'Ir a la Fundación LA NACION',
             href: 'https://fundacionlanacion.org.ar/',
             target: '_blank'
         },
         {
             text: 'Avisos solidarios',
+            alt: 'Ir a los clasificados solidarios',
             href: 'https://solidarios.lanacion.com.ar/',
             target: '_blank'
         }
@@ -102,20 +170,28 @@ const Index = ({
     const listText3 = [
         {
             text: 'Mapa del sitio',
+            alt: 'Ir al mapa del sitio de LA NACION',
             href: 'https://www.lanacion.com.ar/mapa-del-sitio/'
         },
-        { text: 'Ayuda', href: 'https://micuenta.lanacion.com.ar/ayuda/' },
+        {
+            text: 'Ayuda',
+            alt: 'Ir a las preguntas frecuentes',
+            href: 'https://micuenta.lanacion.com.ar/ayuda/'
+        },
         {
             text: 'Términos y condiciones',
+            alt: 'Ir a los términos y condiciones',
             href: 'https://micuenta.lanacion.com.ar/tyc/'
         },
         {
             text: '¿Cómo anunciar?',
+            alt: 'Cómo anunciar en el sitio de LA NACION',
             href: 'https://www.lanacion.in/',
             target: '_blank'
         },
         {
             text: 'Suscribirse al diario impreso',
+            alt: 'Ir a las suscripciones al diario',
             href: 'https://suscripciones.lanacion.com.ar/suscribirme/'
         }
     ];
@@ -124,11 +200,13 @@ const Index = ({
         { text: 'Protegido por reCAPTCHA:' },
         {
             text: 'Condiciones',
+            alt: 'Ir a las condiciones de Google',
             href: 'https://policies.google.com/terms?hl=es-419',
             target: '_blank'
         },
         {
             text: 'Privacidad',
+            alt: 'Ir a la privacidad de Google',
             href: 'https://policies.google.com/privacy?hl=es-419',
             target: '_blank'
         }
@@ -149,25 +227,26 @@ const Index = ({
                                 name="facebook-filled"
                                 href="https://www.facebook.com/lanacion"
                                 target="_blank"
-                                title="Facebook"
+                                title="Seguirnos en Facebook"
                             />
                             <Icon
                                 name="twitter-filled"
                                 href="https://twitter.com/LANACION"
                                 target="_blank"
-                                title="Twitter"
+                                title="Seguirnos en Twitter"
                             />
                             <Icon
                                 name="instagram"
-                                href="https://www.instagram.com/lanacioncom/"
+                                href="https://www.instagram.com/lanacioncom"
                                 target="_blank"
-                                title="Instagram"
+                                title="Seguirnos en Instagram"
                             />
                             <Icon
                                 name="rss"
-                                href="https://servicios.lanacion.com.ar/herramientas/rss/ayuda/"
+                                //href="https://servicios.lanacion.com.ar/herramientas/rss/ayuda"
+                                href="https://www.lanacion.com.ar/arc/outboundfeeds/rss/?outputType=xml"
                                 target="_blank"
-                                title="Rss"
+                                title="Ir a Rss"
                             />
                         </div>
                         <div className="col-desksm-4 --center">
@@ -176,7 +255,7 @@ const Index = ({
                                 color
                                 size="--sm"
                                 href="https://www.lanacion.com.ar/"
-                                title="LA NACION"
+                                title="Ir a la página principal"
                             />
                         </div>
                         <div className="col-desksm-4 --right">
@@ -184,7 +263,7 @@ const Index = ({
                                 src={getAssetsPath(contextPath)(deployment)(
                                     'google-play.svg'
                                 )}
-                                alt="Disponible en Google Play"
+                                alt="Descargar nuestra app en Google Play"
                                 width="120"
                                 height="35"
                                 href="https://play.google.com/store/apps/details?id=app.lanacion.activity&hl=es_419"
@@ -195,7 +274,7 @@ const Index = ({
                                 src={getAssetsPath(contextPath)(deployment)(
                                     'app-store.svg'
                                 )}
-                                alt="Consíguelo en el App Store"
+                                alt="Descargar nuestra app en el App Store"
                                 width="120"
                                 height="35"
                                 href="https://apps.apple.com/ar/app/la-nacion/id410689702"
