6908b881851db495b906d8e6cbe74da858ca22fb
Mariaelisa Rodrigues <mrodrigues@lanacion.com.ar>
2021-06-15 12:12:39 -0300

    wrapping Footer in Fusion's Static component

--- components/layouts/LN-Home_Main/default.jsx
+++ components/layouts/LN-Home_Main/default.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
 import Consumer from 'fusion:consumer';
+import Static from 'fusion:static';
 import get from '../../private/common/utils/get';
 import throttle from '../../private/common/utils/throttle';
 import Header from '../../private/LN/common/header';
@@ -606,7 +607,9 @@ const LNMainHome = props => {
                             </div>
                         </div>
                     </main>
-                    <Footer />
+                    <Static id="StaticFooter">
+                        <Footer />
+                    </Static>
                 </div>
                 <LoadBanners blocksBanners={blocksBanners.bloque1} />
                 {blocksToLoad.bloque2 && (
