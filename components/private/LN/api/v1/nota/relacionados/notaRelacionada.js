ca00160f2dc8b3451bb80d43c4a9a0e7ce1deac7
Sally Flores <bg_sflores@lanacion.com.ar>
2021-07-22 10:14:18 -0300

    Postegar cambio para validar match

--- components/private/LN/api/v1/nota/relacionados/notaRelacionada.js
+++ components/private/LN/api/v1/nota/relacionados/notaRelacionada.js
@@ -14,8 +14,9 @@ const notaRelacionada = dataNota => {
     const resp = {
         id,
         titulo,
-        url: websiteUrl || canonicalUrl,
-        enviarApp: !matchObject(dataNota)
+        url: websiteUrl || canonicalUrl
+        // TODO: pendiente agregar validacion para match
+        //enviarApp: !matchObject(dataNota)
     };
 
     const volanta = get(dataNota, 'label.volanta');
