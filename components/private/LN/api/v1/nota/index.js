1afd393d488e37efb4fcd56c9463b068b0fcb863
Matias Velasquez <asap_mvelasquez@lnexternos.com.ar>
2021-08-12 14:42:53 -0300

    merge

--- components/private/LN/api/v1/nota/index.js
+++ components/private/LN/api/v1/nota/index.js
@@ -6,6 +6,7 @@ import Relacionados from './relacionados';
 import dateAndTimeUtil from '../../../../common/utils/dateAndTimeUtil';
 import { getPrincipalCategory } from '../../common/category';
 import { removeEmptyItems } from '../../common/utils/responseCleaner';
+import matchObject from '../../common/utils/matchObject';
 
 const indexNota = dataNota => {
     if (!dataNota) throw new Error(`La información de la nota esta vacia`);
