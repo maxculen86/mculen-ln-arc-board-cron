1b80468734f17cdb6dcf3900c5905eb3dac7589a
Daro <daguilar@lanacion.com.ar>
2021-08-13 08:51:41 -0300

    fix window undefined

--- components/private/LN/common/header/headerDesktop.jsx
+++ components/private/LN/common/header/headerDesktop.jsx
@@ -134,7 +134,7 @@ const HeaderDesktop = ({
                     id="user-menu"
                     className={`com-usuario${active}${loadingUserData}`}
                 >
-                    {!loginData.subscription && (
+                    {!loginData.subscription && typeof window !== 'undefined' && (
                         // <button
                         //     className="com-button --special"
                         //     id="btnsuscribite"
