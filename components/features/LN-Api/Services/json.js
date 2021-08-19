dbad58e87c0fed970b394a3188feeeaa15f30e33
Matias Velasquez <asap_mvelasquez@lnexternos.com.ar>
2021-08-19 14:54:44 -0300

    endpoint  services v1

---
+++ components/features/LN-Api/Services/json.js
@@ -0,0 +1,30 @@
+import Consumer from 'fusion:consumer';
+//import IndexNotaV1 from '../../../private/LN/api/v1/nota';
+import browser from '../../../private/common/utils/browser';
+
+class Story {
+    constructor(props) {
+        this.props = props;
+
+        // Responde al resolver que permite pasar las versiones existentes
+        // Regex actual: ^/api/v([1]+)/notas/byId/(.+)/$
+        /*this.versions = {
+            1: IndexNotaV1
+        };*/
+    }
+
+    render() {
+        const indexNota = this.versions[
+            browser.getApiVersion(this.props.requestUri)
+        ];
+        const { globalContent } = this.props;
+        try {
+            return globalContent;
+            //return indexNota(globalContent);
+        } catch (err) {
+            return { Success: false, Message: err.message };
+        }
+    }
+}
+
+export default Consumer(Story);
