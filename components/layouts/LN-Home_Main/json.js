42ab43f5347f99a08f24aae16a6b24f40735d8e1
Matias Velasquez <asap_mvelasquez@lnexternos.com.ar>
2021-08-19 18:02:27 -0300

    cambios en resp acu v2

--- components/layouts/LN-Home_Main/json.js
+++ components/layouts/LN-Home_Main/json.js
@@ -1,6 +1,5 @@
 import Consumer from 'fusion:consumer';
 import home from '../../private/LN/api/v1/home';
-//import home from '../../private/LN/api/common/article/index';
 import pageBuilderSections from '../config/LN-PageBuilder.config.json';
 
 import {
