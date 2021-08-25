62cd688f420ca46c3144149f831cb64cf7c94cd8
Daro <daguilar@lanacion.com.ar>
2021-08-25 12:29:57 -0300

    cambio de prop en text receta

--- components/private/common/listItems.jsx
+++ components/private/common/listItems.jsx
@@ -19,7 +19,7 @@ const TypeList = ({ ol, children }) =>
 const ListItemsFactory = ({ list, titleList, listNumeric }) => {
     return (
         <div>
-            <Text size="2xs" weight="bold" tag="h4" content={titleList} />
+            <Text size="2xs" weight="bold" tag="h4" text={titleList} />
             <TypeList ol={listNumeric}>
                 {list.map((item, key) => (
                     <li key={key} className="com-item">
