7416e1120224f5b8e5d8ee620e15b62a47e3c36c
Daro <daguilar@lanacion.com.ar>
2021-09-07 12:58:05 -0300

    modulo video

--- components/private/common/mod-video.jsx
+++ components/private/common/mod-video.jsx
@@ -2,20 +2,28 @@ import React from 'react';
 import PropTypes from 'fusion:prop-types';
 
 import ComSource from './com-source';
-
 //import '../../../resources/dist/css/ln/modules/mod-video.css';
 
 const ModVideo = props => {
-    const { image, video } = props;
+    const {
+        image,
+        video,
+        autoplay,
+        controls,
+        muted,
+        loop,
+        playsinline
+    } = props;
     if (!video) return null;
 
     return (
         <video
-            loop="true"
-            autoplay="autoplay"
             className="mod-video"
-            muted="true"
-            playsinline="true"
+            loop={loop}
+            autoplay={autoplay}
+            controls={controls}
+            muted={muted}
+            playsinline={playsinline}
             poster={image}
         >
             <ComSource src={video} type="video/mp4" />
@@ -24,11 +32,23 @@ const ModVideo = props => {
 };
 
 ModVideo.propTypes = {
-    src: PropTypes.string.isRequired,
-    srcset: PropTypes.string.isRequired,
-    media: PropTypes.string,
-    alt: PropTypes.string,
-    classCondition: PropTypes.string
+    image: PropTypes.string,
+    video: PropTypes.string.isRequired,
+    autoplay: PropTypes.bool,
+    controls: PropTypes.bool,
+    muted: PropTypes.string,
+    playsinline: PropTypes.bool,
+    loop: PropTypes.bool
+};
+
+ModVideo.defaultProps = {
+    image: undefined,
+    video: undefined,
+    autoplay: '',
+    controls: undefined,
+    muted: 'muted',
+    playsinline: 'true',
+    loop: 'true'
 };
 
 export default ModVideo;
