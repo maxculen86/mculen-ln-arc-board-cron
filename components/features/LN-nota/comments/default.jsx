2eb95e973a239c14ec168c41fc7ee4592ced9e60
Fernando Caino <fcaino@lanacion.com.ar>
2021-09-07 11:05:30 -0300

    Fix loading comentarios

--- components/features/LN-nota/comments/default.jsx
+++ components/features/LN-nota/comments/default.jsx
@@ -13,9 +13,18 @@ const CommentsFeature = props => {
         globalContent: { comments }
     } = props;
     const displayComments = get(comments, 'display_comments', true);
+    console.log(
+        '🚀 ~ file: default.jsx ~ line 16 ~ displayComments',
+        displayComments
+    );
     const [isReady, setIsReady] = useState(false);
+    console.log('🚀 ~ file: default.jsx ~ line 18 ~ isReady', isReady);
     // const [isLoading, setIsLoading] = useState(false);
     const showLivefyre = findTermica('livefyre');
+    console.log(
+        '🚀 ~ file: default.jsx ~ line 19 ~ showLivefyre',
+        showLivefyre
+    );
 
     useEffect(() => {
         const handleScrollForComments = () => {
@@ -46,10 +55,8 @@ const CommentsFeature = props => {
             window.removeEventListener('scroll', handleScrollForComments);
     });
 
-    const loading = display => (!display ? <></> : <LoadingIcon />);
-    if (!isReady || !showLivefyre || !displayComments)
-        return loading(displayComments);
-    // if (!isReady) return <LoadingIcon />;
+    if (!showLivefyre || !displayComments) return <></>;
+    if (!isReady) return <LoadingIcon />;
 
     return <Comments {...props} />;
 };
