0419159dae93d366f63eb1ccbafa24522d4a6ffd
Mariaelisa Rodrigues <mrodrigues@lanacion.com.ar>
2021-10-22 16:56:51 -0300

    remove shouldLoadViafouraSSR from commentsHelper

--- components/private/common/utils/commentsHelper.js
+++ components/private/common/utils/commentsHelper.js
@@ -1,6 +1,5 @@
 import { LOGIN_URL, SITIO_SEGURO_REGISTRACION } from 'fusion:environment';
 import { useContext } from 'react';
-import { useContent } from 'fusion:content';
 import { GlobalContext } from '../context/globalContext';
 import findTermica from './findTermica';
 import get from './get';
@@ -29,43 +28,15 @@ export const shouldLoadViafoura = inputDate => {
     );
 };
 
-export const shouldLoadViafouraSSR = props => {
-    const {
-        globalContent: { first_publish_date: firstPublishDate } = {},
-        arcSite: website
-    } = props;
-    return useContent({
-        sourceName: 'navigationTreeSource',
-        query: {
-            website
-        },
-        transform: resp => {
-            const showComments =
-                get(props, 'globalContent.type') === 'story' &&
-                get(props, 'globalContent._id') &&
-                get(props, 'globalContent.comments.display_comments', true) &&
-                get(resp, 'Termicas.livefyre', true);
-            const deadlineLivefyre = get(resp, 'migration.deadline_livefyre');
-            const deadlineDate = deadlineLivefyre && new Date(deadlineLivefyre);
-            const articlePublishDate =
-                firstPublishDate && new Date(firstPublishDate);
-            return (
-                showComments &&
-                deadlineDate &&
-                articlePublishDate &&
-                articlePublishDate.setHours(0, 0, 0, 0) >=
-                    deadlineDate.setHours(0, 0, 0, 0)
-            );
-        }
-    });
-};
-
 export const validateComments = (props, subscription = false) => {
     const allow = get(props, 'globalContent.comments.allow_comments', true);
     const show = get(props, 'globalContent.comments.display_comments', true);
+    const firstPublishDate = get(props, 'globalContent.first_publish_date');
     // const subscription = get(props, 'globalContent.subscription', false);
+    const shouldLoad =
+        allowComments(props) && shouldLoadViafoura(firstPublishDate);
     return {
-        shouldLoad: shouldLoadViafouraSSR(props),
+        shouldLoad,
         allowComments: allow,
         showComments: show,
         messageType:
@@ -105,6 +76,5 @@ export const getMessageProps = (props, messageType) => {
 export default {
     allowComments,
     shouldLoadViafoura,
-    shouldLoadViafouraSSR,
     validateComments
 };
