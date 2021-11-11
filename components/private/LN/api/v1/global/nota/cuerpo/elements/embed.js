99bcbb05a6a7e30ff6be2fd2f3bef9d21b7874ab
Matias Velasquez <asap_mvelasquez@lnexternos.com.ar>
2021-11-11 12:03:18 -0300

    refactor emebed content common

--- components/private/LN/api/v1/global/nota/cuerpo/elements/embed.js
+++ components/private/LN/api/v1/global/nota/cuerpo/elements/embed.js
@@ -1,52 +1,10 @@
-import get from 'lodash.get';
-import getEmbedHref from '../../../../../../../common/utils/getEmbedHref';
+import embedContent from '../../../../common/nota/cuerpo/elements/embed';
 
 const embed = (nodo, dataNota) => {
-    if (!nodo) return null;
-
-    const valor = get(nodo, 'raw_oembed');
-
-    if (!valor.html) return null;
-
-    const resp = {
-        _t: 'ext'
-    };
-
-    switch (valor.type) {
-        case 'tiktok':
-        case 'instagram':
-            resp.id = 'html';
-            resp.src = valor.html;
-            break;
-        case 'twitter':
-            resp.id = 'html';
-            resp.src = `<blockquote class="twitter-tweet" style="min-height: 130px;"><a href = "${valor.url}"></a></blockquote><script async src="https://platform.twitter.com/widgets.js" charset ="utf-8"></script>`;
-            break;
-        case 'facebook-video':
-            resp.id = 'facebook-post';
-            resp.src = getEmbedHref('href', valor.html);
-            break;
-        case 'facebook-post':
-            resp.id = 'facebook-post';
-            resp.src = getEmbedHref('href', valor.html);
-            break;
-        case 'youtube':
-        case 'vimeo':
-        case 'dailymotion':
-        case 'spotify':
-            resp.id = nodo.subtype;
-            resp.src = getEmbedHref('src', valor.html);
-            break;
-        default:
-            return null;
-    }
-
     return {
         _t: 'p',
-        valor: resp
+        valor: embedContent(nodo, dataNota)
     };
 };
 
 embed.type = 'oembed_response';
-
-export default embed;
