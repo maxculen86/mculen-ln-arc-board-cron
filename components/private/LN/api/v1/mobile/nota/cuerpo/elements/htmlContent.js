e62a53e9c2bb57ac3ab335b29c4d23315029f05c
Matias Velasquez <asap_mvelasquez@lnexternos.com.ar>
2021-11-11 11:56:11 -0300

    refactor common element html content

--- components/private/LN/api/v1/mobile/nota/cuerpo/elements/htmlContent.js
+++ components/private/LN/api/v1/mobile/nota/cuerpo/elements/htmlContent.js
@@ -1,34 +1,8 @@
-import { OPTA_WIDGET_URL } from 'fusion:environment';
-import getEmbedHref from '../../../../../../../common/utils/getEmbedHref';
+import htmlContent from '../../../../common/nota/cuerpo/elements/htmlContent';
 
 const html = (nodo, notaId) => {
-    if (!nodo || !nodo.content) return null;
-    const { _id: contentId, content } = nodo;
-    const hrefRegex = new RegExp('(?<=</?)([^ >/]+)');
-    const htmlTag = hrefRegex.exec(nodo.content)[1];
-
-    const resp = {
-        _t: 'ext'
-    };
-
-    switch (htmlTag) {
-        case 'iframe':
-            resp.src = getEmbedHref('src', content).trim();
-            resp.id = 'ifrme';
-            break;
-        case 'opta-widget':
-            resp.src = `${OPTA_WIDGET_URL}/${contentId}/${notaId}/?_website=la-nacion-ar&outputType=opta`;
-            resp.id = 'html';
-            break;
-        default:
-            resp.id = 'html';
-            resp.src = content;
-            break;
-    }
-
-    return resp;
+    return htmlContent(nodo, notaId);
 };
-
 html.type = 'raw_html';
 
 export default html;
