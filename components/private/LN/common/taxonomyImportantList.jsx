4bf643a60d96efd8cf334f8c95ca740ba7ac0884
Barandiaran Lautaro <lbarandiaran@lanacion.com.ar>
2021-11-16 13:19:04 -0300

    estilos de los tags de recetas modificado

--- components/private/LN/common/taxonomyImportantList.jsx
+++ components/private/LN/common/taxonomyImportantList.jsx
@@ -2,43 +2,31 @@ import React from 'react';
 import PropTypes from 'fusion:prop-types';
 
 import '../../../../resources/dist/css/ln/modules/mod-themes.css';
-//import ComButton from '../../common/com-button';
 import ComLink from '../../common/com-link';
-//import ComContainer from '../../common/com-container';
 
 /**
  * Renderiza elementos relacionados destacados (como categorias o tags)
  */
 const TaxonomyImportantList = ({ list, destacado }) => {
+    console.log(
+        '🚀 ~ file: taxonomyImportantList.jsx ~ line 11 ~ TaxonomyImportantList ~ list',
+        list
+    );
+
     return (
         <section className="mod-themes">
-            {/*<div
-            className={`${
-                destacado ? 'com-tag cont_tags' : 'com-secondary-tag'
-            }`}
-        > */}
             {list.map(v => (
-                /*<a
-                    className="com-item"
-                    key={v.text}
-                    href={type === 'tag' ? `/tema/${v.path}/` : `${v.path}/`}
-                >
-                    {v.text}
-                </a> */
-
                 <ComLink
                     link={v.type === 'tag' ? `/tema/${v.path}/` : `${v.path}/`}
                     keytext={v.text}
-                    classCondition="com-button --secondary --compact"
+                    classCondition={
+                        v.type === 'tag'
+                            ? '--tags --twoxs'
+                            : 'com-button --secondary --compact'
+                    }
                     title={`Ir a notas de ${v.text}`}
                 >
-                    {/* <ComButton
-                        classesNames="--secondary"
-                        classCondition="--compact"
-                        size="--fivexs"
-                    > */}
                     {v.text}
-                    {/* </ComButton> */}
                 </ComLink>
             ))}
         </section>
