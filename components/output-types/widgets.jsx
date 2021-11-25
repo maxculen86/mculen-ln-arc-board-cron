4ad8e81bd0ca4da19f5a3993163059a2615801c6
Daro <daguilar@lanacion.com.ar>
2021-11-25 09:09:30 -0300

    agrego espacio y dejo una sola llamada de fontface

--- components/output-types/widgets.jsx
+++ components/output-types/widgets.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'fusion:prop-types';
 import { SITE_LANACION } from 'fusion:environment';
-import FontFace from '../private/common/fontface';
+//import FontFace from '../private/common/fontface';
 
 const TAGS_BY_WIDGET = {
     viafoura: globalContent => {
@@ -31,13 +31,21 @@ const TAGS_BY_WIDGET = {
                         name="viewport"
                         content="width=device-width,initial-scale=1.0,minimum-scale=0.5,maximum-scale=5.0,user-scalable=yes"
                     />,
-                    <FontFace outputType="default" />,
+                    // <FontFace outputType="default" />,
+                    <link
+                        rel="preload"
+                        href="/pf/resources/fonts/suecaslab-bold-webfont.woff2?d=613"
+                        as="font"
+                        crossorigin="anonymous"
+                    />,
                     <style
                         dangerouslySetInnerHTML={{
                             __html: `
                             .widget {
-                                padding: 0 16px;
-                            }`
+                                padding: 32px 16px 0;
+                            }
+                            @font-face {font-family:'SuecaSlab';src:url('/pf/resources/fonts/suecaslab-bold-webfont.woff2?d=613') format('woff2');font-weight: 700;font-style: normal;font-display: swap;}
+                            `
                         }}
                     />
                 ],
