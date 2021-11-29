5c6b84aba5faa035d824f162a700427726ca5662
Daro <daguilar@lanacion.com.ar>
2021-11-29 15:58:19 -0300

    agrego otros svg

--- components/private/common/icons/index.js
+++ components/private/common/icons/index.js
@@ -1,1 +1,2 @@
 export { default as FacebookFilled } from './social/FacebookFilled';
+export { default as InstagramFilled } from './social/InstagramFilled';
--- components/private/common/icons/mapperIcon.jsx
+++ components/private/common/icons/mapperIcon.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import { FacebookFilled } from './index';
+import { FacebookFilled, InstagramFilled } from './index';
 
 export const mapperIcon = {
-    facebook: <FacebookFilled />,
-    'facebook-filled': <FacebookFilled />,
-    'twitter-filled': <FacebookFilled />,
-    'instagram-filled': <FacebookFilled />,
-    instagram: <FacebookFilled />,
-    'whatsapp-filled': <FacebookFilled />,
-    email: <FacebookFilled />,
-    rss: <FacebookFilled />,
-    club: <FacebookFilled />,
-    section: <FacebookFilled />,
-    user: <FacebookFilled />,
-    home: <FacebookFilled />
+    facebook: ({ ...r }) => <FacebookFilled {...r} />,
+    'facebook-filled': ({ ...r }) => <FacebookFilled {...r} />,
+    'twitter-filled': ({ ...r }) => <InstagramFilled {...r} />,
+    'instagram-filled': ({ ...r }) => <InstagramFilled {...r} />,
+    instagram: ({ ...r }) => <FacebookFilled {...r} />,
+    'whatsapp-filled': ({ ...r }) => <FacebookFilled {...r} />,
+    email: ({ ...r }) => <FacebookFilled {...r} />,
+    rss: ({ ...r }) => <FacebookFilled {...r} />,
+    club: ({ ...r }) => <FacebookFilled {...r} />,
+    section: ({ ...r }) => <FacebookFilled {...r} />,
+    user: ({ ...r }) => <FacebookFilled {...r} />,
+    home: ({ ...r }) => <FacebookFilled {...r} />
 };
