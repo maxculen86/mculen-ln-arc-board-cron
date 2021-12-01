af81bdb908045f0190d7e2f5e9a19e4a42b7671b
Matias Velasquez <asap_mvelasquez@lnexternos.com.ar>
2021-12-01 17:35:39 -0300

    unit test api video mobile

--- __tests__/components/private/LN/api/v1/mobile/nota/cuerpo/elements/image.test.js
+++ __tests__/components/private/LN/api/v1/mobile/nota/cuerpo/elements/image.test.js
@@ -10,6 +10,12 @@ describe('Test de json de imagen en el cuerpo de la nota', () => {
     it('Valores del json de imagen', () => {
         const resp = Image(ArticleImage);
         expect(resp['_t']).toBe('image');
+        expect(resp['url']).toBe(
+            '/resizer/{{param}}/cloudfront-us-east-1.images.arcpublishing.com/sandbox.lanacionar/77NRHRWIWFCFDOCDN34LGQ32SE.jpg'
+        );
         expect(resp['parameters']).toHaveLength(5);
+        expect(resp['credits']).toBe('Miguel Acevedo Riu - LA NACION');
+        expect(resp['source']).toBe('LA NACION');
+        expect(resp['epigraph']).toBe('Esto es un epigrafe');
     });
 });
