55ed47a7d9e42ecff23bb470660cc6d1a8a96db3
Matias Velasquez <asap_mvelasquez@lnexternos.com.ar>
2021-12-07 17:57:40 -0300

    Retornar banners en JSON Nota primera version

--- components/private/LN/api/v1/mobile/story/index.js
+++ components/private/LN/api/v1/mobile/story/index.js
@@ -3,7 +3,21 @@ import cuerpo from './cuerpo/index';
 import { removeEmptyItems } from '../../common/utils/responseCleaner';
 
 const indexNota = dataNota => {
-    return removeEmptyItems(indexNotaData(dataNota, cuerpo));
+    const resp = indexNotaData(dataNota, cuerpo);
+    const banners = [
+        { id: 1, name: 'caja' },
+        { id: 2, name: 'caja' },
+        { id: 3, name: 'caja' },
+        { id: 4, name: 'caja' },
+        { id: 5, name: 'caja' }
+    ];
+    resp.contenido.reduce((r, e) => {
+        if (e && Array.isArray(e) && e.length > 0) {
+            return r.concat(e);
+        }
+        return r;
+    }, []);
+    return removeEmptyItems(resp);
 };
 
 export default indexNota;
