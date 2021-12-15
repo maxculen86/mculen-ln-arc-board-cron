bae23a4a287c7225e96ba269ad3631b6c8584195
Matias Velasquez <asap_mvelasquez@lnexternos.com.ar>
2021-12-15 14:59:26 -0300

    hotfix

--- components/private/LN/api/v1/mobile/story/index.js
+++ components/private/LN/api/v1/mobile/story/index.js
@@ -1,9 +1,22 @@
+import get from 'lodash.get';
 import indexNotaData from '../../common/story/indexNotaData';
+import { openComments } from '../../common/story/comments';
 import cuerpo from './cuerpo/index';
 import { removeEmptyItems } from '../../common/utils/responseCleaner';
 
 const indexNota = dataNota => {
-    return removeEmptyItems(indexNotaData(dataNota, cuerpo));
+    const comentariosId = get(dataNota, 'label.livefyre_entrada_id.text', null);
+    const allowComments = get(dataNota, 'comments.allow_comments', null);
+    const id = get(dataNota, '_id', null);
+    const resp = {
+        ...indexNotaData(dataNota, cuerpo),
+        comentarios: {
+            abiertoComentarios: openComments(dataNota),
+            permitirComentarios: allowComments
+        },
+        comentariosId: comentariosId || id
+    };
+    return removeEmptyItems(resp);
 };
 
 export default indexNota;
