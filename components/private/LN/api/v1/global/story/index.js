e423fc807756348d2b96e9254e0e251b07e0c9fc
Matias Velasquez <asap_mvelasquez@lnexternos.com.ar>
2021-12-16 12:25:47 -0300

    refactor common comments api global mobile

--- components/private/LN/api/v1/global/story/index.js
+++ components/private/LN/api/v1/global/story/index.js
@@ -1,23 +1,16 @@
 import get from 'lodash.get';
 import indexNotaData from '../../common/story/indexNotaData';
-import { openComments } from '../../common/story/comments';
 import cuerpo from './cuerpo/index';
 import { removeEmptyItems } from '../../common/utils/responseCleaner';
 
 const indexNota = dataNota => {
     const comentariosId = get(dataNota, 'label.livefyre_entrada_id.text', null);
-    const allowComments = get(dataNota, 'comments.allow_comments', null);
     const id = get(dataNota, '_id', null);
     const resp = {
         ...indexNotaData(dataNota, cuerpo),
         comentariosId: comentariosId || id,
-        abiertoComentarios: false,
-        comentarios: {
-            abiertoComentarios: openComments(dataNota),
-            permitirComentarios: allowComments
-        }
+        abiertoComentarios: false
     };
     return removeEmptyItems(resp);
 };
-
 export default indexNota;
--- components/private/LN/api/v1/mobile/story/index.js
+++ components/private/LN/api/v1/mobile/story/index.js
@@ -1,19 +1,9 @@
-import get from 'lodash.get';
 import indexNotaData from '../../common/story/indexNotaData';
-import { openComments } from '../../common/story/comments';
 import cuerpo from './cuerpo/index';
 import { removeEmptyItems } from '../../common/utils/responseCleaner';
 
 const indexNota = dataNota => {
-    const allowComments = get(dataNota, 'comments.allow_comments', null);
-    const resp = {
-        ...indexNotaData(dataNota, cuerpo),
-        comentarios: {
-            abiertoComentarios: openComments(dataNota),
-            permitirComentarios: allowComments
-        }
-    };
-    return removeEmptyItems(resp);
+    return removeEmptyItems(indexNotaData(dataNota, cuerpo));
 };
 
 export default indexNota;
