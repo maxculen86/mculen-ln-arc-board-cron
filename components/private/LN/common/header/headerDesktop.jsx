de720770013ccb5df60bfc2309495c0980cd48e6
adivianahd <adivianahd07@gmail.com>
2021-12-15 21:33:34 -0300

    WIP - se agregaron los iconos y se realizo una prueba con el componente LogoLN

--- components/private/LN/common/header/headerDesktop.jsx
+++ components/private/LN/common/header/headerDesktop.jsx
@@ -134,7 +134,7 @@ const HeaderDesktop = ({
                     target="_top"
                     title="Ir a la página principal"
                 /> */}
-                    <LogoLN />
+                    <LogoLN name="data-fiscal" />
                 </div>
                 <div className="col-4 header__right">
                     <div
---
+++ components/private/common/logos/mapperLogos.jsx
@@ -0,0 +1,13 @@
+export const mapperLogos = {
+    hola: 'hola.svg',
+    jardin: 'jardin.svg',
+    brando: 'brando.svg',
+    'data-fiscal': 'data-fiscal.svg',
+    living: 'living.svg',
+    lugares: 'lugares.svg',
+    'rolling-stone': 'rolling.svg',
+    ohlala: 'ohlala.svg',
+    propiedades: 'propiedades.svg',
+    campo: 'campo.svg',
+    deportes: 'deportes.svg'
+};
