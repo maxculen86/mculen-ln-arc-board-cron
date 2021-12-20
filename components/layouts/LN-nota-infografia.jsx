81dc2349fdda78061024d03254052cd9a08cbf45
tcabral <tcabral@lanacion.com.ar>
2021-12-20 17:05:40 -0300

    create nota main component for nota noticia and nota infografia, WIP

--- components/layouts/LN-nota-infografia.jsx
+++ components/layouts/LN-nota-infografia.jsx
@@ -2,14 +2,12 @@
 import React from 'react';
 import Consumer from 'fusion:consumer';
 import PropTypes from 'fusion:prop-types';
-import Static from 'fusion:static';
-import Header from '../private/LN/common/header';
-import Footer from '../private/LN/common/footer';
 import GlobalProvider from '../private/common/context/globalContext';
 import { getSectionLogo } from '../private/common/utils/sectionUtils';
 import getBannerMegatop from '../private/common/utils/getBannerMegatop';
 import LoadBannersSSR from '../private/common/banners/LoadBannersSSR';
 import PwaModals from '../private/LN/common/pwaModals';
+import NotaMain from '../private/common/layouts/notaMain';
 
 const lnNotaInfografia = ({
     children,
@@ -30,65 +28,7 @@ const lnNotaInfografia = ({
         <GlobalProvider>
             {bannerMegatop}
             <div id="wrapper" className={`nota --info ${magazine} ${amp}`}>
-                <Header />
-                <main id="content">
-                    {children[1]}
-                    <div className="lay --apertura">
-                        <div className="row">
-                            <div className="col-12">
-                                {/* Titulo (breadcrumb, logo+titulo) */}
-                                {children[2]}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="lay-sidebar">
-                        {/* Cuerpo */}
-                        <div className="sidebar__main">
-                            <div className="row">
-                                <div className="col-12 ">
-                                    {/* Bajada y autor fecha más apertura */}
-                                    {children[3]}
-                                </div>
-                            </div>
-                            <section className="cuerpo__nota">
-                                <div className="row">
-                                    <div className="col-12 col-desksm-1">
-                                        {/* hlp-mobile-show */}
-                                        {/* Left-Cuerpo Shared */}
-                                        {children[4]}
-                                    </div>
-                                    <div className="col-deskxl-10 offset-deskxl-1 col-desksm-11">
-                                        <div className="row">
-                                            <div className="col-12">
-                                                {/* Pos-Apertura */}
-                                                {children[5]}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </section>
-                        </div>
-                        {/* Tercera */}
-                        <div className="sidebar__aside hlp-tabletlm-none">
-                            {children[6]}
-                        </div>
-                    </div>
-                    {/* Newsletter */}
-                    <div className="lay">{children[8]}</div>
-                    <div className="lay-sidebar">
-                        <div className="sidebar__main">
-                            {/* Bottom */}
-                            {children[9]}
-                        </div>
-                        <div className="sidebar__aside hlp-tabletlm-none">
-                            {/* Bottom-Tercera */}
-                            {children[10]}
-                        </div>
-                    </div>
-                </main>
-                <Static id="StaticFooter">
-                    <Footer />
-                </Static>
+                <NotaMain>{children}</NotaMain>
             </div>
             <LoadBannersSSR />
             <PwaModals />
