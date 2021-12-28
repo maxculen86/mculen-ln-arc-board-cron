f713072e6d8e162b4001d1686ab326295a2a34b5
tcabral <tcabral@lanacion.com.ar>
2021-12-28 16:30:33 -0300

    pass green test, mock context, consumer

--- __tests__/components/features/LN-nota/tePuedeInteresar.test.js
+++ __tests__/components/features/LN-nota/tePuedeInteresar.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, shallow } from 'enzyme';
 import TePuedeInteresar from '../../../../components/features/LN-nota/tePuedeInteresar/default';
 import Consumer from 'fusion:consumer';
+import Context from 'fusion:context';
 
 jest.mock('fusion:consumer', Component => {
     return function(Component) {
@@ -9,9 +10,27 @@ jest.mock('fusion:consumer', Component => {
     };
 });
 
+jest.mock('fusion:context', Component => {
+    return function(Component) {
+        return props => <Component {...props} />;
+    };
+});
+
+const requestUri = '';
+
+const globalContent = {};
+
 describe('Te puede interesar default test', () => {
+    const props = {
+        customFields: {}
+    };
+
     it('Matches Snapshot', () => {
-        const wrapper = render(<TePuedeInteresar />);
+        Context.useAppContext = jest.fn(() => ({
+            globalContent,
+            requestUri
+        }));
+        const component = shallow(<TePuedeInteresar {...props} />);
         expect(component).toMatchSnapshot();
     });
 });
