f758cd8fb8bc7f18a1f2a09584cd44dc3d95872c
adivianahd <adivianahd07@gmail.com>
2022-01-10 08:24:35 -0300

    se borraron lineas comentadas innecesarias

--- components/private/common/ampIndex.jsx
+++ components/private/common/ampIndex.jsx
@@ -10,7 +10,6 @@ import {
     config
 } from './utils/scripts/amp/helper';
 import getOembedScripts from './scriptManager/getOembedScripts';
-//import { getStyleFontsInLine } from './fontface';
 import { CriticalCSSString } from './criticalcss';
 import get from './utils/get';
 
