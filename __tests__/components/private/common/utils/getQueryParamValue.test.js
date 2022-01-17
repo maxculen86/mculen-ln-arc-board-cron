2186efe608c0a90eb9d26a18f7b0ce4aa49e4273
Juan Carlos Diaz <jcDiaz@lanacion.com.ar>
2022-01-17 12:46:02 -0300

    Refactor del title y meta desciption en outputType default.
    - Se agrego la logica del title y meta description en un archivo aparte.
    - Se agrego el caso de adstest en el util getQueryParamValue.
    - Se removio el util hasAdstesParam.
    - Se agrego el nuevo caso de uso en el test del util getQueryParamValue.
    - Se crearon los test correspondientes para el outputTypeHelper.

--- __tests__/components/private/common/utils/getQueryParamValue.test.js
+++ __tests__/components/private/common/utils/getQueryParamValue.test.js
@@ -5,8 +5,24 @@ describe('Common - utils - getQueryParamValue', () => {
         'https://www.lanacion.com.ar/buscador/?query=cristina&_website=la-nacion-ar';
     const nameQueryParam = 'query';
 
+    const siteUrl2 =
+        'https://www.lanacion.com.ar/politica/?adstest=true&_website=la-nacion-ar';
+    const siteUrl3 =
+        'https://www.lanacion.com.ar/politica/?adstest=null&_website=la-nacion-ar';
+    const nameQueryParam2 = 'adstest';
+
     test('Test del return de getQueryParamValue', () => {
         const query = getQueryParamValue(siteUrl, nameQueryParam);
         expect(query).toStrictEqual('cristina');
+    });
+
+    test('Test para el caso del query param: adstest', () => {
+        const query = getQueryParamValue(siteUrl2, nameQueryParam2);
+        expect(query).toStrictEqual('true');
+    });
+
+    test('Test para el caso de que el query param adstest sea null', () => {
+        const query = getQueryParamValue(siteUrl3, nameQueryParam2);
+        expect(query).toStrictEqual('false');
     });
 });
---
+++ components/private/common/utils/outputTypeHelper.js
@@ -0,0 +1,55 @@
+import getQueryParamValue from './getQueryParamValue';
+import getMetaDescriptionForAcum from './getMetaDescriptionForAcum';
+
+export const getTitle = (
+    _nodeType = '',
+    metaValue,
+    layout = '',
+    requestUri = '',
+    { host, longTitle, title: defaultTitle }
+) => {
+    const title = _nodeType === 'home' ? longTitle : metaValue || defaultTitle;
+    const query =
+        host && requestUri
+            ? getQueryParamValue(`${host}${requestUri}`, 'query')
+            : null;
+
+    return layout === 'LN-buscador' && query ? `${query}: ${title}` : title;
+};
+
+export const getMetaDescriptionDefault = (
+    metaValue,
+    layout,
+    defaultDescription,
+    host,
+    requestUri,
+    _nodeType,
+    _id,
+    Payload,
+    nodeType,
+    name,
+    arcSite
+) => {
+    const query =
+        host && requestUri
+            ? getQueryParamValue(`${host}${requestUri}`, 'query')
+            : null;
+
+    if (_nodeType === 'acumulado') {
+        return (
+            getMetaDescriptionForAcum(
+                metaValue,
+                _id,
+                Payload,
+                nodeType,
+                name,
+                arcSite,
+                layout
+            ) || ''
+        );
+    }
+
+    return layout === 'LN-buscador' && query
+        ? metaValue.replace(/[+]/, query)
+        : defaultDescription;
+};
