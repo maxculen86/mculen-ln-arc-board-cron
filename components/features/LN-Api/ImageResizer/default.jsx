273383b2ff4bad1c6d912f9f3d227a90f7ea8b52
Isaias Anzola <ianzola@lanacion.com.ar>
2022-01-31 14:36:23 -0300

    Refactor de 'ImageResizeSource' (No se utilizaba) Creacion de feature ImageResizer Flujo:
    El feature esta asociado al ImageSource
    El feature extrae la Url de la imagen y llama a ImageResizeSource
    Transforma el objeto en formato Api Arc.

---
+++ components/features/LN-Api/ImageResizer/default.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const index = props => {
+    return <h2>Cambiar a OutputType JSON para visualizar el contenido</h2>;
+};
+
+index.label = 'LN-Api-ImageResizer';
+
+export default index;
