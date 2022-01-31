ef36ee3d0eeba3591a2d87ef705e7573678a217e
Isaias Anzola <ianzola@lanacion.com.ar>
2022-01-31 14:36:23 -0300

    Refactor de 'ImageResizeSource' (No se utilizaba) Creacion de feature ImageResizer Flujo:
    El feature esta asociado al ImageSource
    El feature extrae la Url de la imagen y llama a ImageResizeSource
    Transforma el objeto en formato Api Arc.

---
+++ components/features/LN-Api/ImageResizer/json.js
@@ -0,0 +1,56 @@
+import Consumer from 'fusion:consumer';
+import browser from '../../../private/common/utils/browser';
+import ImageV1 from '../../../private/LN/api/v1/common/story/image';
+
+// Responde al resolver que permite pasar las versiones existentes
+// Regex actual: ^\/api\/(?:mobile\/)?v([1-2]+)\/images\/(byId\/(.+)\/$)
+class ImageResizer {
+    constructor(props) {
+        this.props = props;
+
+        const {
+            globalContent: { url }
+        } = props;
+
+        this.fetch(url);
+
+        this.apiData = {
+            global: {
+                1: ImageV1
+            },
+            mobile: {
+                1: ImageV1
+            }
+        };
+    }
+
+    fetch(imageUrl) {
+        this.fetchContent({
+            imageResizeSource: {
+                source: 'imageResizeSource',
+                query: {
+                    url: imageUrl
+                }
+            }
+        });
+    }
+
+    render() {
+        try {
+            const { imageResizeSource } = this.state;
+            const { globalContent } = this.props;
+            const indexImage = this.apiData[
+                browser.getApiType(this.props.requestUri)
+            ][browser.getApiVersion(this.props.requestUri)];
+
+            return indexImage({
+                ...globalContent,
+                resized_urls: imageResizeSource
+            });
+        } catch (err) {
+            return { Success: false, Message: err.message };
+        }
+    }
+}
+
+export default Consumer(ImageResizer);
