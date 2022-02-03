4282c4d931d9e0415b66d3ba40d18405d962073e
JuanPravata <bg_jpravata@lnexternos.com.ar>
2022-02-03 10:04:03 -0300

    Se limpia codigo de test de mod dolar

--- __tests__/components/private/common/mod-dolar.test.js
+++ __tests__/components/private/common/mod-dolar.test.js
@@ -62,46 +62,6 @@ describe('with data list', () => {
             });
         }
     });
-    // liTags.forEach((item, index) => {
-    //     const children = liTags.at(index).children();
-    //     if (index === liTags.length - 1) {
-    //         it('validate item image InvertirOnline ', () => {
-    //             const linkComponent = children.find('mock-com-link');
-    //             const imageComponent = children.find('mock-com-image');
-
-    //             expect(linkComponent.exists()).toBeTruthy();
-    //             expect(linkComponent.prop('target')).toBe('_blank');
-    //             expect(linkComponent.prop('link')).toBe(
-    //                 'https://www.invertironline.com/'
-    //             );
-
-    //             expect(imageComponent.exists()).toBeTruthy();
-    //             expect(imageComponent.prop('src')).toBe(imageUrl);
-    //             expect(imageComponent.prop('alt')).toBe('invertirOnline.com');
-    //         });
-    //     } else {
-    //         const { title, compra, venta, sourceName } = data[index];
-    //         const titleComponent = children.find('mock-com-title');
-    //         const spanTags = children.find('strong');
-
-    //         it(`Validate item ${sourceName} - ${title}`, () => {
-    //             const saleValue = spanTags.at(0).prop('children')[1];
-
-    //             if (sourceName === 'dccl') {
-    //                 expect(spanTags.length).toBe(1);
-    //             } else {
-    //                 const buyValue = spanTags.at(1).prop('children')[1];
-    //                 expect(titleComponent.exists()).toBeTruthy();
-    //                 expect(titleComponent.prop('content')).toBe(title);
-    //                 expect(titleComponent.prop('size')).toBe('--twoxs');
-    //                 expect(titleComponent.prop('tag')).toBe('h2');
-    //                 expect(spanTags.length).toBe(2);
-    //                 expect(spanTags.at(0).html()).toContain(saleValue);
-    //                 expect(spanTags.at(1).html()).toContain(buyValue);
-    //             }
-    //         });
-    //     }
-    // });
 
     it('snapshot', () => {
         expect(result.render()).toMatchSnapshot();
