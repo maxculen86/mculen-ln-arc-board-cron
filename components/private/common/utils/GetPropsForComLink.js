6f2723605ab3d5185e77342b86a1733b495f9fca
JuanPravata <bg_jpravata@lnexternos.com.ar>
2022-02-10 17:09:14 -0300

    limpieza de codigo

--- components/private/common/utils/GetPropsForComLink.js
+++ components/private/common/utils/GetPropsForComLink.js
@@ -12,39 +12,6 @@ const generateProps = (
     SIZE_CLASS,
     EXTRA_CLASS
 ) => {
-    console.log(
-        '🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ EXTRA_CLASS',
-        EXTRA_CLASS
-    );
-    console.log(
-        '🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ SIZE_CLASS',
-        SIZE_CLASS
-    );
-    console.log('🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ style', style);
-    console.log(
-        '🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ children',
-        children
-    );
-    console.log(
-        '🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ isString',
-        isString
-    );
-    console.log(
-        '🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ textname',
-        textname
-    );
-    console.log('🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ title', title);
-    console.log('🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ target', target);
-    console.log('🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ rel', rel);
-    console.log(
-        '🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ dataSection',
-        dataSection
-    );
-    console.log(
-        '🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ dataEvent',
-        dataEvent
-    );
-    console.log('🚀 ~ file: GetPropsForComLink.js ~ line 15 ~ link', link);
     return {
         ...(link && { href: link }),
         ...(dataEvent && { 'data-event': dataEvent }),
