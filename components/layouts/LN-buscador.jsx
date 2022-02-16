abd3b245246f34ad5d62d8661caf13ca759fea35
Juan Carlos Diaz <jcDiaz@lanacion.com.ar>
2022-02-16 16:58:30 -0300

    Fix title y metaDescription en buscador

--- components/layouts/LN-buscador.jsx
+++ components/layouts/LN-buscador.jsx
@@ -1,21 +1,26 @@
+import Consumer from 'fusion:consumer';
 import React from 'react';
 import PropTypes from 'fusion:prop-types';
 import Static from 'fusion:static';
 import Header from '../private/LN/common/header';
 import Footer from '../private/LN/common/footer';
 import ComTitle from '../private/common/com-title';
-
-import '../../resources/dist/css/ln/pages/buscador.css';
-
 import GlobalProvider from '../private/common/context/globalContext';
 import PwaModals from '../private/LN/common/pwaModals';
+import insertTitleAndDescrition from '../private/common/utils/lnBuscadorHelper';
+import '../../resources/dist/css/ln/pages/buscador.css';
 
-const lnBuscador = ({ children }) => {
+const lnBuscador = ({ children, metaValue }) => {
     let searchResults = '';
     if (typeof window !== 'undefined') {
         const urlSerachParams =
             new URLSearchParams(window.location.search) || {};
         searchResults = urlSerachParams ? urlSerachParams.get('query') : '';
+        insertTitleAndDescrition(
+            searchResults,
+            metaValue('title'),
+            metaValue('description')
+        );
     }
 
     return (
@@ -45,7 +50,8 @@ const pageBuilderSections = ['Cuerpo'];
 
 lnBuscador.sections = pageBuilderSections;
 lnBuscador.propTypes = {
-    children: PropTypes.arrayOf(PropTypes.node).isRequired
+    children: PropTypes.arrayOf(PropTypes.node).isRequired,
+    metaValue: PropTypes.func.isRequired
 };
 
-export default lnBuscador;
+export default Consumer(lnBuscador);
--- components/output-types/default.jsx
+++ components/output-types/default.jsx
@@ -130,8 +130,7 @@ const Default = props => {
         siteProperties,
         renderables,
         globalContent,
-        outputType,
-        requestUri
+        outputType
     } = props;
 
     const {
@@ -152,7 +151,7 @@ const Default = props => {
     const { meta_title: metaTitle, basic: basicTitle } = headlines || {};
     const { basic: descriptionBasic } = description || {};
     const { name: distributorName } = distributor || {};
-    const { host, description: defaultDescription } = siteProperties;
+    const { description: defaultDescription } = siteProperties;
 
     const metaTitleBasic = metaTitle || basicTitle;
 
@@ -194,7 +193,6 @@ const Default = props => {
         _nodeType,
         metaValue('title'),
         layout,
-        requestUri,
         siteProperties
     );
 
@@ -202,8 +200,6 @@ const Default = props => {
         metaValue('description'),
         layout,
         defaultDescription,
-        host,
-        requestUri,
         _nodeType,
         _id,
         Payload,
@@ -236,7 +232,7 @@ const Default = props => {
                     content="width=device-width,initial-scale=1.0,minimum-scale=0.5,maximum-scale=5.0,user-scalable=yes"
                 />
                 <meta name="theme-color" content="#ffffff" />
-                <title>{title}</title>
+                {layout !== 'LN-buscador' && <title>{title}</title>}
                 <link
                     rel="preload"
                     as="font"
@@ -331,17 +327,19 @@ const Default = props => {
                     defaultTitle={siteProperties.longTitle}
                     metaValue={title}
                 />
-                <MetaDescription
-                    subtype={subtype}
-                    nodeType={nodeType}
-                    _id={_id}
-                    description={descriptionBasic}
-                    metaTitleBasic={metaTitleBasic}
-                    subheadlines={subheadlines && subheadlines.basic}
-                    arcSite={arcSite}
-                    section={_nodeType}
-                    metaDescription={metaDescription}
-                />
+                {layout !== 'LN-buscador' && (
+                    <MetaDescription
+                        subtype={subtype}
+                        nodeType={nodeType}
+                        _id={_id}
+                        description={descriptionBasic}
+                        metaTitleBasic={metaTitleBasic}
+                        subheadlines={subheadlines && subheadlines.basic}
+                        arcSite={arcSite}
+                        section={_nodeType}
+                        metaDescription={metaDescription}
+                    />
+                )}
                 <MetaViafoura {...props} />
                 <Syndication
                     type={type}
