9dc75df5a5f0204cb0706e8987a402f66de31cf3
Alejandro Sanoja <hsanoja@lanacion.com.ar>
2022-02-23 14:18:21 -0300

    Fix zoom image

--- components/private/LN/common/media/imageBase/index.jsx
+++ components/private/LN/common/media/imageBase/index.jsx
@@ -4,18 +4,6 @@ import ComImage from '../../../../common/com-image';
 import ComPicture from '../../../../common/com-picture';
 import { getSourceSet } from '../../utils/mediaHelper';
 
-const sourceMapper = source => {
-    return source.map(e => {
-        return (
-            <source
-                key={e.option.media}
-                media={e.option.media}
-                srcSet={e.resizedUrl}
-            />
-        );
-    });
-};
-
 const ImageArticle = props => {
     const { image, href, outputType, active, isVertical, isApertura } = props;
 
@@ -35,37 +23,12 @@ const ImageArticle = props => {
 
     // TODO: ver este tema de source sets con maquetacion
     const srcset = getSourceSet(isVertical, image, sourceActive);
-    const sizes = sourceActive
-        .map(x => `${x.option.media} ${x.option.width}px`)
-        .join();
+    const sizes =
+        sourceActive &&
+        sourceActive.map(x => `${x.option.media} ${x.option.width}px`).join();
 
     return (
         <ComPicture href={href} amp={outputType === 'amp'}>
-            {/* {!active &&
-                outputType !== 'amp' &&
-                sources &&
-                sources.map(x => {
-                    return (
-                        <source
-                            key={x.option.media}
-                            media={x.option.media}
-                            srcSet={x.resizedUrl}
-                        />
-                    );
-                })} */}
-            {/* {active &&
-                sourcesZoom &&
-                sourcesZoom.map(x => {
-                    return (
-                        <source
-                            key={x.option.media}
-                            media={x.option.media}
-                            srcSet={x.resizedUrl}
-                        />
-                    );
-                })} */}
-            {sourceMapper(sources)}
-            {active && sourcesZoom && sourceMapper(sourcesZoom)}
             <ComImage
                 srcset={srcset}
                 sizes={sizes}
