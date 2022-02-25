209837a6e7149663a5a171c4b5f598a763e6fed5
JuanPravata <bg_jpravata@lnexternos.com.ar>
2022-02-25 15:13:36 -0300

    Avances logica content source para serivces

--- content/sources/servicesSource.js
+++ content/sources/servicesSource.js
@@ -1,6 +1,8 @@
 import { ARC_ACCESS_TOKEN, CONTENT_BASE } from 'fusion:environment';
 import request from 'request-promise-native';
 import { resolve as sectionSourceResolve } from './sectionSource';
+import defaultWidget from './utils/widgets/defaultWidget';
+import { getAuthForRequest } from './utils/widgets/helper';
 
 const getRequest = query => {
     const opt = {
@@ -15,18 +17,55 @@ const getRequest = query => {
     return request(opt).then(data => data);
 };
 
+const SERVICES = {
+    loterias: defaultWidget,
+    default: defaultWidget
+};
+
 const fetch = async (query, { cachedCall }) => {
+    const {
+        id = '',
+        service = '',
+        serviceItem = '',
+        uri = '',
+        'arc-site': arcSite = 'la-nacion-ar'
+    } = query;
+    console.log(
+        '🚀 ~ file: servicesSource.js ~ line 33 ~ fetch ~ query',
+        query
+    );
+
+    console.log('🚀 ~ file: servicesSource.js ~ line 28 ~ fetch ~ id', id);
     const sectionSourceData = await cachedCall('sectionSource', getRequest, {
         query: `${CONTENT_BASE}${sectionSourceResolve(query)}`
     });
-
     return sectionSourceData;
+    const { request: serviceRequest, resolve, reject, transform, getUri } =
+        SERVICES[service] || SERVICES.defaultWidget;
+
+    return serviceRequest({
+        queryData: query,
+        getUri,
+        auth: getAuthForRequest(ARC_ACCESS_TOKEN)
+    })
+        .then(response =>
+            resolve({
+                response: { ...sectionSourceData, dataService: response },
+                transform,
+                query
+            })
+        )
+        .catch(error => {
+            return reject({ error, uri, arcSite });
+        });
 };
 
 export default {
     fetch,
     params: {
         id: 'text',
+        service: 'text',
+        serviceItem: 'text',
         website: 'text',
         outputType: 'text',
         redirectUrl: 'text',
