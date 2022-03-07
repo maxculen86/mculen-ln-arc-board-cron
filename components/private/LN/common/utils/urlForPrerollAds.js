9923708060e61fe69a411be5fb757792a4a38f2a
Marcelo Fanego <pt_mfanego@lnexternos.com.ar>
2022-03-07 12:13:46 -0300

    Implementacion de preroll para video en AMP, con posicion preroll_amp de adserver.

--- components/private/LN/common/utils/urlForPrerollAds.js
+++ components/private/LN/common/utils/urlForPrerollAds.js
@@ -4,7 +4,10 @@ import { getCustParamsEnconde } from './getDataFormated';
 
 const urlForPrerollAds = device => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { requestUri, globalContent } = useAppContext();
+    const { requestUri, globalContent, outputType } = useAppContext();
+
+    const amp = outputType === 'amp' ? outputType : false;
+
     const deviceResolution = {
         desktop: 'dsk',
         tablet: 'tab',
@@ -28,9 +31,10 @@ const urlForPrerollAds = device => {
     // TODO: por ahora esta hardcodeado "Nota" en la url. Ver si hace falta hacer
     // alguna logica para completar ese campo
     return withPrerolAds
-        ? `https://pubads.g.doubleclick.net/gampad/ads?slotname=/133919216/la_nacion_${device}/Nota/preroll_${
-              deviceResolution[device]
-          }&sz=640x480|400x300&ciu_szs=300x250&unviewed_position_start=1&output=vast&impl=s&env=vp&gdfp_req=1&ad_rule=0&vad_type=linear&vpos=preroll&cust_params=section%3D${
+        ? `https://pubads.g.doubleclick.net/gampad/ads?slotname=/133919216/la_nacion_${device}/Nota/preroll_${amp ||
+              deviceResolution[
+                  device
+              ]}&sz=640x480|400x300&ciu_szs=300x250&unviewed_position_start=1&output=vast&impl=s&env=vp&gdfp_req=1&ad_rule=0&vad_type=linear&vpos=preroll&cust_params=section%3D${
               custParamsEncoded // eslint-disable-next-line prettier/prettier
           }&pod=3&ppos=1&lip=true&min_ad_duration=0&max_ad_duration=30000&vrid=6256&url=${url}&description_url=${
               url // eslint-disable-next-line prettier/prettier
