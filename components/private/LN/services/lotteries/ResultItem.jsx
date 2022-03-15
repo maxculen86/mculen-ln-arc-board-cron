b3d4d8bbe578e67d93975e1d8dd467cf323ebab9
adivianahd <adivianahd07@gmail.com>
2022-03-15 10:31:35 -0300

    WIP - se agrega componente resultItem

---
+++ components/private/LN/services/lotteries/ResultItem.jsx
@@ -0,0 +1,30 @@
+import { React } from 'react';
+import PropTypes from 'prop-types';
+import { Text } from '@lana-ui/common';
+
+const ResultItem = ({ text, result }) => {
+    return (
+        <div className="result-item">
+            <Text size="5xs" color="gray">
+                {text}
+            </Text>
+            <Text className="results-secondary" weight="bold" size="5xs">
+                {result.map(number => (
+                    <Text key={number}>{number}</Text>
+                ))}
+            </Text>
+        </div>
+    );
+};
+
+ResultItem.propTypes = {
+    text: PropTypes.string,
+    result: PropTypes.arrayOf
+};
+
+ResultItem.defaultProps = {
+    text: '',
+    result: []
+};
+
+export default ResultItem;
