bd59b44c5b499915ff88b7317fcb6af0d040b74e
tcabral <tcabral@lanacion.com.ar>
2022-03-17 16:18:55 -0300

    add transform detail for services source
    -Add mocks, failing tests for quini cases, poceada, provincia and telekino
    -Add Loto mock and test case
    -Add additional properties and corresponding destructuring
    -Refactor transform detail function

--- content/sources/utils/servicesSource/lottery/lotteryHelper.js
+++ content/sources/utils/servicesSource/lottery/lotteryHelper.js
@@ -2,7 +2,82 @@
 import transformISODate from '../../../../../components/private/common/utils/transformISODate';
 import { games } from './_config';
 
-export const transformLotteryDetail = data => data;
+export const transformLotteryDetail = data => {
+    const newLotteries = data.map(lottery => {
+        const {
+            name = '',
+            id = '',
+            lottery_draw_id = '',
+            date = '',
+            additional_properties = {},
+            prizes = [],
+            results = []
+        } = lottery;
+        const {
+            letters = [],
+            jackpot = [],
+            estimated_pot = [],
+            meaning = ''
+        } = additional_properties;
+        const winnersTable = prizes
+            .filter(prize => prize.name !== 'carton')
+            .map(prize => {
+                const {
+                    name: prizeName = '',
+                    winners = '',
+                    amount = ''
+                } = prize;
+                const newPrizeName = prizeName.split(' ');
+                return {
+                    name:
+                        newPrizeName.length < 2
+                            ? newPrizeName.shift()
+                            : newPrizeName
+                                  .filter(
+                                      item =>
+                                          item !== 'aciertos' &&
+                                          item !== 'jackpot'
+                                  )
+                                  .join(''),
+                    ...(winners && { winners }),
+                    ...(amount && { amount })
+                };
+            });
+        const winnersCarton = prizes
+            .filter(prize => prize.name === 'carton')
+            .map(prize => {
+                const { winners = '', amount = '' } = prize;
+                return {
+                    numbers: winners,
+                    amount
+                };
+            });
+        return {
+            name,
+            id,
+            ...(lottery_draw_id && { lottery_draw_id }),
+            date,
+            ...(letters.length && {
+                letters: letters.shift()
+            }),
+            ...(jackpot.length && {
+                jackpot
+            }),
+            ...(meaning && {
+                meaning
+            }),
+            ...(estimated_pot.length && {
+                estimated_pot: estimated_pot.shift()
+            }),
+            results,
+            ...(winnersTable.length && { winners_table: winnersTable }),
+            ...(winnersCarton.length && {
+                winner_carton: winnersCarton
+            })
+        };
+    });
+    return newLotteries;
+};
 
 const getValue = (input, key) => input.filter(e => e.id === key);
 
