526a2819cf872646c5feb8d52f3d7da856309824
JuanPravata <bg_jpravata@lnexternos.com.ar>
2022-03-28 15:44:13 -0300

    Se integra details table a detalle de loteria

--- components/features/LN-services/lotteryDetailOpening/default.jsx
+++ components/features/LN-services/lotteryDetailOpening/default.jsx
@@ -1,12 +1,28 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'fusion:prop-types';
+import { useAppContext } from 'fusion:context';
 import StaticValidation from '../../../private/common/staticValidation';
+import DetailsTable from '../../../private/LN/services/lotteries/DetailsTable';
+import get from '../../../private/common/utils/get';
 
-const LotteryDetailOpening = ({ id: featureId }) => (
-    <StaticValidation id={featureId} htmlOnly persistent>
-        <div className="lottery-detail-box lay">Apertura Detalle Lotería</div>
-    </StaticValidation>
-);
+const LotteryDetailOpening = ({ id: featureId }) => {
+    const lottery = get(
+        useAppContext(),
+        'globalContent.dataService.lotteryDetail',
+        []
+    );
+    return (
+        <StaticValidation id={featureId} htmlOnly persistent>
+            <div className="lottery-detail-box lay">
+                Apertura Detalle Lotería
+            </div>
+            {lottery.map(game => {
+                return <DetailsTable data={game} />;
+            })}
+        </StaticValidation>
+    );
+};
 
 LotteryDetailOpening.label = 'LN Loteria Detalle Apertura';
 
