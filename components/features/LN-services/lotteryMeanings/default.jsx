9746be0a6db43bdb82a498bcb55dca74867ce9e8
Mariaelisa Rodrigues <mrodrigues@lanacion.com.ar>
2022-03-28 12:19:21 -0300

    adding meanings urls for sandbox (lotteries)

--- components/features/LN-services/lotteryMeanings/default.jsx
+++ components/features/LN-services/lotteryMeanings/default.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'fusion:prop-types';
+import { API_ENV } from 'fusion:environment';
 import StaticValidation from '../../../private/common/staticValidation';
 import ServiceMiniCard from '../../../private/common/serviceMiniCard';
 
@@ -13,7 +14,7 @@ const LotteryMeanings = ({ id: featureId }) => {
                     <ServiceMiniCard
                         key={meaningInfo.title}
                         title={meaningInfo.title}
-                        link={meaningInfo.link}
+                        link={meaningInfo.link[API_ENV || 'sandbox']}
                         linkTitle={meaningInfo.linkTitle}
                         icon={meaningInfo.icon}
                         labeled="Significado de numeros"
