d8aef109d569c6da08527794fac2e067fd611771
JuanPravata <bg_jpravata@lnexternos.com.ar>
2022-03-29 12:12:02 -0300

    Se añade link a data factory en detalle de loterias

--- components/features/LN-services/lotteryDetailOpening/default.jsx
+++ components/features/LN-services/lotteryDetailOpening/default.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable camelcase */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
@@ -8,6 +9,8 @@ import DetailsTable from '../../../private/LN/services/lotteries/DetailsTable';
 import get from '../../../private/common/utils/get';
 import { components } from '../lotteryGrid/default';
 import TableHorizontalResults from '../../../private/LN/services/lotteries/TableHorizontalResults';
+import ComLink from '../../../private/common/com-link';
+import Text from '../../../private/common/text';
 
 const LotteryDetailOpening = ({ id: featureId }) => {
     const lottery = get(
@@ -39,6 +42,19 @@ const LotteryDetailOpening = ({ id: featureId }) => {
                         );
                     })}
                 </div>
+
+                <Text>
+                    Información provista por
+                    {`${' '}`}
+                    <ComLink
+                        link="https://www.datafactory.la/"
+                        target="_blank"
+                        rel="nofollow"
+                        title="Ir a Data Factory"
+                    >
+                        Datafactory
+                    </ComLink>
+                </Text>
             </StaticValidation>
         )
     );
