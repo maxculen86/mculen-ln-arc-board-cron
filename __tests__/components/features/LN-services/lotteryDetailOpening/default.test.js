3731b440655f19db747215b9bb6f7c46d934a7a1
tcabral <tcabral@lanacion.com.ar>
2022-04-04 15:57:47 -0300

    add brinco example and grid lotteries jsons for tests, solve commentend tests for lottery detail opeining, rules, grid and meaning, fix react imports in details table and result item

--- __tests__/components/features/LN-services/lotteryDetailOpening/default.test.js
+++ __tests__/components/features/LN-services/lotteryDetailOpening/default.test.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import Context from 'fusion:context';
 import LotteryDetailOpening from '../../../../../components/features/LN-services/lotteryDetailOpening/default';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import brincoExample from '../../../../../__mocks__/data/lottery/brincoExample.json';
 
 jest.mock(
     '../../../../../components/private/common/staticValidation.jsx',
     () => 'mock-static-validation'
 );
 
+jest.mock('fusion:context', () => () => ({
+    default: props => {
+        const mockAvailableProps = {};
+        return props.children(mockAvailableProps);
+    },
+    useAppContext: jest.fn(() => ({}))
+}));
+
+Context.useAppContext = jest.fn(() => ({
+    globalContent: {
+        dataService: {
+            lotteryDetail: brincoExample
+        }
+    }
+}));
+
 describe('Features - LN-servicios - LN Loteria Detalle =>', () => {
     const { container } = render(<LotteryDetailOpening id="QWERTYUIOP" />);
     const StaticValidation = container.querySelector('mock-static-validation');
-    /* it('should be wrapped by StaticValidation component', () => {
+    it('should be wrapped by StaticValidation component', () => {
         expect(
             screen.getByText(
                 (content, element) =>
@@ -19,13 +37,18 @@ describe('Features - LN-servicios - LN Loteria Detalle =>', () => {
             )
         ).toBeVisible();
     });
-    it('should return a list of lotteries', () => {
+    it('should return a lottery detail', () => {
+        render(<LotteryDetailOpening id="QWERTYUIOP" />);
         expect(
             StaticValidation.getElementsByClassName('lottery-detail-box').length
         ).toBe(1);
-        expect(StaticValidation.firstChild.textContent).toBe(
-            'Apertura Detalle Lotería'
+        expect(StaticValidation.firstChild.textContent).toContain(
+            'Últimos resultados'
         );
-    }); */
-    it('should ', () => {});
+        expect(screen.getAllByRole('row').length).toBe(7);
+        expect(screen.getByText('$73.000.000')).toBeDefined();
+        expect(screen.getAllByRole('table')[0]).toHaveClass(
+            'table --winners-table'
+        );
+    });
 });
