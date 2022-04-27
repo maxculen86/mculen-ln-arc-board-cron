303b4344f3719a301666695c078be20dabb9ea2f
Wilbert <wbelandria@lanacion.com.ar>
2022-04-27 18:50:59 -0300

    Carga de imgen de nota de apertura en home:
    - Pasamano de isInApertura a los sources articleSourceNora & relatedImageSource
    	para definir en resizer que url utilizar
    - Pasa menos desde getDataToLinkImage
    - Pasa menos desde noteCard

--- components/private/common/utils/image/resizer.js
+++ components/private/common/utils/image/resizer.js
@@ -6,7 +6,7 @@ import { FOTOAL100, RECETA, STORYTELLING } from '../subtypes/subtypeHelper';
 import get from '../get';
 import { getAspectRatio } from '../../../../../content/sources/utils/getRatio';
 
-export const createResizer = (resizerKey, resizerUrl) => {
+export const createResizer = (resizerKey, resizerUrl, isInApertura = false) => {
     const Thumbor =
         // eslint-disable-next-line no-eval
         typeof window === 'undefined' ? eval('require("thumbor")') : () => {};
@@ -63,7 +63,9 @@ export const createResizer = (resizerKey, resizerUrl) => {
             .resize(newWidth, newHeight)
             .buildUrl();
 
-        return url.replace(/^.*\/\/[^\/]+/, RESIZER_URL_PUBLIC);
+        return isInApertura
+            ? url
+            : url.replace(/^.*\/\/[^\/]+/, RESIZER_URL_PUBLIC);
     };
 
     const resizeUrls = (
@@ -322,7 +324,8 @@ export const addResizedUrls = (ansDoc, options) => {
             zoomSizes = []
         },
         presetsDefault,
-        subtype
+        subtype,
+        isInApertura
     } = options;
     const {
         promo_items: promoItems,
@@ -334,7 +337,7 @@ export const addResizedUrls = (ansDoc, options) => {
             'Debe proporcionar el resizerSecret, resizerUrl y presets'
         );
 
-    const resizer = createResizer(resizerSecret, resizerUrl);
+    const resizer = createResizer(resizerSecret, resizerUrl, isInApertura);
 
     const { defaultResize } = getDefaultSize(subtype);
 
