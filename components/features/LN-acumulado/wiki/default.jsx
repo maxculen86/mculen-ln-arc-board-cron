f0be392244cc73cba4810817127427894a7c3441
Juan Pravata <jpravata@lanancion.com.ar>
2022-05-02 11:31:12 -0300

    Se corrigen test de wikiTag feature

--- components/features/LN-acumulado/wiki/default.jsx
+++ components/features/LN-acumulado/wiki/default.jsx
@@ -8,7 +8,7 @@ const WikiFeature = () => {
     const { isWiki } = props;
     const wikiSourceData = useContent({
         source: isWiki ? 'wikiTagSource' : null,
-        query: { type: 'person', imageId, imageConfig: 'aperturaAcu' }
+        query: { type: 'person', imageConfig: 'aperturaAcu' }
     });
     const {
         image: { resizedUrls = [], url = '', alt = '' }
