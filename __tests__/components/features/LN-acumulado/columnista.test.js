86f1944647675b15f9bc2d2fcfb40a258ce16ae2
Matias Velasquez <mvelasquez@lanacion.com.ar>
2022-05-16 13:44:40 -0300

    unit test columnist

--- __tests__/components/features/LN-acumulado/columnista.test.js
+++ __tests__/components/features/LN-acumulado/columnista.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import Consumer from 'fusion:consumer';
 import { useContent } from 'fusion:content';
 import { shallow } from 'enzyme';
-import COLUMNIST_DATA from '../../../../__mocks__/data/columnista/columnista';
-import Columnista from '../../../../components/features/LN-acumulado/columnista/default';
+import COLUMNIST_DATA from '../../../../__mocks__/data/columnista/columnista.json';
+import ColumnistaComponent from '../../../../components/features/LN-acumulado/columnista/default';
+import * as Columnista from '../../../../components/features/LN-acumulado/columnista/json';
 
+jest.mock('fusion:consumer', component => {
+    return function(component) {
+        return class extends component {
+            constructor(props) {
+                super(props);
+                this.props = props;
+            }
+            fetchContent(param) {}
+        };
+    };
+});
 describe('features - LaNacion - Acumulado - columnista', () => {
     useContent.mockImplementation(() => COLUMNIST_DATA);
     const props = {
@@ -13,7 +24,12 @@ describe('features - LaNacion - Acumulado - columnista', () => {
         }
     };
     it('Test de snapshot Columnista', () => {
-        const component = shallow(<Columnista {...props} />);
+        const component = shallow(<ColumnistaComponent {...props} />);
         expect(component).toMatchSnapshot();
+    });
+    it('undefined data', () => {
+        const objArticle = new Columnista.default(props);
+        const result = objArticle.render();
+        expect(result).toBeUndefined();
     });
 });
