b761b84237913d8ab3cbde4d6a1deca43c65ca25
Isaias Anzola <ianzola@lanacion.com.ar>
2022-05-24 11:36:59 -0300

    Refactor defensivo image common

--- components/private/LN/api/v1/common/image/index.js
+++ components/private/LN/api/v1/common/image/index.js
@@ -1,16 +1,15 @@
 import get from '../../../../../common/utils/get';
 import epigrafeAndCreditsData from '../../../../../common/utils/epigrafeAndCreditsData';
 
-const newRegex = /.*\/resizer\/([a-zA-Z0-9_\-=]+\/[0-9x]+(?:\/smart)?(?:\/+(?:filters:.+?)?)?)\/.*/;
-const hrefRegex = new RegExp(
-    /\/resizer\/([a-zA-Z0-9_\-=]+\/[0-9x]+(?:\/smart)?(?:\/+(?:filters:.+?)?)?)\/.*/
-);
-
 const imageCommon = image => {
     if (!image) return null;
     const { _id: id, resized_urls: resizedUrls } = image;
-
     if (!resizedUrls || resizedUrls.length === 0) return null;
+
+    const newRegex = /.*\/resizer\/([a-zA-Z0-9_\-=]+\/[0-9x]+(?:\/smart)?(?:\/+(?:filters:.+?)?)?)\/.*/;
+    const hrefRegex = new RegExp(
+        /\/resizer\/([a-zA-Z0-9_\-=]+\/[0-9x]+(?:\/smart)?(?:\/+(?:filters:.+?)?)?)\/.*/
+    );
 
     const absoluteUrl = resizedUrls[0].resizedUrl.replace(
         newRegex,
@@ -30,44 +29,53 @@ const imageCommon = image => {
         parametros: []
     };
     const regex = /.*\/resizer\/([a-zA-Z0-9_\-=]+\/[0-9x]+(?:\/smart)?(?:\/+(?:filters:.+?)?)?)\/.*/;
-    // Object.keys(resizedUrls)
-    //     .sort(function orderPhotos(a, b) {
-    //         const mediaA = resizedUrls[a].option.width;
-    //         const mediaB = resizedUrls[b].option.width;
+    Object.keys(resizedUrls)
+        .sort(function orderPhotos(a, b) {
+            const mediaA = get(resizedUrls, `[${a}].option.width`, 0);
+            const mediaB = get(resizedUrls, `[${b}].option.width`, 0);
+            return orderPattern(mediaA, mediaB);
+        })
+        .forEach((element, index) => {
+            let { media } = get(resizedUrls, `[${index}].option`, {});
+            if (media) {
+                media = parseInt(media.match(/\d+/)[0], 10);
+            } else {
+                media = get(resizedUrls, `[${index}].option.width`, 0);
+            }
+            const ancho = get(resizedUrls, `[${index}].option.width`, 0);
+            const alto = get(resizedUrls, `[${index}].option.height`, 0);
+            const firma =
+                (resizedUrls[index] &&
+                    resizedUrls[index].resizedUrl &&
+                    resizedUrls[index].resizedUrl.match(regex) &&
+                    resizedUrls[index].resizedUrl.replace(regex, '$1')) ||
+                '';
 
-    //         return orderPattern(mediaA, mediaB);
-    //     })
-    //     .forEach((element, index) => {
-    //         let { media } = resizedUrls[index].option;
-    //         if (media) {
-    //             media = parseInt(media.match(/\d+/)[0], 10);
-    //         } else {
-    //             media = resizedUrls[index].option.width;
-    //         }
-    //         resp.parametros.push({
-    //             media,
-    //             ancho: resizedUrls[index].option.width,
-    //             firma: resizedUrls[index].resizedUrl.match(regex)
-    //                 ? resizedUrls[index].resizedUrl.replace(regex, '$1')
-    //                 : ''
-    //         });
-    //     });
-
-    for (let index = 0; index < resizedUrls.length; index += 1) {
-        let { media } = resizedUrls[index].option;
-        if (media) {
-            media = parseInt(media.match(/\d+/)[0], 10);
-        } else {
-            media = resizedUrls[index].option.width;
-        }
-        resp.parametros.push({
-            media,
-            ancho: resizedUrls[index].option.width,
-            firma: resizedUrls[index].resizedUrl.match(regex)
-                ? resizedUrls[index].resizedUrl.replace(regex, '$1')
-                : ''
+            if (firma) {
+                resp.parametros.push({
+                    media,
+                    ancho,
+                    alto,
+                    firma
+                });
+            }
         });
-    }
+
+    // for (let index = 0; index < resizedUrls.length; index += 1) {
+    //     let { media } = resizedUrls[index].option;
+    //     if (media) {
+    //         media = parseInt(media.match(/\d+/)[0], 10);
+    //     } else {
+    //         media = resizedUrls[index].option.width;
+    //     }
+    //     resp.parametros.push({
+    //         media,
+    //         ancho: resizedUrls[index].option.width,
+    //         firma: resizedUrls[index].resizedUrl.match(regex)
+    //             ? resizedUrls[index].resizedUrl.replace(regex, '$1')
+    //             : ''
+    //     });
+    // }
 
     return resp;
 };
