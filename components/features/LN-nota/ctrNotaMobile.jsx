c54f1b941a8d6ab5be9d3cb0609d66aea847b944
Juan Pravata <jpravata@lanancion.com.ar>
2022-05-24 13:22:41 -0300

    Se añade en el diccionario _helper de ranking el objeto para la consulta al ranking inverso, se consulta al ranking inverso desde el feature CTR nota mobile

--- components/features/LN-nota/ctrNotaMobile.jsx
+++ components/features/LN-nota/ctrNotaMobile.jsx
@@ -6,11 +6,6 @@ import { isSubscribed } from '../../private/LN/common/utils/contextHelper';
 import get from '../../private/common/utils/get';
 
 const CTRNota = () => {
-    const sectionId = get(
-        useAppContext(),
-        'globalContent.taxonomy.primary_section.name',
-        {}
-    );
     const globalContent = get(useAppContext(), 'globalContent', {});
     const { _id: actualArticleId } = globalContent;
 
@@ -27,10 +22,6 @@ const CTRNota = () => {
                 website: 'la-nacion-ar'
             }
         }) || [];
-    console.log(
-        '🚀 ~ file: ctrNotaMobile.jsx ~ line 22 ~ CTRNota ~ data',
-        data
-    );
 
     const { articles = [] } = data;
     const articleToShow =
