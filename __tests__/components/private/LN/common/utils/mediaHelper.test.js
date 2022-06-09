41428fd6a14ef53bf6777edac02a7710eafd2ca5
Juan Pravata <jpravata@lanancion.com.ar>
2022-06-09 11:15:43 -0300

    Se agrega preload de imagen de wiki en getDataToLinkImage, se crea utilitario para agregar triple W a urls de imagen resizeadas de wiki y su respectivo test y se corrigen test en componente de wiki

--- __tests__/components/private/LN/common/utils/mediaHelper.test.js
+++ __tests__/components/private/LN/common/utils/mediaHelper.test.js
@@ -1,6 +1,22 @@
-import { buildScriptResizeSSRInfography } from '../../../../../../components/private/LN/common/utils/mediaHelper';
+import {
+    buildScriptResizeSSRInfography,
+    wikiImagesWithWWW
+} from '../../../../../../components/private/LN/common/utils/mediaHelper';
+import getProperties from 'fusion:properties';
 import articlePictureOpening from '../../../../../../__mocks__/data/articles/4HFO7YPZBFEYVB6K5XY6IFV3XY';
 import articleVideoOpening from '../../../../../../__mocks__/data/articles/KMD6TFFRHRC7XBPE2DDNKOTALE';
+import wikiSourceData from '../../../../../../__mocks__/data/wikiTag/wikiSourceData.json';
+
+jest.mock('fusion:environment', () => {
+    return {
+        RESIZER_URL_PUBLIC: 'https://resizer.glanacion.com',
+        SITE_LANACION: 'https://www.lanacion.com.ar'
+    };
+});
+
+jest.mock('fusion:properties', () => () => ({
+    getProperties: () => ({ host: 'https://www.lanacion.com.ar' })
+}));
 
 describe('Private - LN - Common - Utils -> mediaHelper', () => {
     const url =
@@ -55,5 +71,48 @@ describe('Private - LN - Common - Utils -> mediaHelper', () => {
         expect(
             buildScriptResizeSSRInfography(articleVideoOpening.promo_items)
         ).toBeNull();
+    });
+    describe('wikiImageWithWWW util', () => {
+        it('Should return the resized urls with WWW', () => {
+            const imagesWithWWW = wikiImagesWithWWW(wikiSourceData);
+            expect(imagesWithWWW).toStrictEqual([
+                {
+                    option: {
+                        height: 480,
+                        proportion: '2:3',
+                        width: 320
+                    },
+                    resizedUrl:
+                        'https://www.lanacion.com.ar/resizer/gCybFe_noQ_U5fN_EZ_G5qYkj7Q=/320x480/smart/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/3XELTA5ECJBZPI6TWPE64BOFJI.png'
+                },
+                {
+                    option: {
+                        height: 630,
+                        proportion: '2:3',
+                        width: 420
+                    },
+                    resizedUrl:
+                        'https://www.lanacion.com.ar/resizer/AgZWlsRI2ABRxZ4bxpUbM7PLYrE=/420x630/smart/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/3XELTA5ECJBZPI6TWPE64BOFJI.png'
+                },
+                {
+                    option: {
+                        height: 960,
+                        proportion: '2:3',
+                        width: 640
+                    },
+                    resizedUrl:
+                        'https://www.lanacion.com.ar/resizer/hL0FJguUDWOHcqU8kM2ZUXrI9eo=/640x960/smart/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/3XELTA5ECJBZPI6TWPE64BOFJI.png'
+                },
+                {
+                    option: {
+                        height: 1260,
+                        proportion: '2:3',
+                        width: 840
+                    },
+                    resizedUrl:
+                        'https://www.lanacion.com.ar/resizer/Xy2RqNNoAVp8cjAoKJ9VfNT9OrA=/840x1260/smart/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/3XELTA5ECJBZPI6TWPE64BOFJI.png'
+                }
+            ]);
+        });
     });
 });
