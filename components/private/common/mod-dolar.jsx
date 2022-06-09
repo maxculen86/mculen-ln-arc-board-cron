c1d13ae25fe9546574de37f39edad3053e100895
adivianahd <adivianahd07@gmail.com>
2022-06-09 17:36:46 -0300

    WIP - modulo de cripto

--- components/private/common/mod-dolar.jsx
+++ components/private/common/mod-dolar.jsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import PropTypes from 'fusion:prop-types';
 
-import CurrencyData from './currencyData/CurrencyData';
+// import CurrencyData from './currencyData/CurrencyData';
 
 import '../../../resources/dist/css/ln/modules/mod-dolar.css';
+import ModCripto from './mod-cripto';
 
-const ModDolar = ({ imageUrl, data = [] }) =>
-    (data.length && (
-        <ul className="mod-dolar">
-            {data.map((item, index) => {
-                const { sourceName, title, compra, venta } = item;
-                return (
-                    <li key={index}>
-                        <CurrencyData
-                            sourceName={sourceName}
-                            title={title}
-                            purchaseValue={compra}
-                            saleValue={sourceName !== 'dccl' && venta}
-                        />
-                    </li>
-                );
-            })}
-            {imageUrl && (
-                <li>
-                    <CurrencyData urlBrand={imageUrl} />
-                </li>
-            )}
-        </ul>
-    )) ||
-    null;
+const ModDolar = ({ imageUrl, data = [] }) => <ModCripto />;
 
 ModDolar.propTypes = {
     data: PropTypes.shape({
