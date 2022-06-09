d3eacb07445c579da0a429c4c93794786302520b
Alejandro Sanoja <hsanoja@lanacion.com.ar>
2022-06-09 10:28:33 -0300

    Hide OTT video opening and fix resizer height

--- components/layouts/OTT-home.jsx
+++ components/layouts/OTT-home.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { urlLiveVideo } from '../private/OTT/home/OTTHomeIndex';
-import VideoOpening from '../private/OTT/home/videoOpening';
-import VideoOpeningTitle from '../private/OTT/home/videoOpeningTitle';
 import LastVideos from '../private/OTT/common/lastVideos';
 import Footer from '../private/OTT/common/footer';
 import loadHeaderEvents from '../private/OTT/common/header/layoutEvents';
@@ -19,8 +16,8 @@ const OTTHomeLayout = ({ children }) => {
             {children[0]}
             <div className="wrapper">
                 <main className="main">
-                    <VideoOpening source={urlLiveVideo} />
-                    <VideoOpeningTitle title="LN+ En Vivo" />
+                    {/* <VideoOpening source={urlLiveVideo} />
+                    <VideoOpeningTitle title="LN+ En Vivo" /> */}
                     {children[1]}
                     <LastVideos />
                     {children[2]}
