4dc1eb6edfa00030d027b5ecc0aca58a08feec9f
Marcelo Fanego <pt_mfanego@lnexternos.com.ar>
2022-06-16 10:17:23 -0300

    Se quita validacion innecesaria de scriptCripto y se realiza correcion en helper de ranking para fixear que en los acumulados tag decia "mas leidas de" en lugar de "mas leidas".

--- components/private/common/scriptManager/scriptCripto.jsx
+++ components/private/common/scriptManager/scriptCripto.jsx
@@ -1,37 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-// import scriptVideoValidator from './scriptVideoValidator';
 
-// TODO: Validar cuantas veces renderiza este componente
-const ScriptCripto = ({ location, globalContent }) => {
-    console.log(
-        '🚀 ~ file: scriptCripto.jsx ~ line 7 ~ ScriptCripto ~ globalContent',
-        globalContent
-    );
-    const isEconomyOrCrypto = location === 'body-bottom';
-
+const ScriptCripto = () => {
     return (
-        isEconomyOrCrypto && (
-            <script
-                async
-                src="https://www.livecoinwatch.com/static/lcw-widget.js"
-            />
-        )
+        <script
+            async
+            src="https://www.livecoinwatch.com/static/lcw-widget.js"
+        />
     );
-};
-
-ScriptCripto.propTypes = {
-    globalContent: PropTypes.shape({
-        content_elements: PropTypes.node.isRequired,
-        promo_items: PropTypes.shape({
-            basic: PropTypes.shape({
-                type: PropTypes.string
-            }),
-            storytelling: PropTypes.shape({
-                type: PropTypes.string
-            })
-        })
-    }).isRequired
 };
 
 export default ScriptCripto;
