6e1af2480544c63a860650f857cfadf387078983
adivianahd <adivianahd07@gmail.com>
2022-07-20 15:57:37 -0300

    agrego estilos a provinces list weather

--- components/private/LN/services/weather/ProvincesList.jsx
+++ components/private/LN/services/weather/ProvincesList.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Text from '../../../common/text';
 import ComLink from '../../../common/com-link';
+import ModHeaderSection from '../../../common/mod-headerSection';
 
 const ProvincesList = ({ provinces }) => {
     return (
-        <div className="province-list">
-            {provinces &&
-                provinces.map(({ name }) => (
-                    <div className="province">
-                        <Text size="--md" weight="bold">
-                            <ComLink
-                                link="#"
-                                title={`Ir a detalle clima de ${name}`}
-                            >
-                                {name}
-                            </ComLink>
-                        </Text>
-                    </div>
-                ))}
-        </div>
+        <>
+            <ModHeaderSection tag="h2" title="Listado de provincias" />
+            <div className="province-list">
+                {provinces &&
+                    provinces.map(({ name }) => (
+                        <div className="province">
+                            <Text size="--md" weight="bold">
+                                <ComLink
+                                    link="#" // TODO agregar linkeo a la provincia
+                                    title={`Ir a detalle clima de ${name}`}
+                                >
+                                    {name}
+                                </ComLink>
+                            </Text>
+                        </div>
+                    ))}
+            </div>
+        </>
     );
 };
 
