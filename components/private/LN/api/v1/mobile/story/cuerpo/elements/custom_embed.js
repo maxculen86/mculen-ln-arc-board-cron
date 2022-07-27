756d66a25bf7488b3ec3564a0cdad22cedaf8005
Sally <seflores@lanacion.com.ar>
2022-07-27 05:38:43 -0300

    Adaptacion time por liveblog

--- components/private/LN/api/v1/mobile/story/cuerpo/elements/custom_embed.js
+++ components/private/LN/api/v1/mobile/story/cuerpo/elements/custom_embed.js
@@ -2,6 +2,17 @@ import get from '../../../../../../../common/utils/get';
 import header from './header';
 import image from './image';
 
+const getTime = time => {
+    if (time) {
+        const timeSplit = time.split(':');
+        if (timeSplit && timeSplit[1]) {
+            return timeSplit[0].concat(':').concat(timeSplit[1]);
+        }
+    }
+
+    return null;
+};
+
 const customEmbed = (nodo, dataNota) => {
     if (!nodo || !['custom-parallax', 'custom-liveblog'].includes(nodo.subtype))
         return null;
@@ -9,7 +20,8 @@ const customEmbed = (nodo, dataNota) => {
     const res = [];
 
     const titleElement = get(nodo, 'embed.config.title', null);
-    const time = get(nodo, 'embed.config.time', null);
+    const time = getTime(get(nodo, 'embed.config.time', null));
+
     if (titleElement) {
         const objTitle = {
             type: 'header',
