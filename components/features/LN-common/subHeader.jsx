bc862c23b6448eb7d2d8126bcc1aa7640a160677
Alejandro Sanoja <hsanoja@lanacion.com.ar>
2022-08-03 10:35:36 -0300

    Remove staticMode

--- components/features/LN-common/subHeader.jsx
+++ components/features/LN-common/subHeader.jsx
@@ -5,10 +5,8 @@ import Subheader from '../../private/LN/common/header/subHeader';
 import useTermica from '../../private/common/hooks/useTermica';
 
 const SubHeader = () => {
-    const { data: dollar } =
-        useContent({ source: 'dolarSource', staticMode: true }) || {};
-    const { weather } =
-        useContent({ source: 'weatherSource', staticMode: true }) || {};
+    const { data: dollar } = useContent({ source: 'dolarSource' }) || {};
+    const { weather } = useContent({ source: 'weatherSource' }) || {};
 
     const dollarValue = useTermica('dolar', dollar);
     const weatherValue = useTermica('weather', weather);
