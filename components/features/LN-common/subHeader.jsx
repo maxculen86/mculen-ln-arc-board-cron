3f18a1204b5c72c6f33e09f0c2987c125b937f0d
Juan Pravata <jpravata@lanancion.com.ar>
2022-08-22 10:45:45 -0300

    Se coloca static mode true en consultas de clima y dolar en subheader para eliminar content cache, se crear un componente pasa mano para no romper las reglas de hooks. Se agrega fallback en locations en subheader private para corregir articulos bug que llegaban al contnt cache

--- components/features/LN-common/subHeader.jsx
+++ components/features/LN-common/subHeader.jsx
@@ -1,26 +1,40 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
 import { useContent } from 'fusion:content';
 import Static from 'fusion:static';
 import Subheader from '../../private/LN/common/header/subHeader';
 import useTermica from '../../private/common/hooks/useTermica';
+import filter from '../../../content/filters/LN/home/subHeaderFilter';
 
 const SubHeader = () => {
-    const { data: dollar = [] } = useContent({ source: 'dolarSource' }) || {};
-    const weather =
-        useContent({
-            source: 'servicesSource',
-            query: {
-                id: '/clima',
-                service: 'clima'
-            }
-        }) || {};
+    const Component = (
+        <>
+            {(() => {
+                const { data: dollar = [] } =
+                    useContent({ source: 'dolarSource', staticMode: true }) ||
+                    {};
+                const weather =
+                    useContent({
+                        source: 'servicesSource',
+                        query: {
+                            id: '/clima',
+                            service: 'clima'
+                        },
+                        staticMode: true,
+                        filter
+                    }) || {};
 
-    const dollarValue = useTermica('dolar', dollar);
-    const weatherValue = useTermica('weather', weather);
-
+                const dollarValue = useTermica('dolar', dollar);
+                const weatherValue = useTermica('weather', weather);
+                return (
+                    <Subheader dollar={dollarValue} weather={weatherValue} />
+                );
+            })()}
+        </>
+    );
     return (
         <Static id="StaticSubHeader" htmlOnly persistent>
-            <Subheader dollar={dollarValue} weather={weatherValue} />
+            {Component}
         </Static>
     );
 };
