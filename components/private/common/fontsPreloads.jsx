42a929aededac4e32b7d8288b51a8d5dc1b12fe7
Julio Quintana <jquintana@lanacion.com.ar>
2022-08-29 14:07:47 -0300

    Limpieza de comentarios

--- components/private/common/fontsPreloads.jsx
+++ components/private/common/fontsPreloads.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FONT_BOLD, FONT_MEDIUM } from 'fusion:environment';
+import { FONT_BOLD } from 'fusion:environment';
 import { useAppContext } from 'fusion:context';
 import { getWebFont } from './fontface';
 
@@ -15,17 +15,6 @@ const FontPreloads = () => {
                 href={getWebFont({ font: FONT_BOLD, contextPath, deployment })}
                 crossOrigin=""
             />
-            {/* <link
-                rel="preload"
-                as="font"
-                type="font/woff2"
-                href={getWebFont({
-                    font: FONT_MEDIUM,
-                    contextPath,
-                    deployment
-                })}
-                crossOrigin=""
-            /> */}
         </>
     );
 };
