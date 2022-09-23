fca32cfbb3330cf93daf94a388836a4cb2611656
Marcelo Fanego <pt_mfanego@lnexternos.com.ar>
2022-09-23 15:44:06 -0300

    Se corrigen y mejoran testeos de breadcrumb columnistas.

--- __tests__/components/private/LN/acumulado/breadcrumbs/breadcrumbColumnista.test.js
+++ __tests__/components/private/LN/acumulado/breadcrumbs/breadcrumbColumnista.test.js
@@ -4,16 +4,21 @@ import '@testing-library/jest-dom';
 import BreadcrumbColumnista from '../../../../../../components/private/LN/acumulado/breadcrumb/breadcrumbColumista';
 
 describe('components - private - LN - acumulado - breadcrumbs - BreadcrumbColumnista', () => {
-    test('Should render columnista breadcrumb correctly', () => {
-        const { container } = render(
-            <BreadcrumbColumnista host="https://www.lanacion.com.ar" />
-        );
-        const columnista = container.getElementsByClassName('com-text');
+    const { container } = render(
+        <BreadcrumbColumnista host="https://www.lanacion.com.ar" />
+    );
+    const link = screen.queryByRole('link');
+    const columnista = container.getElementsByClassName('com-text');
 
+    test('Should render columnista breadcrumb correctly with two bullets and "Columnistas"', () => {
         expect(container.getElementsByClassName('--bullet')).toHaveLength(2);
-        expect(screen.queryAllByRole('link')).toHaveLength(1);
         expect(columnista).toHaveLength(1);
         expect(columnista[0].innerHTML).toMatch(/Columnistas/gi);
         expect(container).toMatchSnapshot();
+    });
+    test('Should have LA NACION link on breadcrumb', () => {
+        expect(link).toBeDefined();
+        expect(link.innerHTML).toMatch(/LA NACION/gi);
+        expect(link).toHaveAttribute('href', 'https://www.lanacion.com.ar/');
     });
 });
