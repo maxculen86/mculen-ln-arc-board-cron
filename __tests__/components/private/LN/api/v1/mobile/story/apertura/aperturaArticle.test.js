62e74fb97318c79bfcb331680ad3f6fa7ca58bd9
Matias Velasquez <mvelasquez@lanacion.com.ar>
2022-10-06 15:33:50 -0300

    imagenes por defecto sandbox and prod

--- __tests__/components/private/LN/api/v1/mobile/story/apertura/aperturaArticle.test.js
+++ __tests__/components/private/LN/api/v1/mobile/story/apertura/aperturaArticle.test.js
@@ -2,7 +2,7 @@ import aperturaArticle from '../../../../../../../../../components/private/LN/ap
 import article from '../../../../../../../../../__mocks__/data/articles/SGLHVRAV2VGFHB5OZZ57PKYAVQ.json';
 
 describe('Test aperura article imagen/video validacion defensiva', () => {
-    test('Validacion', () => {
+    test('Medio destacado Básico "Origen del vídeo" con una imagen', () => {
         const resp = aperturaArticle(article);
     });
 });
