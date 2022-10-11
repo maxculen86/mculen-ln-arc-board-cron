08ec423bd245a072a86ab90598ce4a33bd8dd9f0
Matias Velasquez <mvelasquez@lanacion.com.ar>
2022-10-11 11:57:36 -0300

    test enviroment

--- __tests__/components/private/LN/api/v1/mobile/story/apertura/aperturaArticle.test.js
+++ __tests__/components/private/LN/api/v1/mobile/story/apertura/aperturaArticle.test.js
@@ -1,8 +1,13 @@
 import aperturaArticle from '../../../../../../../../../components/private/LN/api/v1/mobile/story/apertura/aperturaArticle';
 import article from '../../../../../../../../../__mocks__/data/articles/SGLHVRAV2VGFHB5OZZ57PKYAVQ.json';
 import Image from '../../../../../../../../../components/private/LN/api/v1/mobile/story/cuerpo/elements/image';
-import imageDefault from '../../../../../../../../../components/private/LN/api/v1/mobile/story/apertura/prodImageDefault.json';
+import imageDefault from '../../../../../../../../../components/private/LN/api/v1/mobile/story/apertura/devImageDefault.json';
 
+jest.mock('fusion:environment', () => {
+    return {
+        IS_SANDBOX: 'true'
+    };
+});
 describe('Test aperura article imagen/video validacion defensiva', () => {
     beforeEach(() => {
         jest.clearAllMocks();
@@ -17,70 +22,6 @@ describe('Test aperura article imagen/video validacion defensiva', () => {
         console.error.mockRestore();
     });
     test('Medio destacado Básico "Origen del vídeo" con una imagen', () => {
-        const x = {
-            titulo: 'Test nota apertura video validacion defensiva',
-            tituloMobile: '',
-            bajada: 'subtitulo',
-            video: {
-                _t: 'video',
-                duration: 74441,
-                showAd: '1',
-                title: 'Cementerio de la Recoleta',
-                multimediaFile: {
-                    _t: 'mmf',
-                    width: 1280,
-                    height: 720,
-                    url:
-                        'https://d20x44kddxtp6m.cloudfront.net/wp-lanacionar/Cristina_Escobedo_Andrea_Marotta/20221003/633b520c562898170264b12f/t_b735440f53f3435c9b1fda9e10d139ec_name_Recoleta/file_1280x720-2000-v3_1.mp4'
-                },
-                thumbnailImage: {
-                    _t: 'mmi',
-                    order: 0,
-                    src:
-                        'https://d3us6z9haan6vf.cloudfront.net/10-03-2022/t_dac1edc00ce444eea90ed80f0276d68d_name_file_1280x720_2000_v3_1_.jpg'
-                }
-            },
-            imagenes: [
-                {
-                    _t: 'image',
-                    url:
-                        'https://resizer.glanacion.com/resizer/{{param}}/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/2D6RL7VJS5DQHBSCJZ3LTOEGMU.png',
-                    parameters: [
-                        {
-                            media: 309,
-                            width: 309,
-                            signature:
-                                'n4SG0ut-TNt39E-NefRqRDZzmoo=/309x206/smart/filters:format(webp):quality(80)'
-                        },
-                        {
-                            media: 351,
-                            width: 351,
-                            signature:
-                                '5d5rFuWv5BF5X3iBCWj3GMT3szY=/351x234/smart/filters:format(webp):quality(80)'
-                        },
-                        {
-                            media: 768,
-                            width: 768,
-                            signature:
-                                '0q_664IeizO4-5QUPNOLDFSIrog=/768x512/smart/filters:format(webp):quality(80)'
-                        },
-                        {
-                            media: 1280,
-                            width: 879,
-                            signature:
-                                'lIKyUHy4R5Xl_nmmpv0KVXvC6Tk=/879x586/smart/filters:format(webp):quality(80)'
-                        },
-                        {
-                            media: 1200,
-                            width: 1200,
-                            signature:
-                                '6THsUh8sQN9JKk8-po1TsdGywoM=/1200x800/smart/filters:format(webp):quality(80)'
-                        }
-                    ],
-                    epigraph: 'Placeholder16-9@3x'
-                }
-            ]
-        };
         const image = Image(imageDefault);
         const resp = aperturaArticle(article);
         expect(resp.imagenes).not.toBeUndefined();
