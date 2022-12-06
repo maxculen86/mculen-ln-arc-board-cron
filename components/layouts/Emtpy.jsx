98b8458c163bbbd9d4ceb37c221f0ec9442c3f42
Machi <jmachicado@lnexternos.com.ar>
2022-12-06 15:47:00 -0300

    tests y mejora loading redirect server side

--- components/layouts/Emtpy.jsx
+++ components/layouts/Emtpy.jsx
@@ -1,41 +1,50 @@
+/* eslint-disable react/no-danger */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { SITE_LANACION } from 'fusion:environment';
 import PropTypes from 'fusion:prop-types';
 import Consumer from 'fusion:consumer';
 import LoadingIcon from '../private/LN/common/loadingIcon';
 import Text from '../private/common/text';
-import getQueryParamValue from '../private/common/utils/getQueryParamValue';
+import redirectNota from '../private/common/utils/redirectNota';
 
 const Emtpy = props => {
-    const { children, globalContent, requestUri } = props;
-    if (globalContent.redirectNotaAsp) {
-        if (typeof window === 'undefined') {
-            return <></>;
-        }
-        const queryObt = getQueryParamValue(
-            'nota_id',
-            SITE_LANACION + requestUri
-        );
-        window.location.replace(
-            `${SITE_LANACION}/${queryObt.replace('/', '')}`
-        );
+    const {
+        children,
+        globalContent: { redirectNotaAsp } = {},
+        requestUri = ''
+    } = props;
+    const redirectUrl = redirectNota({
+        redirect: redirectNotaAsp,
+        requestUri
+    });
+    if (redirectUrl) {
         return (
-            <div id="wrapper" className="error404">
-                <main>
-                    <div className="lay">
-                        <LoadingIcon />
-                        <Text
-                            font="arial"
-                            extraClass="hlp-text-center"
-                            weight="bold"
-                            tag="h2"
-                        >
-                            Redirigiendo ...
-                        </Text>
-                    </div>
-                </main>
-            </div>
+            <>
+                <div id="wrapper" className="error404">
+                    <main>
+                        <div className="lay">
+                            <LoadingIcon />
+                            <Text
+                                font="arial"
+                                extraClass="hlp-text-center"
+                                weight="bold"
+                                tag="h2"
+                            >
+                                Redirigiendo ...
+                            </Text>
+                        </div>
+                    </main>
+                </div>
+                <script
+                    type="text/javascript"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+        window.addEventListener('DOMContentLoaded', () => {
+            window.location.replace("${redirectUrl}");
+        })`
+                    }}
+                />
+            </>
         );
     }
     return <>{children}</>;
