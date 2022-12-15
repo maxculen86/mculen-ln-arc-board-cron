683e188af7c99a802f62cc527e4e5b8d62f3f6f8
tcabral <tcabral@lanacion.com.ar>
2022-12-15 14:55:45 -0300

    New Layout Home LN10 -Add Caja apertura Chain -Add Caja Bomba Chain -Add Caja Canal Chain -Add Caja Collection LN10 chain -Add Caja Manual LN10 chain -Add new home layout in ranking helper -Add new layout LN10-Home_Main -Add config for new LN10 layout -Add layout to checkHydrate -Add sectionHelperLN10 and validateSectionHomeLN10 -Add home layout to prop sites

---
+++ components/chains/LN10_Caja_Apertura/default.jsx
@@ -0,0 +1,163 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import Consumer from 'fusion:consumer';
+import PropTypes from 'fusion:prop-types';
+import {
+    cajaTemasCustomsFields,
+    getCommonProps,
+    getChildrenFromAperturaHome,
+    getChildrenFromSectionHome
+} from '../../private/LN/common/utils/cajaTemasHelper';
+import { validateChainManual } from '../../private/LN/common/utils/cajaTemasValidators';
+import CajaTema from '../../private/LN/common/cajaTema';
+import PageBuilderMessage from '../../private/LN/home/common/components/pageBuilderMessage/pageBuilderMessage';
+import {
+    customFieldValidation,
+    childrenValidation
+} from '../utils/contentValidations';
+import { productClickFromClient } from '../../private/common/utils/viewability';
+import StaticContent from '../../private/common/staticContent';
+import setFilteredChildren from '../../private/LN/common/utils/setFilteredChildren';
+
+const CajaApertura = props => {
+    const {
+        id: featureId,
+        isAdmin,
+        customFields: { url, title, layout = '', imageId, hideTitle, hideCaja },
+        outputType,
+        childProps,
+        children,
+        renderables = [],
+        layout: pageLayout
+    } = props;
+
+    if (hideCaja)
+        return (
+            <StaticContent id={featureId}>
+                <></>
+            </StaticContent>
+        );
+
+    const {
+        notesQuantity,
+        classCondition,
+        position,
+        sectionName,
+        positionInsideSection
+    } = getCommonProps(props);
+
+    const aperturasChildren = getChildrenFromAperturaHome(
+        renderables,
+        childProps
+    );
+
+    const multimediaChildren = getChildrenFromSectionHome(
+        renderables,
+        'Multimedia',
+        5
+    );
+
+    const isInApertura = customFieldValidation({
+        featureId,
+        sectionChildren: aperturasChildren
+    });
+
+    const isMultimedia = customFieldValidation({
+        featureId,
+        sectionChildren: multimediaChildren
+    });
+
+    const features = renderables.filter(r => r.collection === 'features');
+    const multimediaCustomFields = ['video', 'html'];
+    const filteredChildren = setFilteredChildren({
+        features,
+        children,
+        conditions: {
+            feature: f => f.type !== 'LN-acumulado/timeline',
+            children: layout !== 'grillaUltimasNoticias'
+        }
+    });
+
+    const [isVideoBackground, containsHTML] = multimediaCustomFields.map(
+        customField =>
+            childrenValidation({
+                featureId,
+                customField,
+                sectionChildren: multimediaChildren
+            })
+    );
+
+    const error = validateChainManual(
+        childProps,
+        layout,
+        isInApertura,
+        isVideoBackground,
+        containsHTML
+    );
+
+    if (isAdmin && error) {
+        return (
+            <div
+                style={{
+                    marginTop: '10px',
+                    marginBottom: '10px',
+                    width: '100%'
+                }}
+            >
+                <PageBuilderMessage
+                    key={featureId}
+                    type={error.type}
+                    message={error.message}
+                />
+            </div>
+        );
+    }
+
+    if (error) return <></>;
+
+    const Component = (
+        <CajaTema
+            title={title}
+            hideTitle={hideTitle}
+            url={url}
+            imageId={imageId}
+            outputType={outputType}
+            layout={layout}
+            classCondition={`${classCondition}${(isInApertura &&
+                layout.includes('focal') &&
+                ' --apertura') ||
+                ''}`}
+            notesQuantity={notesQuantity}
+            position={position}
+            positionInsideSection={positionInsideSection}
+            sectionName={sectionName}
+            _children={filteredChildren}
+            handleClick={productClickFromClient}
+            features={features}
+            pageLayout={pageLayout}
+            isMultimedia={isMultimedia}
+        />
+    );
+    return isMultimedia ? (
+        Component
+    ) : (
+        <StaticContent id={featureId}>{Component}</StaticContent>
+    );
+};
+
+CajaApertura.label = 'LN10 Caja Apertura';
+
+CajaApertura.propTypes = {
+    id: PropTypes.string.isRequired,
+    isAdmin: PropTypes.bool.isRequired,
+    outputType: PropTypes.string,
+    customFields: PropTypes.shape({
+        ...cajaTemasCustomsFields('cajaManual')
+    }).isRequired
+};
+
+CajaApertura.defaultProps = {
+    outputType: 'default'
+};
+
+export default Consumer(CajaApertura);
