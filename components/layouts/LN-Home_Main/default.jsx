efe43a043b9ecf9afd5ba7d90c7166bc1d043bad
JQuintana <jquintana@lanacion.com.ar>
2022-12-16 10:54:20 -0300

    Init package ui

--- components/layouts/LN-Home_Main/default.jsx
+++ components/layouts/LN-Home_Main/default.jsx
@@ -3,7 +3,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Consumer from 'fusion:consumer';
-import { Button } from '@ln/common-ui-button';
 import Header from '../../private/LN/common/header';
 import Footer from '../../private/LN/common/footer';
 import GlobalProvider from '../../private/common/context/globalContext';
@@ -27,8 +26,6 @@ import {
 import createBannersIntersectionObserver from '../../private/common/banners/createBannersIntersectionObserver';
 import StaticContent from '../../private/common/staticContent';
 import bannersHome from '../../private/common/banners/bannersDivHome';
-
-import '../../../resources/packages/css/@ln/common-ui-button/index.css';
 
 const LNMainHome = props => {
     const { children, outputType, isAdmin, renderables } = props;
