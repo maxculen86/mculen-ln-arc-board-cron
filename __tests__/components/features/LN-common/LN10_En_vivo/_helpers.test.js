5508d9ec13bb5271d0c45cf29db46342c02bc3a0
Juan Carlos Diaz <jcDiaz@lanacion.com.ar>
2022-12-23 12:54:06 -0300

    Se crea feature EnVivo.
    - Se crean los grupos de customFields para manejar la chapita, los titulos y los ids de notas.
    - Se suma validacion para el caso de que agreguen un ID de nota incorrecto.
    - Se crea la estructura del directorio del feature.
    - Se crea un Buiild para evaluar cuando mostrar una alerta o no y parqa cargar las notas.

---
+++ __tests__/components/features/LN-common/LN10_En_vivo/_helpers.test.js
@@ -0,0 +1,13 @@
+import { calculateTimePublish } from '../../../../../components/features/LN-common/LN10_En_Vivo/_helpers';
+
+describe('Tests helpers component EnVivo', () => {
+    // const mockActualMinutes = new Date(1);
+    // const spy = jest
+    //     .spyOn(global, 'Date')
+    //     .mockImplementationOnce(() => mockActualMinutes)
+
+    const publishDate = '2022-12-22T12:00:24.647Z';
+    test('Deberia retornar que fue publicado hace un minuto', () => {
+        expect(calculateTimePublish(publishDate)).toStrictEqual('Hace 1 min');
+    });
+});
--- components/private/common/utils/getTitleText.js
+++ components/private/common/utils/getTitleText.js
@@ -1,9 +1,9 @@
-const getTitleText = (headlines = {}, label = '') => {
+const getTitleText = (headlines = {}, label = '', withVolanta = true) => {
     const { basic = '', mobile = '' } = headlines;
     const volanta = label && label.volanta && label.volanta.text;
     const volantaComponent = volanta ? `${volanta} ` : '';
     const titleText = `${mobile || basic}`;
-    return `${volantaComponent}${titleText}`;
+    return withVolanta ? `${volantaComponent}${titleText}` : titleText;
 };
 
 export default getTitleText;
