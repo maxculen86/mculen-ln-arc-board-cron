1259d9fae2c80a9a318e55b7e8840b6e9435594c
Marcelo Fanego <pt_mfanego@lnexternos.com.ar>
2023-01-05 18:00:51 -0300

    Se corrige comportamiento de resizer v2 en entornos, modificando la funcion replaceUrlResizerToWWW. Se agregan parcialmente tests de los casos. Además se cambia SITE_LANACION de dev a sandbox.lanacion.com.ar. Por ultimo, se corrige importacion de script crypto en scriptHelper, ya que se debe importar con features CrypoCarrousel y cajaCrypto, ya que cajaDolar no utiliza mas script cripto.

--- content/sources/utils/replaceUrlResizerToWWW.js
+++ content/sources/utils/replaceUrlResizerToWWW.js
@@ -2,13 +2,16 @@ import { RESIZER_URL_PUBLIC, API_ENV, SITE_LANACION } from 'fusion:environment';
 import getProperties from 'fusion:properties';
 
 const replaceUrlResizerToWWW = (originalPromoItems = {}) => {
-    const { host = 'https://www.lanacion.com.ar' } =
+    const { host = 'https://www.lanacion.com.ar/' } =
         getProperties('la-nacion-ar') || {};
     const { url = '', type, resized_urls: resizedUrls } = originalPromoItems;
 
-    // TODO: hacer test sobre esta funcio que cambia la url segun si usa glanacion public
+    // TODO: limpiar logica de resizer v1 y v2 cuando se deje de usar v1
+
     const replaceUrlToWWW = _url => {
-        if (API_ENV === 'prod') {
+        const isV2 = new RegExp(/\/resizer\/v2\//).test(_url);
+
+        if (!isV2 || API_ENV === 'prod') {
             return _url.replace(RESIZER_URL_PUBLIC, host);
         }
         return _url.replace(RESIZER_URL_PUBLIC, SITE_LANACION);
