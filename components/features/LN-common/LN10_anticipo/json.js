e44c16b012ac42dc555f94212c53a1852ae75d18
Matias Velasquez <mvelasquez@lanacion.com.ar>
2023-01-06 16:48:44 -0300

    feature

---
+++ components/features/LN-common/LN10_anticipo/json.js
@@ -0,0 +1,18 @@
+const LN10CajaAnticipo = ({
+    customFields: { hide, title, link, textBadge, lead, video }
+}) => {
+    if (!title) return null;
+
+    return {
+        information: {
+            hideCaja: hide || false,
+            title,
+            url: link,
+            textBadge,
+            lead,
+            video
+        }
+    };
+};
+
+export default LN10CajaAnticipo;
--- components/private/LN/api/global/utils/getSectionAliasbyFeature.js
+++ components/private/LN/api/global/utils/getSectionAliasbyFeature.js
@@ -2,6 +2,7 @@ const getSectionAliasbyFeature = nameContainer => {
     const boxToSetSectionAliasMobile = {
         'LN-common/anexo': 'AnexoMobile',
         'LN-common/anexoMobile': 'AnexoMobile',
+        'LN-common/cajaAnticipo': 'Anticipo',
         'LN-acumulado/timeline': 'Timeline'
     };
 
