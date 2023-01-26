fa5e2416e46656b63846bafd08368aedfc1f0be5
Andrea Eurrieta <deyeurrieta@gmail.com>
2023-01-26 09:27:26 -0300

    -Se importa layoutsName de config para validar los layouts

--- components/output-types/Helper/hasNotAMP.js
+++ components/output-types/Helper/hasNotAMP.js
@@ -1,11 +1,11 @@
 import Redirect from '../../../content/sources/utils/redirect';
+import config from '../../../properties/sites/la-nacion-ar';
+
+const { layoutsName = {} } = config || {};
 
 const hasNotAMP = (layout, requestUri) => {
-    const layoutsWithoutAmp = [
-        'LN-acumulado',
-        'LN-Home_Main',
-        'LN-Home_Sports'
-    ];
+    const { Home, HomeLN10, Acumulado, Deportes } = layoutsName;
+    const layoutsWithoutAmp = [Home, HomeLN10, Acumulado, Deportes];
     if (typeof window === 'undefined' && layoutsWithoutAmp.includes(layout)) {
         throw new Redirect(
             `${requestUri.replace(/[&|?]outputType=amp/, '')}`,
