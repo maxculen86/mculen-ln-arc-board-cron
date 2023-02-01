1478f3fb09f7e8fd398440d9e3b1a91dfaf963cd
Sally <seflores@lanacion.com.ar>
2023-02-01 14:57:22 -0300

    utils helper to Article

--- components/private/LN/api/v1/global/home/article/utils/helpers.js
+++ components/private/LN/api/v1/global/home/article/utils/helpers.js
@@ -1,15 +1,7 @@
 import fieldsToArticles from '../config/configFieldsArticlesByTypeChain.json';
 
-export const getFieldsInArticleByTypeChain2 = typeChain => {
-    const keyTypeChain = typeChain || 'default';
-    const boxFieldsArticlesByTypeChain = {
-        hashtag:
-            'taxonomy,distributor.name,related_content.basic,_id,last_updated_date,headlines,workflow,description,label,promo_items,canonical_website,subtype,first_publish_date,publish_date,website,website_url,taxonomy.primary_section',
-        default:
-            'taxonomy,distributor.name,related_content.basic,_id,last_updated_date,headlines,workflow,subheadlines,description,label,promo_items,canonical_website,credits,subtype,first_publish_date,publish_date,website,website_url,taxonomy.primary_section'
-    };
-
-    return boxFieldsArticlesByTypeChain[keyTypeChain];
+export const getFieldsType = typeChain => {
+    return { resp: typeChain };
 };
 
 export const getFieldsArticlesByTypeChain = typeChain => {
