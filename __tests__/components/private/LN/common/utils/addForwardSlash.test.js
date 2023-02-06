38d1c4b061fff5d5f42509063c166ea07d66c952
Andrea Eurrieta <deyeurrieta@gmail.com>
2023-02-06 14:42:28 -0300

    Se crea función addForwardSlashInParagraphsLinks y addForwardSlashInInterstitialLink con sus respectivos test

--- __tests__/components/private/LN/common/utils/addForwardSlash.test.js
+++ __tests__/components/private/LN/common/utils/addForwardSlash.test.js
@@ -1,27 +1,134 @@
-import addForwardSlash from '../../../../../../components/private/LN/common/utils/addForwardSlash';
+import addForwardSlash, {
+    addForwardSlashInInterstitialLink,
+    addForwardSlashInParagraphsLinks
+} from '../../../../../../components/private/LN/common/utils/addForwardSlash';
 
-describe('Private - LN - common - utils -> addForwardSlash', () => {
-    describe('With no string parameter', () => {
-        it('Should return null', () => {
-            expect(addForwardSlash()).toBeNull();
+describe('Private - LN - common - utils', () => {
+    describe('Tests addForwardSlash function', () => {
+        describe('With no string parameter', () => {
+            it('Should return null', () => {
+                expect(addForwardSlash()).toBeNull();
+            });
+        });
+
+        describe('With string finished in forward slash', () => {
+            it('Should return same string', () => {
+                const stringMock = 'https://www.lanacion.com.ar/';
+                expect(addForwardSlash(stringMock)).toStrictEqual(stringMock);
+            });
+        });
+
+        describe('With string NOT finished in forward slash', () => {
+            it('Should return string with added forward slash', () => {
+                expect(addForwardSlash('https://www.lanacion.com.ar')).toMatch(
+                    /(.*)\/$/gim
+                );
+                expect(
+                    addForwardSlash('https://www.lanacion.com.ar')
+                ).toStrictEqual('https://www.lanacion.com.ar/');
+            });
         });
     });
 
-    describe('With string finished in forward slash', () => {
-        it('Should return same string', () => {
-            const stringMock = 'https://www.lanacion.com.ar/';
-            expect(addForwardSlash(stringMock)).toStrictEqual(stringMock);
+    describe('Test addForwardSlashInParagraphsLinks function', () => {
+        const paragraphWithALink =
+            'Se prepara el <a href="https://www.lanacion.com.ar/feriados/2024/mayo/enero/"';
+        const paragraphWithTwoLinks = `Por otro lado, se prepara el <a href="https://www.lanacion.com.ar/feriados/2024/mayo/enero" 
+            target="_blank">almíbar</a>. Link 2do sin barra: 
+            <a href="https://www.lanacion.com.ar/feriados/2024/mayo/febrero" target="_blank">almíbar</a>`;
+        const paragraphWithTwoLinksWithoutAForwardSlash = `Por otro lado, se prepara el <a href="https://www.lanacion.com.ar/feriados/2024/mayo/enero" 
+            target="_blank">almíbar</a>. Link 2do sin barra: 
+            <a href="https://www.lanacion.com.ar/feriados/2024/mayo/febrero/" target="_blank">almíbar</a>`;
+        const paragraphWithTwoIdenticalLinksWithoutSlashesAtTheEnd = `Se prepara el <a href="https://www.lanacion.com.ar/feriados/2024/mayo" 
+            target="_blank">almíbar</a>. Link 2do 
+            <a href="https://www.lanacion.com.ar/feriados/2024/mayo" target="_blank">almíbar</a>`;
+        const paragraphWithTwoIdenticalLinksWithoutAForwardSlash = `Se prepara el <a href="https://www.lanacion.com.ar/feriados/2024/mayo" 
+            target="_blank">almíbar</a>. Link 2do 
+            <a href="https://www.lanacion.com.ar/feriados/2024/mayo/" target="_blank">almíbar</a>`;
+        const result = `Por otro lado, se prepara el <a href="https://www.lanacion.com.ar/feriados/2024/mayo/enero/" 
+            target="_blank">almíbar</a>. Link 2do sin barra: 
+            <a href="https://www.lanacion.com.ar/feriados/2024/mayo/febrero/" target="_blank">almíbar</a>`;
+        const resultIdenticalLinks = `Se prepara el <a href="https://www.lanacion.com.ar/feriados/2024/mayo/" 
+            target="_blank">almíbar</a>. Link 2do 
+            <a href="https://www.lanacion.com.ar/feriados/2024/mayo/" target="_blank">almíbar</a>`;
+
+        describe('When the paragraph has only one url', () => {
+            it('Should return the paragraph with the slash at the end of the url', () => {
+                expect(
+                    addForwardSlashInParagraphsLinks(
+                        'Se prepara el <a href="https://www.lanacion.com.ar/feriados/2024/mayo/enero"'
+                    )
+                ).toBe(paragraphWithALink);
+            });
+
+            it('Should return the paragraph with the url exactly the same when it has a forward slash', () => {
+                expect(
+                    addForwardSlashInParagraphsLinks(paragraphWithALink)
+                ).toBe(paragraphWithALink);
+            });
+        });
+
+        describe('When the paragraph has two urls exactly the same', () => {
+            it('Should return the paragraph with the urls, adding the slash at the end when they don´t have it', () => {
+                expect(
+                    addForwardSlashInParagraphsLinks(
+                        paragraphWithTwoIdenticalLinksWithoutSlashesAtTheEnd
+                    )
+                ).toBe(resultIdenticalLinks);
+            });
+
+            it('Should return the paragraph exactly the same', () => {
+                expect(
+                    addForwardSlashInParagraphsLinks(resultIdenticalLinks)
+                ).toBe(resultIdenticalLinks);
+            });
+
+            it('Should return the paragraph with the urls, adding the slash at the end only to the url that does not have it', () => {
+                expect(
+                    addForwardSlashInParagraphsLinks(
+                        paragraphWithTwoIdenticalLinksWithoutAForwardSlash
+                    )
+                ).toBe(resultIdenticalLinks);
+            });
+        });
+
+        describe('When the paragraph has two different urls', () => {
+            it('Should return the paragraph with the urls, adding the slash at the end when they don´t have it', () => {
+                expect(
+                    addForwardSlashInParagraphsLinks(paragraphWithTwoLinks)
+                ).toBe(result);
+            });
+
+            it('Should return the paragraph exactly the same', () => {
+                expect(addForwardSlashInParagraphsLinks(result)).toBe(result);
+            });
+
+            it('Should return the paragraph with the urls, adding the slash at the end only to the url that does not have it', () => {
+                expect(
+                    addForwardSlashInParagraphsLinks(
+                        paragraphWithTwoLinksWithoutAForwardSlash
+                    )
+                ).toBe(result);
+            });
         });
     });
 
-    describe('With string NOT finished in forward slash', () => {
-        it('Should return string with added forward slash', () => {
-            expect(addForwardSlash('https://www.lanacion.com.ar')).toMatch(
-                /(.*)\/$/gim
+    describe('Tests addForwardSlashInInterstitialLink function', () => {
+        const linkWithoutSlash =
+            'https://www.lanacion.com.ar/feriados/2024/mayo/enero';
+        const linkWithSlash =
+            'https://www.lanacion.com.ar/feriados/2024/mayo/enero/';
+
+        it('Should return the url exactly the same when it has slash', () => {
+            expect(addForwardSlashInInterstitialLink(linkWithSlash)).toBe(
+                linkWithSlash
             );
-            expect(
-                addForwardSlash('https://www.lanacion.com.ar')
-            ).toStrictEqual('https://www.lanacion.com.ar/');
+        });
+
+        it('Should return url with added forward slash when url not finished in forward slash', () => {
+            expect(addForwardSlashInInterstitialLink(linkWithoutSlash)).toBe(
+                linkWithSlash
+            );
         });
     });
 });
--- components/private/LN/common/utils/addForwardSlash.js
+++ components/private/LN/common/utils/addForwardSlash.js
@@ -4,4 +4,26 @@ const addForwardSlash = str => {
     return str.concat('/');
 };
 
+export const addForwardSlashInParagraphsLinks = content => {
+    if (typeof content !== 'string') return null;
+    let newContent = content;
+    const regLN = /<a href="(\S*?lanacion.com.ar\S*?)(?<!\/)"/g;
+    const links = content.match(regLN) || [];
+    const newArrayLinks = [...new Set(links)];
+
+    newArrayLinks.forEach(etiquetaA => {
+        const link = etiquetaA.replace(regLN, '$1');
+        const re = new RegExp(`"${link}"`, 'g');
+        newContent = newContent.replace(re, `"${addForwardSlash(link)}"`);
+    });
+    return newContent;
+};
+
+export const addForwardSlashInInterstitialLink = url => {
+    if (typeof url !== 'string') return null;
+    const link = url.replace(/https:\/\/"(.*?lanacion.com.ar.*?)"/g, '$1');
+    const newUrl = url.replace(link, addForwardSlash(link));
+    return newUrl;
+};
+
 export default addForwardSlash;
