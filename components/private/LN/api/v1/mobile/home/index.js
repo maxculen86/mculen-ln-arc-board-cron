a664aaf4ae82c08f11f71d56eee5abc8ac2010f6
Sally <seflores@lanacion.com.ar>
2023-02-06 11:44:59 -0300

    Adaptacion version 1 home

--- components/private/LN/api/v1/mobile/home/index.js
+++ components/private/LN/api/v1/mobile/home/index.js
@@ -11,27 +11,29 @@ let paramsFromPage = {
     rootPath:
         'https://www.lanacion.com.ar/?_website=la-nacion-ar&outputType=json'
 };
+import getTypeSection from './config/getTypeSection';
 // Sirve para agregar información a la sección o caja desde el campo sectionAliasMobile establecido en page/index.js
-const typeSection = {
-    Anticipo: { tipoSeccion: 'anticipo', idSeccion: 501 },
-    Bomba: { tipoSeccion: 'bomba', idSeccion: 102 },
-    Apertura: { tipoSeccion: 'apertura', idSeccion: 200 },
-    Anexo: { tipoSeccion: 'anexo', idSeccion: 0 },
-    AnexoMobile: { tipoSeccion: 'anexoMobile', idSeccion: 603 },
-    Opinion: { tipoSeccion: 'opinion', idSeccion: 1001 },
-    Comercial: { tipoSeccion: 'comercial', idSeccion: 1101 },
-    Banner: { tipoSeccion: 'banner' },
-    Dolar: {
-        tipoSeccion: 'dolar',
-        idSeccion: 2000,
-        tituloCaja: 'Cotización hoy',
-        url: 'https://www.lanacion.com.ar/economia/dolar/'
-    },
-    Multimedia: { tipoSeccion: 'tema', idSeccion: 305 },
-    Timeline: { tipoSeccion: 'tema', idSeccion: 3000 },
-    Aside: { tipoSeccion: 'aside', idSeccion: 306 },
-    default: { tipoSeccion: 'tema', idSeccion: 305 }
-};
+// const typeSection = {
+//     Anticipo: { tipoSeccion: 'anticipo', idSeccion: 501 },
+//     Bomba: { tipoSeccion: 'bomba', idSeccion: 102 },
+//     Apertura: { tipoSeccion: 'apertura', idSeccion: 200 },
+//     Anexo: { tipoSeccion: 'anexo', idSeccion: 0 },
+//     AnexoMobile: { tipoSeccion: 'anexoMobile', idSeccion: 603 },
+//     Opinion: { tipoSeccion: 'opinion', idSeccion: 1001 },
+//     Comercial: { tipoSeccion: 'comercial', idSeccion: 1101 },
+//     Banner: { tipoSeccion: 'banner' },
+//     Dolar: {
+//         tipoSeccion: 'dolar',
+//         idSeccion: 2000,
+//         tituloCaja: 'Cotización hoy',
+//         url: 'https://www.lanacion.com.ar/economia/dolar/'
+//     },
+//     Multimedia: { tipoSeccion: 'tema', idSeccion: 305 },
+//     Timeline: { tipoSeccion: 'tema', idSeccion: 3000 },
+//     Aside: { tipoSeccion: 'aside', idSeccion: 306 },
+//     default: { tipoSeccion: 'tema', idSeccion: 305 }
+// };
+const typeSection = getTypeSection();
 
 const featureInformation = (information, section) => {
     const type = typeSection[section] || typeSection.default;
