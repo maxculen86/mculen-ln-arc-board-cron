e63d9701c0c39893d0312546a97ef713a8015247
Matias Velasquez <mvelasquez@lanacion.com.ar>
2023-03-01 12:21:09 -0300

    mapeo de secciones

--- content/sources/utils/pageSource/pageHome/v1/bitacora/transform.js
+++ content/sources/utils/pageSource/pageHome/v1/bitacora/transform.js
@@ -1,3 +1,15 @@
+import {
+    infoLNMainLN10,
+    infoLNMain
+} from '../../../../../../../components/private/LN/api/common/home/config/configInfoSectionsByLayout';
+
+const getFeature = sectionAliasMobile => {
+    const infoEntry =
+        infoLNMainLN10[sectionAliasMobile] ??
+        infoLNMain[sectionAliasMobile] ??
+        infoLNMain.default;
+    return infoEntry.tipoSeccion;
+};
 const transform = async (dataPage, query) => {
     const {
         information: { layoutPage },
@@ -9,8 +21,27 @@ const transform = async (dataPage, query) => {
         elementsPageHome =
             elementsPageHome &&
             elementsPageHome.filter(elem => elem && elem.type === 0);
+        // return elementsPageHome;
         // Returns boxes that type equal 0, becacuse have articles
-        return elementsPageHome;
+        const cajas = elementsPageHome.map((elem, i) => {
+            const notas = elem.articles.map((article, j) => {
+                return {
+                    // eslint-disable-next-line no-underscore-dangle
+                    id_nota: article._id,
+                    url_nota: article.website_url,
+                    posicion: (j + 1).toString().padStart(2, '0')
+                };
+            });
+            return {
+                id_caja: (i + 1).toString().padStart(2, '0'),
+                visible: elem.information.hideCaja || true,
+                feature: getFeature(elem.sectionAliasMobile),
+                diagramacion_caja: elem.information.layout,
+                notas
+            };
+        });
+
+        return { cajas };
     } catch (error) {
         // eslint-disable-next-line no-console
         console.warn(
