a3bce5b273d72b4306173df885147eff8c7c843a
Matias Velasquez <mvelasquez@lanacion.com.ar>
2023-03-06 15:34:27 -0300

    caja timeline validacion

--- content/sources/utils/pageSource/pageHome/v1/bitacora/transform.js
+++ content/sources/utils/pageSource/pageHome/v1/bitacora/transform.js
@@ -3,6 +3,47 @@ import {
     infoLNMain
 } from '../../../../../../../components/private/LN/api/common/home/config/configInfoSectionsByLayout';
 
+const createNotasArray = elem => {
+    const notasArray = [];
+    const resp = {};
+    for (let j = 0; j < elem.articles.length; j += 1) {
+        const article = elem.articles[j];
+        if (article.sectionAliasMobile === 'ln-acumulado/timeline') {
+            const notas = createNotasArray(article);
+            const timelineCaja = createCaja(
+                'timeline',
+                article.information?.hideCaja,
+                getFeature(elem.sectionAliasMobile),
+                article.information?.layout,
+                notas.notasArray
+            );
+            resp.timelineCaja = timelineCaja;
+            // eslint-disable-next-line no-continue
+            continue;
+        }
+        const nota = {
+            // eslint-disable-next-line no-underscore-dangle
+            id_nota: article._id,
+            url_nota: article.website_url,
+            posicion: (j + 1).toString().padStart(2, '0')
+        };
+        notasArray.push(nota);
+    }
+    return {
+        ...resp,
+        notasArray
+    };
+};
+
+const createCaja = (id, visible, feature, layout, notas) => {
+    return {
+        id_caja: id,
+        visible: visible || true,
+        feature,
+        diagramacion_caja: layout,
+        notas
+    };
+};
 const getFeature = sectionAliasMobile => {
     const infoEntry =
         infoLNMainLN10[sectionAliasMobile] ??
@@ -15,37 +56,30 @@ const transform = async (dataPage, query) => {
         information: { layoutPage },
         content_elements: elementsPage
     } = dataPage;
-
     try {
         const omitSections = {
-            'ln-acumulado/timeline': true,
             'ln-common/opinion': true,
             'ln-common/editoriales': true
         };
-        const cajas = elementsPage
-            ?.filter(elem => elem && elem.type === 0)
-            .map((elem, i) => {
-                if (omitSections[elem.sectionAliasMobile]) {
-                    return null; // Omitir la caja
-                }
-                const notas = elem.articles.map((article, j) => {
-                    return {
-                        // eslint-disable-next-line no-underscore-dangle
-                        id_nota: article._id,
-                        url_nota: article.website_url,
-                        posicion: (j + 1).toString().padStart(2, '0')
-                    };
-                });
-                return {
-                    id_caja: (i + 1).toString().padStart(2, '0'),
-                    visible: elem.information.hideCaja || true,
-                    feature: getFeature(elem.sectionAliasMobile),
-                    diagramacion_caja: elem.information.layout,
-                    notas
-                };
-            })
-            .filter(caja => caja !== null); // Eliminar cajas nulas
+        let cajaCount = 1;
+        const cajas = [];
+        elementsPage.forEach((elem, i) => {
+            if (elem.type !== 0) return; // Ignorar elementos que no son cajas
+            if (omitSections[elem.sectionAliasMobile]) return; // Ignorar cajas que deben omitirse
 
+            const notas = createNotasArray(elem);
+            const caja = createCaja(
+                cajaCount.toString().padStart(2, '0'),
+                elem.information.hideCaja,
+                getFeature(elem.sectionAliasMobile),
+                elem.information.layout,
+                notas.notasArray
+            );
+
+            cajaCount += 1;
+            cajas.push(caja);
+            if (notas.timelineCaja) cajas.push(notas.timelineCaja);
+        });
         return { cajas };
     } catch (error) {
         // eslint-disable-next-line no-console
