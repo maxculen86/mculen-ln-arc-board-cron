50a5f1a03a499b95a747d10ef1a4ccd8893dc779
Juan Pravata <jpravata@lanancion.com.ar>
2023-03-08 10:09:55 -0300

    Se hace mas defensivo el script de permutive identify. Se le crea test a permutiveIdentify.js

--- components/private/LN/common/utils/permutiveIdentify.js
+++ components/private/LN/common/utils/permutiveIdentify.js
@@ -3,27 +3,34 @@ import handleCookie from './handleCookie';
 
 const crypto = require('crypto');
 
-const permutiveIdentify = UsuarioDetalleEmail => {
+const permutiveIdentify = usuarioDetalleEmail => {
     const { getCookie } = handleCookie();
     const idUser = getCookie('usuario%5Fid');
+    const identitiesArray = [];
+
+    isValidString(usuarioDetalleEmail) &&
+        usuarioDetalleEmail &&
+        identitiesArray.push({
+            id: crypto
+                .createHash('sha256')
+                .update(usuarioDetalleEmail)
+                .digest('hex'),
+            tag: 'email_sha256',
+            priority: 0
+        });
+
+    isValidString(idUser) &&
+        idUser &&
+        identitiesArray.push({
+            id: idUser,
+            tag: 'userID',
+            priority: 1
+        });
 
     return (
+        identitiesArray.length > 0 &&
         window.permutive &&
-        window.permutive.identify([
-            isValidString(UsuarioDetalleEmail) && {
-                id: crypto
-                    .createHash('sha256')
-                    .update(UsuarioDetalleEmail)
-                    .digest('hex'),
-                tag: 'email_sha256',
-                priority: 0
-            },
-            isValidString(idUser) && {
-                id: idUser,
-                tag: 'userID',
-                priority: 1
-            }
-        ])
+        window.permutive.identify(identitiesArray)
     );
 };
 
