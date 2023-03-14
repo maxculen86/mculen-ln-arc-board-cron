6ec67717c395480c42101641b56b0a27dc723810
Juan Pravata <jpravata@lanancion.com.ar>
2023-03-14 16:05:33 -0300

    Se agregan validaciones en link canonical para armar urls de acumulados que compartan contexto con otros acus ya existentes para que armen el canonical link a partir de siteUrl. Se ajustan test y se agrega el caso de dolar-hoy

--- components/private/common/linkCanonical.jsx
+++ components/private/common/linkCanonical.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 import PropTypes from 'fusion:prop-types';
 import addForwardSlash from '../LN/common/utils/addForwardSlash';
 import canonicalIdChecker from './utils/canonicalIdChecker';
+import { isEmptyString } from './utils/dataValidation';
 
 const LinkCanonical = (props = {}) => {
-    const { _id = '', canonicalUrl = '', host = '', nodeType = '' } = props;
+    const {
+        _id = '',
+        canonicalUrl = '',
+        host = '',
+        nodeType = '',
+        site = {},
+        template = ''
+    } = props;
+
+    const { site_url: siteUrl = null } = site;
 
     return host && (canonicalUrl || _id || nodeType === 'home') ? (
         <link
             rel="canonical"
-            href={addForwardSlash(
-                `${host}${canonicalUrl || canonicalIdChecker(_id)}`
-            )}
+            href={
+                !isEmptyString(siteUrl) &&
+                !siteUrl.includes(_id) &&
+                template.includes('page')
+                    ? siteUrl
+                    : addForwardSlash(
+                          `${host}${canonicalUrl || canonicalIdChecker(_id)}`
+                      )
+            }
         />
     ) : (
         <></>
@@ -21,6 +37,10 @@ LinkCanonical.propTypes = {
     _id: PropTypes.string.isRequired,
     canonicalUrl: PropTypes.string.isRequired,
     host: PropTypes.string.isRequired,
-    nodeType: PropTypes.string.isRequired
+    nodeType: PropTypes.string.isRequired,
+    site: PropTypes.shape({
+        site_url: PropTypes.string
+    }).isRequired,
+    template: PropTypes.string.isRequired
 };
 export default LinkCanonical;
