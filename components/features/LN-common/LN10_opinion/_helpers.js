a1d77072fab3cafa2ce448443cc0389722a0e49b
Machi <jmachicado@lnexternos.com.ar>
2023-03-17 10:57:12 -0300

    LN10 caja opinion

---
+++ components/features/LN-common/LN10_opinion/_helpers.js
@@ -0,0 +1,199 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+import PropTypes from 'fusion:prop-types';
+import { Card } from '@ln/contenidos-ui-card';
+import { cajaTemasCustomsFields } from '../../../private/LN/common/utils/cajaTemasHelperLN10';
+import getArticleInCollection from '../../../private/LN/common/hooks/useGetArticleInCollection';
+import getCardConfig, {
+    getTitleAndLeadForHome
+} from '../../../private/LN10/home/components/CommonCollection/_helper';
+import { getDataAttributesForViewability } from '../../LN-10/article/_helper';
+import getAuthorsAsString from '../../../private/common/utils/getAuthorsAsString';
+import get from '../../../private/common/utils/get';
+
+const ajusCollection = 'Ajuste Collection';
+
+export const getDataOpinion = ({
+    idCollectionOpinion,
+    idCollectionEditorial,
+    rules,
+    notesQuantity,
+    initialPosition,
+    idsArticlesToExclude,
+    diagramation,
+    isInSiteService,
+    layout,
+    website,
+    isHome
+}) => {
+    const articlesOpinion = !isInSiteService
+        ? getArticleInCollection(
+              rules.length || notesQuantity,
+              diagramation,
+              idCollectionOpinion,
+              20,
+              Number(initialPosition) - 1,
+              idsArticlesToExclude,
+              true,
+              !isInSiteService,
+              layout,
+              website,
+              isHome
+          )
+        : [];
+
+    const articlesEditorial = !isInSiteService
+        ? getArticleInCollection(
+              2,
+              diagramation,
+              idCollectionEditorial,
+              20,
+              0,
+              [],
+              true,
+              !isInSiteService,
+              layout,
+              website,
+              isHome
+          )
+        : [];
+    return {
+        articlesOpinion,
+        articlesEditorial
+    };
+};
+export const editorialRules = [
+    {
+        cardSize: 'm',
+        imagePosition: {
+            mobile: 'img-right',
+            tablet: 'img-bottom',
+            desktop: 'img-bottom'
+        }
+    },
+    {
+        cardSize: 'm',
+        imagePosition: {
+            mobile: 'img-none',
+            tablet: 'img-none',
+            desktop: 'img-none'
+        }
+    }
+];
+const createCards = ({ articles = [], skipCard = 0, rules }) => {
+    return (
+        articles.map((article, indexArray) => {
+            const index = indexArray + skipCard;
+            const {
+                cardSize,
+                marqueeImg,
+                withImage,
+                mediaData
+            } = getCardConfig(rules[index], article);
+            const { titleTag, subheadTag } = rules[index];
+
+            const extraOpts = getDataAttributesForViewability(
+                article._id,
+                '98',
+                index
+            );
+
+            const { title } = getTitleAndLeadForHome(article);
+            const author = getAuthorsAsString(article, true);
+            return (
+                <Card
+                    variant="author"
+                    title={title}
+                    href={get(article, 'website_url', '')}
+                    titleTag={titleTag}
+                    subheadTag={subheadTag}
+                    marquee={author}
+                    marqueeImg={marqueeImg}
+                    mediaData={mediaData}
+                    cardSize={cardSize}
+                    withMedia={withImage}
+                    {...extraOpts}
+                />
+            );
+        }) || []
+    );
+};
+export const getCardsOpinion = ({ articlesOpinion = [], rules }) => {
+    const quantityOpinionTop = 4;
+    const haveCardsBottom = articlesOpinion.length === 8;
+    const cardsOpinionTop = createCards({
+        articles: articlesOpinion.slice(0, quantityOpinionTop),
+        rules
+    });
+
+    const cardsOpinionBottom =
+        haveCardsBottom &&
+        createCards({
+            articles: articlesOpinion.slice(-quantityOpinionTop),
+            skipCard: quantityOpinionTop,
+            rules
+        });
+
+    return {
+        cardsOpinionTop: cardsOpinionTop || [],
+        cardsOpinionBottom: cardsOpinionBottom || []
+    };
+};
+
+export const getCardsEditorial = (articlesEditorial = []) => {
+    return (
+        articlesEditorial.map((article, index) => {
+            const { cardSize, imagePosition, mediaData } = getCardConfig(
+                editorialRules[index],
+                article
+            );
+
+            const extraOpts = getDataAttributesForViewability(
+                article._id,
+                '99',
+                index
+            );
+
+            const { title } = getTitleAndLeadForHome(article);
+            return (
+                <Card
+                    title={title}
+                    href={get(article, 'website_url', '')}
+                    mediaData={mediaData}
+                    cardSize={cardSize}
+                    imagePosition={imagePosition}
+                    {...extraOpts}
+                />
+            );
+        }) || []
+    );
+};
+
+export const customFieldsOpinion = {
+    idCollectionOpinion: PropTypes.string.tag({
+        label: 'ID opinion',
+        description: 'Ingrese aquí el ID de la collection de opinion',
+        defaultValue: '',
+        group: ajusCollection,
+        hidden: false
+    }).isRequired,
+    idCollectionEditorial: PropTypes.string.tag({
+        label: 'ID editorial',
+        description: 'Ingrese aquí el ID de la collection de editorial',
+        defaultValue: '',
+        group: ajusCollection,
+        hidden: false
+    }).isRequired,
+    ...cajaTemasCustomsFields('cajaOpinion'),
+    navigator: PropTypes.string.tag({
+        name: 'Navegador',
+        description:
+            'Ingrese aquí el nombre de una navegación creada en site services',
+        defaultValue: '',
+        group: 'Techo'
+    })
+};
+
+export default customFieldsOpinion;
