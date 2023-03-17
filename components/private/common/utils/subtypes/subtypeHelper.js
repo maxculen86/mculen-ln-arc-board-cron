fdc4e85ae68111633ca6c8162c789ae699955616
fsanchez <fsanchez@lanacion.com.ar>
2023-03-17 15:26:29 -0300

    Se quita el tag amphtml de la sección '/juegos/'

--- components/private/common/utils/subtypes/subtypeHelper.js
+++ components/private/common/utils/subtypes/subtypeHelper.js
@@ -43,7 +43,7 @@ export const isFotoAl100orStorytelling = subtype => {
 };
 
 export const subtypeNotesWithoutAmp = canonicalUrl => {
-    const subtypeList = ['/agencias', '/estados-unidos/'];
+    const subtypeList = ['/agencias', '/estados-unidos/', '/juegos/'];
 
     return subtypeList.some(subtype => canonicalUrl.startsWith(subtype));
 };
