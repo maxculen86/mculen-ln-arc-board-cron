9cbbb2bff9579e6900d8a45b498a6a6567731d5c
Sally <seflores@lanacion.com.ar>
2023-03-20 20:53:08 -0300

    Adaptacion para no enviar anexo con html en LN10

--- components/private/LN/api/v1/mobile/home/article/cardAnexo/index.js
+++ components/private/LN/api/v1/mobile/home/article/cardAnexo/index.js
@@ -3,9 +3,11 @@ import get from '../../../../../../../common/utils/get';
 export const CardAnexo = article => {
     const url = get(article[0], 'url', null);
     const alto = get(article[0], 'alto', null);
-    if (url && alto) return [{ src: url, alto }];
-    if (url) return null;
-    return [{ src: get(article[0], 'html') }];
+    if (url && alto) return [{ src: url, url, alto }];
+    // TODO: Reemplazar cuando Mobile confirme la adaptacion para recibir html
+    return null;
+    // if (url) return null;
+    // return [{ src: get(article[0], 'html') }];
 };
 
 export default CardAnexo;
