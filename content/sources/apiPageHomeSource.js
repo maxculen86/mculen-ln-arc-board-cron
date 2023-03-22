3700d69fb4967d6b6604a817fe53fc7b8ea76983
Sally <seflores@lanacion.com.ar>
2023-03-22 13:53:05 -0300

    Parametro cookie dinamico en Api Home Source

--- content/sources/apiPageHomeSource.js
+++ content/sources/apiPageHomeSource.js
@@ -25,6 +25,11 @@ const fetch = async (query, { cachedCall } = {}) => {
             transformPage: { 1: transformv1, 2: transformv2 },
             transformHome: { 1: homev1, 2: homev2 }
         },
+        homeLN10: {
+            aliasPage: '/homepage-ln10',
+            transformPage: { 1: transformv1, 2: transformv2 },
+            transformHome: { 1: homev1, 2: homev2 }
+        },
         sports: {
             aliasPage: '/deportes',
             transformPage: { 1: transformv1, 2: transformv2 },
@@ -41,6 +46,7 @@ const fetch = async (query, { cachedCall } = {}) => {
         const version = get(query, 'versionUri', 1);
         const versionDeploy = get(query, 'versionDeploy', null);
         const alias = get(query, 'namePage', 'home');
+        const cookie = get(query, 'useCookie', null);
         let configItemPage = configPages[alias];
 
         if (!configItemPage) {
@@ -66,7 +72,8 @@ const fetch = async (query, { cachedCall } = {}) => {
             ticksCache,
             website,
             isPage: true,
-            versionDeploy
+            versionDeploy,
+            cookie
         };
 
         const resultPage = await cachedCall(keyCachedCall, pages.fetch, {
@@ -127,7 +134,8 @@ export default {
         versionUri: 'text',
         namePage: 'text',
         ticks: 'text',
-        versionDeploy: 'text'
+        versionDeploy: 'text',
+        useCookie: 'text'
     },
     ttl: 120
 };
