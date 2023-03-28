56b9ceb8a52110db0c03e5fe359bc1bd62ad3799
Matias Velasquez <mvelasquez@lanacion.com.ar>
2023-03-28 16:29:14 -0300

    eslint fix

--- content/sources/apiPageHomeSource.js
+++ content/sources/apiPageHomeSource.js
@@ -100,7 +100,7 @@ const fetch = async (query, { cachedCall } = {}) => {
             queryParams
         );
         // Para revisar la data formateada con la informacion de todas la secciones
-        //return resultPageTransform;
+        // return resultPageTransform;
 
         if (
             !configItemPage.transformHome ||
