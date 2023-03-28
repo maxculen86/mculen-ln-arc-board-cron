e0ed9db4a18056298793bcfa90b987fbac49e60d
fsanchez <fsanchez@lanacion.com.ar>
2023-03-28 14:15:10 -0300

    Se le coloca la barra final a agencias para que quede la misma convención en todos los casos

--- components/private/common/utils/subtypes/subtypeHelper.js
+++ components/private/common/utils/subtypes/subtypeHelper.js
@@ -45,7 +45,7 @@ export const isFotoAl100orStorytelling = subtype => {
 };
 
 export const subtypeNotesWithoutAmp = canonicalUrl => {
-    const subtypeList = ['/agencias', '/estados-unidos/', '/juegos/'];
+    const subtypeList = ['/agencias/', '/estados-unidos/', '/juegos/'];
 
     return subtypeList.some(subtype => canonicalUrl.startsWith(subtype));
 };
