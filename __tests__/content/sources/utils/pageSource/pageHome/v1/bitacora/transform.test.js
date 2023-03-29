b57bd93ea14df2891d1cfe2b826fe41ab83d909c
Matias Velasquez <mvelasquez@lanacion.com.ar>
2023-03-29 15:59:16 -0300

    fix test

--- __tests__/content/sources/utils/pageSource/pageHome/v1/bitacora/transform.test.js
+++ __tests__/content/sources/utils/pageSource/pageHome/v1/bitacora/transform.test.js
@@ -1,3 +1,4 @@
+import 'regenerator-runtime/runtime';
 import * as resultPage from '../../../../../../../../__mocks__/data/LN10_Bitacora/resultPageLN10.json';
 import transform from '../../../../../../../../content/sources/utils/pageSource/pageHome/v1/bitacora/transform';
 describe('Transform bitacora v1 test', () => {
