d59131367485fb77b76d2b1f2e76ae13d98766e8
Sally <seflores@lanacion.com.ar>
2023-04-04 14:48:08 -0300

    default url Editoriales LN10

--- components/private/LN/api/common/home/boxInformation/LN10/boxes/boxInfoEditoriales.js
+++ components/private/LN/api/common/home/boxInformation/LN10/boxes/boxInfoEditoriales.js
@@ -5,14 +5,12 @@ export const boxInfoEditorial = (information, section, typeSection) => {
     if (box) {
         box.tituloCaja = 'Editoriales';
         box.diagramacion = 'editoriales2';
+        box.url = 'https://www.lanacion.com.ar/editoriales/';
         if (!box.parameters) {
             box.parameters = {};
         }
         if (box.parameters) {
             box.parameters.title = 'Editoriales';
-        }
-        if (box.parameters && box.parameters.url) {
-            box.url = 'https://www.lanacion.com.ar/editoriales/';
             box.parameters.url = 'https://www.lanacion.com.ar/editoriales/';
         }
     }
