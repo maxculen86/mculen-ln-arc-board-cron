cc061d0d5bdddc3c19ffd73818c1d396a12258e3
Sally <seflores@lanacion.com.ar>
2023-04-11 16:07:57 -0300

    chapita Exclusivo Suscriptor

--- components/private/LN/api/v1/mobile/home/article/elements/chapita/index.js
+++ components/private/LN/api/v1/mobile/home/article/elements/chapita/index.js
@@ -1,8 +1,36 @@
 import get from '../../../../../../../../common/utils/get';
 
-export const badgeStyles = {
-    'exclusive-ln': 'Exclusivo suscriptor'
-};
+export const listBadgetsByConfigs = [
+    {
+        fieldsBadge: {
+            badgeStyle: 'exclusive-ln',
+            badge: 'EXCLUSIVO SUSCRIPTOR',
+            chapita: 'EXCLUSIVO SUSCRIPTOR'
+        },
+        Equal: [
+            {
+                key: 'informationBox.sectionAliasMobile',
+                value: 'sub-exclusive'
+            },
+            {
+                key: 'additionalProperties.chapitaStyle',
+                value: 'exclusive-ln'
+            },
+            {
+                key: 'label.chapita.text',
+                value: 'EXCLUSIVO SUSCRIPTOR'
+            },
+            {
+                key: 'content_restrictions.content_code',
+                value: 'cerrada'
+            },
+            {
+                key: 'additionalProperties.chapita',
+                value: 'EXCLUSIVO SUSCRIPTOR'
+            }
+        ]
+    }
+];
 
 export const getArticleChapitaStyle = article => {
     return get(article, 'additionalProperties.chapitaStyle', null);
@@ -11,12 +39,40 @@ export const getArticleChapitaStyle = article => {
 export const getArticleChapita = article => {
     const originalTag = get(article, 'label.chapita.text', null);
     const tag = get(article, 'additionalProperties.chapita', null);
-    const tagConfig =
-        getArticleChapitaStyle(article) &&
-        badgeStyles[getArticleChapitaStyle(article)]
-            ? badgeStyles[getArticleChapitaStyle(article)]
-            : null;
 
-    const result = tagConfig || originalTag || tag || null;
+    const result = originalTag || tag || null;
     return result ? result.toUpperCase() : result;
+};
+
+export const getBadgebyConfig = article => {
+    let fieldsBadge = {};
+    listBadgetsByConfigs &&
+        listBadgetsByConfigs.some(configBadge => {
+            return (
+                configBadge &&
+                configBadge.Equal &&
+                configBadge.Equal.some(configEqual => {
+                    if (
+                        (
+                            get(article, configEqual.key, '') || ''
+                        ).toLowerCase() === configEqual.value.toLowerCase()
+                    ) {
+                        fieldsBadge = configBadge.fieldsBadge;
+                        return true;
+                    }
+                    return false;
+                })
+            );
+        });
+
+    if (!fieldsBadge.badgeStyle) {
+        fieldsBadge.badgeStyle = getArticleChapitaStyle(article);
+    }
+    if (!fieldsBadge.badge) {
+        fieldsBadge.badge = getArticleChapita(article);
+    }
+
+    fieldsBadge.chapita = fieldsBadge.badge;
+
+    return fieldsBadge;
 };
