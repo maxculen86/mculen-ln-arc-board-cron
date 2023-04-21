a4adfa113124310229d5513d740dec886c84fdcc
Sally <seflores@lanacion.com.ar>
2023-04-21 13:12:17 -0300

    Adaptacion test Caja Apertura

--- __tests__/components/chains/LN10_Caja_Manual/json.test.js
+++ __tests__/components/chains/LN10_Caja_Manual/json.test.js
@@ -1,21 +1,9 @@
 import GetCajaManual from '../../../../components/private/LN/api/global/components/chains/LN10/getCajaManual';
-import LN10CajaApertura from '../../../../components/chains/LN10_Caja_Apertura/json';
+import LN10CajaManual from '../../../../components/chains/LN10_Caja_Manual/json';
 
 jest.mock('fusion:consumer', component => {
     return function(component) {
         return component;
-    };
-});
-
-jest.mock('../../../../components/chains/utils/getChildrenBySection.js', () => {
-    return {
-        __esModule: true,
-        default: params => {
-            if (!params) {
-                return null;
-            }
-            return params.renderables;
-        }
     };
 });
 
@@ -24,6 +12,12 @@ jest.mock(
     () => {
         return {
             __esModule: true,
+            default: children => {
+                if (!children) {
+                    return null;
+                }
+                return children;
+            },
             validateChildrensApi: children => {
                 if (!children) {
                     return null;
@@ -35,11 +29,11 @@ jest.mock(
 );
 
 jest.mock(
-    '../../../../components/chains/LN10_Caja_Apertura/common/_helper-WebApi.js',
+    '../../../../components/chains/LN10_Caja_Manual/common/_helper-WebApi.js',
     () => {
         return {
             __esModule: true,
-            validateChain: (childrenRenders, layout, isInOpening) => {
+            default: (layout, childProps = []) => {
                 if (layout === 'error') {
                     return { message: 'error lalala' };
                 }
@@ -50,17 +44,46 @@ jest.mock(
 );
 
 jest.mock(
-    '../../../../components/private/LN/api/global/components/chains/LN10/getCajaManual',
+    '../../../../components/private/LN/api/global/components/chains/common/respChildrens/index',
     () => {
         return {
             __esModule: true,
-            default: (props, typeChain) => {
-                return this;
+            default: (props, containerImage) => {
+                if (props.flagError === 'error') {
+                    throw new Error('Error');
+                }
+                return {
+                    information: { ...props.customFields },
+                    articles: []
+                };
             }
         };
     }
 );
 
+jest.mock(
+    '../../../../components/private/LN/api/global/components/chains/common/props/validatePropsChains.js',
+    () => {
+        return {
+            __esModule: true,
+            validatePropsChains: (props, typeChain, version) => {
+                return props;
+            }
+        };
+    }
+);
+/* jest.mock(
+    '../../../../components/private/LN/api/global/components/chains/LN10/getCajaManual',
+    () => {
+        return {
+            __esModule: true,
+            default: (props, typeChain) => {
+                return {};
+            }
+        };
+    }
+);
+ */
 describe('components - chains - LN10_Caja_Manual - json', () => {
     beforeEach(() => {
         jest.clearAllMocks();
@@ -69,7 +92,7 @@ describe('components - chains - LN10_Caja_Manual - json', () => {
         console.warn.mockImplementation(() => null);
     });
     const propsChain = {};
-    propsChain.id = '1';
+    propsChain.id = 1;
     propsChain.customFields = {
         layout: 'bn_1_3_grid',
         initialPosition: 1,
@@ -96,105 +119,64 @@ describe('components - chains - LN10_Caja_Manual - json', () => {
                 id: '2'
             },
             children: [1, 2, 3]
-        },
-        {
-            props: {
-                id: '3'
-            },
-            children: [1, 2, 3]
-        },
-        {
-            props: {
-                id: '4'
-            },
-            children: [1, 2, 3]
         }
     ];
 
-    test('LN10 Caja Apertura OK', () => {
-        const props = Object.assign({}, propsChain);
-
-        const cajaApertura = LN10CajaApertura;
-        const resultChain = new cajaApertura(props);
-        resultChain.renderResponse = jest.fn().mockImplementation(props => {
-            return {
-                information: { ...props.customFields },
-                articles: []
-            };
-        });
-        resultChain.props = props;
-        resultChain.state = {};
-
-        resultChain.state.containerImage = {
-            promo_items: {},
-            _id:
-                '6ab4c6fbd7a33de3058066487fc4a3b1291b066e47ed979b9385a228e04a23c3'
-        };
-        const resultCajaApertura = resultChain.render();
-
-        expect(Object.keys(resultChain).sort()).toEqual(
-            ['props', 'renderResponse', 'state', 'validate'].sort()
-        );
-        expect(Object.keys(resultCajaApertura).sort()).toEqual(
-            ['articles', 'information'].sort()
-        );
-    });
-
-    test('LN10 Caja Apertura when children is null', () => {
-        const props = Object.assign({}, propsChain);
-        props.children = null;
-        const cajaApertura = LN10CajaApertura;
-        const resultChain = new cajaApertura(props);
-        resultChain.renderResponse = jest.fn().mockImplementation(props => {
-            return {
-                information: { ...props.customFields },
-                articles: []
-            };
-        });
-        resultChain.props = props;
-        resultChain.state = {};
-
-        resultChain.state.containerImage = {
-            promo_items: {},
-            _id:
-                '6ab4c6fbd7a33de3058066487fc4a3b1291b066e47ed979b9385a228e04a23c3'
-        };
-        const resultCajaApertura = resultChain.render();
-
-        expect(Object.keys(resultChain).sort()).toEqual(
-            ['props', 'renderResponse', 'state', 'validate'].sort()
-        );
-        expect(resultCajaApertura).toBeNull();
-    });
-
-    test('LN10 Caja Apertura when validate is Error', () => {
+    test('LN10 Caja Manual OK', () => {
         const props = Object.assign({}, propsChain);
         const customFields = Object.assign({}, propsChain.customFields);
-        customFields.layout = 'error';
+        customFields.imageId = 'AAAAAABBBBBB';
         props.customFields = customFields;
 
-        const cajaApertura = LN10CajaApertura;
-        const resultChain = new cajaApertura(props);
-        resultChain.renderResponse = jest.fn().mockImplementation(props => {
-            return {
-                information: { ...props.customFields },
-                articles: []
-            };
-        });
-        resultChain.props = props;
-        resultChain.state = {};
+        // Mock fetchContent in exteded class
+        const getCajaManual = Object.getPrototypeOf(LN10CajaManual.prototype);
+        getCajaManual.fetchContent = jest.fn();
+        const cajaManual = LN10CajaManual;
+        cajaManual.prototype.prototype = getCajaManual;
+        const resultChain = new cajaManual(props);
 
         resultChain.state.containerImage = {
             promo_items: {},
             _id:
                 '6ab4c6fbd7a33de3058066487fc4a3b1291b066e47ed979b9385a228e04a23c3'
         };
-        const resultCajaApertura = resultChain.render();
+        const resultCajaManual = resultChain.render();
 
+        const resultGetCajaManual = getCajaManual.render();
+        console.log(resultCajaManual);
         expect(Object.keys(resultChain).sort()).toEqual(
             ['props', 'renderResponse', 'state', 'validate'].sort()
         );
-        expect(resultCajaApertura).toBeNull();
+        expect(resultCajaManual.information).toMatchObject({
+            layout: 'bn_1_3_grid',
+            initialPosition: 1,
+            hideTitle: false,
+            hideCaja: false,
+            title: 'Tensión política',
+            url: 'https://www.lanacion.com.ar/ultimas-noticias/',
+            pbInternal_cloneId: 'c0fUbCAOj3bz5Bd',
+            idCollection: 'LJSSBABHGJGGDLLAOOGFOFXXIY',
+            typeChain: null,
+            nameChain: 'LN10_Caja_Manual',
+            idRender: 'c0fw64w2jaz0cQV'
+        });
+        expect(resultGetCajaManual).toBeNull();
+    });
+
+    test('LN10 Caja Manual when children is invalid', () => {
+        const props = Object.assign({}, propsChain);
+        props.children = null;
+        const resultChain = new LN10CajaManual(props);
+        resultChain.state.containerImage = {
+            promo_items: {},
+            _id:
+                '6ab4c6fbd7a33de3058066487fc4a3b1291b066e47ed979b9385a228e04a23c3'
+        };
+        const result = resultChain.render();
+        expect(Object.keys(resultChain).sort()).toEqual(
+            ['props', 'renderResponse', 'state', 'validate'].sort()
+        );
+        expect(result).toBeNull();
     });
 
     test('LN10 Caja Manual when hideCaja is null', () => {
@@ -204,50 +186,54 @@ describe('components - chains - LN10_Caja_Manual - json', () => {
         customFields.hideCaja = null;
         props.customFields = customFields;
 
-        const cajaApertura = LN10CajaApertura;
-        const resultChain = new cajaApertura(props);
-        resultChain.renderResponse = jest.fn().mockImplementation(props => {
-            return {
-                information: { ...props.customFields },
-                articles: []
-            };
-        });
-        resultChain.props = props;
-        resultChain.state = {};
-
+        const resultChain = new LN10CajaManual(props);
         resultChain.state.containerImage = {
             promo_items: {},
             _id:
                 '6ab4c6fbd7a33de3058066487fc4a3b1291b066e47ed979b9385a228e04a23c3'
         };
-        const resultCajaApertura = resultChain.render();
+        const result = resultChain.render();
         expect(Object.keys(resultChain).sort()).toEqual(
             ['props', 'renderResponse', 'state', 'validate'].sort()
         );
-        expect(resultCajaApertura.information.hideCaja).toBe(false);
+        expect(result.information.hideCaja).toBe(false);
     });
-    test('LN10 Caja Apertura when throw Error', () => {
+    test('LN10 Caja Manual with validate Error', () => {
         const props = Object.assign({}, propsChain);
+        const customFields = Object.assign({}, propsChain.customFields);
+        customFields.layout = 'error';
+        props.customFields = customFields;
 
-        const cajaApertura = LN10CajaApertura;
-        const resultChain = new cajaApertura(props);
-        resultChain.renderResponse = jest.fn().mockImplementation(props => {
-            throw new Error('Error');
-        });
-        resultChain.props = props;
-        resultChain.state = {};
-
+        const resultChain = new LN10CajaManual(props);
         resultChain.state.containerImage = {
             promo_items: {},
             _id:
                 '6ab4c6fbd7a33de3058066487fc4a3b1291b066e47ed979b9385a228e04a23c3'
         };
-        const resultCajaApertura = resultChain.render();
-
+        resultChain.render();
         expect(Object.keys(resultChain).sort()).toEqual(
             ['props', 'renderResponse', 'state', 'validate'].sort()
         );
-        expect(resultCajaApertura.Message).toBe('Error');
-        expect(resultCajaApertura.Success).toBe(false);
+        expect(console.warn).toHaveBeenCalledTimes(1);
+        expect(console.warn.mock.calls[0][0]).toBe(
+            'error - {"message":"error lalala"}'
+        );
+    });
+
+    test('LN10 Caja Manual when throw Error', () => {
+        const props = Object.assign({}, propsChain);
+        props.flagError = 'error';
+        const resultChain = new LN10CajaManual(props);
+        resultChain.state.containerImage = {
+            promo_items: {},
+            _id:
+                '6ab4c6fbd7a33de3058066487fc4a3b1291b066e47ed979b9385a228e04a23c3'
+        };
+        const result = resultChain.render();
+        expect(Object.keys(resultChain).sort()).toEqual(
+            ['props', 'renderResponse', 'state', 'validate'].sort()
+        );
+        expect(result.Message).toBe('Error');
+        expect(result.Success).toBe(false);
     });
 });
