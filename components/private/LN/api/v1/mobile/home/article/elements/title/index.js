8bb4018afd92da7e2b1006b62dc1a6224cab6ffa
Javier Gribaudo <jgribaudo@lnexternos.com.ar>
2023-05-02 11:07:29 -0300

    diseño M no se mandan subtitulos

--- components/private/LN/api/v1/mobile/home/article/elements/title/index.js
+++ components/private/LN/api/v1/mobile/home/article/elements/title/index.js
@@ -7,6 +7,10 @@ export const getArticleTitleEditorial = article => {
 
 export const getLiveblogSubtitles = article => {
     const contentElements = get(article, 'content_elements', []);
+    const design = get(article, 'additionalProperties.diseno', null);
+
+    if (design && design.size === 'M') return null;
+
     return contentElements.reduce((acc, currentValue) => {
         if (currentValue.type === 'custom_embed' && acc.length < 3) {
             return [
