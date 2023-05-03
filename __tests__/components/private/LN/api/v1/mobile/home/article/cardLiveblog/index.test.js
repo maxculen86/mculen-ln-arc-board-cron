e16001db3d6a2c9ac734ee909ca4a939b8a7796f
Javier Gribaudo <jgribaudo@lnexternos.com.ar>
2023-05-03 08:57:57 -0300

    Test horas y minutos liveblog

--- __tests__/components/private/LN/api/v1/mobile/home/article/cardLiveblog/index.test.js
+++ __tests__/components/private/LN/api/v1/mobile/home/article/cardLiveblog/index.test.js
@@ -60,4 +60,25 @@ describe('Test-CardLiveblog when size is XL', () => {
             newArticle.content_elements[4].embed.config.time
         );
     });
+    it('should return time in format hh:mm', () => {
+        const newArticle = JSON.parse(JSON.stringify(article));
+      
+        const resp = CardLiveblog(newArticle);
+       
+        expect(resp.subtitles[0].time).toBe(
+            newArticle.content_elements[0].embed.config.time.split(':')
+            .slice(0, 2)
+            .join(':')
+        );
+        expect(resp.subtitles[1].time).toBe(
+            newArticle.content_elements[2].embed.config.time.split(':')
+            .slice(0, 2)
+            .join(':')
+        );
+        expect(resp.subtitles[2].time).toBe(
+            newArticle.content_elements[4].embed.config.time.split(':')
+            .slice(0, 2)
+            .join(':')
+        );
+    });
 });
