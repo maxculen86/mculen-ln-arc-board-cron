5b09c81ddd01318e4b03f32f789329ab73b13095
Juan Pravata <jpravata@lanancion.com.ar>
2023-05-03 15:34:53 -0300

    Se borra varaible customTitle ya que era innecesaria

--- components/private/common/metaTitle.jsx
+++ components/private/common/metaTitle.jsx
@@ -24,8 +24,7 @@ const MetaTitle = ({
         let metaTitleForAcum = '';
 
         if (acusWithMeta.includes(nodeType)) {
-            const customTitle = title;
-            metaTitleForAcum = customTitle;
+            metaTitleForAcum = title;
             metaTitleForStory = undefined;
         }
 
