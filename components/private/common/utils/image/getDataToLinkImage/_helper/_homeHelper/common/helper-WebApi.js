2b75c913f3946aa508ab66df4438e8915bab8a60
Alejandro Sanoja <hsanoja@lanacion.com.ar>
2023-05-09 23:59:15 -0300

    [ADD] www preload in caja apertura desktop

--- components/private/common/utils/image/getDataToLinkImage/_helper/_homeHelper/common/helper-WebApi.js
+++ components/private/common/utils/image/getDataToLinkImage/_helper/_homeHelper/common/helper-WebApi.js
@@ -1,10 +1,8 @@
 import getElementFromRenderables from '../../../../../getElementFromRenderables';
 import { isHomeLN10 } from '../../common/helper-WebApi';
 import sectionsValidationLN10 from '../../../../../../../../layouts/config/LN10-Home.config.json';
-import sectionsValidation from '../../../../../../../../layouts/config/LN-Home.config.json';
 
 export const getValidElementForPreload = (layout, renderables) => {
-    // TODO: Remover el return default cuando salga home ln 10
     if (isHomeLN10(layout)) {
         const bomba = getElementFromRenderables({
             position: 'Pre_Apertura.position',
@@ -24,24 +22,6 @@ export const getValidElementForPreload = (layout, renderables) => {
             []
         );
     }
-
-    const bomba = getElementFromRenderables({
-        position: 'Bomba.position',
-        config: sectionsValidation,
-        typeElement: 'LN-common/bomba',
-        renderables,
-        propNameHide: 'hideFeature'
-    });
-
-    return (
-        bomba ||
-        getElementFromRenderables({
-            position: 'Apertura_1.position',
-            config: sectionsValidation,
-            typeElement: 'Ln_Caja_Manual',
-            renderables
-        }) ||
-        []
-    );
 };
+
 export default getValidElementForPreload;
