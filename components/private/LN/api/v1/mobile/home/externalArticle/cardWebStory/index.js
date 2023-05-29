e0826936f1fa3c4df4f98c7b0137bfa271255652
Javier Gribaudo <jgribaudo@lnexternos.com.ar>
2023-05-29 11:56:46 -0300

    Se usan los elementos de la card webstories

---
+++ components/private/LN/api/v1/mobile/home/externalArticle/cardWebStory/index.js
@@ -0,0 +1,22 @@
+import { getArticleImage } from '../../../../../common/article/elements/image/index';
+import { getVolanta } from '../../../../../common/article/elements/volanta/index';
+import { getExternalArticleId } from '../elements/id';
+import { getExternalArticleLink } from '../elements/link';
+import { getArticleTitle } from '../../../../../common/article/elements/title';
+
+export const CardWebStory = article => {
+    // "id":"webstory1"
+    // "url": "https://lanota.com/xxxxxx",
+    // "titulo": "Es de Mar del Plata, \"argentinizó\" a su novia sueca y se volvieron furor en TikTok con sus desopilantes videos",
+    // "volanta": "Santiago emigró al país europeo y conoció a Isa, con quien en pocos meses tendrá una hija; con sus diferencias crearon su imagen en las redes y comparten divertidas filmaciones ",
+    // "imagen": {....muy parecido al de articulo}
+    return {
+        titulo: getArticleTitle(article),
+        id: getExternalArticleId(article),
+        url: getExternalArticleLink(article),
+        volanta: getVolanta(article),
+        imagen: getArticleImage(article)
+    };
+};
+
+export default CardWebStory;
