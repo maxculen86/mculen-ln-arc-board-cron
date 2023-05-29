2c07d76f44678df4175b5667cc00a2ec8a85a381
Javier Gribaudo <jgribaudo@lnexternos.com.ar>
2023-05-29 11:50:33 -0300

    Adaptacion CajaManual a Webstory

--- components/chains/LN10_Caja_WebStories/json.js
+++ components/chains/LN10_Caja_WebStories/json.js
@@ -1,12 +1,15 @@
 import Consumer from 'fusion:consumer';
 import get from '../../private/common/utils/get';
 import GetCajaManual from '../../private/LN/api/global/components/chains/LN10/getCajaManual';
-import validateCajaManual from '../LN10_Caja_Manual/common/_helper-WebApi';
 import { validateChildrensApi } from '../../private/LN/api/global/components/common/utils/_helpers';
+import {
+    validateChain,
+    filterWebStoriesChildren
+} from './common/_helper-WebApi';
 
-class CajaManual extends GetCajaManual {
+class CajaWebStories extends GetCajaManual {
     constructor(props) {
-        super(props, null);
+        super(props, 'webstories');
     }
 
     validate = propsValidate => {
@@ -24,17 +27,34 @@ class CajaManual extends GetCajaManual {
         /*         const childrenRendersProps = childrenRenders.map(x => x.props);
          */
         return validateCajaManual(layout, childrenRenders);
+    validate = ({ id: chainId, renderables = [], children }) => {
+        const childrenRenders = renderables
+            .find(x => get(x, 'props.id', null) === chainId)
+            .children.map(child => ({
+                ...child,
+                key: child.props.id
+            }));
+
+        const filteredChildren = filterWebStoriesChildren(
+            renderables,
+            childrenRenders
+        );
+
+        return validateChain(filteredChildren, children);
     };
 
     render() {
         try {
-            const { containerImage } = this.state || {};
             const { children, customFields: { layout = '' } = {} } = this.props;
+
             if (!validateChildrensApi(children)) {
                 return null;
             }
+
             const error = this.validate(this.props);
+
             if (error) {
+                // eslint-disable-next-line
                 console.warn(
                     `${layout} - ${
                         typeof error === 'object' ? JSON.stringify(error) : ''
@@ -49,11 +69,17 @@ class CajaManual extends GetCajaManual {
                 this.props.customFields.hideCaja =
                     this.props.customFields.hideBox || false;
             }
-            return this.renderResponse(this.props, containerImage);
+            return {
+                information: {
+                    ...this.props.customFields,
+                    typeChain: this.props.typeChain
+                },
+                articles: children
+            };
         } catch (err) {
             return { Success: false, Message: err.message };
         }
     }
 }
 
-export default Consumer(CajaManual);
+export default Consumer(CajaWebStories);
