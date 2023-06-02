52285487f4bd4f0cebe9fef795f1e3c4821ca061
Ezequiel Dapena <edapena@lnexternos.com.ar>
2023-06-02 10:30:02 -0300

    revert api-key

--- content/sources/rankingArticlesSource.js
+++ content/sources/rankingArticlesSource.js
@@ -1,5 +1,4 @@
 import request from 'request-promise-native';
-import { API_ENV, API_KEY_ARC_SERVICES_PROD } from 'fusion:environment';
 import logger from '../../components/private/common/utils/logger';
 import get from '../../components/private/common/utils/get';
 import { isNotRecommend } from './utils/collectionsHelper';
@@ -21,11 +20,7 @@ const fetch = (query, { cachedCall } = {}) => {
     const source = 'content/sources/rankingArticlesSource';
     return request({
         uri: uriArcServicesAPI,
-        json: true,
-        headers: {
-            Referer: API_ENV,
-            'api-key': API_KEY_ARC_SERVICES_PROD
-        }
+        json: true
     })
         .then(storiesUrls => {
             const stories = getCanonicalUrls(storiesUrls);
--- content/sources/tagSource.js
+++ content/sources/tagSource.js
@@ -10,6 +10,7 @@ import logger from '../../components/private/common/utils/logger';
 import NotFoundError from './utils/notFoundError';
 import getRequest from './utils/getRequest';
 import transformWikiTagData from './utils/transformWikiTagData';
+import { SUSCRIPTOR_SECTION } from '../../components/private/common/utils/subtypes/subtypeHelper';
 
 const resolve = key => {
     const { slug, outputType } = key;
