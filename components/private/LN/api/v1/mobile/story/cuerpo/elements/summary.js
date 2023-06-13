58deb082c0332f3e6fa5112bf13c67ae405b2ebe
Javier Gribaudo <jgribaudo@lnexternos.com.ar>
2023-06-13 16:15:31 -0300

    Se crea el elemento Summary para resumen de nota

---
+++ components/private/LN/api/v1/mobile/story/cuerpo/elements/summary.js
@@ -0,0 +1,37 @@
+import get from '../../../../../../../common/utils/get';
+import htmlText from '../../../../../common/elements/story/cuerpo/elements/htmlText';
+import {
+    validateValueText,
+    validateArrayNull
+} from '../../../../../common/utils/validateValue';
+
+const summary = nodo => {
+    if (!nodo) return null;
+
+    const listElements = get(nodo, 'items');
+    if (!listElements || listElements.length === 0) return null;
+
+    const resp = {
+        _t: 'list',
+        title: 'RESUMEN DE NOTA',
+        disclaimer: 'Resumen generada por inteligencia artificial',
+        type: 'ul'
+    };
+
+    resp.items = listElements.map(bullet => {
+        if (validateValueText(bullet)) return null;
+        const value = htmlText(bullet);
+        if (value) {
+            return {
+                _t: 'li',
+                value: bullet
+            };
+        }
+
+        return null;
+    });
+    if (validateArrayNull(resp.items)) return null;
+    return resp;
+};
+
+export default summary;
