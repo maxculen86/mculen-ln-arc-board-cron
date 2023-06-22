cf9198f6e37aacb34a95be1e226ab709082a1c5f
Machi <jmachicado@lanacion.com.ar>
2023-06-22 15:40:31 -0300

    fix resumen feature

--- components/features/LN-common/resumen/default.jsx
+++ components/features/LN-common/resumen/default.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'fusion:prop-types';
 import { useAppContext } from 'fusion:context';
 import { groupCustomFields } from '../../../private/common/utils/propTypesHelper';
-import StaticContent from '../../../private/common/staticContent';
 import get from '../../../private/common/utils/get';
 import SummaryNote from '../../../private/LN/common/summaryNote';
 
@@ -14,17 +13,11 @@ const Resumen = ({ customFields: { hide } = {} }) => {
         []
     );
 
-    return !hide ? (
-        <>
-            <StaticContent>
-                <SummaryNote paragraphs={arrayBullets} />
-            </StaticContent>
-        </>
-    ) : (
-        <></>
-    );
+    return <SummaryNote paragraphs={arrayBullets} />;
 };
 Resumen.label = 'LN-Resumen-Nota';
+
+Resumen.static = true;
 
 Resumen.propTypes = {
     customFields: PropTypes.shape({
