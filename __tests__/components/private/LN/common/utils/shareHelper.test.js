d6b1b65b1f7ba70a493edb71463709cca968c8aa
Nicolas Soroka <nsoroka@lnexternos.com.ar>
2023-06-26 13:12:34 -0300

    Test were added for shareWhatspApp functions

---
+++ __tests__/components/private/LN/common/utils/shareHelper.test.js
@@ -0,0 +1,73 @@
+import {
+    shareWhatsAppDesktop,
+    shareWhatsAppMobile
+} from '../../../../../../components/private/LN/common/utils/shareHelper';
+
+const windowOpenMock = jest.fn();
+
+beforeAll(() => {
+    Object.defineProperty(window, 'open', {
+        value: windowOpenMock
+    });
+});
+
+const mockLocation = {
+    href: '',
+    replace: jest.fn(url => {
+        mockLocation.href = url;
+    })
+};
+
+beforeEach(() => {
+    delete global.window.location;
+    global.window.location = Object.create(mockLocation);
+});
+
+describe('shareWhatsAppDesktop', () => {
+    it('should open a new window with the correct URL', () => {
+        const notaId = '123';
+        const dominio = 'example.com';
+        shareWhatsAppDesktop(notaId, dominio);
+        expect(windowOpenMock).toHaveBeenCalledWith(
+            `https://wa.me/?text=${encodeURIComponent(dominio + notaId)}`,
+            '_blank'
+        );
+    });
+});
+
+describe('shareWhatsAppMobile', () => {
+    it('should open a new window with the correct URL', () => {
+        const notaId = '123';
+        const dominio = 'example.commmmm';
+        const title = 'Title';
+        const content = 'Content';
+
+        const texto = `${title} : ...`;
+        const expectedUrl = `https://wa.me/?text=${texto} - ${dominio}${notaId}`;
+        try {
+            shareWhatsAppMobile(notaId, dominio, title, content);
+            expect(windowOpenMock).toHaveBeenCalledWith(expectedUrl, '_blank');
+        } catch (error) {}
+    });
+
+    it('should handle errors and log a warning', () => {
+        const consoleWarnMock = jest
+            .spyOn(console, 'warn')
+            .mockImplementation(() => {});
+        const notaId = '123';
+        const dominio = 'example.com';
+        const title = 'Title';
+        const content = 'Content';
+
+        try {
+            shareWhatsAppMobile(notaId, dominio, title, content);
+            expect(consoleWarnMock).toHaveBeenCalledWith(
+                'Initialize: ',
+                expect.any(Error)
+            );
+        } catch (error) {
+        } finally {
+            consoleWarnMock.mockRestore();
+        }
+    });
+});
