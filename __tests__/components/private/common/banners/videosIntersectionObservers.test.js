4cae45c422967027ee3531b47afaefe617f1a05a
tcabral <tcabral@lanacion.com.ar>
2023-07-06 11:38:21 -0300

    add yt videos intersection obs, fix videos obs and add tests

---
+++ __tests__/components/private/common/banners/videosIntersectionObservers.test.js
@@ -0,0 +1,129 @@
+import * as observers from '../../../../../components/private/common/banners/intersectionObservers';
+
+window.IntersectionObserver = jest.fn();
+
+afterEach(() => {
+    jest.restoreAllMocks();
+    jest.clearAllMocks();
+});
+
+describe('videos banners intersection observers', () => {
+    beforeEach(() => {
+        global.IntersectionObserver = jest.fn((callback, options) => ({
+            observe: jest.fn()
+        }));
+    });
+
+    it('should test createDifferVideosObserver', () => {
+        document.querySelectorAll = jest.fn(() => ['video']);
+        observers.createDifferVideosObserver();
+
+        const observerInstance =
+            global.IntersectionObserver.mock.results[0].value;
+
+        expect(global.IntersectionObserver).toHaveBeenCalledTimes(1);
+        expect(observerInstance.observe).toHaveBeenCalledWith('video');
+    });
+
+    it('should test createDifferYoutubeVideosObserver', () => {
+        document.querySelectorAll = jest.fn(() => ['video']);
+        observers.createDifferYoutubeVideosObserver();
+
+        const observerInstance =
+            global.IntersectionObserver.mock.results[0].value;
+
+        expect(global.IntersectionObserver).toHaveBeenCalledTimes(1);
+        expect(observerInstance.observe).toHaveBeenCalledWith('video');
+    });
+
+    it('should call videos play pause and change src', () => {
+        const mockPause = jest.fn();
+        const mockPlay = jest.fn();
+
+        const mockPauseEntry = [
+            {
+                isIntersecting: true,
+                target: {
+                    dataset:
+                        'https://lanacionar-prod.video.arc-cdn.net/wp-lanacionar/20230706/64a6a5abac2d3801cb3c35e',
+                    pause: mockPause
+                }
+            }
+        ];
+
+        const mockPlayEntry = [
+            {
+                isIntersecting: true,
+                target: {
+                    dataset: {
+                        src:
+                            'https://lanacionar-prod.video.arc-cdn.net/wp-lanacionar/20230706/64a6a5abac2d3801cb3c35e'
+                    },
+                    play: mockPlay,
+                    paused: true
+                }
+            }
+        ];
+
+        const [lazyVideo] = mockPlayEntry;
+
+        const observe = jest.fn();
+
+        window.IntersectionObserver.mockImplementationOnce(() => ({
+            observe
+        }));
+
+        const observer = observers.createDifferVideosObserver();
+
+        const [callback] = window.IntersectionObserver.mock.calls[0];
+
+        callback(mockPauseEntry, observer);
+
+        expect(observe).toBeCalledTimes(1);
+        expect(mockPause).toHaveBeenCalledTimes(1);
+
+        callback(mockPlayEntry, observer);
+        expect(mockPlay).toHaveBeenCalledTimes(1);
+        expect(lazyVideo.target.src).toStrictEqual(
+            lazyVideo.target.dataset.src
+        );
+    });
+
+    it('should change yt videos src and  call handleVideoEvents', () => {
+        const mockPostMessage = jest.fn();
+
+        const mockedYtEntry = [
+            {
+                isIntersecting: true,
+                target: {
+                    children: [
+                        {
+                            contentWindow: { postMessage: mockPostMessage }
+                        }
+                    ],
+                    dataset: { src: 'www.youtube.com/wjHE8klk' }
+                }
+            }
+        ];
+
+        const [lazyYtVideo] = mockedYtEntry;
+
+        const observe = jest.fn();
+
+        window.IntersectionObserver.mockImplementationOnce(() => ({
+            observe
+        }));
+
+        const observer = observers.createDifferYoutubeVideosObserver();
+
+        const [callback] = window.IntersectionObserver.mock.calls[0];
+
+        callback(mockedYtEntry, observer);
+
+        expect(mockPostMessage).toHaveBeenCalledTimes(1);
+        expect(observe).toBeCalledTimes(1);
+        expect(lazyYtVideo.target.children[0].src).toStrictEqual(
+            lazyYtVideo.target.dataset.src
+        );
+    });
+});
