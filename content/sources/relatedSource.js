5d64c08b9ac872edcc19ac9fd7873418d111eba6
ccabrera <ccabrera@lnexternos.com.ar>
2023-08-17 16:10:13 -0300

    Se traen todas las notas publicadas

--- content/sources/relatedSource.js
+++ content/sources/relatedSource.js
@@ -2,14 +2,12 @@ import { CONTENT_BASE, ARC_ACCESS_TOKEN } from 'fusion:environment';
 import request from 'request-promise-native';
 
 const resolve = (key, a) => {
-    const { includedFields, id, notPublished = false } = key;
-
+    const { includedFields, id } = key;
     const arcSite = key['arc-site'];
     const iFields = includedFields ? `&included_fields=${includedFields}` : '';
+    const notPublishedParam = '&published=false'; // Siempre es false asi me trae todas las notas, no solo las publicadas
 
-    const basePath = `/content/v4/stories/?website=${arcSite}${
-        notPublished ? '&published=false' : ''
-    }`;
+    const basePath = `/content/v4/stories/?website=${arcSite}${notPublishedParam}`;
 
     return `${basePath}&_id=${id}${iFields}`;
 };
