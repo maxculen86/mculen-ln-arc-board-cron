7b18451b8ff3163e9d375161f384472e6f32c3c4
ccabrera <ccabrera@lnexternos.com.ar>
2023-08-18 12:10:29 -0300

    Se arregla la logica de relatedSource

--- content/sources/relatedSource.js
+++ content/sources/relatedSource.js
@@ -2,12 +2,13 @@ import { CONTENT_BASE, ARC_ACCESS_TOKEN } from 'fusion:environment';
 import request from 'request-promise-native';
 
 const resolve = (key, a) => {
-    const { includedFields, id } = key;
+    const { includedFields, id, notPublished = true } = key;
     const arcSite = key['arc-site'];
     const iFields = includedFields ? `&included_fields=${includedFields}` : '';
-    const notPublishedParam = '&published=false'; // Siempre es false asi me trae todas las notas, no solo las publicadas
 
-    const basePath = `/content/v4/stories/?website=${arcSite}${notPublishedParam}`;
+    const basePath = `/content/v4/stories/?website=${arcSite}${
+        notPublished ? '&published=false' : ''
+    }`;
 
     return `${basePath}&_id=${id}${iFields}`;
 };
