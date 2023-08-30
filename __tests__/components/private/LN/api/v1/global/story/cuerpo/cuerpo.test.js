97038daa7fbff90c2b448fe7293a8a8437d61326
Sally <sallytweens@hotmail.com>
2023-08-30 09:31:34 -0300

    test adaptacion cuerpo

--- __tests__/components/private/LN/api/v1/global/story/cuerpo/cuerpo.test.js
+++ __tests__/components/private/LN/api/v1/global/story/cuerpo/cuerpo.test.js
@@ -27,9 +27,9 @@ describe('Test Json Text del cuerpo de la nota', () => {
     });
 
     it('Test para validar la cantidad de elementos del cuerpo', () => {
-        const resp = Cuerpo(ArticleCuerpo);
+        const resp = eno qCuerpo(ArticleCuerpo);
         expect(resp.elements.length).toBe(
-            ArticleCuerpo.content_elements.length - 1
+            ArticleCuerpo.content_elements.length
         );
     });
 
