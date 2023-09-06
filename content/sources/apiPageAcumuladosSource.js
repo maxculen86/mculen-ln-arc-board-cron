6eeca25cfea714e014fd8212c3e4d6bbb69a7f03
fcabrera <planit_fcabrera@lnexternos.com.ar>
2023-09-06 09:25:52 -0300

    api pages de acumulados v2

--- content/sources/apiPageAcumuladosSource.js
+++ content/sources/apiPageAcumuladosSource.js
@@ -1,172 +1,92 @@
 import { SITE_LANACION } from 'fusion:environment';
 import get from '../../components/private/common/utils/get';
 import pages from './utils/pageSource/index';
-import sectionSource from './sectionSource';
-import sectionsInPages from './utils/pageSource/pageAcumulados/config/configSectionPage.json';
-import transform from './utils/pageSource/pageAcumulados/v1/mobile/transform';
-import transformAcu from './utils/pageSource/acumulados/v1/mobile/bySection/transform';
-import home from '../../components/private/LN/api/v1/mobile/home';
-
-// Run with the url http://172.17.0.1/api/mobile/v1/page/bySection/ultimas-noticias/params=size:30;page:0/32/?_website=la-nacion-ar&outputType=json
-// params: page 0 when load the page od Accumulated
-// params: page >=1 when load the accumulated with pagination.
-
-const getParamsSectionSource = data => {
-    const title = get(
-        data,
-        'acumuladoGeneral.hierarchy_navigation',
-        get(data, 'name', null)
-    );
-    const restriction = get(
-        data,
-        'acumuladoGeneral.mostrar_en_acu_apps',
-        'true'
-    );
-    const configuration = get(data, 'configuration', null);
-    return { title, restriction, configuration };
-};
+import jsonSectionsInPages from './utils/pageSource/pageAcumulados/config/configSectionPage.json';
+import transform from './utils/pageSource/pageAcumulados/v2/mobile/transform';
+import home from '../../components/private/LN/api/v2/mobile/homeAccumulated';
+import pageTransformV2Format from './utils/pageSource/acumulados/v2/mobile/bySection/pageTransformV2Format';
 
 const fetch = async (query, { cachedCall }) => {
-    let queryParams = {};
-    let restriction = 'true';
-    let resultSectionSource = null;
-    let resultPage = null;
-    let configuration = null;
-
     try {
-        const { uri = '', website, versionUri } = query;
-        const ticksCache = get(query, 'ticks', '').replace('/', '');
-        const sectionId = get(query, 'sectionId', '').replace('/', '');
-        const sectionIdParam = sectionId.substring(0, 2).includes('/')
-            ? sectionId
-            : `${''}`.concat(`/${sectionId || ''}`);
+        const {
+            uri,
+            website,
+            versionUri,
+            ticksCache,
+            categoryUri,
+            sectionData,
+            sectioninPage
+        } = getParamsFromQuery(query);
 
-        const categoryUri = get(query, 'categoryUri', '').replace('/', '');
-        const sectionsinPage = sectionsInPages?.find(
-            e => e.sectionId === sectionId
-        );
-
-        const sectioninPage =
-            sectionsinPage && sectionsinPage.namePage
-                ? sectionsinPage.namePage
-                : sectionId;
-
-        const sectionsCustom = sectionsInPages
-            ?.filter(e => e?.isCustom)
-            ?.map(e => e.sectionId);
-        if (!versionUri) {
-            throw new Error('The api page must have a version');
-        }
-
-        let regexParams = new RegExp(/size:(\d+)/);
-        let matches = regexParams.exec(get(query, 'params', ''));
-        let title = sectionsinPage && sectionsinPage.aliasTitle;
-        title = title == null ? sectionId.replace('/', '') : title;
-
-        const size = matches && matches.length > 1 ? matches[1] : 30;
-        regexParams = new RegExp(/page:(\d+)/);
-        matches = regexParams.exec(get(query, 'params', ''));
-
-        let page = matches && matches.length > 1 ? parseInt(matches[1], 0) : 0;
-        page = page < 0 ? 0 : page;
-
-        const isPage = page === 0 && get(sectionsinPage, 'isPage', false);
-
-        const isCustom =
-            sectionsinPage && sectionsinPage.isCustom
-                ? sectionsinPage.isCustom
-                : false;
-
-        queryParams = {
-            id: sectionIdParam,
-            website: query?.website
-        };
-        if (!isCustom) {
-            resultSectionSource = await cachedCall(
-                'apiPageSectionSource',
-                sectionSource.fetch,
-                {
-                    query: queryParams
-                }
-            );
-        }
-        const paramsSectionsSource = getParamsSectionSource(
-            resultSectionSource
-        );
-
-        title =
-            get(paramsSectionsSource, 'title', null) == null
-                ? title
-                : get(paramsSectionsSource, 'title', null);
-
-        restriction = get(paramsSectionsSource, 'restriction', true);
-        configuration = get(paramsSectionsSource, 'configuration', null);
-
-        if (isPage || sectionsinPage?.featureAcumuladosInPage) {
-            queryParams = {
-                rootPath: `${SITE_LANACION}/${sectioninPage}`,
-                ticksCache: ticksCache.toString(),
-                website,
-                restriction,
-                uri,
-                title: sectioninPage,
-                configuration: null,
-                categoryUri,
-                versionUri,
-                featureInPage: sectionsinPage?.featureAcumuladosInPage,
-                sectionSource: resultSectionSource,
-                isPage
-            };
-
-            resultPage = await cachedCall('ApiPageAcumulados', pages.fetch, {
-                query: queryParams,
-                ttl: 120
-            });
-            const { information } = resultPage;
-
-            // Para revisar la data cruda que viene del Layout
-            // return resultPage;
-            if (isPage) {
-                const resultPageTransform = await transform(
-                    resultPage,
-                    queryParams
-                );
-                // Para revisar la data formateada con la informacion de todas la secciones
-                // return resultPageTransform;
-                queryParams.information = information;
-                const resultHome = home(resultPageTransform, queryParams);
-                // Para revisar la data despues del transform, estructura final
-                // return resultHome;
-                return Array.isArray(resultHome) ? resultHome[0] : {};
-            }
-        }
-
-        queryParams = {
-            sectionId: sectionsCustom?.includes(sectionId)
-                ? null
-                : sectionIdParam,
-            page: page > 0 ? page : 1,
-            size,
-            restriction,
+        const queryParams = {
+            rootPath: `${SITE_LANACION}/${sectioninPage}`,
+            ticksCache: ticksCache.toString(),
             website,
             uri,
-            title,
-            configuration,
+            title: sectioninPage,
             categoryUri,
-            versionUri,
-            featureInPage: sectionsinPage?.featureAcumuladosInPage,
-            isPage
+            versionUri
         };
-        return await transformAcu(resultPage, queryParams);
+
+        const resultPage = await cachedCall('ApiPageAcumulados', pages.fetch, {
+            query: queryParams,
+            ttl: 120
+        });
+
+        // Para revisar la data cruda que viene del Layout
+        //return resultPage;
+        const { information } = resultPage;
+
+        const resultPageTransform = await transform(resultPage);
+
+        // Para revisar la data formateada con la informacion de todas la secciones
+        // return resultPageTransform;
+        queryParams.information = information;
+        const resultHomeTransformation = home(resultPageTransform, queryParams);
+
+        // Para revisar la data despues del transform, estructura final
+        const resultPageData = Array.isArray(resultHomeTransformation)
+            ? resultHomeTransformation[0]
+            : {};
+        return pageTransformV2Format(resultPageData, sectionData);
     } catch (error) {
         // eslint-disable-next-line no-console
         console.warn(
-            `Error content/apiPageAcumuladosSource : ${JSON.stringify(
+            `Error content/apiTestAcumuladoSource : ${JSON.stringify(
                 query
             )} - errorMsj:${error.message}`
         );
         throw new Error(error);
     }
+};
+
+const getParamsFromQuery = query => {
+    const { uri = '', website, versionUri } = query;
+    const ticksCache = get(query, 'ticks', '').replace('/', '');
+    const categoryUri = get(query, 'categoryUri', '').replace('/', '');
+
+    const sectionId = get(query, 'sectionId', '').replace('/', '');
+
+    const sectionData = jsonSectionsInPages?.find(
+        e => e.sectionId === sectionId
+    );
+
+    const sectioninPage =
+        sectionData && sectionData.namePage ? sectionData.namePage : sectionId;
+
+    if (!versionUri) {
+        throw new Error('The api page must have a version');
+    }
+
+    return {
+        uri,
+        website,
+        versionUri,
+        ticksCache,
+        categoryUri,
+        sectionData,
+        sectioninPage
+    };
 };
 
 export default {
