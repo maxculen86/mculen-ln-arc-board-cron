59637fc060d81e130b979b51da58dab10165b7fd
Ezequiel Dapena <edapena@lnexternos.com.ar>
2023-10-04 12:10:32 -0300

    FooditHtmlBanner

---
+++ components/features/foodit/HtmlBanner/_helper.js
@@ -0,0 +1,27 @@
+import get from '../../../private/common/utils/get';
+import pageBuilderValidator from '../../../private/common/utils/pageBuilderValidator';
+
+const validateSpaceBlank = (customFields, field) =>
+    !get(customFields, `${field}`, '').trim();
+
+const validateHtmlFeature = ({ customFields }) => {
+    const { heightMobile, heightTablet, heightDesktop } = customFields || {};
+    const rules = [
+        {
+            validation: validateSpaceBlank(customFields, 'html'),
+            message: 'El campo "HTML" es obligatorio.'
+        },
+        {
+            validation: validateSpaceBlank(customFields, 'title'),
+            message: 'Debe proporcionar un título.'
+        },
+        {
+            validation: !heightMobile || !heightTablet || !heightDesktop,
+            message: 'Debe definir altos para todos los dispositivos.'
+        }
+    ];
+
+    return pageBuilderValidator(rules);
+};
+
+export default validateHtmlFeature;
