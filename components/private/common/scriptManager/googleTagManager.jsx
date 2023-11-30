0dbd4a34d2ad4221f47e2e07ee35a0932e914f0f
Julio Quintana <jquintana@lanacion.com.ar>
2023-11-30 15:40:45 -0300

    Update load de gtm

--- components/private/common/scriptManager/googleTagManager.jsx
+++ components/private/common/scriptManager/googleTagManager.jsx
@@ -8,12 +8,12 @@ export default class GTM extends Component {
         super(props);
 
         const { id, layerName = 'dataLayer', location = 'head' } = props;
-        const script = `((w,l)=>{w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});})(window,'${layerName}');`;
-        const _layerName = layerName !== 'dataLayer' ? `&l=${layerName}` : '';
-        const params = {
-            async: 'true',
-            src: `https://www.googletagmanager.com/gtm.js?id=${id}${_layerName}`
-        };
+        const domanGtm = 'https://www.googletagmanager.com';
+        const script = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '${domanGtm}/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','${layerName}','${id}');`;
 
         switch (location) {
             case 'head':
@@ -22,15 +22,14 @@ export default class GTM extends Component {
                         defer
                         type="text/javascript"
                         dangerouslySetInnerHTML={{ __html: script }}
-                    />,
-                    <script {...params} />
+                    />
                 ];
                 break;
             case 'body-top':
                 this.nodes = [
                     <noscript>
                         <iframe
-                            src={params.src}
+                            src={`${domanGtm}/ns.html?id=${id}`}
                             height="0"
                             width="0"
                             style={{ display: 'none', visibility: 'hidden' }}
