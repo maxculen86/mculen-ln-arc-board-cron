7622bcab51506a0690e230c5785570b0f21e6830
Maurizio De Marchis <mdemarchis@lnexternos.com.ar>
2023-12-05 11:15:01 -0300

    changed Beyondwords player Id to Produccion-Contenidos

--- environment/index.js
+++ environment/index.js
@@ -84,5 +84,5 @@ export default {
     CLL_BACK_BASE_URL: 'https://api-cll-services.lanacion.com.ar/graphql/',
     CLL_BACK_API_KEY: 'da2-6dgg5zavxjdlpmgjs2ztndfnl4',
     MARFEEL_ACCOUNT_ID: '3353',
-    BEYONDWORDS_PROJECT_ID: 38983
+    BEYONDWORDS_PROJECT_ID: 37324
 };
