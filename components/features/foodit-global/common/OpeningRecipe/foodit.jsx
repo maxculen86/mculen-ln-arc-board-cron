3a07e0c1edd7516e5bac2bacf3b839ad8f022ca7
Machi <jmachicado@lanacion.com>
2023-12-27 17:07:55 -0300

    actions buttons (share) foodit

--- components/features/foodit-global/common/OpeningRecipe/foodit.jsx
+++ components/features/foodit-global/common/OpeningRecipe/foodit.jsx
@@ -11,8 +11,8 @@ import { getImagesToLoadWithPicture } from '../../../../private/LN/common/utils/
 import { Badge } from '@ln/foodit-ui-badge';
 import { Button } from '@ln/foodit-ui-button';
 import { Icon } from '@ln/common-ui-icon';
-import { Bookmark, Facebook, Instagram, Twitter } from '@ln/foodit-ui-assets';
-import { Dropdown } from '@ln/common-ui-dropdown';
+import { Bookmark } from '@ln/foodit-ui-assets';
+import ActionsButtons from '../ActionsButtons/foodit';
 import StaticContent from '../../../../private/common/staticContent';
 import VideoPlayer from '../../../private-global/common/videoPlayer/foodit';
 
@@ -34,149 +34,66 @@ export const OpeningRecipe = ({ article = {} }) => {
     const badge = 'FACIL';
     const videoJW = get(promo_items, 'video_jw', null);
 
-    const Share = () => {
-        // TODO: Check if the device has support for native sharing
-        const hasNavigator = false;
-
-        if (hasNavigator) {
-            // const shareData = { title: 'some title', text: 'some body', url: 'some URL' };
-            // const shareFn = () => navigator.share(shareData)
-            return (
-                <Button title="Compartir" variant="link">
-                    <Icon size={24}>
-                        <Bookmark />
-                    </Icon>
-                </Button>
-            );
-        }
-
-        return (
-            <Dropdown hideArrow className="flex">
-                <Dropdown.Toggle title="Compartir">
-                    <Icon size={24}>
-                        <Bookmark />
-                    </Icon>
-                </Dropdown.Toggle>
-                <Dropdown.Menu
-                    alignment="center"
-                    className="bg-light-1 p-24 rounded-4 shadow-center"
-                >
-                    <div className="flex ai-center gap-16">
-                        <Button
-                            title="Compartir por Facebook"
-                            variant="link"
-                            href="#"
-                        >
-                            <Icon size={24}>
-                                <Facebook />
-                            </Icon>
-                        </Button>
-                        <Button
-                            title="Compartir por Instagram"
-                            variant="link"
-                            href="#"
-                        >
-                            <Icon size={24}>
-                                <Instagram />
-                            </Icon>
-                        </Button>
-                        <Button
-                            title="Compartir por Twitter"
-                            variant="link"
-                            href="#"
-                        >
-                            <Icon size={24}>
-                                <Twitter />
-                            </Icon>
-                        </Button>
-                    </div>
-                </Dropdown.Menu>
-            </Dropdown>
-        );
-    };
     // TODO: Make a new filter for foodit, the actual filter doesnt have some embed properties
     // TODO: Icons still pending design definitions
     // TODO: Add functions to buttons
     return (
-        <>
-            <Recipe>
-                <Recipe.Media>
-                    <StaticContent>
-                        {videoJW ? (
-                            <VideoPlayer
-                                data={videoJW}
-                                tituloNota={get(headlines, 'basic', '')}
-                                className="w-100 ratio-16-9"
-                            />
-                        ) : (
-                            <Image
-                                alt={get(promo_items, 'basic.caption', '')}
-                                src={get(promo_items, 'basic.url', '')}
-                                className="w-100 ratio-3-2"
-                                fetchPriority="high"
-                                loading="eager"
-                                sources={getImagesToLoadWithPicture(
-                                    get(promo_items, 'basic.resized_urls', [])
-                                )}
-                            />
-                        )}
-                        {badge && (
-                            <Badge className="lg-none absolute bottom-0 left-0 m-8">
-                                {badge}
-                            </Badge>
-                        )}
-                    </StaticContent>
-                </Recipe.Media>
-                <Recipe.Body>
-                    <StaticContent>
-                        <div className="flex flex-column ai-start gap-12">
-                            {badge && (
-                                <Badge className="lg-only">{badge}</Badge>
+        <Recipe>
+            <Recipe.Media>
+                <StaticContent>
+                    {videoJW ? (
+                        <VideoPlayer
+                            data={videoJW}
+                            tituloNota={get(headlines, 'basic', '')}
+                            className="w-100 ratio-16-9"
+                        />
+                    ) : (
+                        <Image
+                            alt={get(promo_items, 'basic.caption', '')}
+                            src={get(promo_items, 'basic.url', '')}
+                            className="w-100 ratio-3-2"
+                            fetchPriority="high"
+                            loading="eager"
+                            sources={getImagesToLoadWithPicture(
+                                get(promo_items, 'basic.resized_urls', [])
                             )}
-                            <div className="flex flex-column ai-start gap-16">
-                                <Text
-                                    className="prumo prumo-black text-28 text-40_md text-48_lg"
-                                    text={get(headlines, 'basic', '')}
-                                    as="h1"
-                                />
-                                <Text
-                                    className="text-14"
-                                    text={author}
-                                    as="h3"
-                                />
-                            </div>
+                        />
+                    )}
+                    {badge && (
+                        <Badge className="lg-none absolute bottom-0 left-0 m-8">
+                            {badge}
+                        </Badge>
+                    )}
+                </StaticContent>
+            </Recipe.Media>
+            <Recipe.Body>
+                <StaticContent>
+                    <div className="flex flex-column ai-start gap-12">
+                        {badge && <Badge className="lg-only">{badge}</Badge>}
+                        <div className="flex flex-column ai-start gap-16">
+                            <Text
+                                className="prumo prumo-black text-28 text-40_md text-48_lg"
+                                text={get(headlines, 'basic', '')}
+                                as="h1"
+                            />
+                            <Text className="text-14" text={author} as="h3" />
                         </div>
-                        <div className="flex ai-center gap-24">
-                            <Button title="Guardar" size={{ sm: 32, lg: 40 }}>
-                                <Icon size={16} className="sm-none">
-                                    <Bookmark />
-                                </Icon>
-                                Guardar
-                            </Button>
-                            <hr className="h-100 lg-only" />
-                            <div className="flex ai-center gap-16 gap-24_md">
-                                <Button title="Copiar" variant="link">
-                                    <Icon size={24}>
-                                        <Bookmark />
-                                    </Icon>
-                                </Button>
-                                <Button title="Imprimir" variant="link">
-                                    <Icon size={24}>
-                                        <Bookmark />
-                                    </Icon>
-                                </Button>
-                                <Share />
-                                <Button title="Comentar" variant="link">
-                                    <Icon size={24}>
-                                        <Bookmark />
-                                    </Icon>
-                                </Button>
-                            </div>
-                        </div>
-                    </StaticContent>
-                </Recipe.Body>
-            </Recipe>
-        </>
+                    </div>
+                </StaticContent>
+                <div className="flex ai-center gap-24">
+                    <Button title="Guardar" size={{ sm: 32, lg: 40 }}>
+                        <Icon size={16} className="sm-none">
+                            <Bookmark />
+                        </Icon>
+                        Guardar
+                    </Button>
+                    <hr className="h-100 lg-only" />
+                    <div className="flex ai-center gap-16 gap-24_md">
+                        <ActionsButtons article={article} />
+                    </div>
+                </div>
+            </Recipe.Body>
+        </Recipe>
     );
 };
 
--- properties/sites/foodit.js
+++ properties/sites/foodit.js
@@ -1,11 +1,13 @@
 import imageConfig from './helperConfigFoodit/imageConfig';
 // TODO: Base de propiedades del sitio RECETAS, mientras tanto usa el mismo imageConfig de LN, queda pendiente armar nuevas configuraciones de ser necesario. El titulo y descripcion son de ejemplo.
+// TODO: host deberia ser url productiva, de momento utilizo url sandbox
 export default {
     longTitle: 'Foodit, recetas faciles y rapidas.',
     title: 'FOODIT',
     description:
         'FOODIT ofrece una inmensa variedad de recetas, que permiten planificar las comidas de la semana',
     imageConfig,
+    host: 'https://lanacionar-foodit-sandbox.web.arc-cdn.net/',
     layoutsName: {
         FooditHome: 'Foodit-home'
     }
