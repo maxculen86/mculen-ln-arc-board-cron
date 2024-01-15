3972de1b01862cfa4d57fee5737e0e04c343cb11
Maurizio De Marchis <mdemarchis@lnexternos.com.ar>
2024-01-15 10:47:59 -0300

    migracion de scripts googlePublisherTag y googlePublisherTagAcumulado

---
+++ src/statics/LN/js/googlePublisherTag.js
@@ -0,0 +1,39 @@
+const serializedCreateHash = document.getElementById(
+    'googlePublisherTag-metadata'
+).dataset.createHash;
+const serializedGetCookie = document.getElementById(
+    'googlePublisherTag-metadata'
+).dataset.getCookie;
+const newTags = JSON.parse(
+    document.getElementById('googlePublisherTag-metadata').dataset.newTags
+);
+
+const createHash = eval(`(${serializedCreateHash})`);
+const googleTagGetCookie = eval(`(${serializedGetCookie})`);
+const googleTagUserCookie = googleTagGetCookie('ProductoPremiumId') || [];
+const googleTagEmailCookie = googleTagGetCookie('usuarioemail') || '';
+const googleTagSuscriptionType = googleTagUserCookie.includes('2')
+    ? 'suscriptor'
+    : 'no suscriptor';
+
+var pbjs = pbjs || {};
+pbjs.que = pbjs.que || [];
+
+window.googletag = window.googletag || { cmd: [] };
+googletag.cmd.push(() => {
+    // initialize
+    googletag.pubads().enableSingleRequest();
+    googletag.pubads().enableAsyncRendering();
+    googletag.pubads().disableInitialLoad();
+
+    if (googleTagEmailCookie) {
+        createHash(googleTagEmailCookie).then(hash => {
+            googletag.pubads().setPublisherProvidedId(hash);
+        });
+    }
+
+    googletag.enableServices();
+    console.log('🚀 ::: setTargeting ON ::: 🚀');
+    googletag.pubads().setTargeting('tags_nuevos', newTags);
+    googletag.pubads().setTargeting('usuario_tipo', googleTagSuscriptionType);
+});
