7f1a5f42ded68f1808f0a499d4497a39c96e133c
Juan Carlos Diaz <jcdiaz@lanacion.com.ar>
2024-02-01 11:25:46 -0300

    Cris: Nuevo schema para el script marfeel

--- components/private/common/scriptManager/MarfeelMarkup.jsx
+++ components/private/common/scriptManager/MarfeelMarkup.jsx
@@ -3,33 +3,25 @@ import { useAppContext } from 'fusion:context';
 import get from '../utils/get';
 
 const MarfeelMarkup = () => {
-    const { contextPath, deployment, globalContent } = useAppContext();
+    // TODO: Verificar un validador adicional para confirmar que solo cargue en la seccion de notas porque ahora carga para todo el sitio
+    const { globalContent } = useAppContext();
     const contentCode = get(
         globalContent,
         'content_restrictions.content_code',
         ''
     );
-
     const isAccessibleForFree = contentCode !== 'cerrada';
-    const schemaOrgData = {
-        '@context': 'https://schema.org',
-        '@type': 'NewsArticle',
-        isAccessibleForFree
-    };
-
-    const schemaOrgJsonString = JSON.stringify(schemaOrgData, null, 2);
-
     return (
-        <>
-            <script
-                id="scriptMarfeelMarkup"
-                type="text/javascript"
-                src={deployment(
-                    `${contextPath}/resources/js/LN/scriptMarfeelMarkup.min.js`
-                )}
-            />
-            <script type="application/ld+json" text={schemaOrgJsonString} />
-        </>
+        <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+                __html: `{
+            "@context": "https://schema.org",
+            "@type": "NewsArticle",
+            "isAccessibleForFree": ${isAccessibleForFree}
+          }}`
+            }}
+        />
     );
 };
 
