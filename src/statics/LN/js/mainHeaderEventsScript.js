aa4f9f001d1e6bc099cf17a8305773cb4506ac37
ccabrera <ccabrera@lnexternos.com.ar>
2024-02-20 14:24:50 -0300

    Se agrega funcion a la hora de cargar el DOM

--- src/statics/LN/js/mainHeaderEventsScript.js
+++ src/statics/LN/js/mainHeaderEventsScript.js
@@ -2,4 +2,5 @@ window.addEventListener('DOMContentLoaded', () => {
     window.LN.eventshelper.setEventSubscribe();
     window.LN.eventshelper.setEventSignIn();
     window.LN.eventshelper.setEventsSections();
+    window.LN.eventshelper.setEventSearch();
 });
