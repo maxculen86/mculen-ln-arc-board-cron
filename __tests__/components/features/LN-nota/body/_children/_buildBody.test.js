a2be4288631f582db2297a16c3b9928e2ae0c99b
Julio Quintana <jquintana@lanacion.com.ar>
2024-04-26 16:08:09 -0300

    Revitalización y expansión de tests para BuildBody con adaptaciones para React 18
    Se han revitalizado y expandido los tests para el componente BuildBody, agregando implementaciones completas para simular comportamientos de usuario y validar la integración de componentes. Se ha añadido la dependencia 'util' para proporcionar polifill a TextEncoder y TextDecoder, necesarios para la compatibilidad con React 18 en el entorno de tests. Además, se ha ajustado la configuración global de Jest para incorporar estas nuevas globalizaciones, asegurando la estabilidad y compatibilidad de los tests en entornos de ejecución más recientes.

--- __tests__/components/features/LN-nota/body/_children/_buildBody.test.js
+++ __tests__/components/features/LN-nota/body/_children/_buildBody.test.js
@@ -1,7 +1,308 @@
-// TODO: ver historico de este archivo y revivir el test
-describe('Prueba false para dejar vivo el archivo con historico del test', () => {
-    test('validar que 2 + 2 sea igual a 4', () => {
-        const resultado = 2 + 2;
-        expect(resultado).toBe(4);
+import React from 'react';
+import Consumer from 'fusion:consumer';
+import { useContent } from 'fusion:content';
+import getProperties from 'fusion:properties';
+import Context from 'fusion:context';
+import BuildBody from '../../../../../../components/features/LN-nota/body/_children/_buildBody';
+import content_elements from '../../../../../../__mocks__/data/nota/body/contentElements.json';
+import siteProperties from '../../../../../../__mocks__/data/nota/body/siteProperties.json';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import * as utils from '../../../../../../components/features/LN-nota/body/_utils/_embedHelper';
+import siteServicesMock from '../../../../../../__mocks__/data/siteServices/siteServices.json';
+import useSiteServices from '../../../../../../components/features/LN-10-global/hooks/useSiteServices';
+
+jest.mock(
+    '../../../../../../components/features/LN-10-global/hooks/useSiteServices',
+    () => jest.fn()
+);
+
+jest.mock('fusion:context', Component => {
+    return function(Component) {
+        return props => <Component {...props} />;
+    };
+});
+
+jest.mock('fusion:consumer', component => {
+    return function(component) {
+        return component;
+    };
+});
+
+useContent.mockImplementation(() => {});
+
+useSiteServices.mockImplementation(() => {
+    return siteServicesMock;
+});
+
+const globalContent = {
+    content_elements,
+    headlines: { basic: 'titulo de la nota' }
+};
+
+const deployment = deploymentValue => deploymentValue;
+
+Context.useAppContext = jest.fn(() => ({
+    globalContent,
+    siteProperties,
+    deployment
+}));
+
+describe('BuildBody', () => {
+    window.dataLayer = [];
+
+    const banners = [
+        { desktop: 'middle_1_dsk', position: 3, sticky: true },
+        { desktop: 'middle_2_dsk', position: 6 },
+        { desktop: 'caja1_amp', position: 3 },
+        { desktop: 'caja2_amp', position: 5 },
+        { mobile: 'caja1_mob', position: 1 },
+        { mobile: 'caja2_mob', position: 4, background: true },
+        { mobile: 'caja3_mob', position: 7 },
+        { mobile: 'caja4_mob', position: 9 },
+        { mobile: 'caja5_mob', position: 11 }
+    ];
+
+    it('deberia renderizar todos los elementos del cuerpo para default', () => {
+        const { container } = render(
+            BuildBody({
+                banners,
+                outputType: 'default',
+                globalContent
+            })
+        );
+        expect(container).toBeTruthy();
+        expect(container).toMatchSnapshot();
+        /* paragraph */
+        expect(container.querySelector(`p`).getAttribute('class')).toEqual(
+            'com-paragraph  --capital --s'
+        );
+        expect(
+            container.querySelectorAll(`p[class*="com-paragraph"]`)
+        ).toHaveLength(57);
+        /* order and unorder list */
+        expect(
+            container.querySelectorAll(`ul[class*="com-ordered"]`)
+        ).toHaveLength(2);
+        expect(
+            container.querySelectorAll(`ul[class*="com-unordered"]`)
+        ).toHaveLength(4);
+        expect(
+            container.querySelectorAll(`ol[class*="com-ordered"]`)
+        ).toHaveLength(1);
+        /* subtitles */
+        expect(
+            container.querySelectorAll(
+                `h2[class*="com-title --font-primary --xl --font-extra"]`
+            )
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(
+                `h3[class*="com-title --font-primary --l --font-medium"]`
+            )
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(
+                `h4[class*="com-title --arial --twoxs --font-medium"]`
+            ) // TODO: REVISAR COMPORTAMIENTO DE FONT EN TITLE
+        ).toHaveLength(1);
+        /* oembed_response */
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --facebook"]`)
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --instagram"]`)
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --spotify"]`)
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --twitter"]`)
+        ).toHaveLength(3);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --youtube"]`)
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --dailymotion"]`)
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --tiktok"]`)
+        ).toHaveLength(1);
+        /* interstitial_link */
+        expect(
+            container.querySelectorAll(`div[class*="com-container --button"]`)
+        ).toHaveLength(1);
+        //expect(getAllByText('Dólar blue hoy: a cuánto cotiza el lunes 8 de junio')).toHaveLength(1);
+        /* gallery */
+        expect(container.querySelectorAll(`div[class*="slide"]`)).toHaveLength(
+            3
+        );
+        expect(container.querySelectorAll(`amp-carousel`)).toHaveLength(0);
+        /* image */
+        expect(
+            container.querySelectorAll(`img[class*="com-image"]`)
+        ).toHaveLength(13);
+        expect(container.querySelectorAll(`amp-img`)).toHaveLength(0);
+        /* blockquiote */
+        expect(
+            container.querySelectorAll(`blockquote[class*="mod-paragraph"]`)
+        ).toHaveLength(2);
+        /* pullquote */
+        expect(
+            container.querySelectorAll(`section[class*="com-cita autor"]`)
+        ).toHaveLength(1);
+        /* raw_html */
+        expect(
+            container.querySelectorAll(`div[class*="com-embed"]`)
+        ).toHaveLength(17);
+        /* banner */
+        expect(
+            container.querySelectorAll(`div[class*="mod-banner"]`)
+        ).toHaveLength(9);
+        /* powerup receta */
+        expect(
+            container.querySelectorAll(`div[class*="--ingredients"]`)
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(`div[class*="--preparation"]`)
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(`table[class*="table"]`)
+        ).toHaveLength(1);
+        /* divider */
+        expect(
+            container.querySelectorAll(`div[class*="divider"]`)
+        ).toHaveLength(1);
+    });
+
+    it('deberia renderizar solo los elementos del cuerpo permitidos para FOTO al 100', () => {
+        const { container } = render(
+            BuildBody({
+                banners,
+                outputType: 'default',
+                globalContent: {
+                    ...globalContent,
+                    subtype: '8'
+                }
+            })
+        );
+
+        expect(container).toBeTruthy();
+        expect(container).toMatchSnapshot();
+        /* paragraph */
+        expect(
+            container.querySelectorAll(`p[class*="com-paragraph"]`)
+        ).toHaveLength(55);
+        /* order and unorder list */
+        expect(
+            container.querySelectorAll(`ul[class*="com-ordered"]`)
+        ).toHaveLength(2);
+        expect(
+            container.querySelectorAll(`ul[class*="com-unordered"]`)
+        ).toHaveLength(2);
+        expect(
+            container.querySelectorAll(`ol[class*="com-ordered"]`)
+        ).toHaveLength(0);
+        /* subtitles */
+        expect(
+            container.querySelectorAll(
+                `h2[class*="com-title --font-primary --xl --font-extra"]`
+            )
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(
+                `h3[class*="com-title --font-primary --l --font-extra"]`
+            )
+        ).toHaveLength(1);
+        expect(
+            container.querySelectorAll(
+                `h4[class*="com-title --arial --twoxs --font-medium"]`
+            )
+        ).toHaveLength(1);
+        /* oembed_response */
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --facebook"]`)
+        ).toHaveLength(0);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --instagram"]`)
+        ).toHaveLength(0);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --spotify"]`)
+        ).toHaveLength(0);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --twitter"]`)
+        ).toHaveLength(0);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --youtube"]`)
+        ).toHaveLength(0);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --dailymotion"]`)
+        ).toHaveLength(0);
+        expect(
+            container.querySelectorAll(`div[class*="com-embed --tiktok"]`)
+        ).toHaveLength(0);
+        /* interstitial_link */
+        expect(
+            container.querySelectorAll(`div[class*="com-container --button"]`)
+        ).toHaveLength(1);
+        //expect(getAllByText('Dólar blue hoy: a cuánto cotiza el lunes 8 de junio')).toHaveLength(1);
+        /* gallery */
+        expect(container.querySelectorAll(`div[class*="slide"]`)).toHaveLength(
+            3
+        );
+        /* image */
+        expect(
+            container.querySelectorAll(`img[class*="com-image"]`)
+        ).toHaveLength(13);
+        /* video */
+        expect(
+            container.querySelectorAll(`div[class*="mod-video"]`)
+        ).toHaveLength(0);
+        /* blockquiote no renderiza porque no matchea subtype en las rules */
+        expect(
+            container.querySelectorAll(`blockquote[class*="mod-paragraph"]`)
+        ).toHaveLength(0);
+        /* pullquote no renderiza porque no matchea subtype en las rules */
+        expect(
+            container.querySelectorAll(`section[class*="com-cita autor"]`)
+        ).toHaveLength(0);
+        /* raw_html */
+        expect(
+            container.querySelectorAll(`div[class*="com-embed"]`)
+        ).toHaveLength(0);
+        /* banner */
+        expect(
+            container.querySelectorAll(`div[class*="mod-banner"]`)
+        ).toHaveLength(9);
+        /* powerup receta no renderiza porque no matchea subtype en las rules */
+        expect(
+            container.querySelectorAll(`div[class*="--ingredients"]`)
+        ).toHaveLength(0);
+        expect(
+            container.querySelectorAll(`div[class*="--preparation"]`)
+        ).toHaveLength(0);
+        expect(
+            container.querySelectorAll(`table[class*="table"]`)
+        ).toHaveLength(0);
+        /* divider */
+        expect(
+            container.querySelectorAll(`div[class*="divider"]`)
+        ).toHaveLength(1);
+        expect(container).toMatchSnapshot();
+    });
+    it('deberia ejecutar funcion para transformar el oembedScript', () => {
+        jest.spyOn(utils, 'transformEmbedScript').mockImplementation(() =>
+            jest.fn()
+        );
+
+        render(
+            BuildBody({
+                banners,
+                outputType: 'default',
+                globalContent
+            })
+        );
+
+        expect(utils.transformEmbedScript).toBeCalledTimes(13);
     });
 });
--- setupTests.js
+++ setupTests.js
@@ -2,3 +2,6 @@ jest.mock(
     './components/features/private-global/common/iconSprite/IconSprite',
     () => 'mock-icon'
 );
+
+global.TextEncoder = require('util').TextEncoder;
+global.TextDecoder = require('util').TextDecoder;
