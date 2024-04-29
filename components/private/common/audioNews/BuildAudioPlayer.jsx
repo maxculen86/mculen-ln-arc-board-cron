62e64d966b5149d7f8397792ac9f6b2f3a90bd69
Julio Quintana <jquintana@lanacion.com.ar>
2024-04-29 10:27:15 -0300

    Refactoriza la inicialización del reproductor para asegurar que el DOM esté listo
    Este commit modifica la secuencia de inicialización del reproductor BeyondWords para asegurarse de que el elemento DOM '.audio-player' esté completamente cargado antes de intentar acceder a él. Se eliminaron las verificaciones de conexión innecesarias y se reestructuraron los useEffect para optimizar la carga y manejo de errores.

--- components/private/common/audioNews/BuildAudioPlayer.jsx
+++ components/private/common/audioNews/BuildAudioPlayer.jsx
@@ -1,10 +1,8 @@
-/* eslint-disable react/require-default-props */
 import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'fusion:prop-types';
 import { BEYONDWORDS_PROJECT_ID } from 'fusion:environment';
 import LoadingIcon from '../../LN/common/loadingIcon';
 import { GlobalContext } from '../context/globalContext';
-import loader from 'sass-loader';
 
 const BuildAudioPlayer = ({
     setOpenPlayer,
@@ -36,59 +34,61 @@ const BuildAudioPlayer = ({
     }, [error, dispatch, setEnableButton, setOpenPlayer]);
 
     useEffect(() => {
-        // Checking for connection before creating the script
-        if (!navigator.onLine) {
-            setError(true);
-        }
         const script = document.createElement('script');
         script.async = true;
         script.defer = true;
         script.src =
             'https://proxy.beyondwords.io/npm/@beyondwords/player@latest/dist/umd.js';
-
-        // Initializing the player
-        const handleScriptLoad = () => {
-            setIsLoading(false);
-            const player = new BeyondWords.Player({
-                target: '.audio-player',
-                projectId: BEYONDWORDS_PROJECT_ID,
-                sourceId: noteId,
-                playbackRates: [1, 1.25, 1.5, 1.7, 2],
-                playbackState: 'playing',
-                skipButtonStyle: 'seconds',
-                logoIconEnabled: false,
-                widgetWidth: '40rem'
-            });
-
-            const handleNoContentAvailable = event => {
-                setError(true);
-            };
-
-            player.addEventListener(
-                'NoContentAvailable',
-                handleNoContentAvailable
-            );
-
-            // Store the event listener remover function in a variable
-            const removeEventListenerFunction = () => {
-                player.removeEventListener(
-                    'NoContentAvailable',
-                    handleNoContentAvailable
-                );
-            };
-
-            // Attach the cleanup function to the component
-            return removeEventListenerFunction;
-        };
-
-        script.onload = handleScriptLoad;
-
         document.head.appendChild(script);
+
+        script.onload = () => {
+            setIsLoading(false);
+        };
 
         return () => {
             document.head.removeChild(script);
         };
-    }, [noteId]);
+    }, []);
+
+    useEffect(() => {
+        if (!isLoading && document.querySelector('.audio-player')) {
+            try {
+                const player = new BeyondWords.Player({
+                    target: '.audio-player',
+                    projectId: BEYONDWORDS_PROJECT_ID,
+                    sourceId: noteId,
+                    playbackRates: [1, 1.25, 1.5, 1.7, 2],
+                    playbackState: 'playing',
+                    skipButtonStyle: 'seconds',
+                    logoIconEnabled: false,
+                    widgetWidth: '40rem'
+                });
+
+                const handleNoContentAvailable = () => {
+                    setError(true);
+                };
+
+                player.addEventListener(
+                    'NoContentAvailable',
+                    handleNoContentAvailable
+                );
+
+                return () => {
+                    player.removeEventListener(
+                        'NoContentAvailable',
+                        handleNoContentAvailable
+                    );
+                };
+            } catch (error) {
+                console.error(
+                    'Failed to initialize the BeyondWords player:',
+                    error
+                );
+                setError(true);
+            }
+        }
+    }, [isLoading, noteId]);
+
     return (
         <>
             {!isLoading && !error ? (
