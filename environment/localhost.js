d89eab7eecac10eadc5b940384bbd9cf0338199e
Julio Quintana <jquintana@lanacion.com.ar>
2024-04-30 12:12:49 -0300

    Añadir archivo de configuración de entorno local para desarrollo y producción

---
+++ environment/localhost.js
@@ -0,0 +1,29 @@
+export default {
+    /**
+     * 🛠️ Configuración de variables de entorno para el entorno de desarrollo.
+     * 🔁 Para alternar entre producción y sandbox, ajusta las siguientes líneas:
+     */
+    // ** 🚀 Variables de producción (descomentar para usar):
+    // IS_SANDBOX: 'false',
+    // API_ENV: "prod",
+    // SITE_LANACION: "https://www.lanacion.com.ar",
+    // RESIZER_URL: "https://resizer.glanacion.com/resizer",
+    // RESIZER_URL_PUBLIC: "https://resizer.glanacion.com",
+    // ** 🏖️ Variables de entorno actuales para sandbox:
+    IS_SANDBOX: 'true',
+    API_ENV: 'sandbox',
+    SITE_LANACION: 'https://sandbox.lanacion.com.ar',
+    RESIZER_URL: 'https://sandbox-resizer.glanacion.com/resizer',
+    RESIZER_URL_PUBLIC: 'https://sandbox-resizer.glanacion.com',
+    // Fin variables de sandbox
+
+    // 🔐 Claves API y configuración:
+    LIFTIGNITER_X_API_KEY:
+        '%{AQICAHh/411m8FGwOHtrUTs9KkxN3n0LVOK3XAvOUKflIN5VygH4H8MiTVHZCbdSuL7xddF4AAAAgzCBgAYJKoZIhvcNAQcGoHMwcQIBADBsBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDHe+801QKRWObkNsMAIBEIA/+JYrRgViUeZiux4Xx9AYqmEhA6cKtfM+dnDk+EbdKHGGSKcmrcaSQFYRToSVNw0bTAHsmp6d3pfGxrYHrwZU}',
+    JSK_ID: '8561ps8ov66e7mim',
+    OPTA_WIDGET_URL: 'http://arc-widgets.lanacion.com.ar/opta-embed',
+    ARC_STATIC: 'http://localhost.lanacion.com.ar',
+    WIDGETS: 'li-nacion-recommended-item-template-1',
+    IS_DEV: 'true',
+    CONTENT_TIMEOUT: '8000000'
+};
