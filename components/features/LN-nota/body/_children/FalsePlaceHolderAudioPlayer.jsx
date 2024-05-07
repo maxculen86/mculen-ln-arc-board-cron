b80e8d97739814d1dc7e5897cee6d45da0194ee7
Lautaro Barandiara <barandiaranlautaro@gmail.com>
2024-05-07 15:31:12 -0300

    Se quitan props que no se usan y se reemplaza componente hardcode por componentes de las libs

--- components/features/LN-nota/body/_children/FalsePlaceHolderAudioPlayer.jsx
+++ components/features/LN-nota/body/_children/FalsePlaceHolderAudioPlayer.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
+import { Icon } from '@ln/common-ui-icon';
+import { Text } from '@ln/contenidos-ui-text';
+import { Button } from '@ln/contenidos-ui-button';
+import IconSprite from '../../../private-global/common/iconSprite/IconSprite';
 
 export const FalsePlaceHolderAudioPlayer = ({ isListenable }) =>
     isListenable && (
         <div className="btn-container l-only flex w-100 mb-32 ai-center transition transition-all transition-ease-in transition-duration-1000 min-h-56">
-            <button
-                className="button ln-button font-bold border border-thin rounded-4 gap-8 bg-blue-500 text-light-0 h-40 px-16 py-12 mr-16 --no-app --primary"
+            <Button
                 title="Escuchar nota"
+                variant="primary"
                 type="button"
                 disabled
             >
-                <i className="icon --icon-24 --inherit">
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                    >
-                        <use href="/pf/resources/images/la-nacion-ar-sprite-default.svg?d=%24LATEST#headphone" />
-                    </svg>
-                </i>
-                <span className="text ln-text">Escuchar</span>
-            </button>
+                <Icon size={24} color="inherit">
+                    <IconSprite name="headphone" />
+                </Icon>
+                <Text>Escuchar</Text>
+            </Button>
         </div>
     );
