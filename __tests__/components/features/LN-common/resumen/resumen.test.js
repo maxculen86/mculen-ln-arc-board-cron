04b1cbc92c4bc772ad141608a95e78e8cd0fcd47
ccabrera <ccabrera@lanacion.com.ar>
2024-05-13 14:26:44 -0300

    se agrega termica para resumen nota

--- __tests__/components/features/LN-common/resumen/resumen.test.js
+++ __tests__/components/features/LN-common/resumen/resumen.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Resumen from '../../../../../components/features/LN-common/resumen/default';
+import useTermica from '../../../../../components/private/common/hooks/useTermica';
 import '@testing-library/jest-dom';
 
 jest.mock('fusion:context', () => ({
@@ -19,22 +20,17 @@ jest.mock('fusion:context', () => ({
     })
 }));
 
+jest.mock('../../../../../components/private/common/hooks/useTermica', () =>
+    jest.fn()
+);
+
 describe('Resumen component', () => {
-    it('renders SummaryNote inside StaticContent when hide prop is false', () => {
+    it('renders SummaryNote inside StaticContent when thermalSummary is true and hide is false', () => {
+        useTermica.mockImplementation(() => true);
         const customFields = { hide: false };
         const { getByTestId } = render(<Resumen customFields={customFields} />);
 
         const summaryNote = getByTestId('summary-note');
         expect(summaryNote).toBeInTheDocument();
-    });
-
-    it('does not render SummaryNote inside StaticContent when hide prop is true', () => {
-        const customFields = { hide: true };
-        const { queryByTestId } = render(
-            <Resumen customFields={customFields} />
-        );
-
-        const summaryNote = queryByTestId('summary-note');
-        expect(summaryNote).not.toBeInTheDocument();
     });
 });
