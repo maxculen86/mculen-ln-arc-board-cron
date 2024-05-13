1883b0187f1b2930d492f0b174bde9588c1b3154
Juan Carlos Diaz <jcDiaz@lanacion.com.ar>
2024-05-13 12:37:02 -0300

    Fix en armado de urls de tags + cambio de nombre de seccion en lista de filtros del buscador.
    - Se crea funcion "getTagList" para armar la lista de tags con sus respectivas URLs en los casos de tags que vienen de strapi y tags que vienen de sections.
    - Se crea funcion setUrlTag responsable del armado de las urls para cada caso.
    - Se agregan tests correspondientes.
    - Se corrige nombre de lista de filtros duplicado. ("Tipo de coccion" pasa a "region")

---
+++ __tests__/components/features/foodit-global/body/powerUpsRecetas/_helpers.test.js
@@ -0,0 +1,202 @@
+import getTagList, {
+    setUrlTag
+} from '../../../../../../components/features/foodit-global/Body/PowerupsReceta/_helper';
+
+jest.mock('fusion:environment', () => {
+    return {
+        SITE_FOODIT: 'https://foodit.lanacion.com.ar'
+    };
+});
+
+describe('Tests - body - PowerupsReceta - helpers', () => {
+    describe('Tests function setUrlTag', () => {
+        test('Returns an empty list if no arguments are provided', () => {
+            expect(setUrlTag()).toEqual([]);
+        });
+
+        test('Returns an empty list if tagList is an empty array', () => {
+            expect(setUrlTag({ tagList: [] })).toEqual([]);
+        });
+
+        test('Returns labels with section URLs correctly', () => {
+            const input = {
+                nameSection: '',
+                primarySection: 'Recetas',
+                tagList: [
+                    { name: 'Al horno', path: '/al-horno' },
+                    { name: 'Frito', path: '/frito' }
+                ]
+            };
+
+            expect(setUrlTag(input)).toEqual([
+                {
+                    text: 'Al horno',
+                    url: '/al-horno'
+                },
+                {
+                    text: 'Frito',
+                    url: '/frito'
+                }
+            ]);
+        });
+
+        test('Returns tags with default URLs if no path is provided in the tags', () => {
+            const input = {
+                nameSection: 'occasions',
+                primarySection: 'Recetas',
+                tagList: ['Desayunos', 'Postres']
+            };
+
+            expect(setUrlTag(input)).toEqual([
+                {
+                    text: 'Desayunos',
+                    url:
+                        'https://foodit.lanacion.com.ar/buscador/?query=Recetas&fkey=occasions&fval=Desayunos'
+                },
+                {
+                    text: 'Postres',
+                    url:
+                        'https://foodit.lanacion.com.ar/buscador/?query=Recetas&fkey=occasions&fval=Postres'
+                }
+            ]);
+        });
+
+        test('Returns an empty list if tagList is null', () => {
+            expect(setUrlTag({ tagList: null })).toEqual([]);
+        });
+
+        test('Returns tags with URL based on primarySection if tagList is not provided', () => {
+            const input = {
+                primarySection: { name: 'Recetas', path: '/recetas/' }
+            };
+
+            expect(setUrlTag(input)).toEqual([]);
+        });
+    });
+
+    describe('tests function getTagList', () => {
+        test('Returns an empty list if no arguments are provided', () => {
+            const result = getTagList();
+            expect(result).toEqual([]);
+        });
+
+        test('Returns an empty list if the arguments value is null', () => {
+            const result = getTagList({
+                cookingTypes: null,
+                occasions: null,
+                taxonomy: null,
+                regions: null
+            });
+            expect(result).toEqual([]);
+        });
+
+        test('Return cookingTypes tags correctly', () => {
+            const result = getTagList({
+                taxonomy: {
+                    primary_section: { name: 'Recetas' }
+                },
+                cookingTypes: ['Al horno']
+            });
+
+            expect(result).toEqual([
+                {
+                    text: 'Al horno',
+                    url:
+                        'https://foodit.lanacion.com.ar/buscador/?query=Recetas&fkey=cookingTypes&fval=Al horno'
+                }
+            ]);
+        });
+
+        test('Returns occasion tags correctly', () => {
+            const result = getTagList({
+                taxonomy: {
+                    primary_section: { name: 'Recetas' }
+                },
+                occasions: ['Día de la Amistad', 'Aniversario']
+            });
+
+            expect(result).toEqual([
+                {
+                    text: 'Día de la Amistad',
+                    url:
+                        'https://foodit.lanacion.com.ar/buscador/?query=Recetas&fkey=occasions&fval=Día de la Amistad'
+                },
+                {
+                    text: 'Aniversario',
+                    url:
+                        'https://foodit.lanacion.com.ar/buscador/?query=Recetas&fkey=occasions&fval=Aniversario'
+                }
+            ]);
+        });
+
+        test('Returns region labels correctly', () => {
+            const input = {
+                taxonomy: {
+                    primary_section: { name: 'Recetas' }
+                },
+                regions: ['Italiana', 'Estadounidense']
+            };
+            const result = getTagList(input);
+            expect(result).toEqual([
+                {
+                    text: 'Italiana',
+                    url:
+                        'https://foodit.lanacion.com.ar/buscador/?query=Recetas&fkey=regions&fval=Italiana'
+                },
+                {
+                    text: 'Estadounidense',
+                    url:
+                        'https://foodit.lanacion.com.ar/buscador/?query=Recetas&fkey=regions&fval=Estadounidense'
+                }
+            ]);
+        });
+
+        test('Returns section labels correctly', () => {
+            const input = {
+                taxonomy: {
+                    primary_section: { name: 'Dulces' },
+                    sections: [
+                        { name: 'Sin gluten', path: '/recetas/sin-gluten' },
+                        { name: 'Postres', path: '/recetas/postres' }
+                    ]
+                }
+            };
+            const result = getTagList(input);
+            expect(result).toEqual([
+                { text: 'Sin gluten', url: '/recetas/sin-gluten' },
+                { text: 'Postres', url: '/recetas/postres' }
+            ]);
+        });
+
+        test('Returns a combined list of tags correctly', () => {
+            const input = {
+                cookingTypes: ['Al horno'],
+                occasions: ['Día de la Amistad'],
+                regions: ['Italiana'],
+                taxonomy: {
+                    primary_section: { name: 'Recetas' },
+                    sections: [{ name: 'Postres', path: '/recetas/postres' }]
+                }
+            };
+            const result = getTagList(input);
+            expect(result).toEqual([
+                {
+                    text: 'Al horno',
+                    url:
+                        'https://foodit.lanacion.com.ar/buscador/?query=Recetas&fkey=cookingTypes&fval=Al horno'
+                },
+                {
+                    text: 'Día de la Amistad',
+                    url:
+                        'https://foodit.lanacion.com.ar/buscador/?query=Recetas&fkey=occasions&fval=Día de la Amistad'
+                },
+                {
+                    text: 'Italiana',
+                    url:
+                        'https://foodit.lanacion.com.ar/buscador/?query=Recetas&fkey=regions&fval=Italiana'
+                },
+                { text: 'Postres', url: '/recetas/postres' }
+            ]);
+        });
+    });
+});
