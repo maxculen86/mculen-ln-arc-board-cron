234792c64f5868509968ca2fe66bada148827177
ccabrera <ccabrera@lanacion.com.ar>
2024-05-16 08:56:16 -0300

    Se valida que boton suscribite no se muestre cuando sos suscr foodit

--- __tests__/src/statics/LN/js/scriptBuildRoof.test.js
+++ __tests__/src/statics/LN/js/scriptBuildRoof.test.js
@@ -1,26 +1,21 @@
 import '@testing-library/jest-dom';
 import { handleScriptBtnSuscription } from '../../../../../src/statics/LN/js/scriptBuildRoof';
-
 describe('scr - statics - LN - js', () => {
     Object.defineProperty(document, 'cookie', {
         writable: true,
         value: 'SomeCookie; ProductoPremiumId=2; AnotherCookie'
     });
-
     jest.spyOn(document, 'querySelector').mockReturnValue({
         classList: {
             add: jest.fn()
-        }
+        },
+        remove: jest.fn() // Mocking remove method
     });
-
     test('hides button when premium product is in the cookie', () => {
         handleScriptBtnSuscription();
-
         expect(document.querySelector).toHaveBeenCalledWith(
             'a.--roof-button.--subscribe'
         );
-        expect(document.querySelector().classList.add).toHaveBeenCalledWith(
-            'none'
-        );
+        expect(document.querySelector().remove).toHaveBeenCalled();
     });
 });
