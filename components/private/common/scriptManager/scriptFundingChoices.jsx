0b78b4f51775a62328acc243c332daa0db323a5e
Andrea Eurrieta <eeurrieta@lanacion.com.ar>
2024-05-21 09:12:07 -0300

    se eliminan comentarios del script

--- components/private/common/scriptManager/scriptFundingChoices.jsx
+++ components/private/common/scriptManager/scriptFundingChoices.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const FundingChoices = () => {
     const scriptContent = `
-        // Make sure that the googlefc property exists on the window.
         window.googlefc = window.googlefc || {};
-        // To guarantee functionality, this must go before the FC tag on the page.
         googlefc.controlledMessagingFunction = function (message) {
             message.proceed(true);
         };
