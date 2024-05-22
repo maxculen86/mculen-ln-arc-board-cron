b503296da965aef9a3ff40543e4353637e498bf9
Machi <jmachicado@lanacion.com>
2024-05-22 16:46:07 -0300

    fix boton ver mas y paginado en acus foodit

--- components/features/foodit/GrillaNotasAcu/helpers/gridArticles.jsx
+++ components/features/foodit/GrillaNotasAcu/helpers/gridArticles.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import { createArticleList } from '.';
 
-export const GridArticlesFoodit = ({
-    articles = [],
-    maxArticles,
-    haveShowButton
-}) => {
-    if (articles.length < maxArticles) {
-        haveShowButton();
-    }
-
+export const GridArticlesFoodit = ({ articles = [] }) => {
     return (
         <div className="grid grid-cols-8 grid-cols-12_md grid-cols-16_lg gap-32">
             {createArticleList({ articles })}
