e13d297096ad4c012ba39c3e4abf9347f028e5e4
Samie Lee <slee@lanacion.com.ar>
2024-05-28 14:37:55 -0300

    Fix de titulo de acus

--- components/private/LN/acumulado/hocs/withAcuCategories.jsx
+++ components/private/LN/acumulado/hocs/withAcuCategories.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import PropTypes from 'fusion:prop-types';
 import { useContent } from 'fusion:content';
 import get from '../../../common/utils/get';
-
 const withAcuCategories = (WrappedComponent, filter, website) => props => {
     const idPrimarySection = get(props, 'globalContent._id', undefined);
     const { hideCategories = 'false', hierarchyManual } = props;
     const isPrimarySection =
         idPrimarySection && idPrimarySection.split('/').splice(1).length === 1;
-
     const _children = get(props, 'globalContent.children', undefined);
-
     const navigationList =
         hierarchyManual &&
         useContent({
@@ -21,28 +18,19 @@ const withAcuCategories = (WrappedComponent, filter, website) => props => {
             },
             filter
         });
-
     const response = get(navigationList, 'children', null);
-
     const navigation = (response && response.length && response) || _children;
-
-    const ComponentWithAcuCategories = props => {
-        return (
-            <WrappedComponent
-                {...props}
-                isPrimarySection={isPrimarySection}
-                navigation={hideCategories === 'false' && navigation}
-            />
-        );
-    };
-
-    return ComponentWithAcuCategories;
+    return (
+        <WrappedComponent
+            {...props}
+            isPrimarySection={isPrimarySection}
+            navigation={hideCategories === 'false' && navigation}
+        />
+    );
 };
-
 withAcuCategories.propTypes = {
     WrappedComponent: PropTypes.func.isRequired,
     filter: PropTypes.string.isRequired,
     website: PropTypes.string.isRequired
 };
-
 export default withAcuCategories;
