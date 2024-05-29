d91b401092a5ca90a0e1ae24a8e0690539e5a2ba
Lautaro <lbarandiaran@lanacion.com.ar>
2024-05-29 13:26:35 -0300

    feat: (foodit) se agrega boton al componente <Search/> - se agrega boton con icono de lupa para el Search - se agregan atributos por SEO, y se oculta X nativa de html para limpiar el campo a pedido de diseño - se instala lib de sass - se testea todo el componente y se actualizan los correspondientes snapshots

--- __tests__/components/features/foodit-global/common/Header/components/search.test.jsx
+++ __tests__/components/features/foodit-global/common/Header/components/search.test.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import { render, fireEvent } from '@testing-library/react';
 import { Search } from '../../../../../../../components/features/foodit-global/common/Header/components/Search';
 
+jest.mock('fusion:environment', () => ({
+    SITE_FOODIT: 'https://foodit-lanacion.com.ar'
+}));
 describe('Components - features - foodit-global - common - header - components - Search', () => {
+    it('renders without crashing', () => {
+        const { getByPlaceholderText, getByTitle } = render(<Search />);
+
+        const inputElement = getByPlaceholderText('¿Qué querés cocinar hoy?');
+        expect(inputElement).toBeInTheDocument();
+
+        const buttonElement = getByTitle('Buscar');
+        expect(buttonElement).toBeInTheDocument();
+    });
+
+    it('updates on change', () => {
+        const { getByPlaceholderText } = render(<Search />);
+        const inputElement = getByPlaceholderText('¿Qué querés cocinar hoy?');
+
+        fireEvent.change(inputElement, { target: { value: 'test' } });
+        expect(inputElement.value).toBe('test');
+    });
+    it('pressing enter redirects to correct url', () => {
+        const { getByPlaceholderText } = render(<Search />);
+
+        const inputElement = getByPlaceholderText('¿Qué querés cocinar hoy?');
+
+        delete window.location;
+        window.location = { href: '' };
+
+        fireEvent.change(inputElement, { target: { value: 'searching' } });
+        fireEvent.keyDown(inputElement, { key: 'Enter', code: 'Enter' });
+
+        expect(window.location.href).toBe(
+            'https://foodit-lanacion.com.ar/buscador/?query=searching'
+        );
+    });
+    it('button "Buscar" as Ancle should be a correctly URL', () => {
+        const { getByPlaceholderText, getByTitle } = render(<Search />);
+
+        const inputElement = getByPlaceholderText('¿Qué querés cocinar hoy?');
+        const buttonElementAsAncle = getByTitle('Buscar');
+
+        fireEvent.change(inputElement, { target: { value: 'searching' } });
+
+        expect(buttonElementAsAncle).toHaveAttribute(
+            'href',
+            'https://foodit-lanacion.com.ar/buscador/?query=searching'
+        );
+    });
+
     it('should match snapshot', () => {
         const { container } = render(<Search />);
         expect(container).toMatchSnapshot();
