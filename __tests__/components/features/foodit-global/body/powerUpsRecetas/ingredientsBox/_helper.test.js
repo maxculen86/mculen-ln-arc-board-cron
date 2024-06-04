8613768cb41072fb5de8be88c65fedc174f3b5c5
Ezequiel Dapena <edapena@lanacion.com.ar>
2024-06-04 09:08:25 -0300

    Boton "Agregar a lista de ingredintes" de ficha-receta dinamico 	-Se agrega la posibilidad de eliminar los ingredientes de la lista desde la misma receta, donde previamente se agregaban

---
+++ __tests__/components/features/foodit-global/body/powerUpsRecetas/ingredientsBox/_helper.test.js
@@ -0,0 +1,56 @@
+import { ingredientsListReduce } from '../../../../../../../components/features/foodit-global/Body/PowerupsReceta/ingredientsBox/_helper';
+
+describe('component - features - foodit-global - body - ingredientsListReduce', () => {
+    it('should return only ingredient lists', () => {
+        const input = [
+            {
+                typeList: 'ingredientes',
+                items: [{ id: '1', includeInShoppingList: true }]
+            },
+            {
+                typeList: 'otros',
+                items: [{ id: '2', includeInShoppingList: false }]
+            }
+        ];
+        const result = input.reduce(ingredientsListReduce, []);
+        console.log(result); // Agregar log para depurar
+        expect(result).toEqual([
+            {
+                typeList: 'ingredientes',
+                items: [{ id: '1', includeInShoppingList: true }]
+            }
+        ]);
+    });
+
+    it('should return lists with filtered items', () => {
+        const input = [
+            {
+                typeList: 'otros',
+                items: [
+                    { id: '1', includeInShoppingList: true },
+                    { id: '2', includeInShoppingList: false }
+                ]
+            }
+        ];
+        const result = input.reduce(ingredientsListReduce, []);
+        console.log(result); // Agregar log para depurar
+        expect(result).toEqual([
+            {
+                typeList: 'otros',
+                items: [{ id: '1', includeInShoppingList: true }]
+            }
+        ]);
+    });
+
+    it('should return empty array if no items to include in shopping list', () => {
+        const input = [
+            {
+                typeList: 'otros',
+                items: [{ id: '1', includeInShoppingList: false }]
+            }
+        ];
+        const result = input.reduce(ingredientsListReduce, []);
+        console.log(result); // Agregar log para depurar
+        expect(result).toEqual([]);
+    });
+});
---
+++ components/features/foodit-global/Body/PowerupsReceta/ingredientsBox/hooks/useIsInShoppingList.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import getBookmarkByArticleId from '../../../../common/bookmark/api/getBookmarkByArticleId';
+
+export const useIsInShoppingList = (articleId = '', isSuscriptor) => {
+    const [bookmarkId, setBookmarkId] = useState(null);
+
+    useEffect(() => {
+        const fetchUserBookmarks = async () => {
+            const { bookmarkId: resBookmarkId } = await getBookmarkByArticleId(
+                'ingredientList',
+                articleId
+            );
+
+            resBookmarkId && setBookmarkId(resBookmarkId);
+        };
+
+        if (isSuscriptor) fetchUserBookmarks();
+    }, []);
+
+    return { bookmarkId, setBookmarkId };
+};
