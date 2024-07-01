e98f2d12edd4bf303af131e5151a8a02ba42a5a9
Samie Lee <slee@lanacion.com.ar>
2024-07-01 16:08:24 -0300

    Footer: Copyright & Edition Details - Se cambia el año para copyright en footer para que sea dinamico - Se arma helper englobando los datos de fecha y numero de edicion junto con el año de copyright. - Se agrega test de helper getEditonDetails() - Se actualiza test de helper

--- __tests__/components/private/LN10/footer/_helper.test.js
+++ __tests__/components/private/LN10/footer/_helper.test.js
@@ -1,17 +1,50 @@
 import commonPropsFooter from '../../../../../components/private/LN10/footer/_helper';
+import { getEditionDetails } from '../../../../../components/private/LN10/footer/_helper';
+import {
+    datesDiffInDays,
+    getArgentinaDateMonthYear,
+    getArgentinaYear,
+} from '../../../../../components/private/common/utils/dateAndTimeUtil';
 
-describe('Tests function commonPropsFooter', () => {
+jest.mock(
+    '../../../../../components/private/common/utils/dateAndTimeUtil',
+    () => ({
+        datesDiffInDays: jest.fn(() => 100),
+        getArgentinaDateMonthYear: jest.fn(() => '01/07/2024'),
+        getArgentinaYear: jest.fn(() => 2024),
+    }),
+);
+
+describe('Tests commonPropsFooter function', () => {
     const dinamycParameters = ['Economia', 'https://lanacion.com.ar/economia/'];
     test('Should return an object with the specified parameters. (href and text)', () => {
         expect(
-            commonPropsFooter('Economia', 'https://lanacion.com.ar/economia/')
+            commonPropsFooter('Economia', 'https://lanacion.com.ar/economia/'),
         ).toStrictEqual({
             href: 'https://lanacion.com.ar/economia/',
-            text: 'Economia'
+            text: 'Economia',
         });
     });
 
     test('It should return an empty object when the parameters are not defined.', () => {
         expect(commonPropsFooter(undefined)).toStrictEqual({});
+    });
+});
+
+describe('Tests getEditionDetails function', () => {
+    it('should return the expected data structure', () => {
+        const result = getEditionDetails();
+
+        expect(result).toEqual({
+            edDate: {
+                date: '01/07/2024',
+                year: 2024,
+            },
+            edNumber: 100,
+        });
+
+        expect(datesDiffInDays).toHaveBeenCalled();
+        expect(getArgentinaDateMonthYear).toHaveBeenCalled();
+        expect(getArgentinaYear).toHaveBeenCalled();
     });
 });
