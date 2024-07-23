6e12f9dd2c9e917c828b007bc78fed990c495919
rrobol <robolfba@gmail.com>
2024-07-23 08:32:24 -0300

    Se actualiza campo duration del VideoObject. Se implementa test para millisecondsToTime.

--- components/private/common/utils/millisecondsToTime.js
+++ components/private/common/utils/millisecondsToTime.js
@@ -1,7 +1,9 @@
 const msToTime = duration => {
+    if (!duration) return 'PT0M0S';
+
     const seconds = Math.floor((duration / 1000) % 60);
     const minutes = Math.floor((duration / (1000 * 60)) % 60);
-    return `T${minutes}M${seconds}S`;
+    return `PT${minutes}M${seconds}S`;
 };
 
 export default msToTime;
