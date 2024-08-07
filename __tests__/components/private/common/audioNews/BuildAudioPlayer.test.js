094e0fbe75e156ee9a0c61181e33cf0c8cdcdfed
Maurizio De Marchis <mdemarchis@lanacion.com.ar>
2024-08-07 15:35:11 -0300

    Se añaden tests para componente BuildAudioPlayer

---
+++ __tests__/components/private/common/audioNews/BuildAudioPlayer.test.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import BuildAudioPlayer from '../../../../../components/private/common/audioNews/BuildAudioPlayer';
+import { GlobalContext } from '../../../../../components/private/common/context/globalContext';
+import * as helpers from '../../../../../components/private/common/audioNews/helpers';
+
+jest.mock('../../../../../components/private/common/audioNews/helpers', () => ({
+    ...jest.requireActual(
+        '../../../../../components/private/common/audioNews/helpers'
+    ),
+    setCookie: jest.fn(),
+    getCookie: jest.fn()
+}));
+
+describe('components - private - common - audioNews - BuildAudioPlayer', () => {
+    const mockDispatch = jest.fn();
+    const defaultContextValue = { dispatch: mockDispatch };
+
+    const defaultProps = {
+        setOpenPlayer: jest.fn(),
+        setEnableButton: jest.fn(),
+        noteId: 'test-note-id',
+        loaderClass: 'test-loader-class'
+    };
+
+    const renderComponent = (props = {}, contextValue = defaultContextValue) =>
+        render(
+            <GlobalContext.Provider value={contextValue}>
+                <BuildAudioPlayer {...defaultProps} {...props} />
+            </GlobalContext.Provider>
+        );
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        helpers.getCookie.mockReturnValue('article');
+    });
+
+    it('should render LoadingIcon when isLoading is true', () => {
+        const { container } = renderComponent({
+            setOpenPlayer: jest.fn(),
+            setEnableButton: jest.fn(),
+            noteId: 'test-note-id',
+            loaderClass: 'test-loader-class'
+        });
+
+        expect(container.querySelector('.loader')).toBeInTheDocument();
+    });
+
+    it('should render audio player when isLoading is false and no error', async () => {
+        helpers.getCookie.mockReturnValue('article');
+        const script = document.createElement('script');
+        script.src =
+            'https://proxy.beyondwords.io/npm/@beyondwords/player@latest/dist/umd.js';
+        script.onload = () => {
+            renderComponent();
+            expect(screen.getByLabelText('')).toBeInTheDocument();
+            expect(screen.getByRole('presentation')).toBeInTheDocument();
+        };
+
+        document.head.appendChild(script);
+
+        await waitFor(() => {
+            expect(screen.queryByText('Loading')).not.toBeInTheDocument();
+        });
+    });
+
+    it('should handle error state', async () => {
+        const script = document.createElement('script');
+        script.src =
+            'https://proxy.beyondwords.io/npm/@beyondwords/player@latest/dist/umd.js';
+        script.onload = () => {
+            jest.spyOn(console, 'error').mockImplementation(() => {});
+            renderComponent();
+            expect(mockDispatch).toHaveBeenCalledWith({
+                type: 'SHOW_MODAL',
+                payload: {
+                    typeModal: 'toast',
+                    open: true,
+                    data: {
+                        status: 'danger',
+                        description: 'Parece que hubo un problema.',
+                        timeout: 2750
+                    }
+                }
+            });
+        };
+
+        document.head.appendChild(script);
+    });
+});
