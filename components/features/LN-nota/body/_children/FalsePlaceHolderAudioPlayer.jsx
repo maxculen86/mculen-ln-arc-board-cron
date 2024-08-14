2ea084d8f4c5baba0fe20bb8e8df22b6f6ad2e7d
Perez Laborda, Dolores (Producto y Tecnologia) <doloresp@lanacion.com.ar>
2024-08-14 17:59:36 +0000

    Merged PR 27333: Fix estilos de audio player
    1. Se elimina border del componente (AudioPlayerDesktop) cuando no hay audio en la nota.
    1. Se agrega margin bottom al componente (AudioPlayerDesktop) en nota con foto al 100.
    
    - Antes:
    ![image (1).png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/27333/attachments/image%20%281%29.png)
    
    - Despues:
    ![image (2).png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/27333/attachments/image%20%282%29.png)
    
    __________________________________________________________________________________________________________________
    
    -Antes:
    ![image (3).png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/27333/attachments/image%20%283%29.png)
    
    -Despues:
    ![image (4).png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/27333/attachments/image%20%284%29.png)
    
    [DEMO-DEV NOTA SIN AUDIO](https://dev.lanacionar.arcpublishing.com/pf//arquitectura/este-es-el-segundo-test-de-nota-en-arc-nid21092020/?_website=la-nacion-ar&d=3387)
    
    [DEMO-SANDBOX NOTA CON AUDIO](https://sandbox.lanacionar.arcpublishing.com/pf/deportes/tenis/rafael-nadal-se-dio-de-baja-en-wimbledon-para-prepararse-de-cara-a-sus-ultimos-juegos-olimpicos-nid18062024/?_website=la-nacion-ar&d=2734)

--- components/features/LN-nota/body/_children/FalsePlaceHolderAudioPlayer.jsx
+++ components/features/LN-nota/body/_children/FalsePlaceHolderAudioPlayer.jsx
@@ -6,10 +6,17 @@ import IconSprite from '../../../private-global/common/iconSprite/IconSprite';
 
 export const FalsePlaceHolderAudioPlayer = ({ isListenable }) =>
     isListenable && (
-        <Button title="Escuchar nota" variant="primary" type="button" disabled>
-            <Icon size={24} color="inherit">
-                <IconSprite name="listen" />
-            </Icon>
-            <Text>Escuchar</Text>
-        </Button>
+        <div className="btn-container l-only w-100 mb-32 ai-start transition transition-all transition-ease-in transition-duration-1000 min-h-56 py-16 grid border border-bottom border-top border-thin border-neutral-light-100">
+            <Button
+                title="Escuchar nota"
+                variant="primary"
+                type="button"
+                disabled
+            >
+                <Icon size={24} color="inherit">
+                    <IconSprite name="listen" />
+                </Icon>
+                <Text>Escuchar</Text>
+            </Button>
+        </div>
     );
--- components/features/LN-nota/body/default.jsx
+++ components/features/LN-nota/body/default.jsx
@@ -66,20 +66,15 @@ const body = ({ customFields }) => {
 
     return (
         <>
-            <div
-                className="btn-container l-only w-100 mb-32 ai-start transition transition-all transition-ease-in transition-duration-1000 min-h-56 py-16 grid border border-bottom border-top border-thin border-neutral-light-100"
-                id="audio-player-desktop"
-            >
-                {isClient ? (
-                    <AudioPlayerDesktop
-                        isListenable={isListenable}
-                        noteId={_id}
-                        className={'--no-app'}
-                    />
-                ) : (
-                    <FalsePlaceHolderAudioPlayer isListenable={isListenable} />
-                )}
-            </div>
+            {isClient ? (
+                <AudioPlayerDesktop
+                    isListenable={isListenable}
+                    noteId={_id}
+                    className={'--no-app'}
+                />
+            ) : (
+                <FalsePlaceHolderAudioPlayer isListenable={isListenable} />
+            )}
             {renderComponents}
         </>
     );
