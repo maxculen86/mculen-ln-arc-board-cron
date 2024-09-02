f67aa438fd5493cbf58cdf12aed49404c634e8e9
Samanta Lee <slee@lanacion.com.ar>
2024-09-02 12:37:39 +0000

    Merged PR 28066: Common Collection timeline fix
    Common Collection timeline fix
    - Se mueve de div el child para la ubicacion del timeline.

--- components/private/LN10/home/components/CommonCollection/default.jsx
+++ components/private/LN10/home/components/CommonCollection/default.jsx
@@ -99,8 +99,8 @@ export default function CommonCollection({
                         />
                     );
                 })}
+                {children}
             </ContainerCards>
-            {children}
         </>
     );
 }
