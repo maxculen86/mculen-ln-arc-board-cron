ae0248967d0c92fb56e4ea7b4160bcc827a5b345
Juan Carlos Diaz <jcDiaz@lanacion.com.ar>
2024-09-11 10:47:40 -0300

    Se reemplaza el parametro de projectId de audio news por la variable de entorno: BEYONDWORDS_PROJECT_ID que quedo apuntando a sandbox

--- components/private/common/audioNews/BuildAudioPlayer.jsx
+++ components/private/common/audioNews/BuildAudioPlayer.jsx
@@ -64,7 +64,7 @@ const BuildAudioPlayer = ({
                 if (!playerRef.current) {
                     playerRef.current = new BeyondWords.Player({
                         target: '.audio-player',
-                        projectId: 38983,
+                        projectId: BEYONDWORDS_PROJECT_ID,
                         sourceId: noteId,
                         playbackRates: [1, 1.25, 1.5, 1.7, 2],
                         playbackState: 'playing',
