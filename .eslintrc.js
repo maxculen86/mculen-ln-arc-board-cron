89a7951065a06e2194bb7daa2ff9b05c332b77d4
Julio Quintana <it_jquintana@lnexternos.com.ar>
2024-09-23 19:15:58 +0000

    Merged PR 28878: Actualiza configuración de Prettier a la versión 3.3.3
    Actualiza configuración de Prettier a la versión 3.3.3
    
    chore: ajustes en configuración de ESLint, Husky y VSCode
    
    • Se desactiva la regla de 'react/jsx-props-no-spreading' en ESLint.
    • Se agrega el hook de pre-commit con Husky para ejecutar lint-staged.
    • Se ajusta lint-staged para ejecutar ESLint con correcciones automáticas.
    • Se excluyen el directorio .vscode y package-lock.json de ESLint.
    
    chore: actualización de ESLint, Prettier y mejoras en scripts
    
    • Se permite el uso de console.warn, console.error y console.info en ESLint.
    • Se actualiza la regla 'import/no-extraneous-dependencies' para ignorar devDependencies en scripts y pruebas.
    • Se actualiza la configuración de Prettier con la opción 'arrowParens' en 'avoid'.
    • Se añade soporte para SonarLint en VSCode y configuración de SonarQube.
    • Se corrigen mensajes de consola y manejo de errores en los scripts de git.
    
    chore: ajusta configuración de ESLint y lint-staged
    
    • Se agrega 'package.json' al archivo '.eslintignore' para excluirlo de las validaciones de ESLint.
    • Se elimina 'eslint-config-react-app' de las extensiones en '.eslintrc.js' para evitar conflictos.
    • Se elimina el uso de 'git add' en el archivo '.lintstagedrc', ya que lint-staged maneja la adición de archivos de manera automática.
    • Se actualiza la versión de 'lint-staged' a la 15.2.10 en el archivo 'package.json'.
    
    chore: actualizar reglas ESLint y configuración de VSCode
    
    • Se eliminan las declaraciones globales 'Atomics' y 'SharedArrayBuffer' de ESLint, ya que no se usan en el proyecto actual.
    • Se actualiza la regla 'import/no-unresolved' para lanzar un error y agregar excepción con el prefijo 'fusion:*'.
    • Se agrega la configuración en VSCode para que aplique las correcciones automáticas de ESLint al guardar archivos.
    • Se valida la configuración de ESLint para JavaScript, React, TypeScript y TypeScript con React.
    
    feat: agregar regla para definir componentes funcionales con arrow functions
    
    • Se añade la regla 'react/function-component-definition' para forzar el uso de arrow functions en los componentes funcionales nombrados y sin nombre.
    • Mejora la consistencia en el estilo de los componentes en todo el proyecto.
    • Ajuste alineado a las buenas prácticas y estilo moderno de React..
    
    demo->
    Nota: https://dev.lanacionar.arcpublishing.com/pf/el-mundo/un-argentino-contrajo-coronavirus-en-el-crucero-en-japon-y-se-convirtio-en-el-primer-paciente-nid07022020/?_website=la-nacion-ar&d=3531
    Home: https://dev.lanacionar.arcpublishing.com/pf/homepage/?_website=la-nacion-ar&d=3531

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,41 @@
 module.exports = {
     env: {
         browser: true,
-        commonjs: true,
         es6: true,
-        node: true
+        node: true,
+        jest: true
     },
     settings: {
-        react: { version: '16.6.1' }
+        react: { version: '18.2.0' }
     },
-    extends: ['airbnb', 'prettier', 'react-app'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
-    },
+    extends: ['airbnb', 'prettier'],
     parserOptions: {
         ecmaFeatures: {
             jsx: true
         },
-        ecmaVersion: 2018,
+        ecmaVersion: 2021,
         sourceType: 'module'
     },
     plugins: ['prettier'],
     rules: {
         'prettier/prettier': ['error'],
-        'jsx-a11y/href-no-hash': [0],
         'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
         'react/jsx-indent': [2, 4],
-        'react/jsx-indent-props': [2, 4]
+        'react/jsx-indent-props': [2, 4],
+        'react/jsx-props-no-spreading': 'off',
+        'import/prefer-default-export': 'off',
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                devDependencies: [
+                    '**/scripts/**',
+                    '**/*.test.js',
+                    '**/*.spec.js',
+                    '**/dev/**'
+                ]
+            }
+        ],
+        'no-console': [2, { allow: ['warn', 'error', 'info'] }]
     },
     overrides: [
         {
@@ -39,11 +48,9 @@ module.exports = {
         {
             files: ['*.js', '*.jsx'],
             rules: {
-                'import/no-unresolved': [0],
+                'import/no-unresolved': ['error', { ignore: ['fusion:'] }],
                 'import/order': [1],
-                'object-shorthand': [1],
-                'no-console': [2],
-                'no-unused-vars': [2]
+                'object-shorthand': [1]
             }
         },
         {
