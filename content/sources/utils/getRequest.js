4d3bdd602282c35aaa97eb6f4c75dcf26cf3336e
Sánchez de la Torre Federico <fsanchez@lanacion.com.ar>
2024-10-09 13:12:43 +0000

    Merged PR 29525: Se migra el util getRequest de la librería request-promise-native a node-fetc...
    - Se migra el util getRequest de la librería request-promise-native a node-fetch.
    - Se realizan pruebas verificando en dónde es utilizado y chequear que el funcionamiento siga siendo el correcto.
    
    ###Demo
    - dollarSource => https://dev.lanacionar.arcpublishing.com/pf/economia/?_website=la-nacion-ar&d=3567
    - servicesSource =>
    https://dev.lanacionar.arcpublishing.com/pf/feriados/?_website=la-nacion-ar&d=3567
    https://dev.lanacionar.arcpublishing.com/pf/clima/?_website=la-nacion-ar&d=3567
     https://dev.lanacionar.arcpublishing.com/pf/horoscopo/?_website=la-nacion-ar&d=3567
    https://dev.lanacionar.arcpublishing.com/pf/loterias/?_website=la-nacion-ar&d=3567
    - tagSource => https://dev.lanacionar.arcpublishing.com/pf/tema/lionel-messi-tid1619/?_website=la-nacion-ar&d=3567
    - addParallaxData => https://dev.lanacionar.arcpublishing.com/pf/tecnologia/prueba-power-up-parallax-nid11042022/?_website=la-nacion-ar&d=3567
    - collectionsSource (últimas noticias) => https://dev.lanacionar.arcpublishing.com/pf/homepage/?_website=la-nacion-ar&d=3567
    - addFollowAnotherNoteData (seguí leyendo) => https://dev.lanacionar.arcpublishing.com/pf/agencias/espana-con-marcos-llorente-y-busquets-contra-suiza-en-la-liga-de-naciones-nid27072022/?_website=la-nacion-ar&d=3567
    
    ###Card
    https://dev.azure.com/lndigital/Gestion%20LANACION-ARC/_boards/board/t/Contenidos-Web-LN/Stories/?workitem=129775
    
    Related work items: #129775

--- content/sources/utils/getRequest.js
+++ content/sources/utils/getRequest.js
@@ -1,17 +1,39 @@
-import request from 'request-promise-native';
+import nodeFetch from 'node-fetch';
 import { ARC_ACCESS_TOKEN } from 'fusion:environment';
+import logger from '../../../components/private/common/utils/logger';
 
 const getRequest = query => {
     const opt = {
-        uri: query,
-        json: true
+        method: 'GET'
     };
+
     if (ARC_ACCESS_TOKEN) {
-        opt.auth = {
-            bearer: ARC_ACCESS_TOKEN
-        };
+        opt.headers = { Authorization: `Bearer ${ARC_ACCESS_TOKEN}` };
     }
-    return request(opt).then(data => data);
+
+    const resolveData = async () => {
+        try {
+            const response = await nodeFetch(query, opt);
+
+            if (!response.ok) {
+                throw new Error(
+                    `HTTP error! status: ${response.status}, URL: ${query}`
+                );
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            logger.push(error, {
+                source: 'utils/getRequest',
+                query
+            });
+
+            return {};
+        }
+    };
+
+    return resolveData();
 };
 
 export default getRequest;
