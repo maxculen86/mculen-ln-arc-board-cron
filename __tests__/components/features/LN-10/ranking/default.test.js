a8d617f01ecfac02a0ceb14e5249af282db19713
Robol, Roberto Daniel (DESARROLLADOR FULL STACK SSR) <rrobol@lanacion.com.ar>
2024-10-23 13:05:00 +0000

    Merged PR 29969: Fix rankings en dev y sandbox v2
    **1)** Se incorporan nuevamente cambios implementados en:
    - [Pull Request 29124: Fix rankings en dev y sandbox](https://dev.azure.com/lndigital/Gestion%20LANACION-ARC/_git/Arc/pullrequest/29124?path=/content/sources/utils/rankingArticlesSource/_helper.js)
    - [Pull Request 29573: Se corrige funcion baseUrl asociada a Resizer](https://dev.azure.com/lndigital/Gestion%20LANACION-ARC/_git/Arc/pullrequest/29573)
    
    **2)** Se corrige el caso en el que retornabamos `null` dentro del `<Static>` en `components/features/LN-10/ranking/default.jsx` agregando un `early return` para evitar el caso anterior que generaba un error.
    
    **3)** Se valida en los distintos escenarios que al renderizar, la terminal no incluya errores.
    
    **Card:** [User Story 126661: ARC > dev y sandbox > fix rankings](https://lndigital.visualstudio.com/Gestion%20LANACION-ARC/_boards/board/t/PVS/Stories/?workitem=126661)
    
    **Demo (probar con versión promovida):**
    Homepage: https://dev.lanacionar.arcpublishing.com/pf/homepage/?_website=la-nacion-ar
    Acumulado: https://dev.lanacionar.arcpublishing.com/pf/deportes/?_website=la-nacion-ar
    Nota (tiene como contenido relacionado una nota NO PUBLICADA para validar que renderice OK la original): https://dev.lanacionar.arcpublishing.com/pf/sociedad/el-hallazgo-de-un-oceano-de-magma-extinto-en-la-luna-que-ayuda-a-explicar-como-se-formo-nuestro-nid22082024/?_website=la-nacion-ar
    
    **Pasos de prueba:**
    - Validar en los distintos casos de uso del ranking, que carguen notas productivas con sus respectivas imágenes.
    - El resto del sitio no debe verse afectado.
    - En `localhost` y apuntando a `producción`, validar que http://localhost/api/v2/notas/byAuthor/cdevanna/params=size:10;page:1/?_website=la-nacion-ar&outputType=json no traiga ningún valor que incluya un `undefined`.
    - **Seria bueno hacer pruebas en localhost para validar que los distintos escenarios rendericen OK y no generen ningún error dentro de la terminal**
    
    **Captura (antes):**
    N/A
    
    **Captura (después):**
    Home
    ![image.png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/29969/attachments/image.png)
    
    Acumulado
    ![Captura de pantalla 2024-10-23 084943.png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/29969/attachments/Captura%20de%20pantalla%202024-10-23%20084943.png)
    
    Nota
    ![Captura de pantalla 2024-10-23 085304.png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/29969/attachments/Captura%20de%20pantalla%202024-10-23%20085304.png)
    
    Nota (mobile)
    ![Captura de pantalla 2024-10-23 085611.png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/29969/attachments/Captura%20de%20pantalla%202024-10-23%20085611.png)
    
    Related work items...

--- __tests__/components/features/LN-10/ranking/default.test.js
+++ __tests__/components/features/LN-10/ranking/default.test.js
@@ -6,8 +6,10 @@ import Context from 'fusion:context';
 import { render } from '@testing-library/react';
 import mockArticles from '../../../../../__mocks__/data/ranking/homeLN10Response.json';
 
+const props = { id: 'rankingHome' };
+
 jest.mock('fusion:consumer', component => {
-    return function(component) {
+    return function (component) {
         return component;
     };
 });
@@ -19,24 +21,26 @@ jest.mock('../../../../../components/features/LN-10/ranking/_helper', () => ({
     getDataContent: jest.fn()
 }));
 
+jest.mock('fusion:context', () => ({ useAppContext: jest.fn() }));
+
 describe('features - LN10 - Ranking', () => {
     Context.useAppContext = jest.fn(() => ({
         contextPath: '/pf',
-        deployment: arg => arg
+        deployment: arg => arg,
+        isAdmin: false
     }));
 
-    test('should returns fragment if articles length is empty', () => {
+    it('should return null if the length of the items array is empty', () => {
         getDataContent.mockImplementation(() => ({ articles: [] }));
 
-        const { container } = render(<Ranking />);
-        const divElement = container.querySelector('div');
-        expect(divElement.textContent).toBe('');
+        const { container } = render(<Ranking {...props} />);
+        expect(container.firstChild).toBeNull();
     });
 
-    test('should returns the right articles length', () => {
+    it('should returns the right articles length', () => {
         getDataContent.mockImplementation(() => ({ articles: mockArticles }));
 
-        const { container } = render(<Ranking />);
+        const { container } = render(<Ranking {...props} />);
         const articles = container.querySelectorAll('article');
 
         expect(articles).toHaveLength(mockArticles.length);
