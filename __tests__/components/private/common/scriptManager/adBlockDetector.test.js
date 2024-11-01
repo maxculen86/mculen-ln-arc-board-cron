cd45004dfa20f72d83358d28925b31c6d9584fcc
Tomas Cabral Hunter <tch@MacBook-Pro-de-Tomas.local>
2024-11-01 11:09:19 -0300

    add tests for scriptManager it 2

---
+++ __tests__/components/private/common/scriptManager/adBlockDetector.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { useAppContext } from 'fusion:context';
+import AdblockDetector from '../../../../../components/private/common/scriptManager/adblockDetector';
+
+jest.mock('fusion:context', () => ({
+    useAppContext: jest.fn()
+}));
+describe('components - private - common - scriptManager - adBlockDetector', () => {
+    beforeEach(() => {
+        useAppContext.mockReturnValue({
+            contextPath: '/path',
+            deployment: jest.fn(path => `https://lanacion.com.ar${path}`)
+        });
+    });
+
+    it('renders a script tag with the correct src attribute', () => {
+        const { container } = render(<AdblockDetector />);
+
+        const scriptTag = container.querySelector('script[defer]');
+
+        expect(scriptTag).toBeInTheDocument();
+        expect(scriptTag).toHaveAttribute(
+            'src',
+            'https://lanacion.com.ar/path/resources/js/LN/scriptAdblockDetector.min.js'
+        );
+    });
+});
---
+++ __tests__/components/private/common/scriptManager/permutive.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { useAppContext } from 'fusion:context';
+import handleCookie from '../../../../../components/private/LN/common/utils/handleCookie';
+import Permutive from '../../../../../components/private/common/scriptManager/Permutive';
+
+jest.mock('fusion:context', () => ({
+    useAppContext: jest.fn()
+}));
+
+jest.mock(
+    '../../../../../components/private/LN/common/utils/handleCookie',
+    () => jest.fn()
+);
+
+describe('components - private - common - scriptManager - permutive', () => {
+    beforeEach(() => {
+        useAppContext.mockReturnValue({
+            contextPath: '/path',
+            deployment: jest.fn(path => `https://lanacion.com.ar${path}`),
+            layout: 'default',
+            globalContent: {
+                publish_date: '2024-10-31',
+                credits: { by: [{ name: 'Author Name' }] },
+                headlines: { basic: 'Sample Headline' },
+                taxonomy: {
+                    tags: [{ text: 'tag1' }, { text: 'tag2' }],
+                    primary_section: { name: 'News' }
+                }
+            }
+        });
+
+        handleCookie.mockReturnValue({
+            getCookie: 'cookieValue'
+        });
+    });
+
+    it('renders script tags with the correct attributes', () => {
+        const { container } = render(<Permutive />);
+
+        const permutiveScript = container.querySelector('#script-permutive');
+        const permutiveExternalScript = container.querySelector(
+            'script[src="https://867f8423-d142-4fd1-ae8d-1a9bbbdf2358.edge.permutive.app/867f8423-d142-4fd1-ae8d-1a9bbbdf2358-web.js"]'
+        );
+
+        expect(permutiveScript).toBeInTheDocument();
+        expect(permutiveScript).toHaveAttribute('data-layout', 'default');
+        expect(permutiveScript).toHaveAttribute(
+            'data-article',
+            JSON.stringify({
+                publishedAt: '2024-10-31',
+                authors: ['Author Name'],
+                keywords: ['tag1', 'tag2'],
+                section: 'News',
+                title: 'Sample Headline'
+            })
+        );
+        expect(permutiveScript).toHaveAttribute(
+            'data-get-cookie',
+            'cookieValue'
+        );
+        expect(permutiveScript).toHaveAttribute('async');
+        expect(permutiveScript).toHaveAttribute(
+            'src',
+            'https://lanacion.com.ar/path/resources/js/LN/Permutive.min.js'
+        );
+
+        expect(permutiveExternalScript).toBeInTheDocument();
+        expect(permutiveExternalScript).toHaveAttribute('async');
+    });
+});
