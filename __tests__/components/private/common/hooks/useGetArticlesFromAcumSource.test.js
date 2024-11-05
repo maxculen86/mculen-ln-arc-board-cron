7cae07340f861a73ec1fa8d3cd59d67de31c1281
Maurizio Gerardo Demarchis <mdemarchis@lnexternos.com.ar>
2024-11-05 19:59:55 +0000

    Merged PR 30604: Encode authorID
    Se emplea encodeURIComponent para setear id de autor con caracteres especiales
    
    DEMO:
    https://dev.lanacionar.arcpublishing.com/pf/autor/carlos-pagni-81/?_website=la-nacion-ar&d=3609
    https://dev.lanacionar.arcpublishing.com/pf/autor/joaquin-morales-sola-51/?_website=la-nacion-ar&d=3609
    https://dev.lanacionar.arcpublishing.com/pf/autor/jorge-fernandez-diaz-82/?_website=la-nacion-ar&d=3609
    
    SCREENSHOT:
    
    ![chrome_FBcClhe3JU.png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/30604/attachments/chrome_FBcClhe3JU.png)

---
+++ __tests__/components/private/common/hooks/useGetArticlesFromAcumSource.test.js
@@ -0,0 +1,57 @@
+import { useContent } from 'fusion:content';
+import mockArticles from '../../../../../__mocks__/data/masNotas/articles.json';
+import filter from '../../../../../content/filters/LN/acumulado/articleMasNotas';
+import useGetArticlesFromAcumSource from '../../../../../components/private/LN/common/hooks/useGetArticlesFromAcumSource';
+
+jest.mock('fusion:content', () => ({
+    useContent: jest.fn()
+}));
+describe('Private - Common - Hooks - useGetArticlesFromAcumSource', () => {
+    const args = {
+        typesOfQuery: {
+            sectionId: '/recetas'
+        },
+        filter,
+        imageConfig: 'boxArticles',
+        size: {
+            tripleSize: 9,
+            originalSize: 6
+        },
+        sourceOrigin: 'composer',
+        excludeSectionId: false,
+        type: 'story',
+        shouldNotFilter: true,
+        website: 'la-nacion-ar',
+        promoItemsOnly: true,
+        staticMode: true
+    };
+    it('should return the corresponding array of articles', () => {
+        useContent.mockReturnValueOnce(mockArticles);
+        expect(useGetArticlesFromAcumSource(args)).toStrictEqual(
+            mockArticles.content_elements
+        );
+    });
+    it('should return an empty array', () => {
+        useContent.mockReturnValueOnce([]);
+        expect(useGetArticlesFromAcumSource(args)).toStrictEqual([]);
+    });
+    it('should encode authorId when it contains special characters', () => {
+        const argsWithSpecialAuthorId = {
+            ...args,
+            typesOfQuery: {
+                ...args.typesOfQuery,
+                authorId: 'maría-peña'
+            }
+        };
+
+        useGetArticlesFromAcumSource(argsWithSpecialAuthorId);
+
+        expect(useContent).toHaveBeenCalledWith(
+            expect.objectContaining({
+                query: expect.objectContaining({
+                    authorId: 'mar%C3%ADa-pe%C3%B1a'
+                })
+            })
+        );
+    });
+});
