80287a9a656ebeb5dbed9894593499e29c3569a7
Lopez, Pia Micaela (Producto y Tecnologia) <plopez@lanacion.com.ar>
2024-11-06 20:16:26 +0000

    Merged PR 30641: Roof Foodit
    Roof Foodit
    - Se agregan helpers para modificar roof de carruseles y buscador en home, notas, recetas y biochef **(los cambios aplican solo en mobile)**
    - Update test
    - Se agrega hook para manejo de layouts
    
    **_ANTES_**
    ![image (2).png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/30641/attachments/image%20%282%29.png)
    
    **_DESPUES_**
    ![image (3).png](https://dev.azure.com/lndigital/336c835e-ca0b-48f3-b1f9-a65e3390b603/_apis/git/repositories/63c943d0-2f87-4bea-b1ca-cf8d35a05c99/pullRequests/30641/attachments/image%20%283%29.png)
    
    [DEMO](https://sandbox-foodit.lanacion.com.ar/?d=3128)
    
    Related work items: #137994

--- components/features/foodit-global/common/Header/hooks/useStickyHeader.js
+++ components/features/foodit-global/common/Header/hooks/useStickyHeader.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 export const useStickyHeader = ({
     observerSelector = '.header-sentinel'
 } = {}) => {
-    const [sticky, setSticky] = useState(false);
+    const [sticky, setSticky] = useState(true);
 
     useEffect(() => {
         const callback = entries => {
@@ -15,9 +15,9 @@ export const useStickyHeader = ({
         const sentinel = document.querySelector(observerSelector);
         const intersectionObserver = new IntersectionObserver(callback);
 
-        sentinel && intersectionObserver.observe(sentinel);
+        if (sentinel) intersectionObserver.observe(sentinel);
         return () => {
-            sentinel && intersectionObserver.unobserve(sentinel);
+            if (sentinel) intersectionObserver.unobserve(sentinel);
         };
     }, [observerSelector]);
 
--- components/layouts/Foodit-chef/foodit.jsx
+++ components/layouts/Foodit-chef/foodit.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Consumer from 'fusion:consumer';
-import BaseLayout from '../../features/foodit-global/common/BaseLayout/foodit';
-import AuthorBiography from '../../features/foodit-global/common/authorBiography/foodit';
 import { Text } from '@ln/common-ui-text';
-import transformSocial from '../../features/private-global/common/utils/transformSocial';
+import BaseLayout from '../../features/foodit-global/common/BaseLayout/foodit';
+import { AuthorBiography } from '../../features/foodit-global/common/authorBiography/foodit';
+import { transformSocial } from '../../features/private-global/common/utils/transformSocial';
 
 const pageBuilderSections = ['Notas'];
 
-const ChefFoodit = props => {
+function ChefFoodit(props) {
     const { children, globalContent = {} } = props;
 
     const {
@@ -45,25 +45,50 @@ const ChefFoodit = props => {
             <div className="flex flex-column gap-40">
                 <AuthorBiography {...authorBiography} />
                 <hr className="floating-button-sentinel" />
-                <section className="flex flex-column gap-24">
-                    <Text
-                        as="h1"
-                        className="prumo prumo-light text-24 text-32_md text-36_lg"
-                    >
-                        Sus recetas
-                    </Text>
+                <section className="flex flex-column gap-24_md">
+                    <div className="roof-sticky py-12 py-0_md">
+                        <Text
+                            as="h1"
+                            className="roof-text-sticky pl-16 pl-0_md"
+                        >
+                            Sus recetas
+                        </Text>
+                    </div>
                     {notas}
                 </section>
             </div>
         </BaseLayout>
     );
-};
+}
 
 ChefFoodit.sections = pageBuilderSections;
 
 ChefFoodit.propTypes = {
-    children: PropTypes.array,
-    globalContent: PropTypes.object
+    children: PropTypes.node.isRequired,
+    globalContent: PropTypes.shape({
+        byline: PropTypes.string,
+        longBio: PropTypes.string,
+        image: PropTypes.shape({
+            url: PropTypes.string
+        }),
+        instagram: PropTypes.string,
+        youtube: PropTypes.string,
+        pinterest: PropTypes.string,
+        twitter: PropTypes.string
+    })
+};
+ChefFoodit.defaultProps = {
+    globalContent: {
+        byline: '',
+        longBio: '',
+        image: {
+            url: ''
+        },
+        instagram: '',
+        youtube: '',
+        pinterest: '',
+        twitter: ''
+    }
 };
 
 export default Consumer(ChefFoodit);
