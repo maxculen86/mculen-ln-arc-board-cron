8a94592c05fa27e5b3d22886b89bb2b451ebe4ba
fnunez <fnunez@lnexternos.com.ar>
2024-12-27 16:06:20 -0300

    add test case

--- __tests__/components/private/common/snippetIndex.test.js
+++ __tests__/components/private/common/snippetIndex.test.js
@@ -1,7 +1,91 @@
-// TODO: ver historico de este archivo y revivir el test
-describe('Prueba false para dejar vivo el archivo con historico del test', () => {
-    test('validar que 2 + 2 sea igual a 4', () => {
-        const resultado = 2 + 2;
-        expect(resultado).toBe(4);
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import SnippetIndex from '../../../../components/private/common/snippet';
+import NotaSnippet from '../../../../components/private/LN/nota/snippet/receta';
+
+jest.mock('../../../../components/private/LN/nota/snippet/receta', () =>
+    jest.fn(() => <div data-testid="nota-snippet-mock" />)
+);
+jest.mock('../../../../components/private/LN/nota/snippet/liveblog', () =>
+    jest.fn(() => <div data-testid="liveblog-snippet-mock" />)
+);
+jest.mock('../../../../components/private/LN/nota/snippet/paywall', () =>
+    jest.fn(() => <div data-testid="paywall-snippet-mock" />)
+);
+
+describe('SnippetIndex Component', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
     });
+
+    const FAKE_MOCK_NOTA = {
+        subtype: 'LiveBlog',
+        type: null
+    };
+    test('Render the requested snippet', () => {
+        render(
+            <SnippetIndex
+                globalContent={FAKE_MOCK_NOTA}
+                arcSite="la-nacion-ar"
+                layout="LN-nota-receta"
+            />
+        );
+
+        const notaSnippet = screen.getByTestId('nota-snippet-mock');
+        expect(notaSnippet).toBeInTheDocument();
+    });
+
+    test('correct props to the Snippet', () => {
+        render(
+            <SnippetIndex
+                globalContent={FAKE_MOCK_NOTA}
+                arcSite="la-nacion-ar"
+                layout="LN-nota-receta"
+            />
+        );
+
+        expect(NotaSnippet).toHaveBeenCalledWith(
+            expect.objectContaining({ globalContent: FAKE_MOCK_NOTA }),
+            expect.anything()
+        );
+    });
+
+    test('not render anything if the layout is not configured', () => {
+        render(
+            <SnippetIndex
+                globalContent={{}}
+                arcSite="la-nacion-ar"
+                layout="layout-inexistente"
+            />
+        );
+
+        expect(
+            screen.queryByTestId('nota-snippet-mock')
+        ).not.toBeInTheDocument();
+    });
+
+    test('render null  configured', () => {
+        <SnippetIndex globalContent={{}} arcSite="test" layout="clear" />;
+    });
+});
+
+test('Render PaywallSnippet and NotaSnippet if conditions allow', () => {
+    const FAKE_MOCK_NOTA = {
+        subtype: 'LIVEBLOG',
+        type: 'story'
+    };
+
+    render(
+        <SnippetIndex
+            globalContent={FAKE_MOCK_NOTA}
+            arcSite="la-nacion-ar"
+            layout="LN-nota-receta"
+        />
+    );
+
+    const notaSnippet = screen.getByTestId('nota-snippet-mock');
+    expect(notaSnippet).toBeInTheDocument();
+
+    const paywallSnippet = screen.getByTestId('paywall-snippet-mock');
+    expect(paywallSnippet).toBeInTheDocument();
 });
