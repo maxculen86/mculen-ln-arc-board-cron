0676d4a30cfd40f5359c3227e90b442516ee660b
Pía Lopez <plopez@lanacion.com.ar>
2025-01-23 17:34:30 -0300

    Foodit acumulados - Se agrega nav de acumulados

---
+++ components/features/foodit-global/common/TagCategories/foodit.jsx
@@ -0,0 +1,35 @@
+import { Link } from '@ln/foodit-ui-link';
+import { Icon } from '@ln/common-ui-icon';
+import React from 'react';
+import IconSprite from '../../../private-global/common/iconSprite/IconSprite';
+
+export function TagCategories({ mockLinks }) {
+    if (mockLinks.length === 0) return null;
+
+    return (
+        <nav className="flex ai-end_md">
+            <ul className="flex flex-wrap gap-8">
+                {mockLinks.map((link, i) => (
+                    <li
+                        key={link.href}
+                        className="flex gap-8 flex ai-center ai-end_md"
+                    >
+                        {i !== 0 && (
+                            <Icon size={16}>
+                                <IconSprite name="bullet-nav" fill="#CCCCCC" />
+                            </Icon>
+                        )}
+                        <Link
+                            variant="secondary"
+                            href={link.href}
+                            title={link.title}
+                            className="uppercase roboto-bold text-12"
+                        >
+                            {link.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
--- components/layouts/Foodit-acumulado/foodit.jsx
+++ components/layouts/Foodit-acumulado/foodit.jsx
@@ -7,6 +7,7 @@ import BaseLayout from '../../features/foodit-global/common/BaseLayout/foodit';
 import { getFooditAcuTitle } from '../../features/foodit-global/common/breadcrumb/_helpers';
 import { UserBookmarks } from '../../features/foodit-global/common/bookmark/components/UserBookmarks';
 import AcuTema from '../../features/foodit-global/common/AcuTema/foodit';
+import { TagCategories } from '../../features/foodit-global/common/TagCategories/foodit';
 
 const pageBuilderSections = ['Apertura', 'Notas'];
 
@@ -17,18 +18,32 @@ function AcumuladoFoodit(props) {
     const { _id: id } = globalContent;
     const renderAcu =
         id === '/tema' ? <AcuTema globalContent={globalContent} /> : notas;
+
+    const mockLinks = [
+        { href: '/link1', title: 'Tortas' },
+        { href: '/link2', title: 'Postres' },
+        { href: '/link3', title: 'Panqueques' },
+        { href: '/link4', title: 'Budines' },
+        { href: '/link5', title: 'tartas' },
+        { href: '/link6', title: 'helados' },
+        { href: '/link7', title: 'batidos' }
+    ];
+
     return (
         <BaseLayout>
             <UserBookmarks />
             <div className="flex flex-column gap-32">
                 <section className="flex flex-column gap-24">
                     <BreadcrumbFoodit globalContent={globalContent} />
-                    <Text
-                        as="h1"
-                        className="prumo prumo-semibold text-28 text-40_md text-48_lg"
-                    >
-                        {getFooditAcuTitle(globalContent)}
-                    </Text>
+                    <div className="flex flex-column flex-row_md gap-16 gap-24_md">
+                        <Text
+                            as="h1"
+                            className="prumo prumo-semibold text-28 text-40_md text-48_lg"
+                        >
+                            {getFooditAcuTitle(globalContent)}
+                        </Text>
+                        <TagCategories mockLinks={mockLinks} />
+                    </div>
                 </section>
                 <section className="grid gap-32">{renderAcu}</section>
             </div>
