7ef0b322f8bb3b62f33dd702adc98f699fd3f9d9
Machi <jmachicado@lanacion.com.ar>
2025-02-11 11:22:28 -0300

    Agrego banner modal home a foodit

---
+++ components/features/foodit-global/Banners/BannerBaseFoodit.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import { getTypeOfDevice } from '@ln/hooks';
+import PropTypes from 'prop-types';
+import isSSR from '../../../private/LN/common/utils/isSSR';
+
+import { useAdManager } from './hooks/useAdManager';
+
+export function BannerBaseFoodit({ bannerType }) {
+    const device = getTypeOfDevice({
+        breakpoints: {
+            mobile: 768,
+            tablet: 1024
+        }
+    });
+    const { devices, getTargetings } = bannerType;
+    const { slotId, size, divId, classParent } = devices[device];
+
+    if (isSSR()) {
+        return (
+            <div className={classParent}>
+                <div id={divId} />
+            </div>
+        );
+    }
+
+    const adManagerError = useAdManager(slotId, size, divId, getTargetings());
+
+    if (adManagerError) {
+        console.error('Error al cargar el banner:', adManagerError);
+        return null;
+    }
+
+    return (
+        <div className={classParent}>
+            <div id={divId} />
+        </div>
+    );
+}
+
+BannerBaseFoodit.propTypes = {
+    bannerType: PropTypes.shape({
+        devices: {
+            desktop: PropTypes.shape({
+                slotId: PropTypes.string.isRequired,
+                size: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number))
+                    .isRequired,
+                divId: PropTypes.string.isRequired,
+                classParent: PropTypes.string.isRequired
+            }),
+            mobile: PropTypes.shape({
+                slotId: PropTypes.string.isRequired,
+                size: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number))
+                    .isRequired,
+                divId: PropTypes.string.isRequired,
+                classParent: PropTypes.string.isRequired
+            }),
+            tablet: PropTypes.shape({
+                slotId: PropTypes.string.isRequired,
+                size: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number))
+                    .isRequired,
+                divId: PropTypes.string.isRequired,
+                classParent: PropTypes.string.isRequired
+            })
+        },
+        getTargetings: PropTypes.func.isRequired
+    }).isRequired
+};
+
+export default BannerBaseFoodit;
---
+++ components/features/foodit-global/Banners/foodit.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { BannerBaseFoodit } from './BannerBaseFoodit';
+import { bannersTypes } from './_helpers/bannersTypes';
+
+export const BannersFoodit = {
+    modal_1x1: () => <BannerBaseFoodit bannerType={bannersTypes.modal_1x1} />
+};
