1fcc65a8cd26f861089ee6efac0433af0bd084a7
doloresp <doloresp@lanacion.com.ar>
2025-02-17 17:19:29 -0300

    se arreglan errores en feature juegos - se modifica tamaño de ribbon - se agrega validacion para diagramaciones de las cards en acu y home - se actualiza lib de card-games - se agrega test para diagramationCard

--- components/features/LN-common/Juego/helper.js
+++ components/features/LN-common/Juego/helper.js
@@ -2,6 +2,15 @@ import { cx } from '@ln/cva';
 import get from '../../../private/common/utils/get';
 import { getChainParentOfFeature } from '../../LN-10/article/common/_helper-WebApi';
 import { cardGameVariant } from './styles';
+import siteConfig from '../../../../properties/sites/la-nacion-ar';
+import { addForwardSlash } from '../../../private/LN/common/utils/addForwardSlash';
+
+export const DIAGRAMATIONS = {
+    oneLargeFourSmall: 'oneLargeFourSmall',
+    twoHorizontal: 'twoHorizontal',
+    fourVertical: 'fourVertical',
+    oneHorizontalThreeVertical: 'oneHorizontalThreeVertical'
+};
 
 export const getParent = (featureId, renderables) =>
     getChainParentOfFeature(featureId, renderables);
@@ -12,7 +21,7 @@ export const getCardPosition = (parent, featureId) =>
     );
 
 export const getParentLayout = parent => {
-    if (!parent || !parent.props) return '';
+    if (!parent || !parent.props) return DIAGRAMATIONS.fourVertical;
 
     return get(parent, 'props.customFields.layout', '');
 };
@@ -21,8 +30,8 @@ export const getFirstCard = (cardPosition, parentLayout) => {
     if (cardPosition !== 0) return false;
 
     const isValidLayout = [
-        'oneLargeFourSmall',
-        'oneHorizontalThreeVertical'
+        DIAGRAMATIONS.oneLargeFourSmall,
+        DIAGRAMATIONS.oneHorizontalThreeVertical
     ].includes(parentLayout);
     return isValidLayout;
 };
@@ -37,8 +46,27 @@ export const getClassName = (parentLayout, firstCard) => {
 export const getDescriptionData = (firstCard, parentLayout, description) => {
     if (!firstCard) return null;
     const isValidLayout = [
-        'oneHorizontalThreeVertical',
-        'oneLargeFourSmall'
+        DIAGRAMATIONS.oneLargeFourSmall,
+        DIAGRAMATIONS.oneHorizontalThreeVertical
     ].includes(parentLayout);
     return isValidLayout ? description : null;
 };
+
+export const getSizeByLayout = layoutName => {
+    const { layoutsName = {} } = siteConfig;
+    const layoutsNameWithSize18 = [layoutsName.HomeLN10];
+    return layoutsNameWithSize18.includes(layoutName) ? 18 : 24;
+};
+
+export const getHrefLink = (type, section, link) =>
+    type === 'Externo' ? addForwardSlash(section) : addForwardSlash(link);
+
+export const getSlotsProps = isSpecialLayout => ({
+    title: { className: isSpecialLayout ? 'min-h-54_md' : null },
+    description: { className: 'text-18 sm-none' }
+});
+
+export const getSlotsClasses = (isSpecialLayout, isFirstCard) => ({
+    icon: isSpecialLayout ? 'mt-14_m' : '',
+    ribbon: isFirstCard ? 'w-40' : ''
+});
