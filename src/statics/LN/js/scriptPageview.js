0a60b2f6aa9dec8fbb7a9c0006f31a073a995ded
Juan Carlos Diaz <jcdiaz@lanacion.com.ar>
2025-04-24 11:17:07 -0300

    Fix sobre registro de evento pageview en schema pageview.
    - Se suma nuevo item al localStorage: "SchemaPageViewMetaRefresh" para evitar usar el del dataLayer, el cual generaba el bug de que se eliminaba el item antes de que el evento se registrara en el schema
    - Se añade logica en el scriptPageview para escuchar el nuevo item, registrar el dato de metarefresh y remover el item al finalizar.

--- src/statics/LN/js/scriptPageview.js
+++ src/statics/LN/js/scriptPageview.js
@@ -5,9 +5,10 @@ if (schemaPageView) {
     const data = JSON.parse(schemaPageView.textContent) || {};
 
     if (pageTypesList.includes(data.pagetype)) {
-        const metarefresh = localStorage.getItem('CDmetaRefresh');
+        const metarefresh = localStorage.getItem('SchemaPageViewMetaRefresh');
         const metarefreshValue = metarefresh != null ? 'yes' : 'no';
         data.metarefresh = metarefreshValue;
         schemaPageView.textContent = JSON.stringify(data, null, 2);
+        localStorage.removeItem('SchemaPageViewMetaRefresh');
     }
 }
