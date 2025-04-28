1b96b80ef4e37f0579315b46a558e2644e6ace87
Juan Carlos Diaz <jcdiaz@lanacion.com.ar>
2025-04-28 09:38:17 -0300

    Fix en registro de schema en paginas no deseadas.
    - Se suma validación para habilitar el schema de pageview unicamente para recetas (acu y notas), acumulados, notas (todos los layouts) acu deportes y home
    - Se suma schema con todas las propiedades en "N/A" salvo la seccion, en las recetas.
    - Se actualizan los tests con los casos necesarios

--- components/features/LN-10-global/common/schemas/schemaPageView/default.jsx
+++ components/features/LN-10-global/common/schemas/schemaPageView/default.jsx
@@ -2,27 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SnippetRender from '../../../../../private/common/snippet/snippetRender';
 import get from '../../../../../private/common/utils/get';
-import getPageType from './_helper';
+import getPageType, {
+    getObjectSchema,
+    layoutsListWithPageview
+} from './_helper';
 
 export default function SchemaPageview({ globalContent, layout }) {
+    if (!layoutsListWithPageview.includes(layout)) {
+        return null;
+    }
     const pagetype = getPageType(layout, get(globalContent, '_id'));
+    const schema = getObjectSchema(globalContent, pagetype);
 
-    const schema = {
-        pagetype: getPageType(layout, get(globalContent, '_id')),
-        ...(pagetype === 'acumulado' && {
-            metarefresh: 'N/A'
-        }),
-        ...(pagetype === 'nota' && {
-            valor: get(globalContent, 'content_restrictions.content_code'),
-            subtype: get(globalContent, 'subtype'),
-            nota_id: get(globalContent, '_id'),
-            isListenable: get(globalContent, 'isListenable', false)
-                ? 'si'
-                : 'no'
-        })
-    };
-
-    return <SnippetRender data={schema} id="pageview" />;
+    return <SnippetRender data={schema[pagetype]} id="pageview" />;
 }
 
 SchemaPageview.propTypes = {
