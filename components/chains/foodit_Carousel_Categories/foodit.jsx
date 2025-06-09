6c269b79a14ba50fc33cf49e656446a07ba24ab0
Pía Lopez <plopez@lanacion.com.ar>
2025-06-09 12:47:55 -0300

    titulos en carrusel de categorias - se suma la posibilidad de que en Page Builder se pueda incluir título, link y ocultar techo en los carruseles de categoria - fix altura de icono bookmark en las cards

--- components/chains/foodit_Carousel_Categories/foodit.jsx
+++ components/chains/foodit_Carousel_Categories/foodit.jsx
@@ -4,11 +4,13 @@ import PropTypes from 'fusion:prop-types';
 import { Carousel } from '../foodit-global/common/Carousel/foodit';
 import { validateCarouselCategory } from './_helper';
 import WarningMessage from '../../private/common/warningMessage/warningMessage';
+import { RoofFoodit } from '../../features/foodit-global/common/RoofFoodit/foodit';
 
 function CarouselCategory({
     isAdmin,
     children,
-    customFields: { hideCarousel }
+    classNameRoof,
+    customFields: { hideCarousel, hideTitle, link, title }
 }) {
     const error = validateCarouselCategory({ children });
     if (hideCarousel) {
@@ -17,7 +19,17 @@ function CarouselCategory({
     if (isAdmin && error) {
         return <WarningMessage type={error.type} message={error.message} />;
     }
-    return <Carousel type="category">{children}</Carousel>;
+    return (
+        <div>
+            <RoofFoodit
+                title={{ text: title }}
+                hide={hideTitle || !title}
+                className={classNameRoof}
+                linkProps={{ href: link, text: title }}
+            />
+            <Carousel type="category">{children}</Carousel>
+        </div>
+    );
 }
 
 CarouselCategory.label = 'foodit Carousel Category';
@@ -25,7 +37,27 @@ CarouselCategory.label = 'foodit Carousel Category';
 CarouselCategory.propTypes = {
     isAdmin: PropTypes.bool,
     children: PropTypes.node.isRequired,
+    classNameRoof: PropTypes.string.isRequired,
     customFields: PropTypes.shape({
+        hideTitle: PropTypes.bool.tag({
+            name: 'Ocultar techo',
+            description: 'Marque para ocultar el techo',
+            defaultValue: false,
+            group: 'Techo'
+        }),
+        title: PropTypes.string.tag({
+            name: 'Texto',
+            description: 'Ingrese aquí el título de la caja.',
+            defaultValue: '',
+            group: 'Techo'
+        }),
+        link: PropTypes.string.tag({
+            label: 'Url',
+            description:
+                'Ingrese la url que redirige al hacer click al titulo. El formato debe empezar con https://',
+            defaultValue: '',
+            group: 'Techo'
+        }),
         hideCarousel: PropTypes.bool.tag({
             name: 'Ocultar Carousel',
             description: 'Marque para ocultar el carousel',
