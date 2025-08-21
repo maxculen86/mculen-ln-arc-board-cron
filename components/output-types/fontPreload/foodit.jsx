802ad5fa3e72172f015c51ae2fc0d2038974725d
Lautaro Barandiaran <lbarandiaran@lanacion.com.ar>
2025-08-21 12:54:09 -0300

    performance:(fonts) se realiza carga de fuentes diferida y critica - se hace preload de roboto_regular y font_prumo - se agrega hook useIdleTask, para diferir el import del resto fuentes. - se actualizan test

--- components/output-types/fontPreload/foodit.jsx
+++ components/output-types/fontPreload/foodit.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { FONT_PRUMO } from 'fusion:environment';
+import { ROBOTO_REGULAR, FONT_PRUMO } from 'fusion:environment';
 
 export function FontPreload({ deployment, contextPath }) {
     return (
-        <link
-            href={deployment(`${contextPath}${FONT_PRUMO}`)}
-            rel="preload"
-            as="font"
-        />
+        <>
+            <link
+                href={deployment(`${contextPath}${ROBOTO_REGULAR}`)}
+                rel="preload"
+                as="font"
+                type="font/woff2"
+                crossOrigin="anonymous"
+            />
+            <link
+                href={deployment(`${contextPath}${FONT_PRUMO}`)}
+                rel="preload"
+                as="font"
+                type="font/woff2"
+                crossOrigin="anonymous"
+            />
+        </>
     );
 }
