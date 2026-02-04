9b3dffbb20a8c7de69ffab4591523d1fcb4e4861
mlucero <mlucero@lnexternos.com.ar>
2026-02-04 14:45:47 -0300

    Se elimina log inecesario de descarte de cajas tipo 9, en api home y acumulados.

--- components/private/LN/api/common/home/boxTypes/index.js
+++ components/private/LN/api/common/home/boxTypes/index.js
@@ -1,18 +1,8 @@
-import { enumTypeError } from '../../enums/enumTypeError';
-import { BackendLnError } from '../../models/backendLnError';
 import { boxTypesLN } from './LN/index';
 import { boxTypesLN10 } from './LN10/index';
 import { boxTypesLN10v2 } from './LN10v2/index';
 
-const discard = (type, layout) => {
-    console.warn(
-        new BackendLnError(
-            `No box is defined for type '${type}' and layout '${layout}'`,
-            enumTypeError.featureError
-        )
-    );
-    return () => null;
-};
+const discard = () => () => null;
 
 export const boxTypeByLayout = (layoutPage, type) => {
     const boxesTypesByLayout = {
@@ -22,7 +12,7 @@ export const boxTypeByLayout = (layoutPage, type) => {
         'LN-acumulado': boxTypesLN10v2
     };
     const boxType = boxesTypesByLayout[layoutPage] || boxTypesLN;
-    return boxType[type] ?? discard(type, layoutPage);
+    return boxType[type] ?? discard();
 };
 
 export default boxTypeByLayout;
