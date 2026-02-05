8f8285fecf4ec3188250d0702efabcb160ab22e4
Maximiliano Culen <planit_mculen@lnexternos.com.ar>
2026-02-05 15:43:38 -0300

    fix(167748): mejoras en breadcrumb y filtrado de nombres vacíos

--- components/features/LN/common/breadcrumb/helpers/buildBreadcrumbSections.js
+++ components/features/LN/common/breadcrumb/helpers/buildBreadcrumbSections.js
@@ -1,10 +1,34 @@
+import get from '../../../../../private/common/utils/get';
 import getPrimaryTree from './getPrimaryTree';
+
+const findSectionById = (sections, id) =>
+    sections && sections.find(section => get(section, '_id') === id);
+
+const findFirstValidSection = sections =>
+    sections && sections.find(section => get(section, 'name'));
+
+const resolvePrimarySection = (primarySection, sections) => {
+    const id = get(primarySection, '_id');
+    const name = get(primarySection, 'name');
+
+    if (id && name) {
+        return primarySection;
+    }
+
+    return (
+        (id && findSectionById(sections, id)) || findFirstValidSection(sections)
+    );
+};
 
 const buildBreadcrumbSections = ({ sections, primarySection, siteTitle }) => {
     const allSections = [];
+    const effectivePrimarySection = resolvePrimarySection(
+        primarySection,
+        sections
+    );
 
-    if (primarySection) {
-        getPrimaryTree(sections, primarySection, allSections);
+    if (effectivePrimarySection) {
+        getPrimaryTree(sections, effectivePrimarySection, allSections);
     }
 
     allSections.push({
@@ -12,7 +36,9 @@ const buildBreadcrumbSections = ({ sections, primarySection, siteTitle }) => {
         path: '/'
     });
 
-    return allSections.reverse();
+    return allSections
+        .filter(section => section.name && section.name.trim() !== '')
+        .reverse();
 };
 
 export default buildBreadcrumbSections;
