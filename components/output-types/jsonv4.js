e3503bc2fa821d343217ccc92c5b892c2be71ccd
mlucero <mlucero@lnexternos.com.ar>
2026-02-05 08:49:46 -0300

    Se modifican outputtypes jsonv para que funcione correctamente el feature homeUpdate, se adapto el mismo para consumir la home jsonv2 directamente.

--- components/output-types/jsonv4.js
+++ components/output-types/jsonv4.js
@@ -1,4 +1,6 @@
-const jsonV4 = ({ children }) => children;
+// Only return the data from the first child.
+const jsonV4 = ({ children }) =>
+    Array.isArray(children) ? children[0] : children || null;
 
 jsonV4.contentType = 'application/json';
 jsonV4.fallback = ['jsonv3', 'jsonv2', 'json', 'default'];
