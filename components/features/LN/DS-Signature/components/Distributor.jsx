646a271f53f51a19f981bbcb59a5ebcbf81a3508
ltalenton <ltalenton@lanacion.com.ar>
2026-03-05 13:02:18 -0300

    fix(biographyaccordion): biographyaccordion text length extended on desktop, distributor now bold
    -
    
    AB#170347

--- components/features/LN/DS-Signature/components/Distributor.jsx
+++ components/features/LN/DS-Signature/components/Distributor.jsx
@@ -17,10 +17,10 @@ function Distributor({ name, mode, subcategory, shouldShowDistributor }) {
                         title={name}
                         color="black"
                     >
-                        <span>{name}</span>
+                        <strong>{name}</strong>
                     </LinkUI>
                     {subcategory.length > 0 && name === 'EL PAIS' && (
-                        <span>{subcategory}</span>
+                        <strong>{subcategory}</strong>
                     )}
                 </div>
             )}
