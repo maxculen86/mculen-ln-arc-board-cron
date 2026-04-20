30ffb4114234abc0f5dd07959f447cac23abd9ef
Pía Lopez <plopez@lanacion.com.ar>
2026-04-20 12:46:23 -0300

    style(foodit-tooltip): elimina tooltip busqueda por voz y campanita del header

--- __tests__/components/chains/foodit_global/common/Header/components/rightOptions/bellButton.test.jsx
+++ __tests__/components/chains/foodit_global/common/Header/components/rightOptions/bellButton.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import BellButton from '../../../../../../../../components/features/foodit-global/common/Header/components/rightOptions/bellButton';
 
 jest.mock('fusion:environment', () => ({
@@ -8,48 +8,14 @@ jest.mock('fusion:environment', () => ({
     API_ENV: 'prod'
 }));
 
-jest.useFakeTimers();
-
 describe('Components - Features - foodit-global - Common - Header - component - rightOptions', () => {
     beforeEach(() => {
-        jest.useFakeTimers();
-        localStorage.clear();
         jest.clearAllMocks();
     });
-
-    afterEach(() => {
-        jest.runOnlyPendingTimers();
-        jest.useRealTimers();
-    });
-
     it('should render bell', async () => {
         const { container } = render(<BellButton />);
-        await waitFor(() => {
-            expect(
-                screen.getAllByText(
-                    'Aquí encontrarás todas nuestras notificaciones'
-                )
-            ).toHaveLength(1);
-            expect(screen.getByRole('tooltip')).toHaveAttribute(
-                'data-visible',
-                'true'
-            );
-            expect(localStorage.getItem('tooltip')).toBe(null);
-            expect(container).toMatchSnapshot();
-        });
-    });
+        screen.getByRole('button', { name: 'Abrir Notificaciones' });
 
-    it('should hide tooltip after 3 seconds', async () => {
-        render(<BellButton />);
-        jest.advanceTimersByTime(3000);
-        await waitFor(() => {
-            expect(localStorage.getItem('tooltip')).toBe('hide');
-        });
-    });
-
-    it('should not show tooltip if localStorage is hide', () => {
-        localStorage.setItem('tooltip', 'hide');
-        render(<BellButton />);
-        expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+        expect(container).toMatchSnapshot();
     });
 });
